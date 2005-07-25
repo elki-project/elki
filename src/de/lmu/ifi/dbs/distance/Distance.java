0f302840101cca5495583adcd46e56c09c0a5cca
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-07-25 11:40:34 +0000

    Distance Interface AbstractDistance deleted

--- src/de/lmu/ifi/dbs/distance/Distance.java
+++ src/de/lmu/ifi/dbs/distance/Distance.java
@@ -2,18 +2,40 @@ package de.lmu.ifi.dbs.distance;
 
 
 /**
+ * The interface Distance defines the requirements of any istance class.
  * 
  * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
 public interface Distance extends Comparable
 {
+
+    /**
+     * Returns a new distance as sum of this distance and the given distance.
+     * 
+     * @param distance the distancce to be added to this distance
+     * @return a new distance as sum of this distance and the given distance
+     */
+    public Distance plus(Distance distance);
     
     /**
-     * Any implementing class should implement a proper formatString-method to obtain a String describing the values of the Distance.
+     * Returns a new Distance by subtracting the given distance
+     * from this distance.
      * 
-     * @return String a String describing the values of the Distance
+     * 
+     * @param distance the distance to be subtracted from this distance
+     * @return a new Distance by subtracting the given distance
+     * from this distance
      */
-    public String formatString();
+    public Distance minus(Distance distance);
+    
+    /**
+     * Returns true, if this distance is an infinite distance,
+     * false otherwise.
+     * 
+     * @return true, if this distance is an infinite distance,
+     * false otherwise
+     */
+    public boolean isInfinite();
     
     /**
      * Any implementing class should implement a proper toString-method for printing the result-values.
@@ -21,39 +43,5 @@ public interface Distance extends Comparable
      * @return String a human-readable representation of the Distance
      */
     public String toString(); 
-    
-    /**
-     * Any implementing class should provide a value
-     * for an infinite Distance
-     * 
-     * 
-     * @return infinite distance
-     */
-    // TODO: public Distance infiniteDistance();
-    
-    /**
-     * Constructs a Distance for the given parameter.
-     * 
-     * @param parameter a String to construct a Distance from
-     * @return a Distance constructed from the given String
-     */
-    public Distance parse(String parameter);
-        
-    
-    /**
-     * Any implementing Class should return a String
-     * to describe the required format of an input String
-     * 
-     * @return a String to describe the required format of an input String
-     */
-    public String requiredInputString();
-    
-    /**
-     * Any implementing class should provide a value for an undefined Distance
-     * 
-     * @return undefined distance
-     */
-    // TODO: public Distance undef();
-    
-    
+            
 }
