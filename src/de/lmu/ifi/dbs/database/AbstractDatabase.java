8ffaa7dc23170d190b0a40bd8fc70188762668b8
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-08-01 07:26:08 +0000

    Changes in abstract class

--- src/de/lmu/ifi/dbs/database/AbstractDatabase.java
+++ src/de/lmu/ifi/dbs/database/AbstractDatabase.java
@@ -1,64 +1,87 @@
 package de.lmu.ifi.dbs.database;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.lmu.ifi.dbs.utilities.UnableToComplyException;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * TODO comment
+ *
  * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public abstract class AbstractDatabase implements Database
-{
-    /**
-     * Counter to provide a new Integer id.
-     */
-    private int counter;
-    
-    private List<Integer> reusableIDs;
-    
-    private boolean reachedLimit;
-    
-    protected AbstractDatabase()
-    {
-        counter = Integer.MIN_VALUE;
-        reachedLimit = false;
-        reusableIDs = new ArrayList<Integer>();
+public abstract class AbstractDatabase implements Database {
+
+  /**
+   * Map to hold association maps.
+   */
+  private final Map<String, Map<Integer, Object>> associations;
+
+  /**
+   * Counter to provide a new Integer id.
+   */
+  private int counter;
+
+  private List<Integer> reusableIDs;
+
+  private boolean reachedLimit;
+
+  /**
+   * @see de.lmu.ifi.dbs.database.Database#associate(java.lang.String, java.lang.Integer, java.lang.Object)
+   */
+  public void associate(String associationID, Integer objectID, Object association) {
+    if (!associations.containsKey(associationID)) {
+      associations.put(associationID, new Hashtable<Integer, Object>());
     }
-    
-    protected Integer newID() throws UnableToComplyException
-    {
-        if(reachedLimit && reusableIDs.size() == 0)
-        {
-            throw new UnableToComplyException("Database reached limit of storage.");
-        }
-        else
-        {
-            Integer id = new Integer(counter);
-            if(counter < Integer.MAX_VALUE && !reachedLimit)
-            {
-                counter++;            
-            }
-            else
-            {
-                if(reusableIDs.size() > 0)
-                {
-                    counter = reusableIDs.remove(0).intValue();
-                }
-                else
-                {
-                    reachedLimit = true;
-                }
-            }
-            return id;
-        }
+    associations.get(associationID).put(objectID, association);
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.database.Database#getAssociation(java.lang.String, java.lang.Integer)
+   */
+  public Object getAssociation(String associationID, Integer objectID) {
+    if (associations.containsKey(associationID)) {
+      return associations.get(associationID).get(objectID);
     }
-    
-    protected void restoreID(Integer id)
-    {
-        {
-            reusableIDs.add(id);
-        }
+    else {
+      return null;
     }
+  }
+
+
+  protected AbstractDatabase() {
+    associations = new Hashtable<String, Map<Integer, Object>>();
+    counter = Integer.MIN_VALUE;
+    reachedLimit = false;
+    reusableIDs = new ArrayList<Integer>();
+  }
+
+  protected Integer newID() throws UnableToComplyException {
+    if (reachedLimit && reusableIDs.size() == 0) {
+      throw new UnableToComplyException("Database reached limit of storage.");
+    }
+    else {
+      Integer id = new Integer(counter);
+      if (counter < Integer.MAX_VALUE && !reachedLimit) {
+        counter++;
+      }
+      else {
+        if (reusableIDs.size() > 0) {
+          counter = reusableIDs.remove(0).intValue();
+        }
+        else {
+          reachedLimit = true;
+        }
+      }
+      return id;
+    }
+  }
+
+  protected void restoreID(Integer id) {
+    {
+      reusableIDs.add(id);
+    }
+  }
 }
