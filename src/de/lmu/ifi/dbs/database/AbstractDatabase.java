5d8748337bb096156e8ef92ea75230f6b467a63b
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-08-01 07:15:10 +0000

    renamed from AbstractSequentialDatabase

---
+++ src/de/lmu/ifi/dbs/database/AbstractDatabase.java
@@ -0,0 +1,64 @@
+package de.lmu.ifi.dbs.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.lmu.ifi.dbs.utilities.UnableToComplyException;
+
+/**
+ * TODO comment
+ * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ */
+public abstract class AbstractDatabase implements Database
+{
+    /**
+     * Counter to provide a new Integer id.
+     */
+    private int counter;
+    
+    private List<Integer> reusableIDs;
+    
+    private boolean reachedLimit;
+    
+    protected AbstractDatabase()
+    {
+        counter = Integer.MIN_VALUE;
+        reachedLimit = false;
+        reusableIDs = new ArrayList<Integer>();
+    }
+    
+    protected Integer newID() throws UnableToComplyException
+    {
+        if(reachedLimit && reusableIDs.size() == 0)
+        {
+            throw new UnableToComplyException("Database reached limit of storage.");
+        }
+        else
+        {
+            Integer id = new Integer(counter);
+            if(counter < Integer.MAX_VALUE && !reachedLimit)
+            {
+                counter++;            
+            }
+            else
+            {
+                if(reusableIDs.size() > 0)
+                {
+                    counter = reusableIDs.remove(0).intValue();
+                }
+                else
+                {
+                    reachedLimit = true;
+                }
+            }
+            return id;
+        }
+    }
+    
+    protected void restoreID(Integer id)
+    {
+        {
+            reusableIDs.add(id);
+        }
+    }
+}
