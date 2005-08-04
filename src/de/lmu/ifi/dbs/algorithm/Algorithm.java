2940440078092814f669d253f5247155ff68ae90
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-08-04 08:00:52 +0000

    comment

--- src/de/lmu/ifi/dbs/algorithm/Algorithm.java
+++ src/de/lmu/ifi/dbs/algorithm/Algorithm.java
@@ -7,6 +7,10 @@ import de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable;
 /**
  * Specifies the requirements for any algorithm that is to be executable by the main class.
  * 
+ * Any implementation needs not to take care of input nor output, parsing and so on.
+ * Those tasks are performed by the framework. An algorithm simply needs to ask for
+ * parameters that are algorithm specific.
+ * 
  * Note that any implementation is supposed to provide a constructor without parameters.
  * 
  * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
