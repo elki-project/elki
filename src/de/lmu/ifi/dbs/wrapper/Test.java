870b620e071e1d986b948dabaeef06f23bdac0ad
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-08-08 08:15:38 +0000

    testing correlation distance

--- src/de/lmu/ifi/dbs/wrapper/Test.java
+++ src/de/lmu/ifi/dbs/wrapper/Test.java
@@ -1,22 +1,21 @@
 package de.lmu.ifi.dbs.wrapper;
 
-import de.lmu.ifi.dbs.database.Database;
-import de.lmu.ifi.dbs.database.RTreeDatabase;
-import de.lmu.ifi.dbs.parser.Parser;
-import de.lmu.ifi.dbs.parser.StandardLabelParser;
-import de.lmu.ifi.dbs.distance.DistanceFunction;
-import de.lmu.ifi.dbs.distance.EuklideanDistanceFunction;
-import de.lmu.ifi.dbs.utilities.QueryResult;
-import de.lmu.ifi.dbs.utilities.Util;
 import de.lmu.ifi.dbs.algorithm.Algorithm;
 import de.lmu.ifi.dbs.algorithm.DBSCAN;
+import de.lmu.ifi.dbs.database.Database;
+import de.lmu.ifi.dbs.database.RTreeDatabase;
+import de.lmu.ifi.dbs.distance.DistanceFunction;
+import de.lmu.ifi.dbs.distance.EuklideanDistanceFunction;
+import de.lmu.ifi.dbs.parser.Parser;
+import de.lmu.ifi.dbs.parser.StandardLabelParser;
+import de.lmu.ifi.dbs.utilities.QueryResult;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -37,7 +36,7 @@ public class Test {
     InputStream in1 = new FileInputStream(file1);
     Parser parser1 = new StandardLabelParser();
     String[] param1 = {"-database", "de.lmu.ifi.dbs.database.RTreeDatabase",
-                       "-" + RTreeDatabase.FILE_NAME_P, "elki.idx"};
+    "-" + RTreeDatabase.FILE_NAME_P, "elki.idx"};
     parser1.setParameters(param1);
     Database db1 = parser1.parse(in1);
     System.out.println(db1);
@@ -53,10 +52,10 @@ public class Test {
 
     DistanceFunction distFunction = new EuklideanDistanceFunction();
     List<QueryResult> r1 = db1.kNNQuery(300, 10, distFunction);
-    System.out.println("r1 "+r1);
+    System.out.println("r1 " + r1);
 
     List<QueryResult> r2 = db2.kNNQuery(300, 10, distFunction);
-    System.out.println("r2 "+r2);
+    System.out.println("r2 " + r2);
 
 //      for (int i = 0; i < 450; i++) {
 //        MetricalObject o = db1.get(new Integer(Integer.MIN_VALUE + i));
@@ -67,14 +66,17 @@ public class Test {
   }
 
   private static void testCorrDist() throws FileNotFoundException {
-      File file = new File("test.txt");
+    File file = new File("test.txt");
 //    File file = new File("10_T_2.ascii");
     InputStream in = new FileInputStream(file);
     Parser parser = new StandardLabelParser();
     String epsilon = "4.0";
     System.out.println(Double.parseDouble(epsilon));
-    String[] param = {"-database", "de.lmu.ifi.dbs.database.SequentialDatabase",
-                       "-epsilon", epsilon};
+    String[] param = {
+    "-database", "de.lmu.ifi.dbs.database.SequentialDatabase",
+    "-epsilon", "4x1000",
+    "-minpts", "10",
+    "-distancefunction", "de.lmu.ifi.dbs.distance.CorrelationDistanceFunction"};
     param = parser.setParameters(param);
     Database db = parser.parse(in);
 
@@ -84,7 +86,6 @@ public class Test {
     Algorithm dbscan = new DBSCAN();
     dbscan.setParameters(param);
     dbscan.run(db);
-
 
 
   }
