685c77465afaa5b412e25c5b426799b15d5b7cf1
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-08-11 10:31:38 +0000

    result package introduced

---
+++ src/de/lmu/ifi/dbs/algorithm/result/PartitionResults.java
@@ -0,0 +1,53 @@
+package de.lmu.ifi.dbs.algorithm.result;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * A result for a partitioning algorithm providing a single result for a single partition.
+ * 
+ * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ */
+public class PartitionResults implements Result
+{
+    /**
+     * Holds the results for the partitions.
+     */
+    private Map<Integer,Result> partitionResults;
+
+    /**
+     * A result for a partitioning algorithm providing a single result for a single partition.
+     * 
+     * @param resultMap a map of partition IDs to results
+     */
+    public PartitionResults(Map<Integer,Result> resultMap)
+    {
+        this.partitionResults = resultMap;
+    }
+    
+    /**
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.result.Result#output(java.io.File)
+     */
+    public void output(File out)
+    {
+        for(Iterator<Integer> resultsIter = partitionResults.keySet().iterator(); resultsIter.hasNext();)
+        {
+            Integer resultID = resultsIter.next();
+            Result result = partitionResults.get(resultID);
+            String marker = "Partition_ID_"+resultID+"_";
+            if(out==null)
+            {
+                System.out.println(marker);
+                result.output(out);
+            }
+            else
+            {
+                File markedOut = new File(out.getAbsolutePath()+marker);
+                result.output(markedOut);
+            }
+        }
+    }
+
+}
