1a07d4a2b90206004f7c44df5e8ca6705c7eb4b4
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-09-15 15:09:21 +0000

    initial revision

---
+++ src/de/lmu/ifi/dbs/parser/FirstLineFormatParser.java
@@ -0,0 +1,166 @@
+package de.lmu.ifi.dbs.parser;
+
+
+import de.lmu.ifi.dbs.data.DoubleVector;
+import de.lmu.ifi.dbs.database.Database;
+import de.lmu.ifi.dbs.normalization.NonNumericFeaturesException;
+import de.lmu.ifi.dbs.utilities.UnableToComplyException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Provides a parser for parsing one point per line, attributes separated by whitespace.
+ * 
+ * Several labels may be given per point. A label must not be parseable as double.
+ * Lines starting with &quot;#&quot; will be ignored.
+ * Dimensionality and place of labels are fixed after parsing the first line.
+ * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ */
+public class FirstLineFormatParser extends StandardLabelParser
+{
+    /**
+     * Provides a parser for parsing one point per line, attributes separated by whitespace.
+     * 
+     * Several labels may be given per point. A label must not be parseable as double.
+     * Lines starting with &quot;#&quot; will be ignored.
+     *
+     */
+    public FirstLineFormatParser()
+    {
+        super();
+    }
+    
+    /**
+     * 
+     * @see de.lmu.ifi.dbs.parser.Parser#parse(java.io.InputStream)
+     */
+    public Database<DoubleVector> parse(InputStream in)
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        int lineNumber = 0;
+        int dimensionality = -1;
+        List<DoubleVector> objects = new ArrayList<DoubleVector>();
+        List<Map<String,Object>> labels = new ArrayList<Map<String,Object>>();
+        try
+        {
+            Set<Integer> labelIndices = new HashSet<Integer>();
+            for(String line; (line=reader.readLine())!=null; lineNumber++)
+            {
+                if(!line.startsWith(COMMENT) && line.length() > 0)
+                {
+                    String[] entries = WHITESPACE.split(line);
+                    List<Double> attributes = new ArrayList<Double>();
+                    StringBuffer label = new StringBuffer();
+                    for(int i = 0; i < entries.length; i++)
+                    {
+                        if(lineNumber==0)
+                        {
+                            try
+                            {
+                                Double attribute = Double.valueOf(entries[i]);
+                                attributes.add(attribute);
+                            }
+                            catch(NumberFormatException e)
+                            {
+                                labelIndices.add(i);
+                                if(label.length() > 0)
+                                {
+                                    label.append(LABEL_CONCATENATION);
+                                }
+                                label.append(entries[i]);
+                            }
+                        }
+                        else
+                        {
+                            if(labelIndices.contains(i))
+                            {
+                                if(label.length() > 0)
+                                {
+                                    label.append(LABEL_CONCATENATION);
+                                }
+                                label.append(entries[i]);
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    Double attribute = Double.valueOf(entries[i]);
+                                    attributes.add(attribute);
+                                }
+                                catch(NumberFormatException e)
+                                {
+                                    throw new IllegalArgumentException("Illegal format of line "+lineNumber+".");
+                                }
+                            }
+                        }
+                    }
+                    if(dimensionality < 0)
+                    {
+                        dimensionality = attributes.size();
+                    }
+                    if(dimensionality != attributes.size())
+                    {
+                        throw new IllegalArgumentException("Differing dimensionality in line "+lineNumber+".");
+                    }
+                    objects.add(new DoubleVector(attributes));
+                    Map<String,Object> association = new Hashtable<String,Object>();
+                    association.put(Database.ASSOCIATION_ID_LABEL,label.toString());
+                    labels.add(association);
+                }                
+            }            
+        }
+        catch(IOException e)
+        {
+            throw new IllegalArgumentException("Error while parsing line "+lineNumber+".");
+        }
+        if(getNormalization() != null)
+        {
+            try
+            {
+                objects = getNormalization().normalize(objects);
+            }
+            catch(NonNumericFeaturesException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        try
+        {
+            database.insert(objects,labels);
+        }
+        catch(UnableToComplyException e)
+        {
+            e.printStackTrace();
+        }
+        return database;
+    }
+
+    /**
+     * 
+     * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#description()
+     */
+    public String description()
+    {
+        StringBuffer description = new StringBuffer();
+        description.append(FirstLineFormatParser.class.getName());
+        description.append(" expects following format of parsed lines:\n");
+        description.append("A single line provides a single point. Attributes are separated by whitespace (");
+        description.append(WHITESPACE.pattern());
+        description.append("). Any substring not containing whitespace is tried to be read as double. If this fails, it will be appended to a label. (Thus, any label must not be parseable as double.) Empty lines and lines beginning with \"");
+        description.append(COMMENT);
+        description.append("\" will be ignored. If any point differs in its dimensionality from other points, the parse method will fail with an Exception.\n");
+        description.append("Dimensionality and place of labels are fixed after parsing the first line.\n");
+        
+        return usage(description.toString());
+    }
+
+}
