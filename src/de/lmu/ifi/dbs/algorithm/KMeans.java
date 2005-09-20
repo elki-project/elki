82f05e724f325824e47d582c45ae32c66cb5e383
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-09-20 14:28:57 +0000

    new abstract class for result objects

--- src/de/lmu/ifi/dbs/algorithm/KMeans.java
+++ src/de/lmu/ifi/dbs/algorithm/KMeans.java
@@ -3,7 +3,6 @@ package de.lmu.ifi.dbs.algorithm;
 import de.lmu.ifi.dbs.algorithm.result.Clusters;
 import de.lmu.ifi.dbs.algorithm.result.Result;
 import de.lmu.ifi.dbs.data.DoubleVector;
-import de.lmu.ifi.dbs.data.FeatureVector;
 import de.lmu.ifi.dbs.database.Database;
 import de.lmu.ifi.dbs.distance.Distance;
 import de.lmu.ifi.dbs.normalization.AttributeWiseDoubleVectorNormalization;
@@ -12,244 +11,212 @@ import de.lmu.ifi.dbs.utilities.Description;
 import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
 import de.lmu.ifi.dbs.utilities.optionhandling.UnusedParameterException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Provides the k-means algorithm.
- * 
+ *
  * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public class KMeans extends DistanceBasedAlgorithm<DoubleVector>
-{
-    /**
-     * Parameter k.
-     */
-    public static final String K_P = "k";
-    
-    /**
-     * Description for parameter k.
-     */
-    public static final String K_D = "<int>k - the number of clusters to find (positive integer)";
+public class KMeans extends DistanceBasedAlgorithm<DoubleVector> {
+  /**
+   * Parameter k.
+   */
+  public static final String K_P = "k";
 
-    /**
-     * Keeps k - the number of clusters to find.
-     */
-    private int k;
-    
-    /**
-     * Keeps the result.
-     */
-    private Clusters result;
-    
-    /**
-     * Provides the k-means algorithm.
-     */
-    public KMeans()
-    {
-        super();
-        parameterToDescription.put(K_P+OptionHandler.EXPECTS_VALUE,K_D);
-        optionHandler = new OptionHandler(parameterToDescription,KMeans.class.getName());
-    }
+  /**
+   * Description for parameter k.
+   */
+  public static final String K_D = "<int>k - the number of clusters to find (positive integer)";
 
-    /**
-     * @see de.lmu.ifi.dbs.algorithm.Algorithm#getDescription()
-     */
-    public Description getDescription()
-    {
-        // TODO reference
-        return new Description("K-Means", "K-Means", "finds a partitioning into k clusters", "...");
-    }
+  /**
+   * Keeps k - the number of clusters to find.
+   */
+  private int k;
 
-    /**
-     * @see de.lmu.ifi.dbs.algorithm.Algorithm#getResult()
-     */
-    public Result getResult()
-    {
-        return result;
-    }
+  /**
+   * Keeps the result.
+   */
+  private Clusters result;
 
-    /**
-     * @see de.lmu.ifi.dbs.algorithm.Algorithm#run(de.lmu.ifi.dbs.database.Database)
-     */
-    public void run(Database<DoubleVector> database) throws IllegalStateException
-    {
-        Random random = new Random();
-        if(database.size() > 0)
-        {
-            DoubleVector randomBase = database.get(database.iterator().next());
-            AttributeWiseDoubleVectorNormalization normalization = new AttributeWiseDoubleVectorNormalization();
-            List<DoubleVector> list = new ArrayList<DoubleVector>(database.size());
-            for(Iterator<Integer> dbIter = database.iterator(); dbIter.hasNext();)
-            {
-                list.add((DoubleVector) database.get(dbIter.next()));
-            }
-            try
-            {
-                normalization.normalize(list);
-            }
-            catch(NonNumericFeaturesException e)
-            {
-                e.printStackTrace();
-            }
-            List<DoubleVector> means = new ArrayList<DoubleVector>(k);
-            List<DoubleVector> oldMeans;
-            List<List<Integer>> clusters = new ArrayList<List<Integer>>(k);
-            if(isVerbose())
-            {
-                System.out.println("initializing random vectors");
-            }
-            for(int i = 0; i < k; i++)
-            {
-                DoubleVector randomVector = (DoubleVector) randomBase.randomInstance(random);
-                try
-                {
-                    means.add((DoubleVector) normalization.restore((DoubleVector) randomVector));
-                }
-                catch(NonNumericFeaturesException e)
-                {
-                    e.printStackTrace();
-                    means.add(randomVector);
-                }
-            }
-            clusters = sort(means, database);
-            boolean changed = true;
-            int iteration = 1;
-            while(changed)
-            {
-                if(isVerbose())
-                {
-                    System.out.println("iteration "+iteration);
-                }
-                oldMeans = new ArrayList<DoubleVector>(k);
-                oldMeans.addAll(means);
-                means = means(clusters, means, database);
-                clusters = sort(means, database);
-                changed = !means.equals(oldMeans);
-                iteration++;
-            }
-            Integer[][] resultClusters = new Integer[clusters.size()][];
-            for(int i = 0; i < clusters.size(); i++)
-            {
-                List<Integer> cluster = clusters.get(i);
-                resultClusters[i] = cluster.toArray(new Integer[cluster.size()]);
-            }
-            result = new Clusters<DoubleVector>(resultClusters,database);
-        }
-        else
-        {
-            result = new Clusters<DoubleVector>(new Integer[0][0], database);
-        }
-    }
-    
-    /**
-     * Returns the mean vectors of the given clusters in the given database.
-     * 
-     * 
-     * @param clusters the clusters to compute the means
-     * @param means the recent means
-     * @param database the database containing the vectors
-     * @return the mean vectors of the given clusters in the given database
-     */
-    protected List<DoubleVector> means(List<List<Integer>> clusters, List<DoubleVector> means, Database<DoubleVector> database)
-    {
-        List<DoubleVector> newMeans = new ArrayList<DoubleVector>(k);
-        for(int i = 0; i < k; i++)
-        {
-            List<Integer> list = clusters.get(i);
-            DoubleVector mean = null;
-            for(Iterator<Integer> clusterIter = list.iterator(); clusterIter.hasNext();)
-            {
-                if(mean == null)
-                {
-                    mean = database.get(clusterIter.next());
-                }
-                else
-                {
-                    mean = (DoubleVector) mean.plus(database.get(clusterIter.next()));
-                }
-            }
-            if(list.size() > 0)
-            {
-                mean = (DoubleVector) mean.multiplicate(1.0 / list.size());
-            }
-            else // mean == null
-            {
-                mean = means.get(i);
-            }
-            newMeans.add(mean);
-        }
-        return newMeans;
-    }
+  /**
+   * Provides the k-means algorithm.
+   */
+  public KMeans() {
+    super();
+    parameterToDescription.put(K_P + OptionHandler.EXPECTS_VALUE, K_D);
+    optionHandler = new OptionHandler(parameterToDescription, KMeans.class.getName());
+  }
 
-    /**
-     * Returns a list of clusters. The k<sup>th</sup> cluster contains the ids
-     * of those FeatureVectors, that are nearest to the
-     * k<sup>th</sup> mean.
-     * 
-     * 
-     * @param means a list of k means
-     * @param database the database to cluster
-     * @return list of k clusters
-     */
-    protected List<List<Integer>> sort(List<DoubleVector> means, Database<DoubleVector> database)
-    {
-        List<List<Integer>> clusters = new ArrayList<List<Integer>>(k);
-        for(int i = 0; i < k; i++)
-        {
-            clusters.add(new ArrayList<Integer>());
+  /**
+   * @see de.lmu.ifi.dbs.algorithm.Algorithm#getDescription()
+   */
+  public Description getDescription() {
+    // TODO reference
+    return new Description("K-Means", "K-Means", "finds a partitioning into k clusters", "...");
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.algorithm.Algorithm#getResult()
+   */
+  public Result getResult() {
+    return result;
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.algorithm.Algorithm#run(de.lmu.ifi.dbs.database.Database)
+   */
+  public void run(Database<DoubleVector> database) throws IllegalStateException {
+    Random random = new Random();
+    if (database.size() > 0) {
+      DoubleVector randomBase = database.get(database.iterator().next());
+      AttributeWiseDoubleVectorNormalization normalization = new AttributeWiseDoubleVectorNormalization();
+      List<DoubleVector> list = new ArrayList<DoubleVector>(database.size());
+      for (Iterator<Integer> dbIter = database.iterator(); dbIter.hasNext();) {
+        list.add((DoubleVector) database.get(dbIter.next()));
+      }
+      try {
+        normalization.normalize(list);
+      }
+      catch (NonNumericFeaturesException e) {
+        e.printStackTrace();
+      }
+      List<DoubleVector> means = new ArrayList<DoubleVector>(k);
+      List<DoubleVector> oldMeans;
+      List<List<Integer>> clusters;
+      if (isVerbose()) {
+        System.out.println("initializing random vectors");
+      }
+      for (int i = 0; i < k; i++) {
+        DoubleVector randomVector = (DoubleVector) randomBase.randomInstance(random);
+        try {
+          means.add((DoubleVector) normalization.restore(randomVector));
         }
-        for(Iterator<Integer> dbIter = database.iterator(); dbIter.hasNext();)
-        {
-            Distance[] distances = new Distance[k];
-            Integer id = dbIter.next();
-            DoubleVector fv = database.get(id);
-            int minIndex = 0;
-            for(int d = 0; d < k; d++)
-            {
-                distances[d] = getDistanceFunction().distance(fv, means.get(d));
-                if(distances[d].compareTo(distances[minIndex]) < 0)
-                {
-                    minIndex = d;
-                }
-            }
-            clusters.get(minIndex).add(id);
+        catch (NonNumericFeaturesException e) {
+          e.printStackTrace();
+          means.add(randomVector);
         }
-        for(Iterator<List<Integer>> clustersIter = clusters.iterator(); clustersIter.hasNext();)
-        {
-            Collections.sort(clustersIter.next());
+      }
+      clusters = sort(means, database);
+      boolean changed = true;
+      int iteration = 1;
+      while (changed) {
+        if (isVerbose()) {
+          System.out.println("iteration " + iteration);
         }
-        return clusters;
+        oldMeans = new ArrayList<DoubleVector>(k);
+        oldMeans.addAll(means);
+        means = means(clusters, means, database);
+        clusters = sort(means, database);
+        changed = !means.equals(oldMeans);
+        iteration++;
+      }
+      Integer[][] resultClusters = new Integer[clusters.size()][];
+      for (int i = 0; i < clusters.size(); i++) {
+        List<Integer> cluster = clusters.get(i);
+        resultClusters[i] = cluster.toArray(new Integer[cluster.size()]);
+      }
+      result = new Clusters<DoubleVector>(resultClusters, database, getParameterSettings());
     }
-    
-    /**
-     * 
-     * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(java.lang.String[])
-     */
-    @Override
-    public String[] setParameters(String[] args) throws IllegalArgumentException
-    {
-        String[] remainingParameters = super.setParameters(args);
-        try
-        {
-            k = Integer.parseInt(optionHandler.getOptionValue(K_P));
-            if(k <= 0)
-            {
-                throw new IllegalArgumentException("Parameter "+K_P+" must be a positive integer.");
-            }
-        }
-        catch(NumberFormatException e)
-        {
-            throw new IllegalArgumentException(e);
-        }
-        catch(UnusedParameterException e)
-        {
-            throw new IllegalArgumentException(e);
-        }
-        return remainingParameters;
+    else {
+      result = new Clusters<DoubleVector>(new Integer[0][0], database, getParameterSettings());
     }
-    
+  }
+
+  /**
+   * Returns the mean vectors of the given clusters in the given database.
+   *
+   * @param clusters the clusters to compute the means
+   * @param means    the recent means
+   * @param database the database containing the vectors
+   * @return the mean vectors of the given clusters in the given database
+   */
+  protected List<DoubleVector> means(List<List<Integer>> clusters, List<DoubleVector> means, Database<DoubleVector> database) {
+    List<DoubleVector> newMeans = new ArrayList<DoubleVector>(k);
+    for (int i = 0; i < k; i++) {
+      List<Integer> list = clusters.get(i);
+      DoubleVector mean = null;
+      for (Iterator<Integer> clusterIter = list.iterator(); clusterIter.hasNext();) {
+        if (mean == null) {
+          mean = database.get(clusterIter.next());
+        }
+        else {
+          mean = (DoubleVector) mean.plus(database.get(clusterIter.next()));
+        }
+      }
+      if (list.size() > 0) {
+        assert mean != null;
+        mean = (DoubleVector) mean.multiplicate(1.0 / list.size());
+      }
+      else // mean == null
+      {
+        mean = means.get(i);
+      }
+      newMeans.add(mean);
+    }
+    return newMeans;
+  }
+
+  /**
+   * Returns a list of clusters. The k<sup>th</sup> cluster contains the ids
+   * of those FeatureVectors, that are nearest to the
+   * k<sup>th</sup> mean.
+   *
+   * @param means    a list of k means
+   * @param database the database to cluster
+   * @return list of k clusters
+   */
+  protected List<List<Integer>> sort(List<DoubleVector> means, Database<DoubleVector> database) {
+    List<List<Integer>> clusters = new ArrayList<List<Integer>>(k);
+    for (int i = 0; i < k; i++) {
+      clusters.add(new ArrayList<Integer>());
+    }
+    for (Iterator<Integer> dbIter = database.iterator(); dbIter.hasNext();) {
+      Distance[] distances = new Distance[k];
+      Integer id = dbIter.next();
+      DoubleVector fv = database.get(id);
+      int minIndex = 0;
+      for (int d = 0; d < k; d++) {
+        distances[d] = getDistanceFunction().distance(fv, means.get(d));
+        if (distances[d].compareTo(distances[minIndex]) < 0) {
+          minIndex = d;
+        }
+      }
+      clusters.get(minIndex).add(id);
+    }
+    for (List<Integer> cluster : clusters) {
+      Collections.sort(cluster);
+    }
+    return clusters;
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(String[])
+   */
+  @Override
+  public String[] setParameters(String[] args) throws IllegalArgumentException {
+    String[] remainingParameters = super.setParameters(args);
+    try {
+      k = Integer.parseInt(optionHandler.getOptionValue(K_P));
+      if (k <= 0) {
+        throw new IllegalArgumentException("Parameter " + K_P + " must be a positive integer.");
+      }
+    }
+    catch (NumberFormatException e) {
+      throw new IllegalArgumentException(e);
+    }
+    catch (UnusedParameterException e) {
+      throw new IllegalArgumentException(e);
+    }
+    return remainingParameters;
+  }
+
+  /**
+   * Returns the parameter setting of this algorithm.
+   * @return the parameter setting of this algorithm
+   */
+  public String[] getParameterSettings() {
+    return new String[]{K_P + " = " + k};
+  }
+
 }
--- src/de/lmu/ifi/dbs/algorithm/result/Clusters.java
+++ src/de/lmu/ifi/dbs/algorithm/result/Clusters.java
@@ -14,142 +14,116 @@ import java.text.NumberFormat;
 
 /**
  * Provides a result of a clustering-algorithm that computes several clusters.
- * 
+ *
  * @author Arthur Zimek (<a
  *         href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public class Clusters<T extends MetricalObject> implements Result<T>
-{
-    /**
-     * Marker for a file name of a cluster.
-     */
-    public static final String CLUSTER_MARKER = "cluster";
+public class Clusters<T extends MetricalObject> extends AbstractResult<T> {
+  /**
+   * Marker for a file name of a cluster.
+   */
+  public static final String CLUSTER_MARKER = "cluster";
 
-    /**
-     * An array of clusters, respectively, where each array provides
-     * the object ids of its members
-     */
-    protected Integer[][] clusters;
+  /**
+   * An array of clusters, respectively, where each array provides
+   * the object ids of its members
+   */
+  protected Integer[][] clusters;
 
-    /**
-     * The database containing the objects of clusters.
-     */
-    protected Database<T> db;
+  /**
+   * Provides a result of a clustering-algorithm that computes several
+   * clusters and remaining noise.
+   *
+   * @param clusters an array of clusters and noise, respectively, where each array
+   *                 provides the object ids of its members
+   * @param db       the database containing the objects of clusters
+   */
+  public Clusters(Integer[][] clusters, Database<T> db, String[] parameters) {
+    super(db, parameters);
+    this.clusters = clusters;
+  }
 
-    /**
-     * Provides a result of a clustering-algorithm that computes several
-     * clusters and remaining noise.
-     * 
-     * @param clusters
-     *            an array of clusters and noise, respectively, where each array
-     *            provides the object ids of its members
-     * @param db
-     *            the database containing the objects of clusters
-     */
-    public Clusters(Integer[][] clusters, Database<T> db)
-    {
-        this.clusters = clusters;
-        this.db = db;
+  /**
+   * @see Result#output(File, Normalization)
+   */
+  public void output(File out, Normalization<T> normalization) throws UnableToComplyException {
+    for (int c = 0; c < this.clusters.length; c++) {
+      String marker = CLUSTER_MARKER + format(c + 1, clusters.length - 1);
+      PrintStream markedOut;
+      try {
+        File markedFile = new File(out.getAbsolutePath() + File.separator + marker);
+        markedFile.getParentFile().mkdirs();
+        markedOut = new PrintStream(new FileOutputStream(markedFile));
+      }
+      catch (Exception e) {
+        markedOut = new PrintStream(new FileOutputStream(FileDescriptor.out));
+        markedOut.println(marker + ":");
+      }
+      try {
+        write(c, markedOut, normalization);
+      }
+      catch (NonNumericFeaturesException e) {
+        throw new UnableToComplyException(e);
+      }
+      markedOut.flush();
     }
 
-    /**
-     * @see Result#output(File, Normalization)
-     */
-    public void output(File out, Normalization<T> normalization) throws UnableToComplyException
-    {
-        for(int c = 0; c < this.clusters.length; c++)
-        {
-            String marker = CLUSTER_MARKER + format(c + 1, clusters.length - 1);
-            PrintStream markedOut;
-            try
-            {
-                File markedFile = new File(out.getAbsolutePath() + File.separator + marker);
-                markedFile.getParentFile().mkdirs();
-                markedOut = new PrintStream(new FileOutputStream(markedFile));
-            }
-            catch(Exception e)
-            {
-                markedOut = new PrintStream(new FileOutputStream(FileDescriptor.out));
-                markedOut.println(marker + ":");
-            }
-            try
-            {
-                write(c, markedOut, normalization);
-            }
-            catch(NonNumericFeaturesException e)
-            {
-                throw new UnableToComplyException(e);
-            }
-            markedOut.flush();
-        }
+  }
 
-    }
+  /**
+   * Returns an integer-string for the given input, that has as many leading
+   * zeros as to match the length of the specified maximum.
+   *
+   * @param input   an integer to be formatted
+   * @param maximum the maximum to adapt the format to
+   * @return an integer-string for the given input, that has as many leading
+   *         zeros as to match the length of the specified maximum
+   */
+  protected String format(int input, int maximum) {
+    NumberFormat formatter = NumberFormat.getIntegerInstance();
+    formatter.setMinimumIntegerDigits(Integer.toString(maximum).length());
+    return formatter.format(input);
+  }
 
-    /**
-     * Returns an integer-string for the given input, that has as many leading
-     * zeros as to match the length of the specified maximum.
-     * 
-     * @param input
-     *            an integer to be formatted
-     * @param maximum
-     *            the maximum to adapt the format to
-     * @return an integer-string for the given input, that has as many leading
-     *         zeros as to match the length of the specified maximum
-     */
-    protected String format(int input, int maximum)
-    {
-        NumberFormat formatter = NumberFormat.getIntegerInstance();
-        formatter.setMinimumIntegerDigits(Integer.toString(maximum).length());
-        return formatter.format(input);
+  /**
+   * Writes a cluster denoted by its cluster number to the designated print
+   * stream.
+   *
+   * @param clusterIndex  the number of the cluster to be written
+   * @param out           the print stream where to write
+   * @param normalization a Normalization to restore original values for output - may
+   *                      remain null
+   * @throws NonNumericFeaturesException if feature vector is not compatible with values initialized
+   *                                     during normalization
+   */
+  private void write(int clusterIndex, PrintStream out, Normalization<T> normalization) throws NonNumericFeaturesException {
+    writeHeader(out, normalization);
+    for (int i = 0; i < clusters[clusterIndex].length; i++) {
+      T mo = db.get(clusters[clusterIndex][i]);
+      if (normalization != null) {
+        mo = normalization.restore(mo);
+      }
+      out.println(mo.toString() + SEPARATOR + db.getAssociation(Database.ASSOCIATION_ID_LABEL, clusters[clusterIndex][i]));
     }
+  }
 
-    /**
-     * Writes a cluster denoted by its cluster number to the designated print
-     * stream.
-     * 
-     * @param clusterIndex
-     *            the number of the cluster to be written
-     * @param out
-     *            the print stream where to write
-     * @param normalization
-     *            a Normalization to restore original values for output - may
-     *            remain null
-     * @throws NonNumericFeaturesException
-     *             if feature vector is not compatible with values initialized
-     *             during normalization
-     */
-    private void write(int clusterIndex, PrintStream out, Normalization<T> normalization) throws NonNumericFeaturesException
-    {
-        for(int i = 0; i < clusters[clusterIndex].length; i++)
-        {
-            T mo = db.get(clusters[clusterIndex][i]);
-            if(normalization != null)
-            {
-                mo = normalization.restore(mo);
-            }
-            out.println(mo.toString() + SEPARATOR + db.getAssociation(Database.ASSOCIATION_ID_LABEL, clusters[clusterIndex][i]));
-        }
-    }
+  /**
+   * Returns the database to which this clustering result belongs to.
+   *
+   * @return the database to which this clustering result belongs to
+   */
+  public Database<T> getDatabase() {
+    return db;
+  }
 
-    /**
-     * Returns the database to which this clustering result belongs to.
-     * 
-     * @return the database to which this clustering result belongs to
-     */
-    public Database<T> getDatabase()
-    {
-        return db;
-    }
-
-    /**
-     * Returns the array of clusters and noise, respectively, where each array
-     * provides the object ids of its members.
-     * 
-     * @return the array of clusters and noise
-     */
-    public Integer[][] getClustersArray()
-    {
-        return clusters;
-    }
+  /**
+   * Returns the array of clusters and noise, respectively, where each array
+   * provides the object ids of its members.
+   *
+   * @return the array of clusters and noise
+   */
+  public Integer[][] getClustersArray() {
+    return clusters;
+  }
 
 }
