8368ace049989d8885ec57e640de467b483c6170
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-09-21 12:39:06 +0000

    renamed

--- src/de/lmu/ifi/dbs/database/DeLiCluTreeDatabase.java
+++ src/de/lmu/ifi/dbs/database/DeLiCluTreeDatabase.java
@@ -1,22 +1,24 @@
 package de.lmu.ifi.dbs.database;
 
-import de.lmu.ifi.dbs.data.FeatureVector;
+import de.lmu.ifi.dbs.data.RealVector;
 import de.lmu.ifi.dbs.index.spatial.SpatialIndex;
 import de.lmu.ifi.dbs.index.spatial.rtree.DeLiCluTree;
 import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
 
+import java.util.List;
+
 /**
- * DeliRTreeDatabase is a database implementation which is supported by a
- * DeliRTree index structure.
+ * DeLiCluTreeDatabase is a database implementation which is supported by a
+ * DeLiCluTree index structure.
  *
  * @author Elke Achtert(<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
  */
-public class DeliRTreeDatabase extends SpatialIndexDatabase {
+public class DeLiCluTreeDatabase<T extends RealVector> extends SpatialIndexDatabase<T> {
 
   /**
    * Empty constructor, creates a new DeliRTreeDatabase.
    */
-  public DeliRTreeDatabase() {
+  public DeLiCluTreeDatabase() {
     super();
     optionHandler = new OptionHandler(parameterToDescription, this.getClass().getName());
   }
@@ -26,8 +28,8 @@ public class DeliRTreeDatabase extends SpatialIndexDatabase {
    *
    * @return the spatial index for this database
    */
-  public SpatialIndex createSpatialIndex(final FeatureVector[] objects) {
-    return new DeLiCluTree(objects, fileName, pageSize, cacheSize);
+  public SpatialIndex<T> createSpatialIndex(List<T> objects) {
+    return new DeLiCluTree<T>(objects, fileName, pageSize, cacheSize);
   }
 
   /**
@@ -36,8 +38,8 @@ public class DeliRTreeDatabase extends SpatialIndexDatabase {
    *
    * @param dimensionality the dimensionality of the objects to be indexed
    */
-  public SpatialIndex createSpatialIndex(int dimensionality) {
-    return new DeLiCluTree(dimensionality, fileName, pageSize, cacheSize);
+  public SpatialIndex<T> createSpatialIndex(int dimensionality) {
+    return new DeLiCluTree<T>(dimensionality, fileName, pageSize, cacheSize);
   }
 
   /**
@@ -45,7 +47,7 @@ public class DeliRTreeDatabase extends SpatialIndexDatabase {
    */
   public String description() {
     StringBuffer description = new StringBuffer();
-    description.append(DeliRTreeDatabase.class.getName());
+    description.append(DeLiCluTreeDatabase.class.getName());
     description.append(" holds all the data in a DeliRTree index structure.\n");
     description.append(optionHandler.usage("", false));
     return description.toString();
@@ -61,5 +63,24 @@ public class DeliRTreeDatabase extends SpatialIndexDatabase {
     return super.setParameters(args);
   }
 
+  /**
+   * Returns the index of this database.
+   *
+   * @return the index of this database
+   */
+  public DeLiCluTree<T> getIndex() {
+    return (DeLiCluTree<T>) index;
+  }
 
+  /**
+   * Marks the specified object as handled and returns the id of the
+   * objects's parent node.
+   *
+   * @param id the id of the object to be marked as handled
+   * @return the id of the objects's parent node
+   */
+  public Integer setHandled(Integer id) {
+    T o = get(id);
+    return getIndex().setHandled(o);
+  }
 }
