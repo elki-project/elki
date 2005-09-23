6cc98f46ca324b8e59c7c81c99b0deedd90e698d
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-09-23 07:32:57 +0000

    import reduced

--- src/de/lmu/ifi/dbs/wrapper/COPACWrapper.java
+++ src/de/lmu/ifi/dbs/wrapper/COPACWrapper.java
@@ -1,16 +1,19 @@
 package de.lmu.ifi.dbs.wrapper;
 
-import de.lmu.ifi.dbs.algorithm.*;
+import de.lmu.ifi.dbs.algorithm.AbortException;
+import de.lmu.ifi.dbs.algorithm.AbstractAlgorithm;
+import de.lmu.ifi.dbs.algorithm.COPAC;
+import de.lmu.ifi.dbs.algorithm.DBSCAN;
+import de.lmu.ifi.dbs.algorithm.KDDTask;
+import de.lmu.ifi.dbs.database.FileBasedDatabaseConnection;
 import de.lmu.ifi.dbs.distance.LocallyWeightedDistanceFunction;
 import de.lmu.ifi.dbs.normalization.AttributeWiseDoubleVectorNormalization;
+import de.lmu.ifi.dbs.preprocessing.KnnQueryBasedCorrelationDimensionPreprocessor;
+import de.lmu.ifi.dbs.utilities.Description;
 import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
 import de.lmu.ifi.dbs.utilities.optionhandling.UnusedParameterException;
-import de.lmu.ifi.dbs.utilities.Description;
-import de.lmu.ifi.dbs.preprocessing.KnnQueryBasedCorrelationDimensionPreprocessor;
-import de.lmu.ifi.dbs.database.FileBasedDatabaseConnection;
 
 import java.util.ArrayList;
-import java.io.File;
 
 /**
  * Wrapper class for COPAC algorithm.
