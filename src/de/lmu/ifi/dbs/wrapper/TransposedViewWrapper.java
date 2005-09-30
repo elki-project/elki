67a3458ec2e7de798e6c0ae75fd40da0fdd2928c
Elke Achtert <achtert@dbs.ifi.lmu.de>
2005-09-30 11:17:59 +0000

    new

---
+++ src/de/lmu/ifi/dbs/wrapper/TransposedViewWrapper.java
@@ -0,0 +1,110 @@
+package de.lmu.ifi.dbs.wrapper;
+
+import de.lmu.ifi.dbs.data.DoubleVector;
+import de.lmu.ifi.dbs.database.Database;
+import de.lmu.ifi.dbs.parser.StandardLabelParser;
+import de.lmu.ifi.dbs.utilities.Util;
+import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
+
+import java.io.*;
+import java.util.Iterator;
+
+/**
+ * This wrapper class reads s data file and writes the transposed view
+ * of the data file to the specified output. Additionmally a script file for gnuplot
+ * is written.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public class TransposedViewWrapper extends AbstractWrapper {
+  /**
+   * Parameter for gnuplot output directory.
+   */
+  public static final String GNUPLOT_P = "gnu";
+
+  /**
+   * Description for parameter gnu.
+   */
+  public static final String GNUPLOT_D = "<filename>file to write the gnuplot script in.";
+
+  /**
+   * The output string for the gnuplot script.
+   */
+  String gnuplot;
+
+
+  /**
+   * Initializes the option handler.
+   */
+  public TransposedViewWrapper() {
+    super();
+    optionHandler = new OptionHandler(parameterToDescription, getClass().getName());
+  }
+
+  /**
+   * @see Wrapper#run(String[])
+   */
+  public void run(String[] args) {
+    this.setParameters(args);
+
+    try {
+      File inFile = new File(input);
+      InputStream in = new FileInputStream(inFile);
+
+      File outFile = new File(output);
+      PrintStream out = new PrintStream(new FileOutputStream(outFile));
+
+
+      // parse the data
+      StandardLabelParser parser = new StandardLabelParser();
+      parser.setParameters(args);
+      Database<DoubleVector> db = parser.parse(in);
+
+      // transpose the data
+      double[][] transposed = new double[db.dimensionality()][db.size()];
+
+      for (int i = 0; i < db.dimensionality(); i++) {
+        Iterator<Integer> it = db.iterator();
+        int j = 0;
+        while (it.hasNext()) {
+          Integer id = it.next();
+          DoubleVector o = db.get(id);
+          transposed[i][j++] = o.getValue(i + 1);
+        }
+      }
+
+      // write to output
+      for (double[] v : transposed) {
+        for (double value : v) {
+          out.print(value + " ");
+        }
+        out.println();
+      }
+      out.flush();
+      out.close();
+
+      // write gnuplot script
+      String gnuplotScript = Util.transposedGnuplotScript(outFile.getName(), db.dimensionality());
+      PrintStream gnuplotOut = new PrintStream(new FileOutputStream(new File(gnuplot)));
+      gnuplotOut.print(gnuplotScript);
+      gnuplotOut.flush();
+      gnuplotOut.close();
+
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void main(String[] args) {
+    TransposedViewWrapper wrapper = new TransposedViewWrapper();
+    try {
+      wrapper.run(args);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getMessage());
+    }
+  }
+}
