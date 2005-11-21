35027c756685d9268bb7ecd7ad2d0051d95e3768
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-11-21 16:26:37 +0000

    mkdirs

--- src/de/lmu/ifi/dbs/algorithm/result/KNNDistanceOrderResult.java
+++ src/de/lmu/ifi/dbs/algorithm/result/KNNDistanceOrderResult.java
@@ -16,39 +16,49 @@ import java.io.PrintStream;
 import java.util.List;
 
 /**
- * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ * @author Arthur Zimek (<a
+ *         href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public class KNNDistanceOrderResult<O extends MetricalObject, D extends Distance<D>> extends AbstractResult<O> {
-  private final List<D> knnDistances;
+public class KNNDistanceOrderResult<O extends MetricalObject, D extends Distance<D>> extends AbstractResult<O>
+{
+    private final List<D> knnDistances;
 
-  /**
-   * @param db
-   */
-  public KNNDistanceOrderResult(final Database<O> db, final List<D> knnDistances) {
-    super(db);
-    this.knnDistances = knnDistances;
-  }
-
-  /**
-   * @see Result#output(java.io.File, de.lmu.ifi.dbs.normalization.Normalization, java.util.List)
-   */
-  public void output(final File out, final Normalization<O> normalization, final List<AttributeSettings> settings) throws UnableToComplyException {
-    PrintStream output;
-    try {
-      output = new PrintStream(new FileOutputStream(out));
-    }
-    catch (Exception e) {
-      output = new PrintStream(new FileOutputStream(FileDescriptor.out));
-    }
-    try {
-      super.writeHeader(output, settings);
-      Util.print(knnDistances, System.getProperty("line.separator"), output);
-      output.println();
-    }
-    catch (NonNumericFeaturesException e) {
-      throw new UnableToComplyException(e);
+    /**
+     * @param db
+     */
+    public KNNDistanceOrderResult(final Database<O> db, final List<D> knnDistances)
+    {
+        super(db);
+        this.knnDistances = knnDistances;
     }
 
-  }
+    /**
+     * @see Result#output(java.io.File,
+     *      de.lmu.ifi.dbs.normalization.Normalization, java.util.List)
+     */
+    public void output(final File out, final Normalization<O> normalization, final List<AttributeSettings> settings) throws UnableToComplyException
+    {
+        PrintStream output;
+        try
+        {
+            out.getParentFile().mkdirs();
+            output = new PrintStream(new FileOutputStream(out));
+        }
+        catch(Exception e)
+        {
+            output = new PrintStream(new FileOutputStream(FileDescriptor.out));
+        }
+        try
+        {
+            super.writeHeader(output, settings);
+            Util.print(knnDistances, System.getProperty("line.separator"), output);
+            output.println();
+        }
+        catch(NonNumericFeaturesException e)
+        {
+            throw new UnableToComplyException(e);
+        }
+
+    }
 
 }
