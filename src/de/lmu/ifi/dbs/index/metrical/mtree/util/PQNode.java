ec62b509d04d537e3f895c5dc5e4084600de0d17
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-11-21 12:46:05 +0000

    serialVersionUID

--- src/de/lmu/ifi/dbs/index/metrical/mtree/util/PQNode.java
+++ src/de/lmu/ifi/dbs/index/metrical/mtree/util/PQNode.java
@@ -5,52 +5,69 @@ import de.lmu.ifi.dbs.utilities.heap.DefaultHeapNode;
 import de.lmu.ifi.dbs.utilities.heap.Identifiable;
 
 /**
- * Encapsulates the attributes for a object that can be stored in a heap. The object to be stored
- * represents a node in a M-Tree and some additional information.
- * Additionally to the DefaultHeapNode this object holds the id of the routing object of
- * the underlying M-Tree node and its covering radius.
- *
- * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ * Encapsulates the attributes for a object that can be stored in a heap. The
+ * object to be stored represents a node in a M-Tree and some additional
+ * information. Additionally to the DefaultHeapNode this object holds the id of
+ * the routing object of the underlying M-Tree node and its covering radius.
+ * 
+ * @author Elke Achtert (<a
+ *         href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
  */
-public class PQNode<D extends Distance> extends DefaultHeapNode<Distance, Identifiable> {
-  /**
-   * The id of the routing object.
-   */
-  private Integer routingObjectID;
+public class PQNode<D extends Distance> extends DefaultHeapNode<D, Identifiable>
+{
+    /**
+     * Generated serialVersionUID.
+     */
+    private static final long serialVersionUID = -671978541634018004L;
 
-  /**
-   * Empty constructor for serialization purposes.
-   */
-  public PQNode() {
-    super();
-  }
+    /**
+     * The id of the routing object.
+     */
+    private Integer routingObjectID;
 
-  /**
-   * Creates a new heap node with the specified parameters.
-   *
-   * @param d_min           the minimum distance of the node
-   * @param nodeID          the id of the node
-   * @param routingObjectID the id of the routing object of the node
-   */
-  public PQNode(D d_min, final Integer nodeID, final Integer routingObjectID) {
-    super(d_min, new Identifiable() {
-      public Integer getID() {
-        return nodeID;
-      }
+    /**
+     * Empty constructor for serialization purposes.
+     */
+    public PQNode()
+    {
+        super();
+    }
 
-      public int compareTo(Identifiable o) {
-        return nodeID - o.getID();
-      }
-    });
+    /**
+     * Creates a new heap node with the specified parameters.
+     * 
+     * @param d_min
+     *            the minimum distance of the node
+     * @param nodeID
+     *            the id of the node
+     * @param routingObjectID
+     *            the id of the routing object of the node
+     */
+    public PQNode(D d_min, final Integer nodeID, final Integer routingObjectID)
+    {
+        super(d_min, new Identifiable()
+        {
+            public Integer getID()
+            {
+                return nodeID;
+            }
 
-    this.routingObjectID = routingObjectID;
-  }
+            public int compareTo(Identifiable o)
+            {
+                return nodeID - o.getID();
+            }
+        });
 
-  /**
-   * Returns the id of the routing object.
-   * @return the id of the routing object
-   */
-  public Integer getRoutingObjectID() {
-    return routingObjectID;
-  }
+        this.routingObjectID = routingObjectID;
+    }
+
+    /**
+     * Returns the id of the routing object.
+     * 
+     * @return the id of the routing object
+     */
+    public Integer getRoutingObjectID()
+    {
+        return routingObjectID;
+    }
 }
