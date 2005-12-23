e0954ac704de512a011b77460b2827b092fafc1e
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2005-12-23 08:29:33 +0000

    initial revision

---
+++ src/de/lmu/ifi/dbs/algorithm/classifier/PriorProbabilityClassifier.java
@@ -0,0 +1,125 @@
+package de.lmu.ifi.dbs.algorithm.classifier;
+
+import de.lmu.ifi.dbs.algorithm.result.PriorProbability;
+import de.lmu.ifi.dbs.algorithm.result.Result;
+import de.lmu.ifi.dbs.data.MetricalObject;
+import de.lmu.ifi.dbs.database.Database;
+import de.lmu.ifi.dbs.utilities.Description;
+import de.lmu.ifi.dbs.utilities.Util;
+import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Classifier to classifiy instances based on the prior probability of classes in the database.
+ * 
+ * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ */
+public class PriorProbabilityClassifier<M extends MetricalObject> extends AbstractClassifier<M>
+{
+    /**
+     * The generated serial version UID.
+     */
+    private static final long serialVersionUID = 376101386316450537L;
+    
+    /**
+     * Holds the prior probabilities.
+     */
+    protected double[] distribution;
+    
+    /**
+     * Index of the most abundant class.
+     */
+    protected int prediction;
+    
+    /**
+     * Holds the database the prior probabilities are based on.
+     */
+    protected Database<M> database;
+    
+    /**
+     * Provides a classifier always predicting the prior probabilities.
+     */
+    public PriorProbabilityClassifier()
+    {
+        super();
+        optionHandler = new OptionHandler(parameterToDescription,PriorProbabilityClassifier.class.getName());
+    }
+
+    /**
+     * Learns the prior probability for all classes.
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.classifier.Classifier#buildClassifier(de.lmu.ifi.dbs.database.Database)
+     */
+    public void buildClassifier(Database<M> database) throws IllegalStateException
+    {
+        labels = AbstractClassifier.classes(database);
+        this.database = database;
+        distribution = new double[labels.length];
+        int[] occurences = new int[labels.length];
+        for(Iterator<Integer> iter = database.iterator(); iter.hasNext();)
+        {
+            String label = (String) database.getAssociation(CLASS,iter.next());
+            int index = Arrays.binarySearch(labels,label);
+            if(index > -1)
+            {
+                occurences[index]++;
+            }
+            else
+            {
+                throw new IllegalStateException("inconsistent state of database - found new label: "+label);
+            }
+        }
+        double size = database.size();
+        for(int i = 0; i < distribution.length; i++)
+        {
+            distribution[i] = ((double) occurences[i] / size);
+        }
+        prediction = Util.getIndexOfMaximum(distribution);
+    }
+    
+    /**
+     * Returns the index of the most abundant class.
+     * According to the prior class probability distribution,
+     * this is the index of the class showing maximum prior probability.
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.classifier.Classifier#classify(null)
+     */
+    @Override
+    public int classify(M instance) throws IllegalStateException
+    {
+        return prediction;
+    }
+
+    /**
+     * Returns the distribution of the classes' prior probabilities.
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.classifier.Classifier#classDistribution(M)
+     */
+    public double[] classDistribution(M instance) throws IllegalStateException
+    {
+        return distribution;
+    }
+
+    /**
+     * Returns an overview over the prior probability based on the database
+     * the classifier is built on.
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.Algorithm#getResult()
+     */
+    public Result<M> getResult()
+    {
+        return new PriorProbability<M>(database,labels,distribution);
+    }
+
+    /**
+     * 
+     * @see de.lmu.ifi.dbs.algorithm.Algorithm#getDescription()
+     */
+    public Description getDescription()
+    {
+        return new Description("Prior Probability Classifier", "Prior Probability Classifier", "Classifier to predict simply prior probabilities for all classes as defined by their relative abundance in a given database.","");
+    }
+
+}
