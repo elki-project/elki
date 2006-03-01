92660799571a2740d4eb89395c2949fedd7387a5
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-03-01 12:57:09 +0000

    id is set to external id

--- src/de/lmu/ifi/dbs/data/ExternalObject.java
+++ src/de/lmu/ifi/dbs/data/ExternalObject.java
@@ -7,24 +7,11 @@ package de.lmu.ifi.dbs.data;
  */
 public class ExternalObject extends AbstractDatabaseObject {
   /**
-   * The id of the external data object.
-   */
-  private Integer externalID;
-
-  /**
    * Creates a new ExternalObject with the specified id.
    *
    * @param externalID the external id
    */
   public ExternalObject(Integer externalID) {
-    this.externalID = externalID;
-  }
-
-  /**
-   * Returns the id of the external data object.
-   * @return the id of the external data object
-   */
-  public Integer getExternalID() {
-    return externalID;
+    setID(externalID);
   }
 }
