33821e4bdef7107831723583868dc9061f424da2
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-03-01 12:53:21 +0000

    new class used in ParsingResult

---
+++ src/de/lmu/ifi/dbs/parser/ObjectAndLabels.java
@@ -0,0 +1,56 @@
+package de.lmu.ifi.dbs.parser;
+
+import de.lmu.ifi.dbs.data.DatabaseObject;
+
+import java.util.List;
+
+/**
+ * Provides a single database objects and a list of labels associated with this object.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public class ObjectAndLabels<O extends DatabaseObject> {
+  /**
+   * The database object.
+   */
+  private final O object;
+
+  /**
+   * The list of labels associated with the database objects.
+   */
+  private final List<String> labels;
+
+  /**
+   * Provides a single database objects and a list of labels associated with this object.
+   * @param object the database object
+   * @param labels the list of string labels associated with this object
+   */
+  public ObjectAndLabels(O object, List<String> labels) {
+    this.object = object;
+    this.labels = labels;
+  }
+
+  /**
+   * Returns the database object.
+   * @return the database object
+   */
+  public O getObject() {
+    return object;
+  }
+
+  /**
+   * Returns the list of string labels associated with the database object.
+   * @return the list of string labels associated with the database object
+   */
+  public List<String> getLabels() {
+    return labels;
+  }
+
+  /**
+   * Returns a string representation of the object.
+   * @return a string representation of the object.
+   */
+  public String toString() {
+    return object.toString() + " " + labels.toString();
+  }
+}
