978f63e473d81ba97fa78f3024a2c07e3fc095a5
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-03-09 13:57:20 +0000

    AbstarctWrapper renamed to AbstarctAlgorithmWrapper

---
+++ src/de/lmu/ifi/dbs/wrapper/AbstractAlgorithmWrapper.java
@@ -0,0 +1,34 @@
+package de.lmu.ifi.dbs.wrapper;
+
+import de.lmu.ifi.dbs.algorithm.KDDTask;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * AbstractAlgorithmWrapper is an abstract super class for all wrapper
+ * classes running algorithms.
+ *
+ * @author Elke Achtert (<a
+ *         href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public abstract class AbstractAlgorithmWrapper extends AbstractWrapperneu {
+  /**
+   * @see Wrapper#run(String[])
+   */
+  public final void run(String[] args) {
+    String[] remainingParameters = setParameters(args);
+
+    List<String> params = initParameters(Arrays.asList(remainingParameters));
+    KDDTask task = new KDDTask();
+    task.setParameters(params.toArray(new String[params.size()]));
+    task.run();
+  }
+
+  /**
+   * Initializes the parameters.
+   *
+   * @return an array containing the parameters to run the algorithm.
+   */
+  public abstract List<String> initParameters(List<String> remainingParameters);
+}
