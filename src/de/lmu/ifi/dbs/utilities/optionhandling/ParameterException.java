a0deb2e3f626c506e8fd1ed9db7b9fb0efa98be9
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-03-13 12:45:18 +0000

    super class for all exceptions ocurring during parametrization

---
+++ src/de/lmu/ifi/dbs/utilities/optionhandling/ParameterException.java
@@ -0,0 +1,15 @@
+package de.lmu.ifi.dbs.utilities.optionhandling;
+
+/**
+ * Abstract super class for all exceptions thrown during parmeterization.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public abstract class ParameterException extends Exception {
+  /**
+   * @see Exception#Exception(String)
+   */
+  protected ParameterException(String message) {
+    super(message);
+  }
+}
