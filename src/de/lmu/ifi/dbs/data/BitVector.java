82e7f0df79c15e82ed2ea03da7d63bea814c1432
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2006-03-15 08:59:21 +0000

    implement WekaObject

--- src/de/lmu/ifi/dbs/data/BitVector.java
+++ src/de/lmu/ifi/dbs/data/BitVector.java
@@ -7,279 +7,322 @@ import java.util.Random;
 
 /**
  * Provides a BitVector wrapping a BitSet.
- *
- * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
+ * 
+ * @author Arthur Zimek (<a
+ *         href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public class BitVector extends NumberVector<Bit> {
-  /**
-   * Storing the bits.
-   */
-  private BitSet bits;
+public class BitVector extends NumberVector<Bit>
+{
+    /**
+     * Storing the bits.
+     */
+    private BitSet bits;
 
-  /**
-   * Dimensionality of this bit vector.
-   */
-  private int dimensionality;
+    /**
+     * Dimensionality of this bit vector.
+     */
+    private int dimensionality;
 
-  /**
-   * Provides a new BitVector corresponding to the specified bits and
-   * of the specified dimensionality.
-   *
-   * @param bits the bits to be set in this BitVector
-   * @param dimensionality the dimensionality of this BitVector
-   * @throws IllegalArgumentException if the specified dimensionality is to small to match
-   * the given BitSet
-   */
-  public BitVector(BitSet bits, int dimensionality) throws IllegalArgumentException{
-    if (dimensionality < bits.length()) {
-      throw new IllegalArgumentException("Specified dimensionality " + dimensionality + " is to low for specified BitSet of length " + bits.length());
-    }
-    this.bits = bits;
-    this.dimensionality = dimensionality;
-  }
-
-  /**
-   * Provides a new BitVector corresponding to the bits in the given array.
-   *
-   * @param bits an array of bits specifiying the bits in this bit vector
-   */
-  public BitVector(Bit[] bits) {
-    this.bits = new BitSet(bits.length);
-    for (int i = 0; i < bits.length; i++) {
-      this.bits.set(i, bits[i].bitValue());
-    }
-    this.dimensionality = bits.length;
-  }
-
-  /**
-   * @see FeatureVector#newInstance(Number[])
-   */
-  public FeatureVector<Bit> newInstance(Bit[] values) {
-    return new BitVector(values);
-  }
-
-  /**
-   * @see FeatureVector#randomInstance(Random)
-   */
-  public FeatureVector<Bit> randomInstance(Random random) {
-    Bit[] randomBits = new Bit[getDimensionality()];
-    for (int i = 0; i < randomBits.length; i++) {
-      randomBits[i] = new Bit(random.nextBoolean());
-    }
-    return new BitVector(randomBits);
-  }
-
-  /**
-   * Returns the same as {@link BitVector#randomInstance(Random) randomInstance(Random)}.
-   *
-   * @see FeatureVector#randomInstance(Number, Number, Random)
-   */
-  public FeatureVector<Bit> randomInstance(Bit min, Bit max, Random random) {
-    return randomInstance(random);
-  }
-
-  /**
-   * @see FeatureVector#getDimensionality()
-   */
-  public int getDimensionality() {
-    return dimensionality;
-  }
-
-  /**
-   * @see FeatureVector#getValue(int)
-   */
-  public Bit getValue(int dimension) {
-    if (dimension < 1 || dimension > dimensionality) {
-      throw new IllegalArgumentException("illegal dimension: " + dimension);
-    }
-    return new Bit(bits.get(dimension));
-  }
-
-  /**
-   * @see FeatureVector#getColumnVector()
-   */
-  public Matrix getColumnVector() {
-    double[] values = new double[dimensionality];
-    for (int i = 0; i < dimensionality; i++) {
-      values[i] = bits.get(i) ? 1 : 0;
-    }
-    return new Matrix(values, values.length);
-  }
-
-  /**
-   * @see FeatureVector#getRowVector()
-   */
-  public Matrix getRowVector() {
-    double[] values = new double[dimensionality];
-    for (int i = 0; i < dimensionality; i++) {
-      values[i] = bits.get(i) ? 1 : 0;
-    }
-    return new Matrix(new double[][]{values.clone()});
-  }
-
-  /**
-   * Returns a bit vector equal to this bit vector,
-   * if k is not 0, a bit vector with all components
-   * equal to zero otherwise.
-   *
-   * @see FeatureVector#multiplicate(double)
-   */
-  public FeatureVector<Bit> multiplicate(double k) {
-    if (k == 0) {
-      return nullVector();
-    }
-    else {
-      return new BitVector(bits, dimensionality);
-    }
-  }
-
-  /**
-   * Returns the inverse of the bit vector.
-   *
-   * @see FeatureVector#negativeVector()
-   */
-  public FeatureVector<Bit> negativeVector() {
-    BitSet newBits = (BitSet) bits.clone();
-    newBits.flip(0, dimensionality);
-    return new BitVector(newBits, dimensionality);
-  }
-
-  /**
-   * Returns a bit vector of equal dimensionality but containing 0 only.
-   *
-   * @see FeatureVector#nullVector()
-   */
-  public FeatureVector<Bit> nullVector() {
-    return new BitVector(new BitSet(), dimensionality);
-  }
-
-  /**
-   * Returns a bit vector corresponding to an XOR operation
-   * on this and the specified bit vector.
-   *
-   * @see FeatureVector#plus(FeatureVector)
-   */
-  public FeatureVector<Bit> plus(FeatureVector<Bit> fv) {
-    Bit[] fv_bits = new Bit[fv.getDimensionality()];
-    for (int i = 0; i < fv.getDimensionality(); i++) {
-      fv_bits[i] = fv.getValue(i);
+    /**
+     * Provides a new BitVector corresponding to the specified bits and of the
+     * specified dimensionality.
+     * 
+     * @param bits
+     *            the bits to be set in this BitVector
+     * @param dimensionality
+     *            the dimensionality of this BitVector
+     * @throws IllegalArgumentException
+     *             if the specified dimensionality is to small to match the
+     *             given BitSet
+     */
+    public BitVector(BitSet bits, int dimensionality) throws IllegalArgumentException
+    {
+        if(dimensionality < bits.length())
+        {
+            throw new IllegalArgumentException("Specified dimensionality " + dimensionality + " is to low for specified BitSet of length " + bits.length());
+        }
+        this.bits = bits;
+        this.dimensionality = dimensionality;
     }
 
-    BitVector bv = new BitVector(fv_bits);
-    bv.bits.xor(this.bits);
-    return bv;
-  }
-
-  /**
-   * Returns whether the bit at specified index is set.
-   *
-   * @param index the index of the bit to inspect
-   * @return true if the bit at index @code{index}
-   *         is set, false otherwise.
-   */
-  public boolean isSet(int index) {
-    return bits.get(index);
-  }
-
-  /**
-   * Returns whether the bits at all of the specified indices are set.
-   *
-   * @param indices the indices to inspect
-   * @return true if the bits at all of the specified indices are set,
-   *         false otherwise
-   */
-  public boolean areSet(int[] indices) {
-    boolean set = true;
-    for (int i = 0; i < indices.length && set; i++) {
-      //noinspection ConstantConditions
-      set &= bits.get(i);
+    /**
+     * Provides a new BitVector corresponding to the bits in the given array.
+     * 
+     * @param bits
+     *            an array of bits specifiying the bits in this bit vector
+     */
+    public BitVector(Bit[] bits)
+    {
+        this.bits = new BitSet(bits.length);
+        for(int i = 0; i < bits.length; i++)
+        {
+            this.bits.set(i, bits[i].bitValue());
+        }
+        this.dimensionality = bits.length;
     }
-    return set;
-  }
 
-  /**
-   * Returns the indices of all set bits.
-   *
-   * @return the indices of all set bits
-   */
-  public int[] setBits() {
-    int[] setBits = new int[bits.size()];
-    int index = 0;
-    for (int i = bits.nextSetBit(0); i >= 0; i = bits.nextSetBit(i + 1)) {
-      setBits[index++] = i;
+    /**
+     * @see FeatureVector#newInstance(Number[])
+     */
+    public FeatureVector<Bit> newInstance(Bit[] values)
+    {
+        return new BitVector(values);
     }
-    return setBits;
-  }
 
-  /**
-   * Returns whether this BitVector contains all bits
-   * that are set to true in the specified BitSet.
-   *
-   * @param bitset the bits to inspect in this BitVector
-   * @return true if this BitVector contains all bits
-   *         that are set to true in the specified BitSet,
-   *         false otherwise
-   */
-  public boolean contains(BitSet bitset) {
-    boolean contains = true;
-    for (int i = bitset.nextSetBit(0); i >= 0 && contains; i = bitset.nextSetBit(i + 1)) {
-      //noinspection ConstantConditions
-      contains &= bits.get(i);
+    /**
+     * @see FeatureVector#randomInstance(Random)
+     */
+    public FeatureVector<Bit> randomInstance(Random random)
+    {
+        Bit[] randomBits = new Bit[getDimensionality()];
+        for(int i = 0; i < randomBits.length; i++)
+        {
+            randomBits[i] = new Bit(random.nextBoolean());
+        }
+        return new BitVector(randomBits);
     }
-    return contains;
-  }
 
-  /**
-   * Returns a copy of the bits currently set in this BitVector.
-   *
-   * @return a copy of the bits currently set in this BitVector
-   */
-  public BitSet getBits() {
-    return (BitSet) bits.clone();
-  }
+    /**
+     * Returns the same as
+     * {@link BitVector#randomInstance(Random) randomInstance(Random)}.
+     * 
+     * @see FeatureVector#randomInstance(Number, Number, Random)
+     */
+    public FeatureVector<Bit> randomInstance(Bit min, Bit max, Random random)
+    {
+        return randomInstance(random);
+    }
 
-  /**
-   * Returns a String representation of this BitVector.
-   * The representation is suitable to be parsed
-   * by {@link de.lmu.ifi.dbs.parser.BitVectorLabelParser BitVectorLabelParser}.
-   *
-   * @see Object#toString()
-   */
-  public String toString() {
-    Bit[] bitArray = new Bit[dimensionality];
-    for (int i = 0; i < dimensionality; i++) {
-      bitArray[i] = new Bit(bits.get(i));
+    /**
+     * @see FeatureVector#getDimensionality()
+     */
+    public int getDimensionality()
+    {
+        return dimensionality;
     }
-    StringBuffer representation = new StringBuffer();
-    for (Bit bit : bitArray) {
-      if (representation.length() > 0) {
-        representation.append(ATTRIBUTE_SEPARATOR);
-      }
-      representation.append(bit.toString());
-    }
-    return representation.toString();
-  }
 
-  /**
-   * This BitVector is equal to a given Object,
-   * if the Object is a BitVector
-   * of same dimensionality and
-   * with identical bits set.
-   * 
-   * @see DatabaseObject#equals(Object)
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof BitVector) {
-      BitVector bv = (BitVector) obj;
-      return this.getDimensionality() == bv.getDimensionality()
-             && this.getBits().equals(bv.getBits());
+    /**
+     * @see FeatureVector#getValue(int)
+     */
+    public Bit getValue(int dimension)
+    {
+        if(dimension < 1 || dimension > dimensionality)
+        {
+            throw new IllegalArgumentException("illegal dimension: " + dimension);
+        }
+        return new Bit(bits.get(dimension));
+    }
 
+    /**
+     * @see FeatureVector#getColumnVector()
+     */
+    public Matrix getColumnVector()
+    {
+        double[] values = new double[dimensionality];
+        for(int i = 0; i < dimensionality; i++)
+        {
+            values[i] = bits.get(i) ? 1 : 0;
+        }
+        return new Matrix(values, values.length);
     }
-    else {
-      return false;
+
+    /**
+     * @see FeatureVector#getRowVector()
+     */
+    public Matrix getRowVector()
+    {
+        double[] values = new double[dimensionality];
+        for(int i = 0; i < dimensionality; i++)
+        {
+            values[i] = bits.get(i) ? 1 : 0;
+        }
+        return new Matrix(new double[][] { values.clone() });
     }
-  }
+
+    /**
+     * Returns a bit vector equal to this bit vector, if k is not 0, a bit
+     * vector with all components equal to zero otherwise.
+     * 
+     * @see FeatureVector#multiplicate(double)
+     */
+    public FeatureVector<Bit> multiplicate(double k)
+    {
+        if(k == 0)
+        {
+            return nullVector();
+        }
+        else
+        {
+            return new BitVector(bits, dimensionality);
+        }
+    }
+
+    /**
+     * Returns the inverse of the bit vector.
+     * 
+     * @see FeatureVector#negativeVector()
+     */
+    public FeatureVector<Bit> negativeVector()
+    {
+        BitSet newBits = (BitSet) bits.clone();
+        newBits.flip(0, dimensionality);
+        return new BitVector(newBits, dimensionality);
+    }
+
+    /**
+     * Returns a bit vector of equal dimensionality but containing 0 only.
+     * 
+     * @see FeatureVector#nullVector()
+     */
+    public FeatureVector<Bit> nullVector()
+    {
+        return new BitVector(new BitSet(), dimensionality);
+    }
+
+    /**
+     * Returns a bit vector corresponding to an XOR operation on this and the
+     * specified bit vector.
+     * 
+     * @see FeatureVector#plus(FeatureVector)
+     */
+    public FeatureVector<Bit> plus(FeatureVector<Bit> fv)
+    {
+        Bit[] fv_bits = new Bit[fv.getDimensionality()];
+        for(int i = 0; i < fv.getDimensionality(); i++)
+        {
+            fv_bits[i] = fv.getValue(i);
+        }
+
+        BitVector bv = new BitVector(fv_bits);
+        bv.bits.xor(this.bits);
+        return bv;
+    }
+
+    /**
+     * Returns whether the bit at specified index is set.
+     * 
+     * @param index
+     *            the index of the bit to inspect
+     * @return true if the bit at index
+     * @code{index} is set, false otherwise.
+     */
+    public boolean isSet(int index)
+    {
+        return bits.get(index);
+    }
+
+    /**
+     * Returns whether the bits at all of the specified indices are set.
+     * 
+     * @param indices
+     *            the indices to inspect
+     * @return true if the bits at all of the specified indices are set, false
+     *         otherwise
+     */
+    public boolean areSet(int[] indices)
+    {
+        boolean set = true;
+        for(int i = 0; i < indices.length && set; i++)
+        {
+            // noinspection ConstantConditions
+            set &= bits.get(i);
+        }
+        return set;
+    }
+
+    /**
+     * Returns the indices of all set bits.
+     * 
+     * @return the indices of all set bits
+     */
+    public int[] setBits()
+    {
+        int[] setBits = new int[bits.size()];
+        int index = 0;
+        for(int i = bits.nextSetBit(0); i >= 0; i = bits.nextSetBit(i + 1))
+        {
+            setBits[index++] = i;
+        }
+        return setBits;
+    }
+
+    /**
+     * Returns whether this BitVector contains all bits that are set to true in
+     * the specified BitSet.
+     * 
+     * @param bitset
+     *            the bits to inspect in this BitVector
+     * @return true if this BitVector contains all bits that are set to true in
+     *         the specified BitSet, false otherwise
+     */
+    public boolean contains(BitSet bitset)
+    {
+        boolean contains = true;
+        for(int i = bitset.nextSetBit(0); i >= 0 && contains; i = bitset.nextSetBit(i + 1))
+        {
+            // noinspection ConstantConditions
+            contains &= bits.get(i);
+        }
+        return contains;
+    }
+
+    /**
+     * Returns a copy of the bits currently set in this BitVector.
+     * 
+     * @return a copy of the bits currently set in this BitVector
+     */
+    public BitSet getBits()
+    {
+        return (BitSet) bits.clone();
+    }
+
+    /**
+     * Returns a String representation of this BitVector. The representation is
+     * suitable to be parsed by
+     * {@link de.lmu.ifi.dbs.parser.BitVectorLabelParser BitVectorLabelParser}.
+     * 
+     * @see Object#toString()
+     */
+    public String toString()
+    {
+        Bit[] bitArray = new Bit[dimensionality];
+        for(int i = 0; i < dimensionality; i++)
+        {
+            bitArray[i] = new Bit(bits.get(i));
+        }
+        StringBuffer representation = new StringBuffer();
+        for(Bit bit : bitArray)
+        {
+            if(representation.length() > 0)
+            {
+                representation.append(ATTRIBUTE_SEPARATOR);
+            }
+            representation.append(bit.toString());
+        }
+        return representation.toString();
+    }
+
+    /**
+     * This BitVector is equal to a given Object, if the Object is a BitVector
+     * of same dimensionality and with identical bits set.
+     * 
+     * @see DatabaseObject#equals(Object)
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj instanceof BitVector)
+        {
+            BitVector bv = (BitVector) obj;
+            return this.getDimensionality() == bv.getDimensionality() && this.getBits().equals(bv.getBits());
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 
 
 }
