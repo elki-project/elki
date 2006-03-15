0845eb248e41984fa1e236a3c85dc12094c1482b
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-03-15 14:05:24 +0000

    distance function: setDatabase with time flag

--- src/de/lmu/ifi/dbs/algorithm/clustering/DBSCAN.java
+++ src/de/lmu/ifi/dbs/algorithm/clustering/DBSCAN.java
@@ -99,7 +99,7 @@ public class DBSCAN<O extends DatabaseObject, D extends Distance<D>> extends Dis
       resultList = new ArrayList<List<Integer>>();
       noise = new HashSet<Integer>();
       processedIDs = new HashSet<Integer>(database.size());
-      getDistanceFunction().setDatabase(database, isVerbose());
+      getDistanceFunction().setDatabase(database, isVerbose(), isTime());
       if (isVerbose()) {
         System.out.println("\nClustering:");
       }
