a059657780152d58e05e8f635ca46ce64e77429b
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-05-04 08:56:39 +0000

    comments

--- src/de/lmu/ifi/dbs/pca/LinearLocalPCA.java
+++ src/de/lmu/ifi/dbs/pca/LinearLocalPCA.java
@@ -12,71 +12,63 @@ import java.util.List;
 /**
  * Performs a linear local PCA based on the covariance matrices of given
  * objects.
- * 
+ *
  * @author Elke Achtert (<a
  *         href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
  */
-public class LinearLocalPCA extends AbstractLocalPCA
-{
+public class LinearLocalPCA extends AbstractLocalPCA {
 
-    /**
-     * Provides a new PCA that cperforms a linear PCA based on the covarince
-     * matrices of given objects of a certain database. Since
-     * LinearCorrelationPCA is a non-abstract class, finally optionHandler is
-     * initialized.
-     */
-    public LinearLocalPCA()
-    {
-        super();
-        optionHandler = new OptionHandler(parameterToDescription, getClass()
-                .getName());
+  /**
+   * Provides a new PCA that performs a linear PCA based on the covarince
+   * matrices of given objects of a certain database. Since
+   * LinearCorrelationPCA is a non-abstract class, finally optionHandler is
+   * initialized.
+   */
+  public LinearLocalPCA() {
+    super();
+    optionHandler = new OptionHandler(parameterToDescription, getClass().getName());
+  }
+
+  /**
+   * Performs the actual eigenvalue decomposition on the specified object ids
+   * stored in the given database.
+   *
+   * @param database the database holding the objects
+   * @param ids      the list of the object ids for which the eigenvalue
+   *                 decomposition should be performed
+   * @return the actual eigenvalue decomposition on the specified object ids
+   *         stored in the given database
+   */
+  protected EigenvalueDecomposition eigenValueDecomposition(
+  Database<RealVector> database, List<Integer> ids) {
+    StringBuffer msg = new StringBuffer();
+
+    // covariance matrix
+    Matrix covariance = Util.covarianceMatrix(database, ids);
+
+    if (DEBUG) {
+      msg.append("\ncov ");
+      msg.append(covariance);
     }
 
-    /**
-     * Performs the actual eigenvalue decomposition on the specified object ids
-     * stored in the given database.
-     * 
-     * @param database
-     *            the database holding the objects
-     * @param ids
-     *            the list of the object ids for which the eigenvalue
-     *            decomposition should be performed
-     * @return the actual eigenvalue decomposition on the specified object ids
-     *         stored in the given database
-     */
-    protected EigenvalueDecomposition eigenValueDecomposition(
-            Database<RealVector> database, List<Integer> ids)
-    {
-        StringBuffer msg = new StringBuffer();
+    EigenvalueDecomposition evd = covariance.eig();
 
-        // covariance matrix
-        Matrix covariance = Util.covarianceMatrix(database, ids);
+    // correlation matrixArray
+    // double[][] cov = covariance.getArray();
+    // double[][] corr = new double[cov.length][];
+    // for (int i=0; i<cov.length; i++) {
+    // corr[i] = new double[cov[i].length];
+    // for (int j=0; j<cov[i].length; j++) {
+    // corr[i][j] = cov[i][j] / Math.sqrt(cov[i][i] * cov [j][j]);
+    // }
+    // }
+    // Matrix correlationMatrix = new Matrix(corr);
+    // EigenvalueDecomposition evd = correlationMatrix.eig();
 
-        if (DEBUG)
-        {
-            msg.append("\ncov ");
-            msg.append(covariance);
-        }
-
-        EigenvalueDecomposition evd = covariance.eig();
-
-        // correlation matrixArray
-        // double[][] cov = covariance.getArray();
-        // double[][] corr = new double[cov.length][];
-        // for (int i=0; i<cov.length; i++) {
-        // corr[i] = new double[cov[i].length];
-        // for (int j=0; j<cov[i].length; j++) {
-        // corr[i][j] = cov[i][j] / Math.sqrt(cov[i][i] * cov [j][j]);
-        // }
-        // }
-        // Matrix correlationMatrix = new Matrix(corr);
-        // EigenvalueDecomposition evd = correlationMatrix.eig();
-
-        if (DEBUG)
-        {
-            logger.info(msg.toString());
-        }
-
-        return evd;
+    if (DEBUG) {
+      logger.info(msg.toString());
     }
+
+    return evd;
+  }
 }
