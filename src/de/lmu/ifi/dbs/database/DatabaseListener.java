c8ad46c09f96625312ccbda2ddd1e662b2739f24
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-05-19 12:09:18 +0000

    concept of database listeners added

---
+++ src/de/lmu/ifi/dbs/database/DatabaseListener.java
@@ -0,0 +1,28 @@
+package de.lmu.ifi.dbs.database;
+
+import org.w3c.dom.events.EventListener;
+
+/**
+ * Defines the interface for an object that listens to changes in a Database.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public interface DatabaseListener extends EventListener {
+  /**
+   * Invoked after objects of the database have been updated in some way.
+   * Use <code>e.getObjects()</code> to get the updated database objects.
+   */
+  void objectsChanged(DatabaseEvent e);
+
+  /**
+   * Invoked after an object has been inserted into the database.
+   * Use <code>e.getObjects()</code> to get the newly inserted database objects.
+   */
+  void objectsInserted(DatabaseEvent e);
+
+  /**
+   * Invoked after an object has been deleted from the database.
+   * Use <code>e.getObjects()</code> to get the inserted database objects.
+   */
+  void objectsRemoved(DatabaseEvent e);
+}
