45757cc70c02ccf0fbd870db52c19e000ce8f79e
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-06-29 08:45:06 +0000

    marker interface for preprocessors computing preference vectors

---
+++ src/de/lmu/ifi/dbs/preprocessing/PreferenceVectorPreprocessor.java
@@ -0,0 +1,9 @@
+package de.lmu.ifi.dbs.preprocessing;
+
+/**
+ * Marker interface for preprocessors computing preference vectors.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public interface PreferenceVectorPreprocessor extends Preprocessor {
+}
