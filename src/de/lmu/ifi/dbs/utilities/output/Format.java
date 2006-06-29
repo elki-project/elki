e37c0c2a0ceba6c55b4d94fbf6d752487e5c1770
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-06-29 09:34:10 +0000

    class that provides formatting methods (todo!!!)

---
+++ src/de/lmu/ifi/dbs/utilities/output/Format.java
@@ -0,0 +1,9 @@
+package de.lmu.ifi.dbs.utilities.output;
+
+/**
+ * Provides several methods for formatting objects for print purposes.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public class Format {
+}
