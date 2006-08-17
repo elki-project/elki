4fbbe07618c99d376d32197aff4954245eb0cc6c
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-08-17 13:00:53 +0000

    wrapper class for ica algorithm

---
+++ src/de/lmu/ifi/dbs/wrapper/ICAWrapper.java
@@ -0,0 +1,131 @@
+package de.lmu.ifi.dbs.wrapper;
+
+import de.lmu.ifi.dbs.algorithm.ICA;
+import de.lmu.ifi.dbs.algorithm.KDDTask;
+import de.lmu.ifi.dbs.distance.EuklideanDistanceFunction;
+import de.lmu.ifi.dbs.utilities.optionhandling.AttributeSettings;
+import de.lmu.ifi.dbs.utilities.optionhandling.OptionHandler;
+import de.lmu.ifi.dbs.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.varianceanalysis.PercentageEigenPairFilter;
+import de.lmu.ifi.dbs.varianceanalysis.ica.FastICA;
+import de.lmu.ifi.dbs.varianceanalysis.ica.KurtosisBasedContrastFunction;
+
+import java.util.List;
+
+/**
+ * Wrapper class for ICA algorithm. Performs an attribute wise normalization on
+ * the database objects.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public class ICAWrapper extends FileBasedDatabaseConnectionWrapper {
+  /**
+   * Description for parameter epsilon.
+   */
+  public static final String EPSILON_D = "the maximum radius of the neighborhood to" +
+                                         "be considerd, must be suitable to " +
+                                         EuklideanDistanceFunction.class.getName();
+
+
+  /**
+   * Main method to run this wrapper.
+   *
+   * @param args the arguments to run this wrapper
+   */
+  public static void main(String[] args) {
+    ICAWrapper wrapper = new ICAWrapper();
+    try {
+      wrapper.setParameters(args);
+      wrapper.run();
+    }
+//    catch (ParameterException e) {
+//      Throwable cause = e.getCause() != null ? e.getCause() : e;
+//      wrapper.exception(wrapper.optionHandler.usage(e.getMessage()), cause);
+//    }
+//    catch (AbortException e) {
+//      wrapper.verbose(e.getMessage());
+//    }
+    catch (Exception e) {
+      e.printStackTrace();
+      wrapper.exception(wrapper.optionHandler.usage(e.getMessage()), e);
+    }
+  }
+
+  /**
+   * Sets the parameters epsilon and minpts in the parameter map additionally to the
+   * parameters provided by super-classes.
+   */
+  public ICAWrapper() {
+    super();
+  }
+
+  /**
+   * @see KDDTaskWrapper#getKDDTaskParameters()
+   */
+  public List<String> getKDDTaskParameters() {
+    List<String> parameters = super.getKDDTaskParameters();
+
+    // algorithm ICA
+    parameters.add(OptionHandler.OPTION_PREFIX + KDDTask.ALGORITHM_P);
+    parameters.add(ICA.class.getName());
+
+    // parser
+//    parameters.add(OptionHandler.OPTION_PREFIX + FileBasedDatabaseConnection.PARSER_P);
+//    parameters.add(RealVectorLabelTransposingParser.class.getName());
+
+    // approach
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.APPROACH_P);
+//    parameters.add(FastICA.Approach.SYMMETRIC.toString());
+    parameters.add(FastICA.Approach.DEFLATION.toString());
+
+    // epsilon
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.EPSILON_P);
+    parameters.add("0.01");
+
+    // contrast function
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.G_P);
+    parameters.add(KurtosisBasedContrastFunction.class.getName());
+//    parameters.add(TanhContrastFunction.class.getName());
+//    parameters.add(ExponentialContrastFunction.class.getName());
+
+    // number ics
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.IC_P);
+    parameters.add("2");
+
+    // initial matrix
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.UNIT_F);
+
+    // max iterations
+    parameters.add(OptionHandler.OPTION_PREFIX + FastICA.MAX_ITERATIONS_P);
+    parameters.add("1000");
+
+    // epsilon
+    parameters.add(OptionHandler.OPTION_PREFIX + PercentageEigenPairFilter.ALPHA_P);
+    parameters.add("0.95");
+
+    return parameters;
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(String[])
+   */
+  public String[] setParameters(String[] args) throws ParameterException {
+    String[] remainingParameters = super.setParameters(args);
+
+    // epsilon, minpts
+//    epsilon = optionHandler.getOptionValue(DBSCAN.EPSILON_P);
+//    minpts = optionHandler.getOptionValue(DBSCAN.MINPTS_P);
+
+    return remainingParameters;
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#getAttributeSettings()
+   */
+  public List<AttributeSettings> getAttributeSettings() {
+    List<AttributeSettings> settings = super.getAttributeSettings();
+//    AttributeSettings mySettings = settings.get(0);
+    return settings;
+  }
+
+}
