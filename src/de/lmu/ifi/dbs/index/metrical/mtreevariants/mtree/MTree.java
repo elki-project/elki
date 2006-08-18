dded5326c1c06f357ed648eb376976b2cf60ebef
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-08-18 10:49:15 +0000

    obsolete

--- src/de/lmu/ifi/dbs/index/metrical/mtreevariants/mtree/MTree.java
+++
@@ -1,9 +0,0 @@
-package de.lmu.ifi.dbs.index.metrical.mtreevariants.mtree;
-
-/**
- * TODO: comment
- *
- * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
- */
-public class MTree {
-}
