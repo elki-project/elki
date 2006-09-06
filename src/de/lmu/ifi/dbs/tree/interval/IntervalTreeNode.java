b31da61b7502839cfa49ec373ff925b62e899749
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-09-06 08:08:50 +0000

    obsolete

--- src/de/lmu/ifi/dbs/tree/interval/IntervalTreeNode.java
+++
@@ -1,9 +0,0 @@
-package de.lmu.ifi.dbs.tree.interval;
-
-/**
- * IntervalTreeNode denotes a node in an IntervalTree.
- *
- * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
- */
-public class IntervalTreeNode {
-}
