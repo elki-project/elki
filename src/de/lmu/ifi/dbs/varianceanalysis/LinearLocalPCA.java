23b4fa73347e635aa7a8c93d8f0044bcdfbd15de
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-12-08 14:51:21 +0000

    type of parameter ids in method pcaMatrix changed to Collection<Integer>

--- src/de/lmu/ifi/dbs/varianceanalysis/LinearLocalPCA.java
+++ src/de/lmu/ifi/dbs/varianceanalysis/LinearLocalPCA.java
@@ -5,7 +5,7 @@ import de.lmu.ifi.dbs.database.Database;
 import de.lmu.ifi.dbs.math.linearalgebra.Matrix;
 import de.lmu.ifi.dbs.utilities.Util;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Performs a linear local PCA based on the covariance matrices of given
@@ -18,9 +18,9 @@ public class LinearLocalPCA extends LocalPCA {
   /**
    * Returns the covariance matrix of the specified ids.
    *
-   * @see LocalPCA#pcaMatrix(de.lmu.ifi.dbs.database.Database, java.util.List)
+   * @see LocalPCA#pcaMatrix(de.lmu.ifi.dbs.database.Database, java.util.Collection)
    */
-  protected Matrix pcaMatrix(Database<RealVector> database, List<Integer> ids) {
+  protected Matrix pcaMatrix(Database<RealVector> database, Collection<Integer> ids) {
     // covariance matrix
     return Util.covarianceMatrix(database, ids);
   }
--- src/de/lmu/ifi/dbs/varianceanalysis/LocalKernelPCA.java
+++ src/de/lmu/ifi/dbs/varianceanalysis/LocalKernelPCA.java
@@ -1,12 +1,12 @@
 package de.lmu.ifi.dbs.varianceanalysis;
 
-import de.lmu.ifi.dbs.distance.similarityfunction.kernel.KernelMatrix;
 import de.lmu.ifi.dbs.data.RealVector;
 import de.lmu.ifi.dbs.database.AssociationID;
 import de.lmu.ifi.dbs.database.Database;
+import de.lmu.ifi.dbs.distance.similarityfunction.kernel.KernelMatrix;
 import de.lmu.ifi.dbs.math.linearalgebra.Matrix;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Performs a local kernel PCA based on the kernel matrices of given
@@ -16,18 +16,18 @@ import java.util.List;
  */
 public class LocalKernelPCA extends LocalPCA {
 
-	/**
-	 * Returns the local kernel matrix of the specified ids.
-	 *
-	 * @see LocalPCA#pcaMatrix(de.lmu.ifi.dbs.database.Database, java.util.List)
-	 */
-	@Override
-	protected Matrix pcaMatrix(final Database<RealVector> database, final List<Integer> ids) {
-		//get global kernel Matrix
-		final KernelMatrix<RealVector> kernelMatrix = (KernelMatrix<RealVector>) database.getGlobalAssociation(AssociationID.KERNEL_MATRIX);
-		//get local submatrix
-		final Matrix localKernelMatrix = kernelMatrix.getSubMatrix(ids);
-		// return centered local kernel matrix
-		return KernelMatrix.centerMatrix(localKernelMatrix);
-	}
+  /**
+   * Returns the local kernel matrix of the specified ids.
+   *
+   * @see LocalPCA#pcaMatrix(de.lmu.ifi.dbs.database.Database, java.util.Collection)
+   */
+  @Override
+  protected Matrix pcaMatrix(final Database<RealVector> database, final Collection<Integer> ids) {
+    //get global kernel Matrix
+    final KernelMatrix<RealVector> kernelMatrix = (KernelMatrix<RealVector>) database.getGlobalAssociation(AssociationID.KERNEL_MATRIX);
+    //get local submatrix
+    final Matrix localKernelMatrix = kernelMatrix.getSubMatrix(ids);
+    // return centered local kernel matrix
+    return KernelMatrix.centerMatrix(localKernelMatrix);
+  }
 }
