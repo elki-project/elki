062e8430f1241141f2c16a09604c8eddf729efd8
Elke Achtert <achtert@dbs.ifi.lmu.de>
2006-12-08 09:25:49 +0000

    superclass for HiSC and DiSH distance functions

--- src/de/lmu/ifi/dbs/distance/distancefunction/PreferenceVectorBasedCorrelationDistanceFunction.java
+++ src/de/lmu/ifi/dbs/distance/distancefunction/PreferenceVectorBasedCorrelationDistanceFunction.java
@@ -3,10 +3,6 @@ package de.lmu.ifi.dbs.distance.distancefunction;
 import de.lmu.ifi.dbs.data.RealVector;
 import de.lmu.ifi.dbs.database.AssociationID;
 import de.lmu.ifi.dbs.distance.PreferenceVectorBasedCorrelationDistance;
-import de.lmu.ifi.dbs.preprocessing.DiSHPreprocessor;
-import de.lmu.ifi.dbs.preprocessing.PreferenceVectorPreprocessor;
-import de.lmu.ifi.dbs.properties.Properties;
-import de.lmu.ifi.dbs.utilities.Util;
 import de.lmu.ifi.dbs.utilities.optionhandling.*;
 
 import java.util.ArrayList;
@@ -18,16 +14,7 @@ import java.util.List;
  *
  * @author Arthur Zimek (<a href="mailto:zimek@dbs.ifi.lmu.de">zimek@dbs.ifi.lmu.de</a>)
  */
-public class PreferenceVectorBasedCorrelationDistanceFunction extends AbstractCorrelationDistanceFunction<PreferenceVectorBasedCorrelationDistance> {
-
-  static {
-    ASSOCIATION_ID = AssociationID.PREFERENCE_VECTOR;
-    PREPROCESSOR_SUPER_CLASS = PreferenceVectorPreprocessor.class;
-    DEFAULT_PREPROCESSOR_CLASS = DiSHPreprocessor.class.getName();
-    PREPROCESSOR_CLASS_D = "<class>the preprocessor to determine the preference vectors of the objects "
-                           + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(PREPROCESSOR_SUPER_CLASS)
-                           + ". (Default: " + DEFAULT_PREPROCESSOR_CLASS;
-  }
+public abstract class PreferenceVectorBasedCorrelationDistanceFunction extends AbstractCorrelationDistanceFunction<PreferenceVectorBasedCorrelationDistance> {
 
   /**
    * The default value for epsilon.
@@ -143,38 +130,7 @@ public class PreferenceVectorBasedCorrelationDistanceFunction extends AbstractCo
    * @param pv2 the second preference vector
    * @return the correlation distance between the two specified vectors
    */
-  public PreferenceVectorBasedCorrelationDistance correlationDistance(RealVector v1, RealVector v2, BitSet pv1, BitSet pv2) {
-    BitSet commonPreferenceVector = (BitSet) pv1.clone();
-    commonPreferenceVector.and(pv2);
-    int dim = v1.getDimensionality();
-
-    // number of zero values in commonPreferenceVector
-    Integer subspaceDim = dim - commonPreferenceVector.cardinality();
-
-    // special case: v1 and v2 are in parallel subspaces
-    if (commonPreferenceVector.equals(pv1) || commonPreferenceVector.equals(pv2)) {
-      double d = weightedDistance(v1, v2, commonPreferenceVector);
-      if (d > 2 * epsilon) {
-        subspaceDim++;
-        if (this.debug) {
-          StringBuffer msg = new StringBuffer();
-          msg.append("\n");
-          msg.append("\nd " + d);
-          msg.append("\nv1 " + getDatabase().getAssociation(AssociationID.LABEL, v1.getID()));
-          msg.append("\nv2 " + getDatabase().getAssociation(AssociationID.LABEL, v2.getID()));
-          msg.append("\nsubspaceDim " + subspaceDim);
-          msg.append("\ncommon pv " + Util.format(dim, commonPreferenceVector));
-          verbose(msg.toString());
-        }
-      }
-    }
-
-    // flip commonPreferenceVector for distance computation in common subspace
-    BitSet inverseCommonPreferenceVector = (BitSet) commonPreferenceVector.clone();
-    inverseCommonPreferenceVector.flip(0, dim);
-
-    return new PreferenceVectorBasedCorrelationDistance(subspaceDim, weightedDistance(v1, v2, inverseCommonPreferenceVector), commonPreferenceVector);
-  }
+  public abstract PreferenceVectorBasedCorrelationDistance correlationDistance(RealVector v1, RealVector v2, BitSet pv1, BitSet pv2);
 
   /**
    * Computes the weighted distance between the two specified vectors
@@ -242,7 +198,6 @@ public class PreferenceVectorBasedCorrelationDistanceFunction extends AbstractCo
     return weightedPrefereneceVectorDistance(getDatabase().get(id1), getDatabase().get(id2));
   }
 
-
   /**
    * Sets the values for the parameters delta and preprocessor if specified.
    * If the parameters are not specified default values are set.
@@ -283,5 +238,13 @@ public class PreferenceVectorBasedCorrelationDistanceFunction extends AbstractCo
     mySetting.addSetting(EPSILON_P, Double.toString(epsilon));
 
     return settings;
+  }
+
+  /**
+   * Returns epsilon.
+   * @return epsilon
+   */
+  public double getEpsilon() {
+    return epsilon;
   }
 }
