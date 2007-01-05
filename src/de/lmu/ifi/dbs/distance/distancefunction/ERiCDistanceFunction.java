07bd704bfaae2472771ea53df1111e09aab684ff
Elke Achtert <achtert@dbs.ifi.lmu.de>
2007-01-05 14:54:17 +0000

    new distance function for ERiCAlgorithm

---
+++ src/de/lmu/ifi/dbs/distance/distancefunction/ERiCDistanceFunction.java
@@ -0,0 +1,13 @@
+package de.lmu.ifi.dbs.distance.distancefunction;
+
+import de.lmu.ifi.dbs.data.RealVector;
+import de.lmu.ifi.dbs.distance.Distance;
+
+/**
+ * TODO: comment
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public class ERiCDistanceFunction<O extends RealVector, D extends Distance> extends AbstractDistanceFunction<O,D> {
+  
+}
