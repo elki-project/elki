37262418fd4abc541416c4649e56f3f730470f84
Elke Achtert <achtert@dbs.ifi.lmu.de>
2007-03-23 14:04:12 +0000

    refactoring Index package

---
+++ src/de/lmu/ifi/dbs/index/tree/metrical/MetricalEntry.java
@@ -0,0 +1,11 @@
+package de.lmu.ifi.dbs.index.tree.metrical;
+
+import de.lmu.ifi.dbs.index.tree.Entry;
+
+/**
+ * Defines the requirements for an entry in a node of a Metrical Index.
+ *
+ * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
+ */
+public interface MetricalEntry extends Entry {
+}
