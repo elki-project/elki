115c83b3cdf15dcfdaf1bad547fc644c3a6fe551
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2007-11-08 17:43:51 +0000

    added overwrite tag

--- src/de/lmu/ifi/dbs/algorithm/result/clustering/EMClusters.java
+++ src/de/lmu/ifi/dbs/algorithm/result/clustering/EMClusters.java
@@ -15,6 +15,7 @@ public class EMClusters<V extends RealVector<V, ?>> extends Clusters<V>
         super(clusters, db);
     }
     
+    @Override
     public boolean isRequiredAssociation(AssociationID id)
     {
         return super.isRequiredAssociation(id) || id == AssociationID.PROBABILITY_CLUSTER_I_GIVEN_X;
