10b1d879ee3f26f39120edfe8594760209118f4d
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-08 11:28:05 +0000

    comments

--- src/de/lmu/ifi/dbs/utilities/optionhandling/DoubleParameter.java
+++ src/de/lmu/ifi/dbs/utilities/optionhandling/DoubleParameter.java
@@ -6,90 +6,96 @@ import java.util.List;
 
 /**
  * Parameter class for a parameter specifying a double value.
- * 
- * @author Steffi Wanka
  *
+ * @author Steffi Wanka
  */
 public class DoubleParameter extends NumberParameter<Double> {
 
-	/**
-	 * Constructs a double parameter with the given name and description
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 */
-	public DoubleParameter(String name, String description) {
-		super(name, description);
+  /**
+   * Constructs a double parameter with the given name and description
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   */
+  public DoubleParameter(String name, String description) {
+    super(name, description);
 
-	}
+  }
 
-	/**
-	 * Constructs a double parameter with the given name, description, and parameter constraint.
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 * @param cons the constraint for this double parameter
-	 */
-	public DoubleParameter(String name, String description, ParameterConstraint<Number> cons) {
-		this(name, description);
-		addConstraint(cons);
-	}
+  /**
+   * Constructs a double parameter with the given name, description, and parameter constraint.
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   * @param cons        the constraint for this double parameter
+   */
+  public DoubleParameter(String name, String description, ParameterConstraint<Number> cons) {
+    this(name, description);
+    addConstraint(cons);
+  }
 
-	/**
-	 * Constructs a double parameter with the given name, description, and list of parameter constraints.
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 * @param cons a list of parameter constraints for this double parameter
-	 */
-	public DoubleParameter(String name, String description, List<ParameterConstraint<Number>> cons) {
-		this(name, description);
-		addConstraintList(cons);
-	}
+  /**
+   * Constructs a double parameter with the given name, description, and list of parameter constraints.
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   * @param cons        a list of parameter constraints for this double parameter
+   */
+  public DoubleParameter(String name, String description, List<ParameterConstraint<Number>> cons) {
+    this(name, description);
+    addConstraintList(cons);
+  }
 
-	@Override
-	public void setValue(String value) throws ParameterException {
+  /**
+   * @see Option#setValue(String)
+   */
+  public void setValue(String value) throws ParameterException {
+    if (isValid(value)) {
+      this.value = Double.parseDouble(value);
+    }
+  }
 
-		if (isValid(value)) {
-			this.value = Double.parseDouble(value);
-		}
-	}
+  /**
+   * @see Option#isValid(String)
+   */
+  public boolean isValid(String value) throws ParameterException {
+    try {
+      Double.parseDouble(value);
+    }
 
-	/* (non-Javadoc)
-	 * @see de.lmu.ifi.dbs.utilities.optionhandling.Option#isValid(java.lang.String)
-	 */
-	public boolean isValid(String value) throws ParameterException {
-		try {
-			Double.parseDouble(value);
-		}
+    catch (NumberFormatException e) {
+      throw new WrongParameterValueException("Wrong parameter format! Parameter \""
+                                             + getName() + "\" requires a double value, read: " + value + "!\n");
+    }
 
-		catch (NumberFormatException e) {
-			throw new WrongParameterValueException("Wrong parameter format! Parameter \""
-					+ getName() + "\" requires a double value, read: " + value + "!\n");
-		}
+    try {
+      for (ParameterConstraint<Number> cons : this.constraints) {
 
-		try {
-			for (ParameterConstraint<Number> cons : this.constraints) {
+        cons.test(Double.parseDouble(value));
+      }
+    }
+    catch (ParameterException ex) {
+      throw new WrongParameterValueException("Specified parameter value for parameter \""
+                                             + getName() + "\" breaches parameter constraint!\n" + ex.getMessage());
+    }
 
-				cons.test(Double.parseDouble(value));
-			}
-		} catch (ParameterException ex) {
-			throw new WrongParameterValueException("Specified parameter value for parameter \""
-					+ getName() + "\" breaches parameter constraint!\n" + ex.getMessage());
-		}
+    return true;
+  }
 
-		return true;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-
-		if(obj == this){
-			return true;
-		}
-		if(! (obj instanceof DoubleParameter)){
-			return false;
-		}
-		return this.value.equals(((DoubleParameter)obj).value);
+  /**
+   * Indicates whether some other object is "equal to" this one.
+   *
+   * @param obj the reference object with which to compare.
+   * @return <code>true</code> if this double parameter has the same
+   *         value as the specified object, <code>false</code> otherwise.
+   */
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (!(obj instanceof DoubleParameter)) {
+      return false;
+    }
+    return this.value.equals(((DoubleParameter)obj).value);
 	}
 }
