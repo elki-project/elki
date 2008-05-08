0c6d6942d1dbc87441a636af5c3073f5addcdd59
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-08 11:15:16 +0000

    comments

--- src/de/lmu/ifi/dbs/utilities/optionhandling/StringParameter.java
+++ src/de/lmu/ifi/dbs/utilities/optionhandling/StringParameter.java
@@ -6,76 +6,70 @@ import java.util.List;
 
 /**
  * Parameter class for a parameter specifying a string value.
- * 
- * @author Steffi Wanka
  *
+ * @author Steffi Wanka
  */
-public class StringParameter extends Parameter<String,String> {
+public class StringParameter extends Parameter<String, String> {
 
-	/**
-	 * Constructs a string parameter with the given name and description
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 */
-	public StringParameter(String name, String description) {
-		super(name, description);
+  /**
+   * Constructs a string parameter with the given name and description
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   */
+  public StringParameter(String name, String description) {
+    super(name, description);
+  }
 
-	}
+  /**
+   * Constructs a string parameter with the given name, description, and a parameter constraint.
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   * @param con         a parameter constraint for this string parameter
+   */
+  public StringParameter(String name, String description, ParameterConstraint<String> con) {
+    this(name, description);
+    addConstraint(con);
+  }
 
-	/**
-	 * Constructs a string parameter with the given name, description, and a parameter constraint.
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 * @param con a parameter constraint for this string parameter
-	 */
-	public StringParameter(String name, String description, ParameterConstraint<String> con) {
-		this(name, description);
-		addConstraint(con);
-	}
-
-	/**
-	 * Constructs a string parameter with the given name, description, and a list of parameter constraints.
-	 * 
-	 * @param name the parameter name
-	 * @param description the parameter description
-	 * @param cons a list of parameter constraints for this string parameter
-	 */
-	public StringParameter(String name, String description, List<ParameterConstraint<String>> cons) {
-		this(name, description);
-		addConstraintList(cons);
-	}
+  /**
+   * Constructs a string parameter with the given name, description, and a list of parameter constraints.
+   *
+   * @param name        the parameter name
+   * @param description the parameter description
+   * @param cons        a list of parameter constraints for this string parameter
+   */
+  public StringParameter(String name, String description, List<ParameterConstraint<String>> cons) {
+    this(name, description);
+    addConstraintList(cons);
+  }
 
 
-	/* (non-Javadoc)
-	 * @see de.lmu.ifi.dbs.utilities.optionhandling.Option#setValue(java.lang.String)
-	 */
-	public void setValue(String value) throws ParameterException {
-		
-		if(isValid(value)){
-			this.value = value;
-		}
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Option#setValue(java.lang.String)
+   */
+  public void setValue(String value) throws ParameterException {
+    if (isValid(value)) {
+      this.value = value;
+    }
+  }
 
-	}
 
-	
-	/* (non-Javadoc)
-	 * @see de.lmu.ifi.dbs.utilities.optionhandling.Option#isValid(java.lang.String)
-	 */
-	public boolean isValid(String value) throws ParameterException {
-		
-		try{
-		for (ParameterConstraint<String> cons : this.constraints) {
-			cons.test(value);
-		}
-		}
-		catch(ParameterException ex){
-			throw new WrongParameterValueException("Specified parameter value for parameter \""
-					+ getName() + "\" breaches parameter constraint!\n" + ex.getMessage());
-		}
-		
-		return true;
-		
-	}
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Option#isValid(java.lang.String)
+   */
+  public boolean isValid(String value) throws ParameterException {
+    try {
+      for (ParameterConstraint<String> cons : this.constraints) {
+        cons.test(value);
+      }
+    }
+    catch (ParameterException ex) {
+      throw new WrongParameterValueException("Specified parameter value for parameter \""
+                                             + getName() + "\" breaches parameter constraint!\n" + ex.getMessage());
+    }
+
+    return true;
+  }
 }
