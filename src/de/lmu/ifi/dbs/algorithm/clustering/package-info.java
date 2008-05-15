fb9e83c4bcc05c3c402aff97da7111c679a73d31
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2008-05-15 15:31:34 +0000

    release vorbereitungen

---
+++ src/de/lmu/ifi/dbs/algorithm/clustering/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>Package contains various clustering algorithms.</p>
+ * 
+ * Clustering algorithms are supposed to implement the {@link de.lmu.ifi.dbs.algorithm.Algorithm}-Interface.
+ * @see de.lmu.ifi.dbs.algorithm
+ */
+package de.lmu.ifi.dbs.algorithm.clustering;
