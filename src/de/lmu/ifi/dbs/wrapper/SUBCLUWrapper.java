1bb5b2a217343a94022c8ac1dde9a34653cd3746
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-15 13:01:35 +0000

    new wrapper class for SUBCLU algorithm

---
+++ src/de/lmu/ifi/dbs/wrapper/SUBCLUWrapper.java
@@ -0,0 +1,99 @@
+package de.lmu.ifi.dbs.wrapper;
+
+import de.lmu.ifi.dbs.algorithm.AbortException;
+import de.lmu.ifi.dbs.algorithm.KDDTask;
+import de.lmu.ifi.dbs.algorithm.clustering.DBSCAN;
+import de.lmu.ifi.dbs.algorithm.clustering.SUBCLU;
+import de.lmu.ifi.dbs.utilities.optionhandling.*;
+import de.lmu.ifi.dbs.utilities.optionhandling.constraints.GreaterConstraint;
+
+import java.util.List;
+
+/**
+ * Wrapper class for SUBCLU algorithm. Performs an attribute wise normalization on
+ * the database objects.
+ *
+ * @author Elke Achtert
+ */
+public class SUBCLUWrapper extends NormalizationWrapper {
+
+  /**
+   * The value of the epsilon parameter.
+   */
+  private String epsilon;
+
+  /**
+   * The value of the minpts parameter.
+   */
+  private int minpts;
+
+  /**
+   * Main method to run this wrapper.
+   *
+   * @param args the arguments to run this wrapper
+   */
+  public static void main(String[] args) {
+    SUBCLUWrapper wrapper = new SUBCLUWrapper();
+    try {
+      wrapper.setParameters(args);
+      wrapper.run();
+    }
+    catch (ParameterException e) {
+      Throwable cause = e.getCause() != null ? e.getCause() : e;
+      wrapper.exception(wrapper.optionHandler.usage(e.getMessage()), cause);
+    }
+    catch (AbortException e) {
+      wrapper.verbose(e.getMessage());
+    }
+    catch (Exception e) {
+      wrapper.exception(wrapper.optionHandler.usage(e.getMessage()), e);
+    }
+  }
+
+  /**
+   * Sets the parameters epsilon and minpts in the parameter map additionally to the
+   * parameters provided by super-classes.
+   */
+  public SUBCLUWrapper() {
+    super();
+    //  parameter epsilon
+    optionHandler.put(SUBCLU.EPSILON_PARAM);
+
+    // parameter min points
+    optionHandler.put(new IntParameter(DBSCAN.MINPTS_P, DBSCAN.MINPTS_D, new GreaterConstraint(0)));
+  }
+
+  /**
+   * @see KDDTaskWrapper#getKDDTaskParameters()
+   */
+  public List<String> getKDDTaskParameters() throws UnusedParameterException {
+    List<String> parameters = super.getKDDTaskParameters();
+
+    // algorithm SUBCLU
+    parameters.add(OptionHandler.OPTION_PREFIX + KDDTask.ALGORITHM_P);
+    parameters.add(SUBCLU.class.getName());
+
+    // epsilon
+    parameters.add(OptionHandler.OPTION_PREFIX + SUBCLU.EPSILON_PARAM);
+    parameters.add(epsilon);
+
+    // minpts
+    parameters.add(OptionHandler.OPTION_PREFIX + DBSCAN.MINPTS_P);
+    parameters.add(Integer.toString(minpts));
+
+    return parameters;
+  }
+
+  /**
+   * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(String[])
+   */
+  public String[] setParameters(String[] args) throws ParameterException {
+    String[] remainingParameters = super.setParameters(args);
+
+    // epsilon, minpts
+    epsilon = optionHandler.getParameterValue(SUBCLU.EPSILON_PARAM);
+    minpts = (Integer) optionHandler.getOptionValue(DBSCAN.MINPTS_P);
+
+    return remainingParameters;
+  }
+}
