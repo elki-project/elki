d94b04d5f3e798e79ed239857f59bc6b7f80f6ac
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-15 13:36:16 +0000

    refactoring

--- src/de/lmu/ifi/dbs/algorithm/clustering/SUBCLU.java
+++ src/de/lmu/ifi/dbs/algorithm/clustering/SUBCLU.java
@@ -32,206 +32,196 @@ import java.util.List;
  * @author Elke Achtert (<a href="mailto:achtert@dbs.ifi.lmu.de">achtert@dbs.ifi.lmu.de</a>)
  */
 public class SUBCLU<V extends NumberVector<V, ?>, D extends Distance<D>> extends AbstractAlgorithm<V> implements Clustering<V> {
+    /**
+     * Parameter to specify the maximum radius of the neighborhood to be considered,
+     * must be suitable to AbstractDimensionsSelectingDoubleDistanceFunction.
+     * <p>Key: (@code -epsilon) </p>
+     */
+    public static final PatternParameter EPSILON_PARAM = new PatternParameter("epsilon",
+                                                                              "the maximum radius of the neighborhood " +
+                                                                              "to be considered, must be suitable to " +
+                                                                              AbstractDimensionsSelectingDoubleDistanceFunction.class.getName());
 
-  /**
-   * The default distance function.
-   */
-  public static final String DEFAULT_DISTANCE_FUNCTION = DimensionsSelectingEuklideanDistanceFunction.class.getName();
-
-  /**
-   * Parameter for distance function.
-   */
-  public static final String DISTANCE_FUNCTION_P = "distancefunction";
-
-  /**
-   * Description for parameter distance function.
-   */
-  public static final String DISTANCE_FUNCTION_D = "the distance function to determine the distance between database objects "
-                                                   + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(AbstractDimensionsSelectingDoubleDistanceFunction.class)
-                                                   + ". Default: "
-                                                   + DEFAULT_DISTANCE_FUNCTION;
-
-  /**
-   * Parameter to specify the maximum radius of the neighborhood to be considered,
-   * must be suitable to AbstractDimensionsSelectingDoubleDistanceFunction.
-   * <p>Key: (@code -epsilon) </p>
-   */
-  public static final PatternParameter EPSILON_PARAM = new PatternParameter("epsilon",
-                                                                            "the maximum radius of the neighborhood " +
-                                                                            "to be considered, must be suitable to " +
-                                                                            AbstractDimensionsSelectingDoubleDistanceFunction.class.getName());
+    /**
+     * Parameter to specify the threshold for minimum number of points in the
+     * epsilon-neighborhood of a point, must be greater than 0.
+     * <p>Key: (@code -minpts) </p>
+     */
+    public static final IntParameter MINPTS_PARAM = new IntParameter("minpts",
+                                                                     "threshold for minimum number of points in the " +
+                                                                     "epsilon-neighborhood of a point",
+                                                                     new GreaterConstraint(0));
 
 
-  /**
-   * The distance function.
-   */
-  private AbstractDimensionsSelectingDoubleDistanceFunction<V> distanceFunction;
+    /**
+     * The default distance function.
+     */
+    public static final Class DEFAULT_DISTANCE_FUNCTION = DimensionsSelectingEuklideanDistanceFunction.class;
 
-
-  /**
-   * The value of parameter epsilon.
-   */
-  private String epsilon;
-
-  /**
-   * Minimum points.
-   */
-  private int minpts;
-
-  /**
-   * Sets epsilon and minimum points to the optionhandler additionally to the
-   * parameters provided by super-classes.
-   */
-  public SUBCLU() {
-    super();
-    this.debug = true;
-
-    // distance function
+    /**
+     * The distance function to determine the distance between database objects.
+     * <p>Default value: DimensionsSelectingEuklideanDistanceFunction </p>
+     * <p>Key: (@code -distancefunction) </p>
+     */
     // noinspection unchecked
-    ClassParameter<AbstractDimensionsSelectingDoubleDistanceFunction<V>> distance = new ClassParameter(DISTANCE_FUNCTION_P, DISTANCE_FUNCTION_D, AbstractDimensionsSelectingDoubleDistanceFunction.class);
-    distance.setDefaultValue(DEFAULT_DISTANCE_FUNCTION);
-    optionHandler.put(distance);
+    public static ClassParameter<AbstractDimensionsSelectingDoubleDistanceFunction> DISTANCEFUNCTION_PARAM = new ClassParameter("distancefunction",
+                                                                                                                                "the distance function to determine the distance between database objects "
+                                                                                                                                + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(AbstractDimensionsSelectingDoubleDistanceFunction.class)
+                                                                                                                                + ". Default: " + DEFAULT_DISTANCE_FUNCTION.getName(),
+                                                                                                                                DEFAULT_DISTANCE_FUNCTION);
 
-    // epsilon
-    optionHandler.put(EPSILON_PARAM);
-    // global constraint
-    try {
-      // noinspection unchecked
-      GlobalParameterConstraint gpc = new GlobalDistanceFunctionPatternConstraint(EPSILON_PARAM, (ClassParameter<? extends AbstractDimensionsSelectingDoubleDistanceFunction<?>>) optionHandler.getOption(DISTANCE_FUNCTION_P));
-      optionHandler.setGlobalParameterConstraint(gpc);
-    }
-    catch (UnusedParameterException e) {
-      verbose("Could not instantiate global parameter constraint concerning parameter " +
-              EPSILON_PARAM.getName() + " and " + DISTANCE_FUNCTION_P +
-              " because parameter " + DISTANCE_FUNCTION_P + " is not specified! " + e.getMessage());
+    static {
+        DISTANCEFUNCTION_PARAM.setDefaultValue(DEFAULT_DISTANCE_FUNCTION.getName());
     }
 
-    // minpts
-    optionHandler.put(new IntParameter(DBSCAN.MINPTS_P, DBSCAN.MINPTS_D, new GreaterConstraint(0)));
-  }
+    /**
+     * The distance function.
+     */
+    private AbstractDimensionsSelectingDoubleDistanceFunction<V> distanceFunction;
+
+    /**
+     * Sets epsilon and minimum points to the optionhandler additionally to the
+     * parameters provided by super-classes.
+     */
+    public SUBCLU() {
+        super();
+        this.debug = true;
+
+        // parameter epsilon
+        optionHandler.put(EPSILON_PARAM);
+
+        // parameter minpts
+        optionHandler.put(MINPTS_PARAM);
+
+        // distance function
+        optionHandler.put(DISTANCEFUNCTION_PARAM);
+
+        // global constraint epsilon <-> distance function
+        // noinspection unchecked
+        GlobalParameterConstraint gpc = new GlobalDistanceFunctionPatternConstraint(EPSILON_PARAM, DISTANCEFUNCTION_PARAM);
+        optionHandler.setGlobalParameterConstraint(gpc);
+    }
 
 
-  /**
-   * Performs the SUBCLU algorithm on the given database.
-   *
-   * @param database the database to run the algorithm on
-   * @throws IllegalStateException if the algorithm has not been initialized
-   *                               properly (e.g. the setParameters(String[]) method has been failed
-   *                               to be called).
-   */
-  protected void runInTime(Database<V> database) throws IllegalStateException {
-    try {
-      int dimensionality = database.dimensionality();
-      // 1. Generate all 1-D clusters
-      if (isVerbose()) {
-        verbose("*** Step 1: Generate all 1-D clusters ***");
-      }
-      for (int d = 0; d < dimensionality; d++) {
-        BitSet selectedDimensions = new BitSet();
-        selectedDimensions.set(d);
-        DBSCAN<V, D> dbscan = initDBSCAN(selectedDimensions);
-        dbscan.run(database);
-        ClustersPlusNoise clusters = dbscan.getResult();
-        if (debug) {
-          debugFine(d + " clusters: " + clusters);
+    /**
+     * Performs the SUBCLU algorithm on the given database.
+     *
+     * @param database the database to run the algorithm on
+     * @throws IllegalStateException if the algorithm has not been initialized
+     *                               properly (e.g. the setParameters(String[]) method has been failed
+     *                               to be called).
+     */
+    protected void runInTime(Database<V> database) throws IllegalStateException {
+        try {
+            int dimensionality = database.dimensionality();
+            // 1. Generate all 1-D clusters
+            if (isVerbose()) {
+                verbose("*** Step 1: Generate all 1-D clusters ***");
+            }
+            for (int d = 0; d < dimensionality; d++) {
+                BitSet selectedDimensions = new BitSet();
+                selectedDimensions.set(d);
+                DBSCAN<V, D> dbscan = initDBSCAN(selectedDimensions);
+                dbscan.run(database);
+                ClustersPlusNoise<V> clusters = dbscan.getResult();
+                if (debug) {
+                    debugFine(d + " clusters: " + clusters);
+                }
+            }
         }
-      }
-    }
-    catch (ParameterException e) {
-      throw new IllegalStateException(e);
-    }
-  }
-
-  /**
-   * Returns the result of the algorithm.
-   *
-   * @return the result of the algorithm
-   */
-  public ClusteringResult<V> getResult() {
-    // todo
-    return null;
-  }
-
-  /**
-   * Returns a description of the algorithm.
-   *
-   * @return a description of the algorithm
-   */
-  public Description getDescription() {
-    return new Description("SUBCLU",
-                           "Density connected Subspace Clustering",
-                           "Algorithm to detect arbitrarily shaped and positioned clusters " +
-                           "in subspaces. SUBCLU delivers for each subspace the same clusters " +
-                           "DBSCAN would have found, when applied to this subspace seperately.. ",
-                           "K. Kailing, H.-P. Kriegel, P. Kroeger: " +
-                           "Density connected Subspace Clustering for High Dimensional Data. " +
-                           "In Proc. SIAM Int. Conf. on Data Mining (SDM'04), Lake Buena Vista, FL, 2004.");
-  }
-
-  /**
-   * Sets the parameters epsilon and minpts additionally to the parameters set
-   * by the super-class' method. Both epsilon and minpts are required
-   * parameters.
-   *
-   * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(String[])
-   */
-  @Override
-  public String[] setParameters(String[] args) throws ParameterException {
-    String[] remainingParameters = super.setParameters(args);
-
-    // distance function
-    String className = (String) optionHandler.getOptionValue(DISTANCE_FUNCTION_P);
-    try {
-      // noinspection unchecked
-      distanceFunction = Util.instantiate(AbstractDimensionsSelectingDoubleDistanceFunction.class, className);
-    }
-    catch (UnableToComplyException e) {
-      throw new WrongParameterValueException(DISTANCE_FUNCTION_P, className, DISTANCE_FUNCTION_D, e);
-    }
-    remainingParameters = distanceFunction.setParameters(remainingParameters);
-    setParameters(args, remainingParameters);
-
-    // epsilon
-    epsilon = optionHandler.getParameterValue(EPSILON_PARAM);
-    // minpts
-    minpts = (Integer) optionHandler.getOptionValue(DBSCAN.MINPTS_P);
-
-    return remainingParameters;
-  }
-
-  /**
-   * Initializes the DBSCAN algorithm
-   *
-   * @return an instance of the DBSCAN algorithm
-   * @throws ParameterException in case of wrong parameter-setting
-   */
-  private DBSCAN<V, D> initDBSCAN(BitSet dimensions) throws ParameterException {
-    DBSCAN<V, D> dbscan = new DBSCAN<V, D>();
-    List<String> parameters = new ArrayList<String>();
-
-    // distance function
-    parameters.add(OptionHandler.OPTION_PREFIX + DISTANCE_FUNCTION_P);
-    parameters.add(distanceFunction.getClass().toString());
-
-    // selected dimensions for distance function
-    parameters.add(OptionHandler.OPTION_PREFIX + AbstractDimensionsSelectingDoubleDistanceFunction.DIMS_P);
-    parameters.add(Util.parseSelectedBits(dimensions, ","));
-
-    // aditional distance function paramaters
-    String[] distanceFunctionParams = distanceFunction.getParameters();
-    for (String param : distanceFunctionParams) {
-      parameters.add(param);
+        catch (ParameterException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
-    // epsilon
-    parameters.add(OptionHandler.OPTION_PREFIX + EPSILON_PARAM.getName());
-    parameters.add(epsilon);
+    /**
+     * Returns the result of the algorithm.
+     *
+     * @return the result of the algorithm
+     */
+    public ClusteringResult<V> getResult() {
+        // todo
+        return null;
+    }
 
-    // minpts
-    parameters.add(OptionHandler.OPTION_PREFIX + DBSCAN.MINPTS_P);
-    parameters.add(Integer.toString(minpts));
+    /**
+     * Returns a description of the algorithm.
+     *
+     * @return a description of the algorithm
+     */
+    public Description getDescription() {
+        return new Description("SUBCLU",
+                               "Density connected Subspace Clustering",
+                               "Algorithm to detect arbitrarily shaped and positioned clusters " +
+                               "in subspaces. SUBCLU delivers for each subspace the same clusters " +
+                               "DBSCAN would have found, when applied to this subspace seperately.. ",
+                               "K. Kailing, H.-P. Kriegel, P. Kroeger: " +
+                               "Density connected Subspace Clustering for High Dimensional Data. " +
+                               "In Proc. SIAM Int. Conf. on Data Mining (SDM'04), Lake Buena Vista, FL, 2004.");
+    }
 
-    dbscan.setParameters(parameters.toArray(new String[parameters.size()]));
-    return dbscan;
-  }
+    /**
+     * Sets the parameters epsilon and minpts additionally to the parameters set
+     * by the super-class' method. Both epsilon and minpts are required
+     * parameters.
+     *
+     * @see de.lmu.ifi.dbs.utilities.optionhandling.Parameterizable#setParameters(String[])
+     */
+    @Override
+    public String[] setParameters(String[] args) throws ParameterException {
+        String[] remainingParameters = super.setParameters(args);
+
+        // distance function
+        try {
+            // noinspection unchecked
+            distanceFunction = Util.instantiate(AbstractDimensionsSelectingDoubleDistanceFunction.class, DISTANCEFUNCTION_PARAM.getValue());
+        }
+        catch (UnableToComplyException e) {
+            throw new WrongParameterValueException(DISTANCEFUNCTION_PARAM.getName(),
+                                                   DISTANCEFUNCTION_PARAM.getValue(),
+                                                   DISTANCEFUNCTION_PARAM.getDescription(),
+                                                   e);
+        }
+        remainingParameters = distanceFunction.setParameters(remainingParameters);
+        setParameters(args, remainingParameters);
+
+        return remainingParameters;
+    }
+
+    /**
+     * Initializes the DBSCAN algorithm
+     *
+     * @param selectedDimensions the dimensions to be considered for distance computation
+     * @return an instance of the DBSCAN algorithm
+     * @throws ParameterException in case of wrong parameter-setting
+     */
+    private DBSCAN<V, D> initDBSCAN(BitSet selectedDimensions) throws ParameterException {
+        DBSCAN<V, D> dbscan = new DBSCAN<V, D>();
+        List<String> parameters = new ArrayList<String>();
+
+        // distance function
+        parameters.add(OptionHandler.OPTION_PREFIX + DISTANCEFUNCTION_PARAM.getName());
+        parameters.add(distanceFunction.getClass().toString());
+
+        // selected dimensions for distance function
+        parameters.add(OptionHandler.OPTION_PREFIX + AbstractDimensionsSelectingDoubleDistanceFunction.DIMS_P);
+        parameters.add(Util.parseSelectedBits(selectedDimensions, ","));
+
+        // aditional distance function paramaters
+        String[] distanceFunctionParams = distanceFunction.getParameters();
+        for (String param : distanceFunctionParams) {
+            parameters.add(param);
+        }
+
+        // epsilon
+        parameters.add(OptionHandler.OPTION_PREFIX + EPSILON_PARAM.getName());
+        parameters.add(EPSILON_PARAM.getValue());
+
+        // minpts
+        parameters.add(OptionHandler.OPTION_PREFIX + MINPTS_PARAM.getName());
+        parameters.add(MINPTS_PARAM.getValue().toString());
+
+        dbscan.setParameters(parameters.toArray(new String[parameters.size()]));
+        return dbscan;
+    }
 }
