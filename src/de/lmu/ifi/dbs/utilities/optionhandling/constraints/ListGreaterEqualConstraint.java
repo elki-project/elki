895962a7f297eb1c12e35d5c22c0833d4c97267d
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-15 13:01:58 +0000

    renamed

--- src/de/lmu/ifi/dbs/utilities/optionhandling/constraints/ListGreaterEqualConstraint.java
+++ src/de/lmu/ifi/dbs/utilities/optionhandling/constraints/ListGreaterEqualConstraint.java
@@ -6,37 +6,37 @@ import de.lmu.ifi.dbs.utilities.optionhandling.WrongParameterValueException;
 import java.util.List;
 
 /**
- * Represents a Less-Than-Number parameter constraint for a list of number values.
+ * Represents a Greater-Equal-Than-Number parameter constraint for a list of number values.
  * All values of the list parameter ({@link de.lmu.ifi.dbs.utilities.optionhandling.ListParameter})
- * tested have to be less than the specified constraint value.
+ * tested have to be greater than or equal to the specified constraint value.
  *
  * @author Elke Achtert
  */
-public class ListLessConstraint extends AbstractNumberConstraint<List<Number>> {
+public class ListGreaterEqualConstraint<N extends Number> extends AbstractNumberConstraint<List<N>> {
   /**
-   * Creates a Less-Than-Number parameter constraint.
+   * Creates a Greater-Equal-Than-Number parameter constraint.
    * <p/>
    * That is, all values of the list parameter
-   * tested have to be less than the specified constraint value.
+   * tested have to be greater than or equal to the specified constraint value.
    *
    * @param constraintValue parameter constraint value
    */
-  public ListLessConstraint(Number constraintValue) {
+  public ListGreaterEqualConstraint(N constraintValue) {
     super(constraintValue);
   }
 
   /**
    * Checks if all number values of the specified list parameter
-   * are less than the constraint value.
+   * are greater than or equal to the constraint value.
    * If not, a parameter exception is thrown.
    *
    * @see ParameterConstraint#test(Object)
    */
-  public void test(List<Number> t) throws ParameterException {
+  public void test(List<N> t) throws ParameterException {
     for (Number n : t) {
-      if (n.doubleValue() >= constraintValue.doubleValue()) {
+      if (n.doubleValue() < constraintValue.doubleValue()) {
         throw new WrongParameterValueException("Parameter Constraint Error: \n"
-                                               + "The parameter values specified have to be less than " + constraintValue.toString()
+                                               + "The parameter values specified have to be greater than or equal to " + constraintValue.toString()
                                                + ". (current value: " + t + ")\n");
       }
     }
