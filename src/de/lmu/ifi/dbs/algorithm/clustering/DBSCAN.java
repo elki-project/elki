1513a2a12b8de271333c385d57e5805da73de8e7
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-05-17 06:55:47 +0000

    AbstractDimensionsSelectingDistanceFunction added

--- src/de/lmu/ifi/dbs/algorithm/clustering/DBSCAN.java
+++ src/de/lmu/ifi/dbs/algorithm/clustering/DBSCAN.java
@@ -25,9 +25,6 @@ import java.util.*;
  * @param <D> the type of Distance used
  */
 public class DBSCAN<O extends DatabaseObject, D extends Distance<D>> extends DistanceBasedAlgorithm<O, D> implements Clustering<O> {
-
-                                                                        "to be considered, must be suitable to " +
-                                                                            "the distance function specified");
     /**
      * Parameter to specify the maximum radius of the neighborhood to be considered,
      * must be suitable to the distance function specified.
