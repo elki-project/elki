d6800cce1da7c4e7b2a631f1dfb3b0eaf0093951
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2008-07-08 14:47:53 +0000

    comments

--- src/de/lmu/ifi/dbs/elki/database/Associations.java
+++ src/de/lmu/ifi/dbs/elki/database/Associations.java
@@ -8,37 +8,73 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * todo arthur comment all
+ * A helper class to facilitate setting of associations in a database.
  *
  * @author Arthur Zimek
  */
 public class Associations extends AbstractLoggable {
+  /**
+   * Holds the objects associated under given association ids.
+   */
     @SuppressWarnings("unchecked")
     private Map<AssociationID, Object> associations;
 
+    /**
+     * Provides an Associations object ready to set and get
+     * Objects for association.
+     *
+     */
     @SuppressWarnings("unchecked")
     public Associations() {
         super(LoggingConfiguration.DEBUG);
         associations = new Hashtable<AssociationID, Object>();
     }
 
+    /**
+     * Sets the specified object under the given AssociationID.
+     * 
+     * 
+     * @param <T> the type of the object to be associated and the AssociationID
+     * @param associationID the AssociationID to point to the object
+     * @param associationObject the object to be associated
+     */
     public <T> void put(AssociationID<T> associationID, T associationObject) {
         this.associations.put(associationID, associationObject);
     }
 
+    /**
+     * Retrieves the object associated under the given association id.
+     * 
+     * 
+     * @param <T> the type of the object to retrieve conforms to the type of the association id
+     * @param associationID the association id pointing to the object to retrieve
+     * @return the object associated under the given association id
+     */
     @SuppressWarnings("unchecked")
     public <T> T get(AssociationID<T> associationID) {
         return (T) this.associations.get(associationID);
     }
 
+    /**
+     * Provides the set of all association ids pointing to objects within this Associations.
+     * 
+     * 
+     * @return the set of all association ids pointing to objects within this Associations
+     */
     @SuppressWarnings("unchecked")
     public Set<AssociationID> keySet() {
         return this.associations.keySet();
     }
 
+    /**
+     * Adds all pairs of association ids and objects currently
+     * set in the given Associations object to this Associations object. 
+     * 
+     * @param associations an Associations object to copy the contained associations from
+     */
     @SuppressWarnings("unchecked")
-    public <T> void putAll(Associations associations) {
-        for (AssociationID<T> associationID : associations.keySet()) {
+    public void putAll(Associations associations) {
+        for (AssociationID associationID : associations.keySet()) {
             put(associationID, associations.get(associationID));
         }
     }
