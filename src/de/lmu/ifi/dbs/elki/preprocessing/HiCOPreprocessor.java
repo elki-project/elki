b3d29d52950f69e785c7603b72237ccb8cde4d66
Erich Schubert <schube@dbs.ifi.lmu.de>
2008-08-13 14:21:40 +0000

    Add support for distance-weighted PCA in HiCO.

--- src/de/lmu/ifi/dbs/elki/preprocessing/HiCOPreprocessor.java
+++ src/de/lmu/ifi/dbs/elki/preprocessing/HiCOPreprocessor.java
@@ -8,12 +8,12 @@ import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
 import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction;
 import de.lmu.ifi.dbs.elki.properties.Properties;
 import de.lmu.ifi.dbs.elki.utilities.Progress;
+import de.lmu.ifi.dbs.elki.utilities.QueryResult;
 import de.lmu.ifi.dbs.elki.utilities.UnableToComplyException;
 import de.lmu.ifi.dbs.elki.utilities.Util;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.*;
 import de.lmu.ifi.dbs.elki.varianceanalysis.LinearLocalPCA;
 import de.lmu.ifi.dbs.elki.varianceanalysis.LocalPCA;
-import de.lmu.ifi.dbs.elki.varianceanalysis.PercentageEigenPairFilter;
 
 import java.util.Iterator;
 import java.util.List;
@@ -21,214 +21,225 @@ import java.util.List;
 /**
  * Abstract superclass for preprocessors for HiCO correlation dimension
  * assignment to objects of a certain database.
- *
+ * 
  * @author Elke Achtert
  */
 public abstract class HiCOPreprocessor<V extends RealVector<V, ?>> extends AbstractParameterizable implements Preprocessor<V> {
-    /**
-     * The default PCA class name.
-     */
-    public static final String DEFAULT_PCA_CLASS = LinearLocalPCA.class.getName();
+  /**
+   * The default PCA class name.
+   */
+  public static final String DEFAULT_PCA_CLASS = LinearLocalPCA.class.getName();
 
-    /**
-     * Parameter for PCA.
-     */
-    public static final String PCA_CLASS_P = "pca";
+  /**
+   * Parameter for PCA.
+   */
+  public static final String PCA_CLASS_P = "pca";
 
-    /**
-     * Description for parameter pca.
-     */
-    public static final String PCA_CLASS_D = "the pca to determine the strong eigenvectors "
-        + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(LocalPCA.class) + ". Default: " + DEFAULT_PCA_CLASS;
+  /**
+   * Description for parameter pca.
+   */
+  public static final String PCA_CLASS_D = "the pca to determine the strong eigenvectors "
+    + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(LocalPCA.class) + ". Default: " + DEFAULT_PCA_CLASS;
 
-    /**
-     * The default distance function for the PCA.
-     */
-    public static final String DEFAULT_PCA_DISTANCE_FUNCTION = EuclideanDistanceFunction.class.getName();
+  /**
+   * The default distance function for the PCA.
+   */
+  public static final String DEFAULT_PCA_DISTANCE_FUNCTION = EuclideanDistanceFunction.class.getName();
 
-    /**
-     * Parameter for pca distance function.
-     */
-    public static final String PCA_DISTANCE_FUNCTION_P = "pcaDistancefunction";
+  /**
+   * Parameter for pca distance function.
+   */
+  public static final String PCA_DISTANCE_FUNCTION_P = "pcaDistancefunction";
 
-    /**
-     * Description for parameter pca distance function.
-     */
-    public static final String PCA_DISTANCE_FUNCTION_D = "the distance function for the PCA to determine the distance between database objects "
-        + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(DistanceFunction.class) + ". Default: " + DEFAULT_PCA_DISTANCE_FUNCTION;
+  /**
+   * Description for parameter pca distance function.
+   */
+  public static final String PCA_DISTANCE_FUNCTION_D = "the distance function for the PCA to determine the distance between database objects " + Properties.KDD_FRAMEWORK_PROPERTIES.restrictionString(DistanceFunction.class) + ". Default: " + DEFAULT_PCA_DISTANCE_FUNCTION;
 
-    /**
-     * The classname of the PCA to determine the strong eigenvectors.
-     */
-    protected String pcaClassName;
+  /**
+   * The classname of the PCA to determine the strong eigenvectors.
+   */
+  protected String pcaClassName;
 
-    /**
-     * The parameter settings for the PCA.
-     */
-    private String[] pcaParameters;
+  /**
+   * The parameter settings for the PCA.
+   */
+  private String[] pcaParameters;
 
-    /**
-     * The distance function for the PCA.
-     */
-    protected DistanceFunction<V, DoubleDistance> pcaDistanceFunction;
+  /**
+   * The distance function for the PCA.
+   */
+  protected DistanceFunction<V, DoubleDistance> pcaDistanceFunction;
 
-    /**
-     * Provides a new Preprocessor that computes the correlation dimension of
-     * objects of a certain database.
-     */
-    public HiCOPreprocessor() {
-        super();
-        // parameter pca-class
-        ClassParameter<LocalPCA<V>> pcaClass = new ClassParameter(PCA_CLASS_P, PCA_CLASS_D, LocalPCA.class);
-        pcaClass.setDefaultValue(DEFAULT_PCA_CLASS);
-        optionHandler.put(pcaClass);
+  /**
+   * Provides a new Preprocessor that computes the correlation dimension of
+   * objects of a certain database.
+   */
+  public HiCOPreprocessor() {
+    super();
+    // parameter pca-class
+    ClassParameter<LocalPCA<V>> pcaClass = new ClassParameter(PCA_CLASS_P, PCA_CLASS_D, LocalPCA.class);
+    pcaClass.setDefaultValue(DEFAULT_PCA_CLASS);
+    optionHandler.put(pcaClass);
 
-        // parameter pca distance function
-        ClassParameter<DistanceFunction<V, DoubleDistance>> pcaDist = new ClassParameter(PCA_DISTANCE_FUNCTION_P, PCA_DISTANCE_FUNCTION_D, DistanceFunction.class);
-        pcaDist.setDefaultValue(DEFAULT_PCA_DISTANCE_FUNCTION);
-        optionHandler.put(pcaDist);
+    // parameter pca distance function
+    ClassParameter<DistanceFunction<V, DoubleDistance>> pcaDist = new ClassParameter(PCA_DISTANCE_FUNCTION_P, PCA_DISTANCE_FUNCTION_D, DistanceFunction.class);
+    pcaDist.setDefaultValue(DEFAULT_PCA_DISTANCE_FUNCTION);
+    optionHandler.put(pcaDist);
+  }
+
+  /**
+   * This method determines the correlation dimensions of the objects stored in
+   * the specified database and sets the necessary associations in the database.
+   * 
+   * @param database the database for which the preprocessing is performed
+   * @param verbose flag to allow verbose messages while performing the
+   *        algorithm
+   * @param time flag to request output of performance time
+   */
+  public void run(Database<V> database, boolean verbose, boolean time) {
+    if(database == null) {
+      throw new IllegalArgumentException("Database must not be null!");
     }
 
-    /**
-     * This method determines the correlation dimensions of the objects stored
-     * in the specified database and sets the necessary associations in the
-     * database.
-     *
-     * @param database the database for which the preprocessing is performed
-     * @param verbose  flag to allow verbose messages while performing the algorithm
-     * @param time     flag to request output of performance time
-     */
-    public void run(Database<V> database, boolean verbose, boolean time) {
-        if (database == null) {
-            throw new IllegalArgumentException("Database must not be null!");
+    try {
+      long start = System.currentTimeMillis();
+      Progress progress = new Progress("Preprocessing correlation dimension", database.size());
+      if(verbose) {
+        verbose("Preprocessing:");
+      }
+
+      int processed = 1;
+      for(Iterator<Integer> it = database.iterator(); it.hasNext();) {
+        Integer id = it.next();
+        List<QueryResult<DoubleDistance>> objs = resultsForPCA(id, database, verbose, false);
+
+        // noinspection unchecked
+        // todo???
+        // FIXME: Can we use a global pca instance, instead of reinstantiating
+        // all the time?
+        LocalPCA<V> pca = Util.instantiate(LocalPCA.class, pcaClassName);
+        pca.setParameters(pcaParameters);
+        pca.runResults(objs, database);
+
+        database.associate(AssociationID.LOCAL_PCA, id, pca);
+        database.associate(AssociationID.LOCALLY_WEIGHTED_MATRIX, id, pca.similarityMatrix());
+        progress.setProcessed(processed++);
+
+        if(verbose) {
+          progress(progress);
         }
+      }
 
-        try {
-            long start = System.currentTimeMillis();
-            Progress progress = new Progress("Preprocessing correlation dimension", database.size());
-            if (verbose) {
-                verbose("Preprocessing:");
-            }
+      long end = System.currentTimeMillis();
+      if(time) {
+        long elapsedTime = end - start;
+        verbose(this.getClass().getName() + " runtime: " + elapsedTime + " milliseconds.");
+      }
+    }
+    catch(ParameterException e) {
+      // tested before
+      throw new RuntimeException("This should never happen!", e);
+    }
+    catch(UnableToComplyException e) {
+      // tested before
+      throw new RuntimeException("This should never happen!", e);
+    }
+  }
 
-            int processed = 1;
-            for (Iterator<Integer> it = database.iterator(); it.hasNext();) {
-                Integer id = it.next();
-                List<Integer> ids = objectIDsForPCA(id, database, verbose, false);
+  /**
+   * Sets the values for the parameters alpha, pca and pcaDistancefunction if
+   * specified. If the parameters are not specified default values are set.
+   * 
+   * @see de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable#setParameters(String[])
+   */
+  @Override
+  public String[] setParameters(String[] args) throws ParameterException {
+    String[] remainingParameters = super.setParameters(args);
 
-                //noinspection unchecked
-                // todo???
-                LocalPCA<V> pca = Util.instantiate(LocalPCA.class, pcaClassName);
-                pca.setParameters(pcaParameters);
-                pca.run(ids, database);
+    // pca distance function
+    String pcaDistanceFunctionClassName = (String) optionHandler.getOptionValue(PCA_DISTANCE_FUNCTION_P);
+    try {
+      // noinspection unchecked
+      // todo
+      pcaDistanceFunction = Util.instantiate(DistanceFunction.class, pcaDistanceFunctionClassName);
+    }
+    catch(UnableToComplyException e) {
+      throw new WrongParameterValueException(PCA_DISTANCE_FUNCTION_P, pcaDistanceFunctionClassName, PCA_DISTANCE_FUNCTION_D);
+    }
+    remainingParameters = pcaDistanceFunction.setParameters(remainingParameters);
 
-                database.associate(AssociationID.LOCAL_PCA, id, pca);
-                database.associate(AssociationID.LOCALLY_WEIGHTED_MATRIX, id, pca.similarityMatrix());
-                progress.setProcessed(processed++);
-
-                if (verbose) {
-                    progress(progress);
-                }
-            }
-
-            long end = System.currentTimeMillis();
-            if (time) {
-                long elapsedTime = end - start;
-                verbose(this.getClass().getName() + " runtime: " + elapsedTime + " milliseconds.");
-            }
-        }
-        catch (ParameterException e) {
-            // tested before
-            throw new RuntimeException("This should never happen!", e);
-        }
-        catch (UnableToComplyException e) {
-            // tested before
-            throw new RuntimeException("This should never happen!", e);
-        }
+    // pca
+    LocalPCA<V> tmpPCA;
+    pcaClassName = (String) optionHandler.getOptionValue(PCA_CLASS_P);
+    try {
+      // noinspection unchecked
+      // todo
+      tmpPCA = Util.instantiate(LocalPCA.class, pcaClassName);
+    }
+    catch(UnableToComplyException e) {
+      throw new WrongParameterValueException(PCA_CLASS_P, pcaClassName, PCA_CLASS_D);
     }
 
-    /**
-     * Sets the values for the parameters alpha, pca and pcaDistancefunction if
-     * specified. If the parameters are not specified default values are set.
-     *
-     * @see de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable#setParameters(String[])
-     */
-    @Override
-    public String[] setParameters(String[] args) throws ParameterException {
-        String[] remainingParameters = super.setParameters(args);
+    remainingParameters = tmpPCA.setParameters(remainingParameters);
+    pcaParameters = tmpPCA.getParameters();
 
-        // pca distance function
-        String pcaDistanceFunctionClassName = (String) optionHandler.getOptionValue(PCA_DISTANCE_FUNCTION_P);
-        try {
-            // noinspection unchecked
-            // todo
-            pcaDistanceFunction = Util.instantiate(DistanceFunction.class, pcaDistanceFunctionClassName);
-        }
-        catch (UnableToComplyException e) {
-            throw new WrongParameterValueException(PCA_DISTANCE_FUNCTION_P, pcaDistanceFunctionClassName, PCA_DISTANCE_FUNCTION_D);
-        }
-        remainingParameters = pcaDistanceFunction.setParameters(remainingParameters);
+    setParameters(args, remainingParameters);
 
-        // pca
-        LocalPCA<V> tmpPCA;
-        pcaClassName = (String) optionHandler.getOptionValue(PCA_CLASS_P);
-        try {
-//    	 noinspection unchecked
-            // todo
-            tmpPCA = Util.instantiate(LocalPCA.class, pcaClassName);
-        }
-        catch (UnableToComplyException e) {
-            throw new WrongParameterValueException(PCA_CLASS_P, pcaClassName, PCA_CLASS_D);
-        }
+    return remainingParameters;
+  }
 
-        // save parameters for pca
-        String[] tmpPCAParameters = new String[remainingParameters.length];
-        System.arraycopy(remainingParameters, 0, tmpPCAParameters, 0, remainingParameters.length);
-        // eigen pair filter
-        tmpPCAParameters = Util.addParameter(tmpPCAParameters, OptionID.PCA_EIGENPAIR_FILTER, PercentageEigenPairFilter.class.getName());
-        remainingParameters = tmpPCA.setParameters(tmpPCAParameters);
-        pcaParameters = tmpPCA.getParameters();
-        setParameters(args, remainingParameters);
+  /**
+   * Returns the parameter setting of the attributes.
+   * 
+   * @return the parameter setting of the attributes
+   */
+  @Override
+  public List<AttributeSettings> getAttributeSettings() {
+    List<AttributeSettings> attributeSettings = super.getAttributeSettings();
 
-        return remainingParameters;
+    try {
+      // noinspection unchecked
+      // todo???
+      LocalPCA<V> pca = Util.instantiate(LocalPCA.class, pcaClassName);
+      pca.setParameters(pcaParameters);
+      attributeSettings.addAll(pca.getAttributeSettings());
+    }
+    catch(UnableToComplyException e) {
+      // tested before!!! TODO more meaningful message
+      throw new RuntimeException("This should never happen!");
+    }
+    catch(ParameterException e) {
+      // tested before!!! TODO more meaningful message
+      throw new RuntimeException("This should never happen!");
     }
 
-    /**
-     * Returns the parameter setting of the attributes.
-     *
-     * @return the parameter setting of the attributes
-     */
-    @Override
-    public List<AttributeSettings> getAttributeSettings() {
-        List<AttributeSettings> attributeSettings = super.getAttributeSettings();
+    return attributeSettings;
+  }
 
-        try {
-//    	 noinspection unchecked
-            // todo???
-            LocalPCA<V> pca = Util.instantiate(LocalPCA.class, pcaClassName);
-            pca.setParameters(pcaParameters);
-            attributeSettings.addAll(pca.getAttributeSettings());
-        }
-        catch (UnableToComplyException e) {
-            // tested before!!! TODO more meaningful message
-            throw new RuntimeException("This should never happen!");
-        }
-        catch (ParameterException e) {
-            // tested before!!! TODO more meaningful message
-            throw new RuntimeException("This should never happen!");
-        }
+  /**
+   * Returns the ids of the objects stored in the specified database to be
+   * considerd within the PCA for the specified object id.
+   * 
+   * @param id the id of the object for which a PCA should be performed
+   * @param database the database holding the objects
+   * @param verbose flag to allow verbose messages while performing the
+   *        algorithm
+   * @param time flag to request output of performance time
+   * @return the list of the object ids to be considerd within the PCA
+   */
+  protected abstract List<Integer> objectIDsForPCA(Integer id, Database<V> database, boolean verbose, boolean time);
 
-        return attributeSettings;
-    }
-
-    /**
-     * Returns the ids of the objects stored in the specified database to be
-     * considerd within the PCA for the specified object id.
-     *
-     * @param id       the id of the object for which a PCA should be performed
-     * @param database the database holding the objects
-     * @param verbose  flag to allow verbose messages while performing the algorithm
-     * @param time     flag to request output of performance time
-     * @return the list of the object ids to be considerd within the PCA
-     */
-    protected abstract List<Integer> objectIDsForPCA(Integer id, Database<V> database, boolean verbose, boolean time);
+  /**
+   * Returns the ids of the objects and distances stored in the specified
+   * database to be considerd within the PCA for the specified object id.
+   * 
+   * @param id the id of the object for which a PCA should be performed
+   * @param database the database holding the objects
+   * @param verbose flag to allow verbose messages while performing the
+   *        algorithm
+   * @param time flag to request output of performance time
+   * @return the list of the object ids to be considerd within the PCA
+   */
+  protected abstract List<QueryResult<DoubleDistance>> resultsForPCA(Integer id, Database<V> database, boolean verbose, boolean time);
 }
