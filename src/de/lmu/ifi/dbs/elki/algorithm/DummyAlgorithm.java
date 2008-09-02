16de6eefe6aa907df845d48baa54062058c4c63a
Erich Schubert <schube@dbs.ifi.lmu.de>
2008-09-02 09:05:45 +0000

    Also run a 10NN query in the dummy algorithm, to show how to use it.

--- src/de/lmu/ifi/dbs/elki/algorithm/DummyAlgorithm.java
+++ src/de/lmu/ifi/dbs/elki/algorithm/DummyAlgorithm.java
@@ -3,17 +3,20 @@ package de.lmu.ifi.dbs.elki.algorithm;
 import java.util.Iterator;
 
 import de.lmu.ifi.dbs.elki.algorithm.result.Result;
-import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction;
 import de.lmu.ifi.dbs.elki.utilities.Description;
 
 /**
- * Dummy Algorithm, which just iterates over all points once. Useful in testing
- * e.g. index structures.
+ * Dummy Algorithm, which just iterates over all points once, doing a 10NN query each.
+ * Useful in testing e.g. index structures.
  * 
  * @author Erich Schubert
  */
-public class DummyAlgorithm<O extends DatabaseObject> extends AbstractAlgorithm<O> {
+public class DummyAlgorithm<V extends NumberVector<V,?>> extends AbstractAlgorithm<V> {
 
   /**
    * Empty constructor. Nothing to do.
@@ -25,18 +28,22 @@ public class DummyAlgorithm<O extends DatabaseObject> extends AbstractAlgorithm<
   /**
    * Iterates over all points in the database.
    */
-  protected void runInTime(Database<O> database) throws IllegalStateException {
+  protected void runInTime(Database<V> database) throws IllegalStateException {
+    DistanceFunction<V,DoubleDistance> distFunc = new EuclideanDistanceFunction<V>();
     for(Iterator<Integer> iter = database.iterator(); iter.hasNext(); ) {
       Integer id = iter.next();
       database.get(id);
+      // run a 10NN query for each point.
+      database.kNNQueryForID(id, 10, distFunc);
     }
   }
 
   public Description getDescription() {
-    return new Description("Dummy","Dummy Algorithm","Iterates once over all points in the database. Useful for unit tests.","");
+    return new Description("Dummy","Dummy Algorithm",
+        "Iterates once over all points in the database. Useful for unit tests.","");
   }
 
-  public Result<O> getResult() {
+  public Result<V> getResult() {
     return null;
   }
 }
