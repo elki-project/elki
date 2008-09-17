93296da54821b2983cf74e4e3fa3c73308cfdefb
Elke Achtert <achtert@dbs.ifi.lmu.de>
2008-09-17 12:57:24 +0000

    renaming variable

--- src/de/lmu/ifi/dbs/elki/parser/RealVectorLabelParser.java
+++ src/de/lmu/ifi/dbs/elki/parser/RealVectorLabelParser.java
@@ -36,148 +36,144 @@ import java.util.List;
  * @param <V> the type of RealVector expected in the {@link ParsingResult}
  */
 public class RealVectorLabelParser<V extends RealVector<V, ?>> extends AbstractParser<V> {
-  /**
-   * OptionID for {@link #FLOAT_PARAM}
-   */
-  private static final OptionID FLOAT_ID = OptionID.getOrCreateOptionID("parser.float",
-      "Flag to specify parsing the real values as floats (default is double).");
+    /**
+     * OptionID for {@link #FLOAT_FLAG}
+     */
+    private static final OptionID FLOAT_ID = OptionID.getOrCreateOptionID("parser.float",
+        "Flag to specify parsing the real values as floats (default is double).");
 
-  private static final Flag FLOAT_FLAG = new Flag(FLOAT_ID);
-  
-  /**
-   * If true, the real values are parsed as floats.
-   */
-  protected boolean parseFloat;
+    private static final Flag FLOAT_FLAG = new Flag(FLOAT_ID);
 
-  /**
-   * The parameter name for an index of a numerical class label.
-   */
-  public static final String CLASS_LABEL_INDEX_P = "numericalClassLabelIndex";
+    /**
+     * If true, the real values are parsed as floats.
+     */
+    protected boolean parseFloat;
 
-  /**
-   * OptionID for {@link #CLASS_LABEL_INDEX_PARAM}
-   */
-  private static final OptionID CLASS_LABEL_INDEX_ID = OptionID.getOrCreateOptionID("parser.classLabelIndex",
-      "Index of a class label (may be numeric), " +
-      "counting whitespace separated entries in a line starting with 0 - " +
-      "the corresponding entry will be treated as a label. " /* +
+    /**
+     * The parameter name for an index of a numerical class label.
+     */
+    public static final String CLASS_LABEL_INDEX_P = "numericalClassLabelIndex";
+
+    /**
+     * OptionID for {@link #CLASS_LABEL_INDEX_PARAM}
+     */
+    private static final OptionID CLASS_LABEL_INDEX_ID = OptionID.getOrCreateOptionID("parser.classLabelIndex",
+        "Index of a class label (may be numeric), " +
+            "counting whitespace separated entries in a line starting with 0 - " +
+            "the corresponding entry will be treated as a label. " /* +
       "To actually set this label as class label, use also the parametrization of " +
       AbstractDatabaseConnection.class.getCanonicalName() + " -" +
       AbstractDatabaseConnection.CLASS_LABEL_INDEX_ID.getName() + " -" +
       AbstractDatabaseConnection.CLASS_LABEL_CLASS_ID.getName()*/);
 
-  /**
-   * The parameter for an index of a numerical class label.
-   * The corresponding numerical value is treated as string label
-   * an can be selected as class label by the {@link AbstractDatabaseConnection}.
-   * A non-numerical class label can be directly selected from the labels after parsing via the
-   * corresponding parameter of the {@link AbstractDatabaseConnection}:
-   * {@link AbstractDatabaseConnection#CLASS_LABEL_INDEX_PARAM}.
-   * <p/>
-   * The parameter is optional and the default value is set to -1.
-   */
-  public static final IntParameter CLASS_LABEL_INDEX_PARAM = new IntParameter(CLASS_LABEL_INDEX_ID, null, -1);
+    /**
+     * The parameter for an index of a numerical class label.
+     * The corresponding numerical value is treated as string label
+     * an can be selected as class label by the {@link AbstractDatabaseConnection}.
+     * A non-numerical class label can be directly selected from the labels after parsing via the
+     * corresponding parameter of the {@link AbstractDatabaseConnection}:
+     * {@link AbstractDatabaseConnection#CLASS_LABEL_INDEX_PARAM}.
+     * <p/>
+     * The parameter is optional and the default value is set to -1.
+     */
+    public static final IntParameter CLASS_LABEL_INDEX_PARAM = new IntParameter(CLASS_LABEL_INDEX_ID, null, -1);
 
-  /**
-   * Keeps the index of an attribute to be treated as a string label.
-   */
-  private int classLabelIndex;
+    /**
+     * Keeps the index of an attribute to be treated as a string label.
+     */
+    private int classLabelIndex;
 
-  /**
-   * Provides a parser for parsing one point per line, attributes separated by
-   * whitespace. <p/> Several labels may be given per point. A label must not
-   * be parseable as double (or float). Lines starting with &quot;#&quot; will
-   * be ignored.
-   */
-  public RealVectorLabelParser() {
-    super();
-    debug = true;
-    addOption(FLOAT_FLAG);
-    addOption(CLASS_LABEL_INDEX_PARAM);
-  }
+    /**
+     * Provides a parser for parsing one point per line, attributes separated by
+     * whitespace. <p/> Several labels may be given per point. A label must not
+     * be parseable as double (or float). Lines starting with &quot;#&quot; will
+     * be ignored.
+     */
+    public RealVectorLabelParser() {
+        super();
+        debug = true;
+        addOption(FLOAT_FLAG);
+        addOption(CLASS_LABEL_INDEX_PARAM);
+    }
 
-  @SuppressWarnings("unchecked")
-  public ParsingResult<V> parse(InputStream in) {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-    int lineNumber = 1;
-    int dimensionality = -1;
-    List<ObjectAndLabels<V>> objectAndLabelsList = new ArrayList<ObjectAndLabels<V>>();
-    try {
-      for (String line; (line = reader.readLine()) != null; lineNumber++) {
-        if (!line.startsWith(COMMENT) && line.length() > 0) {
-          String[] entries = WHITESPACE_PATTERN.split(line);
-          List<Double> attributes = new ArrayList<Double>();
-          List<String> labels = new ArrayList<String>();
-          for (int i = 0; i < entries.length; i++) {
-            if (i != classLabelIndex) {
-              try {
-                Double attribute = Double.valueOf(entries[i]);
-                attributes.add(attribute);
-              }
-              catch (NumberFormatException e) {
-                labels.add(entries[i]);
-              }
+    @SuppressWarnings("unchecked")
+    public ParsingResult<V> parse(InputStream in) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        int lineNumber = 1;
+        int dimensionality = -1;
+        List<ObjectAndLabels<V>> objectAndLabelsList = new ArrayList<ObjectAndLabels<V>>();
+        try {
+            for (String line; (line = reader.readLine()) != null; lineNumber++) {
+                if (!line.startsWith(COMMENT) && line.length() > 0) {
+                    String[] entries = WHITESPACE_PATTERN.split(line);
+                    List<Double> attributes = new ArrayList<Double>();
+                    List<String> labels = new ArrayList<String>();
+                    for (int i = 0; i < entries.length; i++) {
+                        if (i != classLabelIndex) {
+                            try {
+                                Double attribute = Double.valueOf(entries[i]);
+                                attributes.add(attribute);
+                            }
+                            catch (NumberFormatException e) {
+                                labels.add(entries[i]);
+                            }
+                        }
+                        else {
+                            labels.add(entries[i]);
+                        }
+                    }
+
+                    if (dimensionality < 0) {
+                        dimensionality = attributes.size();
+                    }
+                    else if (dimensionality != attributes.size()) {
+                        throw new IllegalArgumentException("Differing dimensionality in line " + lineNumber + ":" + attributes.size() + " != " + dimensionality);
+                    }
+
+                    ObjectAndLabels<V> objectAndLabels;
+                    if (parseFloat) {
+                        objectAndLabels = new ObjectAndLabels<V>((V) new FloatVector(Util.convertToFloat(attributes)), labels);
+                    }
+                    else {
+                        objectAndLabels = new ObjectAndLabels<V>((V) new DoubleVector(attributes), labels);
+                    }
+                    objectAndLabelsList.add(objectAndLabels);
+                }
             }
-            else {
-              labels.add(entries[i]);
-            }
-          }
-
-          if (dimensionality < 0) {
-            dimensionality = attributes.size();
-          }
-          else if (dimensionality != attributes.size()) {
-            throw new IllegalArgumentException("Differing dimensionality in line " + lineNumber + ":" + attributes.size() + " != " + dimensionality);
-          }
-
-          //V featureVector;
-          ObjectAndLabels<V> objectAndLabel;
-          if (parseFloat) {
-            //featureVector = (V) new FloatVector(Util.convertToFloat(attributes));
-            objectAndLabel = new ObjectAndLabels<V>((V) new FloatVector(Util.convertToFloat(attributes)), labels);
-          }
-          else {
-            //featureVector = (V) new DoubleVector(attributes);
-            objectAndLabel = new ObjectAndLabels<V>((V) new DoubleVector(attributes), labels);
-          }
-          //ObjectAndLabels<V> objectAndLabel = new ObjectAndLabels<V>(featureVector, labels);
-          objectAndLabelsList.add(objectAndLabel);
         }
-      }
-    }
-    catch (IOException e) {
-      throw new IllegalArgumentException("Error while parsing line " + lineNumber + ".");
-    }
+        catch (IOException e) {
+            throw new IllegalArgumentException("Error while parsing line " + lineNumber + ".");
+        }
 
-    return new ParsingResult<V>(objectAndLabelsList);
-  }
-
-  @Override
-  public String parameterDescription() {
-    StringBuffer description = new StringBuffer();
-    description.append(RealVectorLabelParser.class.getName());
-    description.append(" expects following format of parsed lines:\n");
-    description.append("A single line provides a single point. Attributes are separated by whitespace (");
-    description.append(WHITESPACE_PATTERN.pattern() + "). ");
-    description.append("If parameter " + FLOAT_FLAG.getName() + " is set, the real values will be parsed as floats (resulting in a set of FloatVectors), " + "otherwise the real values will be parsed as as doubles (resulting in a set of DoubleVectors -- default).");
-    description.append("Any substring not containing whitespace is tried to be read as double (or float). " + "If this fails, it will be appended to a label. (Thus, any label must not be parseable " + "as double nor as float.) Empty lines and lines beginning with \"");
-    description.append(COMMENT);
-    description.append("\" will be ignored. If any point differs in its dimensionality from other points, " + "the parse method will fail with an Exception.\n");
-
-    return usage(description.toString());
-  }
-
-  @Override
-  public String[] setParameters(String[] args) throws ParameterException {
-    String[] remainingParams = super.setParameters(args);
-    parseFloat = FLOAT_FLAG.isSet();
-    if (CLASS_LABEL_INDEX_PARAM.isSet()) {
-      classLabelIndex = CLASS_LABEL_INDEX_PARAM.getValue();
-    }
-    else {
-      classLabelIndex = CLASS_LABEL_INDEX_PARAM.getDefaultValue();
+        return new ParsingResult<V>(objectAndLabelsList);
     }
 
-    return remainingParams;
-  }
+    @Override
+    public String parameterDescription() {
+        StringBuffer description = new StringBuffer();
+        description.append(RealVectorLabelParser.class.getName());
+        description.append(" expects following format of parsed lines:\n");
+        description.append("A single line provides a single point. Attributes are separated by whitespace (");
+        description.append(WHITESPACE_PATTERN.pattern()).append("). ");
+        description.append("If parameter ").append(FLOAT_FLAG.getName()).append(" is set, the real values will be parsed as floats (resulting in a set of FloatVectors), " + "otherwise the real values will be parsed as as doubles (resulting in a set of DoubleVectors -- default).");
+        description.append("Any substring not containing whitespace is tried to be read as double (or float). " + "If this fails, it will be appended to a label. (Thus, any label must not be parseable " + "as double nor as float.) Empty lines and lines beginning with \"");
+        description.append(COMMENT);
+        description.append("\" will be ignored. If any point differs in its dimensionality from other points, " + "the parse method will fail with an Exception.\n");
+
+        return usage(description.toString());
+    }
+
+    @Override
+    public String[] setParameters(String[] args) throws ParameterException {
+        String[] remainingParams = super.setParameters(args);
+        parseFloat = FLOAT_FLAG.isSet();
+        if (CLASS_LABEL_INDEX_PARAM.isSet()) {
+            classLabelIndex = CLASS_LABEL_INDEX_PARAM.getValue();
+        }
+        else {
+            classLabelIndex = CLASS_LABEL_INDEX_PARAM.getDefaultValue();
+        }
+
+        return remainingParams;
+    }
 }
