e97401292eeac1fa9c2dbbd01d5ad889695f65f2
Erich Schubert <schube@dbs.ifi.lmu.de>
2008-09-18 12:08:42 +0000

    Some more refactoring of Abod. Extract Mean and Variance computation to elki.math.MeanVariance

---
+++ src/de/lmu/ifi/dbs/elki/math/MeanVariance.java
@@ -0,0 +1,97 @@
+package de.lmu.ifi.dbs.elki.math;
+
+/**
+ * Do some simple statistics (mean, average).
+ * 
+ * This class can repeatedly be fed with data using the add() methods,
+ * The resulting values for mean and average can be queried at any time using
+ * getMean() and getVariance().
+ * 
+ * Trivial code, but replicated a lot. The class is final so it should come at low cost.
+ * 
+ * @author Erich Schubert
+ *
+ */
+public final class MeanVariance {
+  public double sum = 0.0;
+  public double sqrSum = 0.0;
+  public double count = 0.0;
+  
+  /**
+   * Empty constructor
+   */
+  public MeanVariance() { }
+
+  /**
+   * Constructor from full internal data.
+   * 
+   * @param sum sum
+   * @param sqrSum sum of squared values
+   * @param count sum of weights
+   */
+  public MeanVariance(double sum, double sqrSum, double count) {
+    this.sum = sum;
+    this.sqrSum = sqrSum;
+    this.count = count;
+  }
+
+  /**
+   * Constructor from other instance
+   * 
+   * @param other other instance to copy data from.
+   */
+  public MeanVariance(MeanVariance other) {
+    this.sum = other.sum;
+    this.sqrSum = other.sqrSum;
+    this.count = other.count;   
+  }
+
+  /**
+   * Add data with a given weight
+   * 
+   * @param val data
+   * @param weight weight
+   */
+  public void addData(double val, double weight) {
+    sum    += weight * val;
+    sqrSum += weight * val * val;
+    count  += weight;
+  }
+  
+  /**
+   * Add a single value with weight 1.0
+   * 
+   * @param val Value
+   */
+  public void addData(double val) {
+    addData(val, 1.0);
+  }
+
+  /**
+   * Join the data of another MeanVariance instance.
+   * 
+   * @param other
+   */
+  public void addData(MeanVariance other) {
+    this.sum += other.sum;
+    this.sqrSum += other.sqrSum;
+    this.count += other.count;
+  }
+
+  /**
+   * Return mean
+   * @return mean
+   */
+  public double getMean() {
+    return sum / count;
+  }
+
+  /**
+   * Return variance
+   * @return variance
+   */
+  public double getVariance() {
+    double mu = sum / count;
+    return (sqrSum / count) - (mu * mu);
+  }
+}
