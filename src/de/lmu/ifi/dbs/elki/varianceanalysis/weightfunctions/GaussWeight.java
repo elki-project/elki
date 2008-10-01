873855ca888752f9a512f908282d29e1f308d6a8
Erich Schubert <schube@dbs.ifi.lmu.de>
2008-10-01 15:39:55 +0000

    fix comment

--- src/de/lmu/ifi/dbs/elki/varianceanalysis/weightfunctions/GaussWeight.java
+++ src/de/lmu/ifi/dbs/elki/varianceanalysis/weightfunctions/GaussWeight.java
@@ -14,7 +14,7 @@ public final class GaussWeight implements WeightFunction {
   public double getWeight(double distance, double max, double stddev) {
     if (max <= 0) return 1.0;
     double relativedistance = distance / max;
-    // -2.303 is log(-.1) to suit the intended range of 1.0-0.1
+    // -2.303 is log(.1) to suit the intended range of 1.0-0.1
     return java.lang.Math.exp(-2.3025850929940455 * relativedistance * relativedistance);
   }
 }
