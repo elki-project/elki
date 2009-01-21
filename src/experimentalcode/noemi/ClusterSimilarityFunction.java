73e185c3433fa9e9d6f52d42d0c700adec07d4f5
Noemi Andor <andor@cip.ifi.lmu.de>
2009-01-21 17:00:06 +0000

    ClusterDistanceFunction, ClusterSimilarityFunction, FIRES-Update

---
+++ src/experimentalcode/noemi/ClusterSimilarityFunction.java
@@ -0,0 +1,30 @@
+package experimentalcode.noemi;
+
+import java.util.Arrays;
+import java.util.Collection;
+import de.lmu.ifi.dbs.elki.data.cluster.Cluster;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.distance.IntegerDistance;
+import de.lmu.ifi.dbs.elki.distance.similarityfunction.AbstractIntegerSimilarityFunction;
+
+public class ClusterSimilarityFunction<M extends Model, C extends Cluster<M>> extends AbstractIntegerSimilarityFunction<Cluster<M>> {
+
+  @Override
+  public IntegerDistance similarity(Integer id1, Integer id2) {
+    Collection<Integer> collection1 = getDatabase().get(id1).getIDs();
+    Collection<Integer> collection2 = getDatabase().get(id2).getIDs();
+    Integer[] data1 = collection1.toArray(new Integer[collection1.size()]);
+    Integer[] data2 = collection2.toArray(new Integer[collection2.size()]);
+    Arrays.sort(data1);
+    Arrays.sort(data2);
+    int intersection = 0;
+    for(Integer id: data1){
+      if(Arrays.binarySearch(data2, id)>=0){
+        intersection++;
+      }
+    }
+    
+    return new IntegerDistance(intersection);
+  }
+
+}
