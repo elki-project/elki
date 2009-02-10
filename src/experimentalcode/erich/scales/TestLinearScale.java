091359ef2076a8cd2328a64b8701987b524ed834
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-02-10 17:14:24 +0000

    Some more unit tests. never hurts.

--- src/experimentalcode/erich/scales/TestLinearScale.java
+++ src/experimentalcode/erich/scales/TestLinearScale.java
@@ -15,6 +15,14 @@ public class TestLinearScale {
     LinearScale b = new LinearScale(-97, -3);
     assertEquals("Minimum for scale -97 : -3 not as expected.", -100.0, b.getMin(), Double.MIN_VALUE);
     assertEquals("Maximum for scale -97 : -3 not as expected.", 0.0, b.getMax(), Double.MIN_VALUE);
+
+    LinearScale c = new LinearScale(-3, 37);
+    assertEquals("Minimum for scale -3 : 37 not as expected.", -10.0, c.getMin(), Double.MIN_VALUE);
+    assertEquals("Maximum for scale -3 : 37 not as expected.", 40.0, c.getMax(), Double.MIN_VALUE);
+  
+    LinearScale d = new LinearScale(-37, 3);
+    assertEquals("Minimum for scale -37 : 3 not as expected.", -40.0, d.getMin(), Double.MIN_VALUE);
+    assertEquals("Maximum for scale -37 : 3 not as expected.", 10.0, d.getMax(), Double.MIN_VALUE);
   }
 
 }
