a7dcece44900b9f2130226afce7942c41cb3c860
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-02-17 15:29:13 +0000

    Add canonical second constructor to AbortException.

--- src/de/lmu/ifi/dbs/elki/algorithm/AbortException.java
+++ src/de/lmu/ifi/dbs/elki/algorithm/AbortException.java
@@ -2,18 +2,27 @@ package de.lmu.ifi.dbs.elki.algorithm;
 
 /**
  * Exception for aborting some process and transporting a message.
- *
+ * 
  * @author Arthur Zimek
  */
 @SuppressWarnings("serial")
 public class AbortException extends RuntimeException {
-    /**
-     * Exception for aborting some process and transporting a message.
-     *
-     * @param message message to be transported
-     */
-    public AbortException(String message) {
-        super(message);
-    }
+  /**
+   * Exception for aborting some process and transporting a message.
+   * 
+   * @param message message to be transported
+   */
+  public AbortException(String message) {
+    super(message);
+  }
 
+  /**
+   * Exception for aborting some process and transporting a message.
+   * 
+   * @param message message to be transported
+   * @param cause
+   */
+  public AbortException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
