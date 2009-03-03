baf879c7880c090e20e1672cfc810544054fe5f4
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-03-03 10:50:22 +0000

    Refactor support for gzip input streams.

--- src/de/lmu/ifi/dbs/elki/database/connection/FileBasedDatabaseConnection.java
+++ src/de/lmu/ifi/dbs/elki/database/connection/FileBasedDatabaseConnection.java
@@ -2,6 +2,7 @@ package de.lmu.ifi.dbs.elki.database.connection;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.util.zip.GZIPInputStream;
 
@@ -52,33 +53,47 @@ public class FileBasedDatabaseConnection<O extends DatabaseObject> extends Input
 
     try {
       in = new FileInputStream(INPUT_PARAM.getValue());
-      // try autodetecting gzip compression.
-      if (!in.markSupported()) {
-        PushbackInputStream pb = new PushbackInputStream(in, 16);
-        in = pb;
-        // read a magic from the file header
-        byte[] magic = {0, 0};
-        pb.read(magic);
-        pb.unread(magic);
-        if (magic[0] == 31 && magic[1] == -117) {
-          in = new GZIPInputStream(pb);
-        }
-      } else
-      if (in.markSupported()) {
-        in.mark(16);
-        if (in.read() == 31 && in.read() == -117) {
-          in.reset();
-          in = new GZIPInputStream(in);
-        } else {
-          // just rewind the stream
-          in.reset();
-        }
-      }
+      in = tryGzipInput(in);
     }
     catch(IOException e) {
       throw new WrongParameterValueException(INPUT_PARAM, INPUT_PARAM.getValue().getPath(), e);
     }
 
     return remainingParameters;
+  }
+  
+  /**
+   * Try to open a stream as gzip, if it starts with the gzip magic.
+   * 
+   * TODO: move to utils package.
+   * 
+   * @param in original input stream
+   * @return old input stream or a {@link GZIPInputStream} if appropriate.
+   * @throws IOException
+   */
+  public static InputStream tryGzipInput(InputStream in) throws IOException {
+    // try autodetecting gzip compression.
+    if (!in.markSupported()) {
+      PushbackInputStream pb = new PushbackInputStream(in, 16);
+      in = pb;
+      // read a magic from the file header
+      byte[] magic = {0, 0};
+      pb.read(magic);
+      pb.unread(magic);
+      if (magic[0] == 31 && magic[1] == -117) {
+        in = new GZIPInputStream(pb);
+      }
+    } else
+    if (in.markSupported()) {
+      in.mark(16);
+      if (in.read() == 31 && in.read() == -117) {
+        in.reset();
+        in = new GZIPInputStream(in);
+      } else {
+        // just rewind the stream
+        in.reset();
+      }
+    }
+    return in;
   }
 }
--- src/de/lmu/ifi/dbs/elki/database/connection/MultipleFileBasedDatabaseConnection.java
+++ src/de/lmu/ifi/dbs/elki/database/connection/MultipleFileBasedDatabaseConnection.java
@@ -3,6 +3,8 @@ package de.lmu.ifi.dbs.elki.database.connection;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -30,204 +32,192 @@ import de.lmu.ifi.dbs.elki.utilities.pairs.SimplePair;
 
 /**
  * Provides a database connection based on multiple files and parsers to be set.
- *
+ * 
  * @author Elke Achtert
- * @param <O> the type of DatabaseObject to be provided by the implementing class as element of the supplied database
+ * @param <O> the type of DatabaseObject to be provided by the implementing
+ *        class as element of the supplied database
  */
-public class MultipleFileBasedDatabaseConnection<O extends DatabaseObject>
-    extends AbstractDatabaseConnection<MultiRepresentedObject<O>> {
+public class MultipleFileBasedDatabaseConnection<O extends DatabaseObject> extends AbstractDatabaseConnection<MultiRepresentedObject<O>> {
 
-    /**
-     * OptionID for {@link #PARSERS_PARAM}
-     */
-    public static final OptionID PARSERS_ID = OptionID.getOrCreateOptionID(
-        "multipledbc.parsers",
-        "A comma separated list of classnames specifying the parsers to provide a database " +
-            Properties.ELKI_PROPERTIES.restrictionString(Parser.class) +
-            ". If this parameter is not set, " +
-            RealVectorLabelParser.class.getName() + " is used as parser for all input files."
-    );
+  /**
+   * OptionID for {@link #PARSERS_PARAM}
+   */
+  public static final OptionID PARSERS_ID = OptionID.getOrCreateOptionID("multipledbc.parsers", "A comma separated list of classnames specifying the parsers to provide a database " + Properties.ELKI_PROPERTIES.restrictionString(Parser.class) + ". If this parameter is not set, " + RealVectorLabelParser.class.getName() + " is used as parser for all input files.");
 
-    /**
-     * Optional parameter to specify the parsers to provide a database,
-     * must extend {@link Parser}. If this parameter is not set,
-     * {@link RealVectorLabelParser} is used as parser for all input files.
-     * <p>Key: {@code -multipledbc.parsers} </p>
-     */
-    private final ClassListParameter<Parser<O>> PARSERS_PARAM = new ClassListParameter<Parser<O>>(
-        PARSERS_ID, Parser.class, true);
+  /**
+   * Optional parameter to specify the parsers to provide a database, must
+   * extend {@link Parser}. If this parameter is not set,
+   * {@link RealVectorLabelParser} is used as parser for all input files.
+   * <p>
+   * Key: {@code -multipledbc.parsers}
+   * </p>
+   */
+  private final ClassListParameter<Parser<O>> PARSERS_PARAM = new ClassListParameter<Parser<O>>(PARSERS_ID, Parser.class, true);
 
-    /**
-     * Holds the instances of the parsers specified by {@link #PARSERS_PARAM}.
-     */
-    private List<Parser<O>> parsers;
+  /**
+   * Holds the instances of the parsers specified by {@link #PARSERS_PARAM}.
+   */
+  private List<Parser<O>> parsers;
 
-    /**
-     * OptionID for {@link #INPUT_PARAM}
-     */
-    public static final OptionID INPUT_ID = OptionID.getOrCreateOptionID(
-        "multipledbc.in",
-        "A comma separated list of the names of the input files to be parsed."
-    );
+  /**
+   * OptionID for {@link #INPUT_PARAM}
+   */
+  public static final OptionID INPUT_ID = OptionID.getOrCreateOptionID("multipledbc.in", "A comma separated list of the names of the input files to be parsed.");
 
-    /**
-     * Parameter that specifies the names of the input files to be parsed.
-     * <p>Key: {@code -multipledbc.in} </p>
-     */
-    private final FileListParameter INPUT_PARAM =
-        new FileListParameter(INPUT_ID,
-            FileListParameter.FilesType.INPUT_FILES);
+  /**
+   * Parameter that specifies the names of the input files to be parsed.
+   * <p>
+   * Key: {@code -multipledbc.in}
+   * </p>
+   */
+  private final FileListParameter INPUT_PARAM = new FileListParameter(INPUT_ID, FileListParameter.FilesType.INPUT_FILES);
 
-    /**
-     * The input files to parse from as specified by {@link #INPUT_PARAM}.
-     */
-    private List<FileInputStream> inputStreams;
+  /**
+   * The input files to parse from as specified by {@link #INPUT_PARAM}.
+   */
+  private List<InputStream> inputStreams;
 
-    /**
-     * Provides a database connection expecting input from several files,
-     * adding parameters
-     * {@link #PARSERS_PARAM}, and {@link #INPUT_PARAM}
-     * to the option handler additionally to parameters of super class.
-     */
-    public MultipleFileBasedDatabaseConnection() {
-        super();
-        forceExternalID = true;
-        // parameter parser
-        addOption(PARSERS_PARAM);
+  /**
+   * Provides a database connection expecting input from several files, adding
+   * parameters {@link #PARSERS_PARAM}, and {@link #INPUT_PARAM} to the option
+   * handler additionally to parameters of super class.
+   */
+  public MultipleFileBasedDatabaseConnection() {
+    super();
+    forceExternalID = true;
+    // parameter parser
+    addOption(PARSERS_PARAM);
 
-        // parameter file list
-        addOption(INPUT_PARAM);
+    // parameter file list
+    addOption(INPUT_PARAM);
+  }
+
+  public Database<MultiRepresentedObject<O>> getDatabase(Normalization<MultiRepresentedObject<O>> normalization) {
+    try {
+      // number of representations
+      final int numberOfRepresentations = inputStreams.size();
+
+      // comparator to sort the ObjectAndLabels lists provided by the
+      // parsers.
+      Comparator<SimplePair<O, List<String>>> comparator = new Comparator<SimplePair<O, List<String>>>() {
+        public int compare(SimplePair<O, List<String>> o1, SimplePair<O, List<String>> o2) {
+          String classLabel1 = o1.getSecond().get(classLabelIndex);
+          String classLabel2 = o2.getSecond().get(classLabelIndex);
+          return classLabel1.compareTo(classLabel2);
+        }
+      };
+
+      // parse
+      List<ParsingResult<O>> parsingResults = new ArrayList<ParsingResult<O>>(numberOfRepresentations);
+      int numberOfObjects = 0;
+      for(int r = 0; r < numberOfRepresentations; r++) {
+        debugFine("Parsing file " + (r + 1) + ".");
+        ParsingResult<O> parsingResult = parsers.get(r).parse(inputStreams.get(r));
+        parsingResults.add(parsingResult);
+        numberOfObjects = Math.max(parsingResult.getObjectAndLabelList().size(), numberOfObjects);
+        // sort the representations according to the external ids
+        List<SimplePair<O, List<String>>> objectAndLabelsList = parsingResult.getObjectAndLabelList();
+        Collections.sort(objectAndLabelsList, comparator);
+      }
+
+      // build the multi-represented objects and their labels
+      List<SimplePair<MultiRepresentedObject<O>, List<String>>> objectAndLabelsList = new ArrayList<SimplePair<MultiRepresentedObject<O>, List<String>>>();
+      for(int i = 0; i < numberOfObjects; i++) {
+        List<O> representations = new ArrayList<O>(numberOfRepresentations);
+        List<String> labels = new ArrayList<String>();
+        for(int r = 0; r < numberOfRepresentations; r++) {
+          ParsingResult<O> parsingResult = parsingResults.get(r);
+          representations.add(parsingResult.getObjectAndLabelList().get(i).getFirst());
+          List<String> rep_labels = parsingResult.getObjectAndLabelList().get(i).getSecond();
+          for(String l : rep_labels) {
+            if(!labels.contains(l))
+              labels.add(l);
+          }
+        }
+        objectAndLabelsList.add(new SimplePair<MultiRepresentedObject<O>, List<String>>(new MultiRepresentedObject<O>(representations), labels));
+      }
+
+      // normalize objects and transform labels
+      List<SimplePair<MultiRepresentedObject<O>, Associations>> objectAndAssociationList = normalizeAndTransformLabels(objectAndLabelsList, normalization);
+
+      // insert into database
+      database.insert(objectAndAssociationList);
+
+      return database;
+    }
+    catch(UnableToComplyException e) {
+      throw new IllegalStateException(e);
+    }
+    catch(NonNumericFeaturesException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  /**
+   * Calls the super method and instantiates {@link #inputStreams} according to
+   * the value of parameter {@link #INPUT_PARAM} and {@link #parsers} according
+   * to the value of parameter {@link #PARSERS_PARAM} . The remaining parameters
+   * are passed to all instances of {@link #parsers}.
+   */
+  @Override
+  public String[] setParameters(String[] args) throws ParameterException {
+    String[] remainingParameters = super.setParameters(args);
+
+    // input files
+    List<File> inputFiles = INPUT_PARAM.getValue();
+    inputStreams = new ArrayList<InputStream>(inputFiles.size());
+    for(File inputFile : inputFiles) {
+      try {
+        inputStreams.add(FileBasedDatabaseConnection.tryGzipInput(new FileInputStream(inputFile)));
+      }
+      catch(FileNotFoundException e) {
+        throw new WrongParameterValueException(INPUT_PARAM, inputFiles.toString(), e);
+      }
+      catch(IOException e) {
+        throw new WrongParameterValueException(INPUT_PARAM, inputFiles.toString(), e);
+      }
     }
 
-    public Database<MultiRepresentedObject<O>> getDatabase(
-        Normalization<MultiRepresentedObject<O>> normalization) {
+    // parsers
+    if(optionHandler.isSet(PARSERS_PARAM)) {
+      parsers = PARSERS_PARAM.instantiateClasses();
+
+      if(parsers.size() != inputStreams.size()) {
+        throw new WrongParameterValueException("Number of parsers and input files does not match (" + parsers.size() + " != " + inputStreams.size() + ")!");
+      }
+    }
+    else {
+      this.parsers = new ArrayList<Parser<O>>(inputStreams.size());
+      for(int i = 0; i < inputStreams.size(); i++) {
         try {
-            // number of representations
-            final int numberOfRepresentations = inputStreams.size();
-
-            // comparator to sort the ObjectAndLabels lists provided by the
-            // parsers.
-            Comparator<SimplePair<O,List<String>>> comparator = new Comparator<SimplePair<O,List<String>>>() {
-                public int compare(SimplePair<O,List<String>> o1, SimplePair<O,List<String>> o2) {
-                    String classLabel1 = o1.getSecond().get(classLabelIndex);
-                    String classLabel2 = o2.getSecond().get(classLabelIndex);
-                    return classLabel1.compareTo(classLabel2);
-                }
-            };
-
-            // parse
-            List<ParsingResult<O>> parsingResults = new ArrayList<ParsingResult<O>>(
-                numberOfRepresentations);
-            int numberOfObjects = 0;
-            for (int r = 0; r < numberOfRepresentations; r++) {
-                ParsingResult<O> parsingResult = parsers.get(r).parse(inputStreams.get(r));
-                parsingResults.add(parsingResult);
-                numberOfObjects = Math.max(parsingResult.getObjectAndLabelList().size(), numberOfObjects);
-                // sort the representations according to the external ids
-                List<SimplePair<O,List<String>>> objectAndLabelsList = parsingResult.getObjectAndLabelList();
-                Collections.sort(objectAndLabelsList, comparator);
-            }
-
-            // build the multi-represented objects and their labels
-            List<SimplePair<MultiRepresentedObject<O>,List<String>>> objectAndLabelsList = new ArrayList<SimplePair<MultiRepresentedObject<O>,List<String>>>();
-            for (int i = 0; i < numberOfObjects; i++) {
-                List<O> representations = new ArrayList<O>(numberOfRepresentations);
-                List<String> labels = new ArrayList<String>();
-                for (int r = 0; r < numberOfRepresentations; r++) {
-                    ParsingResult<O> parsingResult = parsingResults.get(r);
-                    representations.add(parsingResult.getObjectAndLabelList().get(i).getFirst());
-                    List<String> rep_labels = parsingResult.getObjectAndLabelList().get(i).getSecond();
-                    for (String l : rep_labels) {
-                        if (!labels.contains(l))
-                            labels.add(l);
-                    }
-                }
-                objectAndLabelsList.add(new SimplePair<MultiRepresentedObject<O>,List<String>>(
-                    new MultiRepresentedObject<O>(representations), labels));
-            }
-
-            // normalize objects and transform labels
-            List<SimplePair<MultiRepresentedObject<O>,Associations>> objectAndAssociationList = normalizeAndTransformLabels(objectAndLabelsList, normalization);
-
-            // insert into database
-            database.insert(objectAndAssociationList);
-
-            return database;
+          Parser<O> parser = Util.instantiateGenerics(Parser.class, RealVectorLabelParser.class.getName());
+          this.parsers.add(i, parser);
         }
-        catch (UnableToComplyException e) {
-            throw new IllegalStateException(e);
+        catch(UnableToComplyException e) {
+          throw new RuntimeException("This should never happen!");
         }
-        catch (NonNumericFeaturesException e) {
-            throw new IllegalStateException(e);
-        }
+      }
     }
 
-    /**
-     * Calls the super method
-     * and instantiates {@link #inputStreams} according to the value of parameter
-     * {@link #INPUT_PARAM}
-     * and {@link #parsers} according to the value of parameter {@link #PARSERS_PARAM} .
-     * The remaining parameters are passed to all instances of {@link #parsers}.
-     */
-    @Override
-    public String[] setParameters(String[] args) throws ParameterException {
-        String[] remainingParameters = super.setParameters(args);
-
-        // input files
-        List<File> inputFiles = INPUT_PARAM.getValue();
-        inputStreams = new ArrayList<FileInputStream>(inputFiles.size());
-        for (File inputFile : inputFiles) {
-            try {
-                inputStreams.add(new FileInputStream(inputFile));
-            }
-            catch (FileNotFoundException e) {
-                throw new WrongParameterValueException(INPUT_PARAM, inputFiles.toString(), e);
-            }
-        }
-
-        // parsers
-        if (optionHandler.isSet(PARSERS_PARAM)) {
-            parsers = PARSERS_PARAM.instantiateClasses();
-
-            if (parsers.size() != inputStreams.size()) {
-                throw new WrongParameterValueException("Number of parsers and input files does not match ("
-                    + parsers.size() + " != "
-                    + inputStreams.size() + ")!");
-            }
-        }
-        else {
-            this.parsers = new ArrayList<Parser<O>>(inputStreams.size());
-            for (int i = 0; i < inputStreams.size(); i++) {
-                try {
-                    Parser<O> parser = Util.instantiateGenerics(Parser.class, RealVectorLabelParser.class.getName());
-                    this.parsers.add(i, parser);
-                }
-                catch (UnableToComplyException e) {
-                    throw new RuntimeException("This should never happen!");
-                }
-            }
-        }
-
-        // set parameters of parsers
-        for (Parser<O> parser : this.parsers) {
-            remainingParameters = parser.setParameters(remainingParameters);
-        }
-        setParameters(args, remainingParameters);
-
-        return remainingParameters;
+    // set parameters of parsers
+    for(Parser<O> parser : this.parsers) {
+      remainingParameters = parser.setParameters(remainingParameters);
     }
+    setParameters(args, remainingParameters);
 
-    /**
-     * Calls the super method
-     * and adds to the returned attribute settings the attribute settings of
-     * all instances of {@link #parsers}.
-     */
-    @Override
-    public List<AttributeSettings> getAttributeSettings() {
-        List<AttributeSettings> attributeSettings = super.getAttributeSettings();
-        for (Parser<O> parser : parsers) {
-            attributeSettings.addAll(parser.getAttributeSettings());
-        }
-        return attributeSettings;
+    return remainingParameters;
+  }
+
+  /**
+   * Calls the super method and adds to the returned attribute settings the
+   * attribute settings of all instances of {@link #parsers}.
+   */
+  @Override
+  public List<AttributeSettings> getAttributeSettings() {
+    List<AttributeSettings> attributeSettings = super.getAttributeSettings();
+    for(Parser<O> parser : parsers) {
+      attributeSettings.addAll(parser.getAttributeSettings());
     }
+    return attributeSettings;
+  }
 }
