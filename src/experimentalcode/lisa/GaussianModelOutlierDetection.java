e02a38058c8f66a4e1588060ebed2326574b8c2c
Lisa Reichert <reichert@dbs.ifi.lmu.de>
2009-03-12 15:39:42 +0000

    <empty commit message>

--- src/experimentalcode/lisa/GaussianModelOutlierDetection.java
+++ src/experimentalcode/lisa/GaussianModelOutlierDetection.java
@@ -6,16 +6,19 @@ import de.lmu.ifi.dbs.elki.algorithm.AbstractAlgorithm;
 import de.lmu.ifi.dbs.elki.data.DatabaseObject;
 import de.lmu.ifi.dbs.elki.data.FeatureVector;
 import de.lmu.ifi.dbs.elki.data.RealVector;
+import de.lmu.ifi.dbs.elki.database.AssociationID;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.math.linearalgebra.Matrix;
 import de.lmu.ifi.dbs.elki.math.linearalgebra.Vector;
+import de.lmu.ifi.dbs.elki.result.AnnotationsFromDatabase;
 import de.lmu.ifi.dbs.elki.result.MultiResult;
+import de.lmu.ifi.dbs.elki.result.OrderingFromAssociation;
 import de.lmu.ifi.dbs.elki.utilities.Description;
 import de.lmu.ifi.dbs.elki.utilities.Util;
 
-public class GaussianModelOutlierDetection<V extends RealVector<V,N>, N extends Number> extends AbstractAlgorithm<V,MultiResult> {
+public class GaussianModelOutlierDetection<V extends RealVector<V,Double>> extends AbstractAlgorithm<V,MultiResult> {
 	MultiResult result;
-	
+	public static final AssociationID<Double> GMOD_MDIST = AssociationID.getOrCreateAssociationID("gmod.mdist", Double.class);
 	@Override
 	protected MultiResult runInTime(Database<V> database) throws IllegalStateException {
 		V mean = Util.centroid(database);
@@ -25,13 +28,23 @@ public class GaussianModelOutlierDetection<V extends RealVector<V,N>, N extends 
 		 for (Iterator<Integer> iter = database.iterator(); iter.hasNext(); ) {
              Integer id = iter.next();
              V x = database.get(id);
-             V dist = x.plus(meanNeg);
-  //           V distTransp = dist.;
-             
+             Vector x_minus_mean = x.plus(meanNeg).getColumnVector();
+             double mDist = x_minus_mean.times(covarianzTransposed).times(x_minus_mean.transpose()).get(0,0);
+             database.associate(GMOD_MDIST, id, mDist); 
 		 }
+		 AnnotationsFromDatabase<V, Double> res1 = new AnnotationsFromDatabase<V, Double>(database);
+	        res1.addAssociation("MDIST", GMOD_MDIST);
+	        // Ordering
+	        OrderingFromAssociation<Double, V> res2 = new OrderingFromAssociation<Double, V>(database, GMOD_MDIST, true); 
+	        // combine results.
+	        result = new MultiResult();
+	        result.addResult(res1);
+	        result.addResult(res2);
+			return result;
+			
 		 
 		 
-		return null;
+	
 	}
 
 	@Override
