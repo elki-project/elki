53aa3a7ab64c3a1a9bf5af3f55cb9a55497538fa
Lisa Reichert <reichert@dbs.ifi.lmu.de>
2009-04-22 13:59:42 +0000

    <empty commit message>

--- src/experimentalcode/lisa/DBOutlierDetection.java
+++ src/experimentalcode/lisa/DBOutlierDetection.java
@@ -22,27 +22,27 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.PatternParameter;
 public class DBOutlierDetection <O extends DatabaseObject, D extends Distance<D>> extends DistanceBasedAlgorithm<O , D , MultiResult> {
 	
 	public static final OptionID D_ID = OptionID.getOrCreateOptionID(
-		      "sibo.d",
+		      "dbod.d",
 		      "size of the D-neighborhood"
 		  );
 
 	public static final OptionID P_ID = OptionID.getOrCreateOptionID(
-				      "sibo.p",
+				      "dbod.p",
 				      "minimum fraction of objects that must be outside the D-neigborhood of an outlier"
 				  );
 		
-		public static final AssociationID<Boolean> SIBO_OFLAG= AssociationID.getOrCreateAssociationID("sibo.oflag", Boolean.class);
+		public static final AssociationID<Double> DBOD_OFLAG= AssociationID.getOrCreateAssociationID("dbod.oflag", Double.class);
 		/**
 		   * Parameter to specify the size of the D-neighborhood,
 		   * 
-		   * <p>Key: {@code -sibo.d} </p>
+		   * <p>Key: {@code -dbod.d} </p>
 		   */
 		  private final PatternParameter D_PARAM = new PatternParameter(D_ID);
 		  
 		  /**
 		   * Parameter to specify the minimum fraction of objects that must be outside the D-neigborhood of an outlier,
 		   * 
-		   * <p>Key: {@code -sibo.p} </p>
+		   * <p>Key: {@code -dbod.p} </p>
 		   */
 		  private final DoubleParameter P_PARAM = new DoubleParameter(P_ID);
 		  /**
@@ -95,16 +95,18 @@ public class DBOutlierDetection <O extends DatabaseObject, D extends Distance<D>
 			D distance = getDistanceFunction().valueOf(d);
 			//maximum number of objects in the D-neighborhood of an outlier 
 		    int m = (int) ((database.size())*(1-p));
+		    debugFine("maximum number of objects  " + m);
 		    //if index exists, kNN query. if the distance to the mth nearest neighbor is more than d -> object is outlier  
 		    if (database instanceof IndexDatabase){
 		    	for(Integer id : database.getIDs()){
+		    	  debugFine("distance to mth nearest neighbour" + database.kNNQueryForID(id, m, getDistanceFunction()).toString());
 		    		if(database.kNNQueryForID(id, m , getDistanceFunction()).get(m-1).getFirst().compareTo(distance)  <= 0){
 		    			//flag as outlier
-		    			database.associate(SIBO_OFLAG, id, true);
+		    			database.associate(DBOD_OFLAG, id, 1.0);
 		    		}
 		    		else {
 		    			//flag as no outlier
-			    		database.associate(SIBO_OFLAG, id, false);
+			    		database.associate(DBOD_OFLAG, id, 0.0);
 		    		}
 		    	}
 		    }
@@ -116,24 +118,27 @@ public class DBOutlierDetection <O extends DatabaseObject, D extends Distance<D>
 			    	while (iterator.hasNext()&& count < m) {
 			            Integer currentID = iterator.next();
 			            D currentDistance = getDistanceFunction().distance(id, currentID);
+			       debugFine("point " +id + "dcurrentdistance  " + currentDistance);
 			            if (currentDistance.compareTo(distance) <= 0) {
 			            	count ++;
 			            }
 			    	}
-			    	if(count<= m){
+			    	
+			    	
+			    	if(count< m){
 			    		//flag as outlier
-			    		database.associate(SIBO_OFLAG, id, true);
+			    		database.associate(DBOD_OFLAG, id, 1.0);
 			    	}
 			    	else {
 			    		//flag as no outlier
-			    		database.associate(SIBO_OFLAG, id, false);
+			    		database.associate(DBOD_OFLAG, id, 0.0);
 			    	}
 		    	}
 		    }
-		    AnnotationsFromDatabase<O, Boolean> res1 = new AnnotationsFromDatabase<O, Boolean>(database);
-	        res1.addAssociation(SIBO_OFLAG);
+		    AnnotationsFromDatabase<O, Double> res1 = new AnnotationsFromDatabase<O, Double>(database);
+	        res1.addAssociation(DBOD_OFLAG);
 	        // Ordering
-	        OrderingFromAssociation<Boolean, O> res2 = new OrderingFromAssociation<Boolean, O>(database, SIBO_OFLAG, true); 
+	        OrderingFromAssociation<Double, O> res2 = new OrderingFromAssociation<Double, O>(database, DBOD_OFLAG, true); 
 	        // combine results.
 	        result = new MultiResult();
 	        result.addResult(res1);
--- src/experimentalcode/lisa/EMOutlierDetection.java
+++ src/experimentalcode/lisa/EMOutlierDetection.java
@@ -1,11 +1,20 @@
 package experimentalcode.lisa;
 
 import de.lmu.ifi.dbs.elki.algorithm.AbstractAlgorithm;
+import de.lmu.ifi.dbs.elki.algorithm.clustering.EM;
+import de.lmu.ifi.dbs.elki.algorithm.outlier.ABOD;
 import de.lmu.ifi.dbs.elki.data.RealVector;
+import de.lmu.ifi.dbs.elki.database.AssociationID;
 import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.result.AnnotationResult;
 import de.lmu.ifi.dbs.elki.result.MultiResult;
+import de.lmu.ifi.dbs.elki.result.Result;
 import de.lmu.ifi.dbs.elki.utilities.Description;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.utilities.pairs.Pair;
+
+import java.util.Iterator;
+import java.util.List;
 /**
  * outlier detection algorithm using EM Clustering. If an object does not belong to any cluster it is supposed to be an outlier.
  * if the probability for an object to belong to the most probable cluster is still relatively low this object is an outlier 
@@ -14,6 +23,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
  * @param <V>
  */
 public class EMOutlierDetection<V extends RealVector<V, ?>> extends AbstractAlgorithm<V, MultiResult>{
+  EM emClustering;
   /**
    * Provides the result of the algorithm.
    */
@@ -35,7 +45,7 @@ public class EMOutlierDetection<V extends RealVector<V, ?>> extends AbstractAlgo
   @Override
   public String[] setParameters(String[] args) throws ParameterException {
       String[] remainingParameters = super.setParameters(args);
-      
+      remainingParameters= emClustering.setParameters(remainingParameters);
       return remainingParameters;
   }
 
@@ -45,7 +55,24 @@ public class EMOutlierDetection<V extends RealVector<V, ?>> extends AbstractAlgo
 
   @Override
   protected MultiResult runInTime(Database<V> database) throws IllegalStateException {
-    
+    AnnotationResult<List<Double>> probabilities; 
+    emClustering.run(database);
+    Iterator<Integer> iter = database.iterator();
+    if (iter.hasNext()){
+      
+      List<AnnotationResult<List<Double>>> annotations = result.filterResults(AnnotationResult.class);
+      for (AnnotationResult<List<Double>> annotation : annotations){
+        for (Pair<String, ?> pair : annotation.getAnnotations(iter.next())){
+          if (pair != null && pair.getFirst() != null && pair.getFirst().equals(AssociationID.PROBABILITY_CLUSTER_I_GIVEN_X)){
+            probabilities= annotation; 
+            probabilities.
+          }
+        }
+      }
+    } else {
+      throw new RuntimeException("Database empty.");
+    }
+  
     
     return null;
   }
