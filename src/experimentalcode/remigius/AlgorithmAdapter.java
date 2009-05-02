0ac1bf7e2a217bfd6139ba91686d47dca893a456
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-05-02 12:36:46 +0000

    AlgorithmAdapters no longer use deprecated getAnnotations(). Changed the way scaling is done.

--- src/experimentalcode/remigius/AlgorithmAdapter.java
+++ src/experimentalcode/remigius/AlgorithmAdapter.java
@@ -1,17 +1,47 @@
 package experimentalcode.remigius;
 
 import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.database.AssociationID;
 import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.result.AnnotationResult;
+import de.lmu.ifi.dbs.elki.result.MultiResult;
 import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
 
-public abstract class AlgorithmAdapter<O extends DatabaseObject> {
+public abstract class AlgorithmAdapter<O extends DatabaseObject, T> {
 	
 	protected Result result;
 	protected Database<O> database;
 	
+	protected AssociationID<T> asID;
+	
 	public AlgorithmAdapter(Database<O> database, Result result){
 		this.database = database;
 		this.result = result;
+	}
+	
+	public Database<O> getDatabase(){
+		return database;
+	}
+	
+	public Result getResult(){
+		return this.result;
+	}
+	
+	public AssociationID<T> getAlgorithmID(){
+		return asID;
+	}
+	
+	protected T getScore(DatabaseObject dbo){
+		return getOutlierAnnotationResult().getValueFor(dbo.getID());
+	}
+	
+	private AnnotationResult<T> getOutlierAnnotationResult(){
+		if (result instanceof MultiResult){
+			return ResultUtil.findAnnotationResult((MultiResult)result, asID);
+		} else {
+			throw new IllegalArgumentException("No MultiResult.");
+		}
 	}
 	
 	public abstract Double getUnnormalized(DatabaseObject dbo);
