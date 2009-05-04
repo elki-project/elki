3b3bf719efd73fd66cffbae2c6ebfc80b0673b2f
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2009-05-04 15:11:01 +0000

    improved info

--- src/de/lmu/ifi/dbs/elki/parser/package-info.java
+++ src/de/lmu/ifi/dbs/elki/parser/package-info.java
@@ -1,4 +1,14 @@
 /**
- * <p>Package collects parser for different file formats and data types.</p> 
+ * <p>Package collects parser for different file formats and data types.</p>
+ * 
+ * <p>The general use-case for any parser is to create {@link DatabaseObject}s out of an
+ * {@link java.io.InputStream} (e.g. by reading a data file).
+ * The {@link DatabaseObject}s are packed in a {@link ParsingResult}-Object which,
+ * in turn, is used by a {@link DatabaseConnection}-Object to create a {@link Database}
+ * containing the corresponding objects.</p>
+ * <p>By default (i.e., if the user does not specify any specific requests), any {@link KDDTask} will
+ * use the {@link FileBasedDatabaseConnection} which, in turn, will use the
+ * {@link DoubleVectorLabelParser} to parse a specified data file creating
+ * a {@link SequentialDatabase} containing {@link DoubleVector}-Objects.</p>
  */
 package de.lmu.ifi.dbs.elki.parser;
