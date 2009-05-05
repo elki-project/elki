4c357416df58b49d66cec763be8d4a2d25be2bc1
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2009-05-05 08:31:18 +0000

    comments

--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/timeseries/package-info.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/timeseries/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <p>Distance functions designed for time series.</p>
  * 
- * Note that some regular distance functions (e.g. Euclidean) are also used on timeseries.
+ * Note that some regular distance functions (e.g. Euclidean) are also used on time series.
  * 
  */
 package de.lmu.ifi.dbs.elki.distance.distancefunction.timeseries;
