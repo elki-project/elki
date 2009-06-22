a62b87228fbd3a0bee7a093a1ca530fdedc8db21
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-06-22 15:13:25 +0000

    Correctly handle empty messages.

--- src/de/lmu/ifi/dbs/elki/logging/MessageFormatter.java
+++ src/de/lmu/ifi/dbs/elki/logging/MessageFormatter.java
@@ -4,32 +4,35 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 /**
- * A formatter to simply retrieve the message of an LogRecord without printing origin information.
- *
+ * A formatter to simply retrieve the message of an LogRecord without printing
+ * origin information.
+ * 
  * @author Arthur Zimek
  */
 public class MessageFormatter extends Formatter {
-    /**
-     * Provides a message formatter
-     * to simply retrieve the message of an LogRecord.
-     */
-    public MessageFormatter() {
-        super();
-    }
+  /**
+   * Provides a message formatter to simply retrieve the message of an
+   * LogRecord.
+   */
+  public MessageFormatter() {
+    super();
+  }
 
-    /**
-     * Retrieves the message as it is set in the given LogRecord.
-     */
-    @Override
-    public String format(LogRecord record) {
-      String msg = record.getMessage();
-      if (msg.charAt(0) == OutputStreamLogger.CARRIAGE_RETURN) {
+  /**
+   * Retrieves the message as it is set in the given LogRecord.
+   */
+  @Override
+  public String format(LogRecord record) {
+    String msg = record.getMessage();
+    if(msg.length() > 0) {
+      if(msg.charAt(0) == OutputStreamLogger.CARRIAGE_RETURN) {
         return msg;
       }
-      if (msg.endsWith(OutputStreamLogger.NEWLINE)) {
+      if(msg.endsWith(OutputStreamLogger.NEWLINE)) {
         return msg;
       }
-      return msg+"\n";
     }
+    return msg + "\n";
+  }
 
 }
