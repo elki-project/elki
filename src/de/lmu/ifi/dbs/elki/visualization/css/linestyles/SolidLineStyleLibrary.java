6ac3545dc9fef688a11bb30000f909835f4e015a
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-07-15 09:16:24 +0000

    Add my own implementation of line styles for CSS.

---
+++ src/de/lmu/ifi/dbs/elki/visualization/css/linestyles/SolidLineStyleLibrary.java
@@ -0,0 +1,60 @@
+package de.lmu.ifi.dbs.elki.visualization.css.linestyles;
+
+import org.apache.batik.util.CSSConstants;
+
+import de.lmu.ifi.dbs.elki.utilities.output.FormatUtil;
+import de.lmu.ifi.dbs.elki.visualization.colors.ColorLibrary;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
+
+/**
+ * Line style library featuring solid lines for default styles only
+ * (combine with a color library to obtain enough classes!)
+ * 
+ * {@link #LineStyleLibrary.FLAG_STRONG} will result in thicker lines.
+ * 
+ * {@link #LineStyleLibrary.FLAG_WEAK} will result in thinner and semi-transparent lines.
+ * 
+ * {@link #LineStyleLibrary.FLAG_INTERPOLATED} will result in dashed lines.
+ * 
+ * @author Erich Schubert
+ *
+ */
+public class SolidLineStyleLibrary implements LineStyleLibrary {
+  /**
+   * Reference to the color library.
+   */
+  private ColorLibrary colors;
+  
+  /**
+   * Constructor.
+   * 
+   * @param colors Color library to use.
+   */
+  public SolidLineStyleLibrary(ColorLibrary colors) {
+    super();
+    this.colors = colors;
+  }
+
+  @Override
+  public void formatCSSClass(CSSClass cls, int style, double width, Object... flags) {
+    cls.setStatement(CSSConstants.CSS_STROKE_PROPERTY, colors.getColor(style));
+    boolean interpolated = false;
+    // process flavoring flags
+    for(Object flag : flags) {
+      if(flag == LineStyleLibrary.FLAG_STRONG) {
+        width = width * 1.5;
+      }
+      else if(flag == LineStyleLibrary.FLAG_WEAK) {
+        cls.setStatement(CSSConstants.CSS_STROKE_OPACITY_PROPERTY, ".50");
+        width = width * 0.75;
+      }
+      else if(flag == LineStyleLibrary.FLAG_INTERPOLATED) {
+        interpolated = true;
+      }
+    }
+    cls.setStatement(CSSConstants.CSS_STROKE_WIDTH_PROPERTY, FormatUtil.format(width * 100, FormatUtil.NF4)+"%");
+    if(interpolated) {
+      cls.setStatement(CSSConstants.CSS_STROKE_DASHARRAY_PROPERTY, ""+(width*2)+","+(width*2));
+    }
+  }
+}
---
+++ src/de/lmu/ifi/dbs/elki/visualization/css/linestyles/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>Generate line styles for plotting in CSS</p>
+ * 
+ */
+package de.lmu.ifi.dbs.elki.visualization.css.linestyles;
