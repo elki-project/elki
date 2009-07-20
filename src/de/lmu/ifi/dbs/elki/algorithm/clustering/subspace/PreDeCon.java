e4ee84ad3ee398290d187339c935091197187196
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-07-20 15:34:40 +0000

    Minor code formatting.

--- src/de/lmu/ifi/dbs/elki/algorithm/clustering/subspace/PreDeCon.java
+++ src/de/lmu/ifi/dbs/elki/algorithm/clustering/subspace/PreDeCon.java
@@ -12,31 +12,24 @@ import de.lmu.ifi.dbs.elki.utilities.Description;
  * a low variance along one or more (but not all) attributes.
  * </p>
  * <p/>
- * Reference:
- * <br>C. Boehm, K. Kailing, H.-P. Kriegel, P. Kroeger:
- * Density Connected Clustering with Local Subspace Preferences.
- * <br>In Proc. 4th IEEE Int. Conf. on Data Mining (ICDM'04), Brighton, UK, 2004.
+ * Reference: <br>
+ * C. B&ouml;hm, K. Kailing, H.-P. Kriegel, P. Kr&ouml;ger: Density Connected Clustering
+ * with Local Subspace Preferences. <br>
+ * In Proc. 4th IEEE Int. Conf. on Data Mining (ICDM'04), Brighton, UK, 2004.
  * </p>
- *
+ * 
  * @author Peer Kr&ouml;ger
  * @param <V> the type of RealVector handled by this Algorithm
  */
 public class PreDeCon<V extends RealVector<V, ?>> extends ProjectedDBSCAN<V> {
 
-    public Description getDescription() {
-        return new Description("PreDeCon", "Subspace Preference weighted Density Connected Clustering",
-            "PreDeCon computes clusters of subspace preference weighted connected points. " +
-                "The algorithm searches for local subgroups of a set of feature vectors having " +
-                "a low variance along one or more (but not all) attributes.",
-            "C. B\u00F6hm, K. Kailing, H.-P. Kriegel, P. Kr\u00F6ger: " +
-                "Density Connected Clustering with Local Subspace Preferences. " +
-                "In Proc. 4th IEEE Int. Conf. on Data Mining (ICDM'04), Brighton, UK, 2004.");
-    }
+  public Description getDescription() {
+    return new Description("PreDeCon", "Subspace Preference weighted Density Connected Clustering", "PreDeCon computes clusters of subspace preference weighted connected points. " + "The algorithm searches for local subgroups of a set of feature vectors having " + "a low variance along one or more (but not all) attributes.", "C. B\u00F6hm, K. Kailing, H.-P. Kriegel, P. Kr\u00F6ger: " + "Density Connected Clustering with Local Subspace Preferences. " + "In Proc. 4th IEEE Int. Conf. on Data Mining (ICDM'04), Brighton, UK, 2004.");
+  }
 
-    @Override
-    public Class<?> preprocessorClass() {
-        return PreDeConPreprocessor.class;
-    }
-
+  @Override
+  public Class<?> preprocessorClass() {
+    return PreDeConPreprocessor.class;
+  }
 
 }
--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/AbstractDistanceFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/AbstractDistanceFunction.java
@@ -7,58 +7,56 @@ import de.lmu.ifi.dbs.elki.distance.Distance;
 import java.util.regex.Pattern;
 
 /**
- * AbstractDistanceFunction provides some methods valid for any extending
- * class.
- *
+ * AbstractDistanceFunction provides some methods valid for any extending class.
+ * 
  * @author Arthur Zimek
  * @param <O> the type of DatabaseObject to compute the distances in between
  * @param <D> the type of Distance used
  */
-public abstract class AbstractDistanceFunction<O extends DatabaseObject, D extends Distance<D>>
-    extends AbstractMeasurementFunction<O, D> implements DistanceFunction<O, D> {
+public abstract class AbstractDistanceFunction<O extends DatabaseObject, D extends Distance<D>> extends AbstractMeasurementFunction<O, D> implements DistanceFunction<O, D> {
 
-    /**
-     * Provides an abstract DistanceFunction based on the given pattern.
-     *
-     * @param pattern a pattern to define the required input format
-     */
-    protected AbstractDistanceFunction(Pattern pattern) {
-        super(pattern);
-    }
+  /**
+   * Provides an abstract DistanceFunction based on the given pattern.
+   * 
+   * @param pattern a pattern to define the required input format
+   */
+  protected AbstractDistanceFunction(Pattern pattern) {
+    super(pattern);
+  }
 
-    /**
-     * Provides an abstract DistanceFunction.
-     * This constructor can be used if the required input pattern is
-     * not yet known at instantiation time and will therefore be set later.
-     */
-    protected AbstractDistanceFunction() {
-        super();
-    }
+  /**
+   * Provides an abstract DistanceFunction. This constructor can be used if the
+   * required input pattern is not yet known at instantiation time and will
+   * therefore be set later.
+   */
+  protected AbstractDistanceFunction() {
+    super();
+  }
 
-    /**
-     * Returns the distance between the two object specified by their object ids.
-     *
-     * @param id1 first object id
-     * @param id2 second object id
-     * @return the distance between the two object specified by their object ids
-     */
-    public D distance(Integer id1, Integer id2) {
-        return distance(getDatabase().get(id1), getDatabase().get(id2));
-    }
+  /**
+   * Returns the distance between the two object specified by their object ids.
+   * 
+   * @param id1 first object id
+   * @param id2 second object id
+   * @return the distance between the two object specified by their object ids
+   */
+  public D distance(Integer id1, Integer id2) {
+    return distance(getDatabase().get(id1), getDatabase().get(id2));
+  }
 
-    public D distance(Integer id1, O o2) {
-        return distance(getDatabase().get(id1), o2);
-    }
+  public D distance(Integer id1, O o2) {
+    return distance(getDatabase().get(id1), o2);
+  }
 
-    public final boolean isInfiniteDistance(D distance) {
-        return distance.equals(infiniteDistance());
-    }
+  public final boolean isInfiniteDistance(D distance) {
+    return distance.equals(infiniteDistance());
+  }
 
-    public final boolean isNullDistance(D distance) {
-        return distance.equals(nullDistance());
-    }
+  public final boolean isNullDistance(D distance) {
+    return distance.equals(nullDistance());
+  }
 
-    public final boolean isUndefinedDistance(D distance) {
-        return distance.equals(undefinedDistance());
-    }
+  public final boolean isUndefinedDistance(D distance) {
+    return distance.equals(undefinedDistance());
+  }
 }
