c0890704899a955bb9101f5131cba9671996a290
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-07-22 12:05:48 +0000

    Code formatting.

--- src/de/lmu/ifi/dbs/elki/data/ClassLabel.java
+++ src/de/lmu/ifi/dbs/elki/data/ClassLabel.java
@@ -1,62 +1,61 @@
 package de.lmu.ifi.dbs.elki.data;
 
-
 /**
  * A ClassLabel to identify a certain class of objects that is to discern from
  * other classes by a classifier.
- *
+ * 
  * @author Arthur Zimek
  */
 public abstract class ClassLabel implements Comparable<ClassLabel> {
+  /**
+   * ClassLabels need an empty constructor for dynamic access. Subsequently, the
+   * init method must be called.
+   */
+  protected ClassLabel() {
+    // requires subsequent call of init
+  }
 
-    /**
-     * ClassLabels need an empty constructor
-     * for dynamic access.
-     * Subsequently, the init method must be called.
-     */
-    protected ClassLabel() {
-        // requires subsequent call of init
+  /**
+   * Initialization of a ClassLabel for the given label.
+   * 
+   * @param label the label to create a ClassLabel
+   */
+  public abstract void init(String label);
+
+  /**
+   * Any ClassLabel should ensure a natural ordering that is consistent with
+   * equals. Thus, if <code>this.compareTo(o)==0</code>, then
+   * <code>this.equals(o)</code> should be <code>true</code>.
+   * 
+   * @param obj an object to test for equality w.r.t. this ClassLabel
+   * @return true, if <code>this==obj || this.compareTo(o)==0</code>, false
+   *         otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if(!(obj instanceof ClassLabel)) {
+      return false;
     }
+    return this == obj || this.compareTo((ClassLabel) obj) == 0;
+  }
 
-    /**
-     * Initialization of a ClassLabel for the given label.
-     *
-     * @param label the label to create a ClassLabel
-     */
-    public abstract void init(String label);
+  /**
+   * Any ClassLabel requires a method to represent the label as a String. If
+   * <code>ClassLabel a.equals((ClassLabel) b)</code>, then also
+   * <code>a.toString().equals(b.toString())</code> should hold.
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public abstract String toString();
 
-    /**
-     * Any ClassLabel should ensure a natural ordering that is consistent with
-     * equals. Thus, if <code>this.compareTo(o)==0</code>, then
-     * <code>this.equals(o)</code> should be <code>true</code>.
-     *
-     * @param obj an object to test for equality w.r.t. this ClassLabel
-     * @return true, if <code>this==obj || this.compareTo(o)==0</code>, false
-     *         otherwise
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof ClassLabel)) return false;
-        return this == obj || this.compareTo((ClassLabel) obj) == 0;
-    }
-
-    /**
-     * Any ClassLabel requires a method to represent the label as a String. If
-     * <code>ClassLabel a.equals((ClassLabel) b)</code>, then also
-     * <code>a.toString().equals(b.toString())</code> should hold.
-     *
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public abstract String toString();
-
-    /**
-     * Returns the hashCode of the String-representation of this ClassLabel.
-     *
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
-    }
+  /**
+   * Returns the hashCode of the String-representation of this ClassLabel.
+   * 
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
+  }
 }
--- src/de/lmu/ifi/dbs/elki/data/SimpleClassLabel.java
+++ src/de/lmu/ifi/dbs/elki/data/SimpleClassLabel.java
@@ -2,84 +2,84 @@ package de.lmu.ifi.dbs.elki.data;
 
 /**
  * A simple class label casting a String as it is as label.
- *
+ * 
  * @author Arthur Zimek
  */
 public class SimpleClassLabel extends ClassLabel {
-    /**
-     * Holds the String designating the label.
-     */
-    private String label;
+  /**
+   * Holds the String designating the label.
+   */
+  private String label;
 
-    /**
-     * @see ClassLabel#ClassLabel()
-     */
-    public SimpleClassLabel() {
-        super();
+  /**
+   * @see ClassLabel#ClassLabel()
+   */
+  public SimpleClassLabel() {
+    super();
+  }
+
+  /**
+   * Provides a simple class label covering the given String.
+   * 
+   * @param label the String to be cast as label
+   */
+  @Override
+  public void init(String label) {
+    this.label = label;
+  }
+
+  /**
+   * The ordering of two SimpleClassLabels is given by the ordering on the
+   * Strings they represent.
+   * <p/>
+   * That is, the result equals <code>this.label.compareTo(o.label)</code>.
+   */
+  public int compareTo(ClassLabel o) {
+    SimpleClassLabel other = (SimpleClassLabel) o;
+    return this.label.compareTo(other.label);
+  }
+
+  /**
+   * The hash code of a simple class label is the hash code of the String
+   * represented by the ClassLabel.
+   */
+  @Override
+  public int hashCode() {
+    return label.hashCode();
+  }
+
+  /**
+   * Any ClassLabel should ensure a natural ordering that is consistent with
+   * equals. Thus, if <code>this.compareTo(o)==0</code>, then
+   * <code>this.equals(o)</code> should be <code>true</code>.
+   * 
+   * @param o an object to test for equality w.r.t. this ClassLabel
+   * @return true, if <code>this==obj || this.compareTo(o)==0</code>, false
+   *         otherwise
+   */
+  @Override
+  public boolean equals(Object o) {
+    if(this == o) {
+      return true;
     }
-
-    /**
-     * Provides a simple class label covering the given String.
-     *
-     * @param label the String to be cast as label
-     */
-    @Override
-    public void init(String label) {
-        this.label = label;
+    if(o == null || getClass() != o.getClass()) {
+      return false;
     }
-
-    /**
-     * The ordering of two SimpleClassLabels is given by the ordering on the
-     * Strings they represent. <p/> That is, the result equals
-     * <code>this.label.compareTo(o.label)</code>.
-     */
-    public int compareTo(ClassLabel o) {
-        SimpleClassLabel other = (SimpleClassLabel) o; 
-        return this.label.compareTo(other.label);
+    if(!super.equals(o)) {
+      return false;
     }
+    final SimpleClassLabel that = (SimpleClassLabel) o;
 
-    /**
-     * The hash code of a simple class label is the hash code of the String
-     * represented by the ClassLabel.
-     */
-    @Override
-    public int hashCode() {
-        return label.hashCode();
-    }
+    return label.equals(that.label);
+  }
 
-    /**
-     * Any ClassLabel should ensure a natural ordering that is consistent with
-     * equals. Thus, if <code>this.compareTo(o)==0</code>, then
-     * <code>this.equals(o)</code> should be <code>true</code>.
-     *
-     * @param o an object to test for equality w.r.t. this ClassLabel
-     * @return true, if <code>this==obj || this.compareTo(o)==0</code>, false
-     *         otherwise
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) {
-            return false;
-        }
-        final SimpleClassLabel that = (SimpleClassLabel) o;
-
-        return label.equals(that.label);
-    }
-
-    /**
-     * Returns a new instance of the String covered by this SimpleClassLabel.
-     *
-     * @return a new instance of the String covered by this SimpleClassLabel
-     */
-    @Override
-    public String toString() {
-        return label;
-    }
-
+  /**
+   * Returns a new instance of the String covered by this SimpleClassLabel.
+   * 
+   * @return a new instance of the String covered by this SimpleClassLabel
+   */
+  @Override
+  public String toString() {
+    return label;
+  }
 }
