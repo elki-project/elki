425905b81e41cac9c5b31d92ec6bae7b9b21f813
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-07-23 15:56:06 +0000

    Tweaks to allow logging reconfiguration

--- src/de/lmu/ifi/dbs/elki/logging/LoggingConfiguration.java
+++ src/de/lmu/ifi/dbs/elki/logging/LoggingConfiguration.java
@@ -1,12 +1,14 @@
 package de.lmu.ifi.dbs.elki.logging;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+
+import de.lmu.ifi.dbs.elki.utilities.FileUtil;
 
 /**
  * Facility for configuration of logging.
@@ -37,82 +39,58 @@ public final class LoggingConfiguration {
   /**
    * Static instance of the configuration
    */
-  protected static LoggingConfiguration config = new LoggingConfiguration();
+  protected static LoggingConfiguration config = new LoggingConfiguration(confbase, CLIConffile);
 
   /**
    * Configure Java Logging API: {@link java.util.logging}
    */
-  private LoggingConfiguration() {
+  private LoggingConfiguration(final String pkg, final String name) {
+    privateReconfigureLogging(pkg, name);
+  }
+  
+  /**
+   * Reconfigure logging.
+   * 
+   * @param pkg Package name the configuration file comes from
+   * @param name File name.
+   */
+  public static void reconfigureLogging(final String pkg, final String name) {
+    config.privateReconfigureLogging(pkg, name);
+  }
+
+  /**
+   * Reconfigure logging.
+   * 
+   * @param pkg Package name the configuration file comes from
+   * @param name File name.
+   */
+  private void privateReconfigureLogging(String pkg, final String name) {
     LogManager logManager = LogManager.getLogManager();
     Logger logger = Logger.getLogger(LoggingConfiguration.class.getName());
-
+    // allow null as package name.
+    if(pkg == null) {
+      pkg = "";
+    }
     // Load logging configuration from resources.
+    String cfgfile = pkg.replace('.', File.separatorChar) + File.separatorChar + name;
     try {
-      InputStream cfgdata = null;
-      // try in the file system first
-      File cfgfile = new File(confbase.replace('.', File.separatorChar) + File.separatorChar + CLIConffile);
-      if(cfgfile.exists() && cfgfile.canRead()) {
-        cfgdata = new FileInputStream(cfgfile.getAbsolutePath());
-      }
-      else // otherwise, load from system resources
-      {
-        cfgdata = ClassLoader.getSystemResourceAsStream(confbase.replace('.', '/') + '/' + CLIConffile);
-      }
-      // Load logging configuration
-      if(cfgdata != null) {
-        logManager.readConfiguration(cfgdata);
-        logger.info("Logging configuration read.");
-      }
-      else {
-        logger.warning("No logging configuration found.");
-      }
+      InputStream cfgdata = FileUtil.openSystemFile(cfgfile);
+      logManager.readConfiguration(cfgdata);
+
+      // also load as properties for us, to get debug flag.
+      InputStream cfgdata2 = FileUtil.openSystemFile(cfgfile);
+      Properties cfgprop = new Properties();
+      cfgprop.load(cfgdata2);
+      DEBUG = Boolean.valueOf(cfgprop.getProperty("debug"));
+
+      logger.info("Logging configuration read.");
+    }
+    catch(FileNotFoundException e) {
+      logger.log(Level.SEVERE, "Could not find logging configuration file: " + cfgfile, e);
     }
     catch(Exception e) {
-      logger.log(Level.SEVERE, "Failed to configure logging.", e);
+      logger.log(Level.SEVERE, "Failed to configure logging from file: " + cfgfile, e);
     }
-    loadConfigurationPrivate(confbase, CLIConffile);
-  }
-
-  /**
-   * Load a configuration file.
-   * 
-   * @param pkg Package name to look in
-   * @param name File name
-   */
-  private void loadConfigurationPrivate(final String pkg, final String name) {
-    final Logger logger = Logger.getLogger(LoggingConfiguration.class.getName());
-    final String base = (pkg == null) ? "" : pkg;
-    if(name == null) {
-      logger.log(Level.SEVERE, "No configuration file name given.");
-      return;
-    }
-    // extra config settings
-    // try in the file system first
-    File cfgfile = new File(base.replace('.', File.separatorChar) + File.separatorChar + name);
-    Properties cfgprop = new Properties();
-    try {
-      InputStream res = ClassLoader.getSystemResourceAsStream(base.replace('.', '/') + '/' + name);
-      if(res != null) {
-        cfgprop.load(res);
-      }
-      if(cfgfile.exists() && cfgfile.canRead()) {
-        cfgprop.load(new FileInputStream(cfgfile.getAbsolutePath()));
-      }
-    }
-    catch(Exception e) {
-      logger.log(Level.SEVERE, "Failed to load logging properties from " + cfgfile.getAbsolutePath(), e);
-    }
-    DEBUG = Boolean.valueOf(cfgprop.getProperty("debug"));
-  }
-
-  /**
-   * Reconfigure logging using a configuration file.
-   * 
-   * @param pkg Package name, may be null
-   * @param name File name, may not be null
-   */
-  public static void loadConfiguration(final String pkg, final String name) {
-    config.loadConfigurationPrivate(pkg, name);
   }
 
   /**
@@ -124,7 +102,8 @@ public final class LoggingConfiguration {
 
   /**
    * Reconfigure logging to enable 'verbose' logging at the top level.
-   * @param verbose verbosity flag
+   * 
+   * @param verbose verbose flag
    */
   public static void setVerbose(boolean verbose) {
     Logger logger = Logger.getLogger(TOPLEVEL_PACKAGE);
