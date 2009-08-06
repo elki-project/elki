606ea449545549827e9a9b00b38809c543d889d0
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-08-06 09:14:57 +0000

    Add package for Marisa.

---
+++ src/experimentalcode/marisa/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>The base-package of Marisa's experimental code for the ELKI framework.</p>
+ * 
+ * 
+ *  <p>Classes not scheduled for a release.</p>
+ *  
+ */
+package experimentalcode.marisa;
