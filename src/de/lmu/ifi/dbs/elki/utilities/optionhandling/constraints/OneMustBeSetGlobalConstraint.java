d122461b10ce917d4b462325610a90421696f130
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-08-07 09:00:20 +0000

    Relax OneMustBeSet and OnlyOneIsAllowedToBeSet global constraints to allow flags.

--- src/de/lmu/ifi/dbs/elki/utilities/optionhandling/constraints/OneMustBeSetGlobalConstraint.java
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/constraints/OneMustBeSetGlobalConstraint.java
@@ -2,6 +2,7 @@ package de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints;
 
 import java.util.List;
 
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.Option;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionUtil;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
@@ -11,43 +12,42 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.WrongParameterValueException
  * Represents a global parameter constraint specifying that at least one
  * parameter value of a given list of parameters ({@link Parameter}) has to be
  * set.
- *
+ * 
  * @author Steffi Wanka
  */
 public class OneMustBeSetGlobalConstraint implements GlobalParameterConstraint {
 
-    /**
-     * List of parameters to be checked.
-     */
-    private List<Parameter<?, ?>> parameters;
+  /**
+   * List of parameters to be checked.
+   */
+  private List<Option<?>> parameters;
 
-    /**
-     * Creates a One-Must-Be-Set global parameter constraint. That is, at least
-     * one parameter value of the given list of parameters has to be set.
-     *
-     * @param params list of parameters
-     */
-    public OneMustBeSetGlobalConstraint(List<Parameter<?, ?>> params) {
-        parameters = params;
+  /**
+   * Creates a One-Must-Be-Set global parameter constraint. That is, at least
+   * one parameter value of the given list of parameters has to be set.
+   * 
+   * @param params list of parameters
+   */
+  public OneMustBeSetGlobalConstraint(List<Option<?>> params) {
+    parameters = params;
+  }
+
+  /**
+   * Checks if at least one parameter value of the list of parameters specified
+   * is set. If not, a parameter exception is thrown.
+   * 
+   */
+  public void test() throws ParameterException {
+    for(Option<?> p : parameters) {
+      if(p.isSet()) {
+        return;
+      }
     }
+    throw new WrongParameterValueException("Global Parameter Constraint Error.\n" + "At least one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " has to be set.");
 
-    /**
-     * Checks if at least one parameter value of the list of parameters
-     * specified is set. If not, a parameter exception is thrown.
-     *
-     */
-    public void test() throws ParameterException {
-        for (Parameter<?, ?> p : parameters) {
-            if (p.isSet()) {
-                return;
-            }
-        }
-        throw new WrongParameterValueException("Global Parameter Constraint Error.\n" +
-            "At least one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " has to be set.");
+  }
 
-	}
-
-    public String getDescription() {
-        return "At least one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " has to be set.";
-    }
+  public String getDescription() {
+    return "At least one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " has to be set.";
+  }
 }
--- src/de/lmu/ifi/dbs/elki/utilities/optionhandling/constraints/OnlyOneIsAllowedToBeSetGlobalConstraint.java
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/constraints/OnlyOneIsAllowedToBeSetGlobalConstraint.java
@@ -3,6 +3,7 @@ package de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints;
 import java.util.List;
 import java.util.Vector;
 
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.Option;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionUtil;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
@@ -11,47 +12,45 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.WrongParameterValueException
 /**
  * Global parameter constraint specifying that only one parameter of a list of
  * parameters ({@link Parameter}) is allowed to be set.
- *
+ * 
  * @author Steffi Wanka
  */
 public class OnlyOneIsAllowedToBeSetGlobalConstraint implements GlobalParameterConstraint {
 
-    /**
-     * List of parameters to be checked.
-     */
-    private List<Parameter<?, ?>> parameters;
+  /**
+   * List of parameters to be checked.
+   */
+  private List<Option<?>> parameters;
 
-    /**
-     * Constructs a global parameter constraint for testing if only one
-     * parameter of a list of parameters is set.
-     *
-     * @param params list of parameters to be checked
-     */
-    public OnlyOneIsAllowedToBeSetGlobalConstraint(List<Parameter<?, ?>> params) {
-        parameters = params;
-    }
+  /**
+   * Constructs a global parameter constraint for testing if only one parameter
+   * of a list of parameters is set.
+   * 
+   * @param params list of parameters to be checked
+   */
+  public OnlyOneIsAllowedToBeSetGlobalConstraint(List<Option<?>> params) {
+    parameters = params;
+  }
 
-    /**
-     * Checks if only one parameter of a list of parameters is set. If not, a
-     * parameter exception is thrown.
-     *
-     */
-    public void test() throws ParameterException {
-        Vector<String> set = new Vector<String>();
-        for (Parameter<?, ?> p : parameters) {
-            if (p.isSet()) {
-                set.add(p.getName());
-            }
-        }
-        if (set.size() > 1) {
-            throw new WrongParameterValueException("Global Parameter Constraint Error.\n" +
-                "Only one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " is allowed to be set. " +
-                "Parameters currently set: " + set.toString());
-        }
+  /**
+   * Checks if only one parameter of a list of parameters is set. If not, a
+   * parameter exception is thrown.
+   * 
+   */
+  public void test() throws ParameterException {
+    Vector<String> set = new Vector<String>();
+    for(Option<?> p : parameters) {
+      if(p.isSet()) {
+        set.add(p.getName());
+      }
     }
+    if(set.size() > 1) {
+      throw new WrongParameterValueException("Global Parameter Constraint Error.\n" + "Only one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " is allowed to be set. " + "Parameters currently set: " + set.toString());
+    }
+  }
 
-    public String getDescription() {
-        return "Only one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " is allowed to be set.";
-    }
+  public String getDescription() {
+    return "Only one of the parameters " + OptionUtil.optionsNamesToString(parameters) + " is allowed to be set.";
+  }
 
 }
