1e4e35c57e8062fee42c770b2f60f329331ed6bf
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-08-14 14:15:06 +0000

    Javadoc.

--- src/de/lmu/ifi/dbs/elki/result/HistogramResult.java
+++ src/de/lmu/ifi/dbs/elki/result/HistogramResult.java
@@ -4,11 +4,29 @@ import java.util.Collection;
 
 import de.lmu.ifi.dbs.elki.data.DatabaseObject;
 
+/**
+ * Histogram result.
+ * 
+ * @author Erich Schubert
+ *
+ * @param <O> Object class (e.g. {@link de.lmu.ifi.dbs.elki.data.DoubleVector})
+ */
 public class HistogramResult<O extends DatabaseObject> extends CollectionResult<O> {
+  /**
+   * Constructor
+   * 
+   * @param col Collection
+   */
   public HistogramResult(Collection<O> col) {
     super(col);
   }
 
+  /**
+   * Constructor
+   * 
+   * @param col Collection
+   * @param header Header information
+   */
   public HistogramResult(Collection<O> col, Collection<String> header) {
     super(col, header);
   }
