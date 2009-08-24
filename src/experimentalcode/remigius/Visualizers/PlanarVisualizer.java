7f30d06eb013dabc81a8642ddad9dd31a92eae41
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-08-24 16:36:52 +0000

    Throw out redundant code

--- src/experimentalcode/remigius/Visualizers/PlanarVisualizer.java
+++ src/experimentalcode/remigius/Visualizers/PlanarVisualizer.java
@@ -32,16 +32,4 @@ public abstract class PlanarVisualizer<O extends DoubleVector> extends DoubleVec
     this.dimx = dimx;
     this.dimy = dimy;
   }
-  
-  /**
-   * Returns a Double representing a position where the object will be placed
-   * 
-   * @param o the object to be positioned.
-   * @param dimx the dimension in which the position will be calculated
-   * @return a Double representing the normalized position of the object in the
-   *         given dimension.
-   */
-  public Double getPositioned(O o, int dim) {
-    return scales[dim].getScaled(o.getValue(dim));
-  }
 }
