518eefb3feb986e9e847ce55790c95f25f9b60c5
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-08-29 17:37:58 +0000

    More histogram-hacking.

--- src/experimentalcode/remigius/Visualizers/HistogramVisualizer.java
+++ src/experimentalcode/remigius/Visualizers/HistogramVisualizer.java
@@ -1,15 +1,25 @@
 package experimentalcode.remigius.Visualizers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.batik.util.SVGConstants;
 import org.w3c.dom.Element;
 
+import de.lmu.ifi.dbs.elki.algorithm.clustering.ByLabelClustering;
+import de.lmu.ifi.dbs.elki.data.Clustering;
 import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.cluster.Cluster;
+import de.lmu.ifi.dbs.elki.data.model.Model;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.math.AggregatingHistogram;
+import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.Flag;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
 import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
 import experimentalcode.remigius.ShapeLibrary;
 import experimentalcode.remigius.VisualizationManager;
@@ -21,8 +31,13 @@ public class HistogramVisualizer<NV extends NumberVector<NV, N>, N extends Numbe
   private boolean row;
 
   private static final String NAME = "Histograms";
-  private static final int BINS = 50;
+  private static final int BINS = 20;
   private double frac;
+
+  private Result result;
+
+  private Clustering<Model> clustering;
+
 
   public HistogramVisualizer(){
     addOption(STYLE_ROW_PARAM);
@@ -36,43 +51,81 @@ public class HistogramVisualizer<NV extends NumberVector<NV, N>, N extends Numbe
     return remainingParameters;
   }
 
-  public void setup(Database<NV> database, VisualizationManager<NV> visManager) {
+  public void setup(Database<NV> database, Result result, VisualizationManager<NV> visManager) {
     init(database, visManager, NAME);
     this.frac = 1. / database.size();
-    setupCSS();
+    this.result = result;
+    setupClustering();
   }
 
-  private void setupCSS() {
+  private void setupClustering(){
+    List<Clustering<?>> clusterings = ResultUtil.getClusteringResults(result);
 
-    // TODO: Set CSS.
+    if (clusterings != null && clusterings.size() > 0) {
+      clustering = (Clustering<Model>) clusterings.get(0);
+    } else {
+      clustering = new ByLabelClustering<NV>().run(database);
+    }
+  }
+
+  //  private void setupCSS() {
+  //
+  //    int clusterID = 0;
+  //    for (Cluster<Model> cluster : clustering.getAllClusters()){
+  //      clusterID+=1;
+  //      CSSClass bin = visManager.createCSSClass(ShapeLibrary.BIN + clusterID);
+  //      bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, "0.5");
+  //      bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, COLORS.getColor(clusterID));
+  //      visManager.registerCSSClass(bin);
+  //    }
+  //  }
+
+  private void setupCSS(int clusterID) {
+    CSSClass bin = visManager.createCSSClass(ShapeLibrary.BIN + clusterID);
+    bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, "0.5");
+    bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, COLORS.getColor(clusterID));
+    visManager.registerCSSClass(bin);
   }
 
   @Override
   public Element visualize(SVGPlot svgp) {
     Element layer = ShapeLibrary.createSVG(svgp.getDocument());
 
-    // Creating a histogram.
-    AggregatingHistogram<Double, Double> hist = AggregatingHistogram.DoubleSumHistogram(BINS, scales[dim].getMin(), scales[dim].getMax());
-    for (Integer id : database){
-      hist.aggregate(database.get(id).getValue(dim).doubleValue(), frac);
+    Map<Integer, AggregatingHistogram<Double, Double>> hists = new HashMap<Integer, AggregatingHistogram<Double, Double>>();
+
+    // Creating histograms
+    int clusterID = 0;
+    for (Cluster<Model> cluster : clustering.getAllClusters()){
+      clusterID+=1;
+      AggregatingHistogram<Double, Double> hist = AggregatingHistogram.DoubleSumHistogram(BINS, scales[dim].getMin(), scales[dim].getMax());
+      for (int id : cluster.getIDs()){
+        hist.aggregate(database.get(id).getValue(dim).doubleValue(), frac);
+      }
+      hists.put(clusterID, hist);
+      setupCSS(clusterID);
     }
 
-    if (row) {
-      // 1 row per bin
-      for (int bin = 0; bin < hist.getNumBins(); bin++){
-        // TODO: calculating the value *must* be simpler. Something is wrong here.
-        double val = hist.get(bin*(scales[dim].getMax() - scales[dim].getMin())/BINS);
-        layer.appendChild(ShapeLibrary.createRow(svgp.getDocument(), getPositioned(bin*hist.getBinsize(), dim), 1 - val, hist.getBinsize(), val, dim, bin));
+    // Visualizing
+    // TODO: Drawing centered instead of left-end values of bins.
+    for (int key = 1; key <= hists.size(); key++){
+      AggregatingHistogram<Double, Double> hist = hists.get(key);
+      if (row) {
+        for (int bin = 0; bin < hist.getNumBins(); bin++){
+          // TODO: calculating the value *must* be simpler. Something is wrong here.
+          double val = hist.get(bin*(scales[dim].getMax() - scales[dim].getMin())/BINS);
+          layer.appendChild(ShapeLibrary.createRow(svgp.getDocument(), getPositioned(bin*hist.getBinsize(), dim), 1 - val, hist.getBinsize(), val, dim, key, bin));
+        }
+      } else {
+        // TODO: Path is still buggy.
+        Element path = ShapeLibrary.createPath(svgp.getDocument(), 1, 1, key, Integer.toString(key));
+        // Just a hack to ensure the path ends at its beginning.
+        ShapeLibrary.addLine(path, 0, 1);
+        for (int bin = 0; bin < hist.getNumBins(); bin++){
+          double val = hist.get((bin*(scales[dim].getMax() - scales[dim].getMin())/BINS));
+          ShapeLibrary.addLine(path, getPositioned(bin*hist.getBinsize(), dim), 1-val);
+        }
+        layer.appendChild(path);
       }
-    } else {
-      // 1 Line
-      Element path = ShapeLibrary.createPath(svgp.getDocument(), 1, 1, "");
-      StringBuffer strb = new StringBuffer();
-      for (int bin = 0; bin < hist.getNumBins(); bin++){
-        double val = hist.get(bin*(scales[dim].getMax() - scales[dim].getMin())/BINS);
-        ShapeLibrary.addLine(path, getPositioned(bin*hist.getBinsize(), dim), 1-val);
-      }
-      layer.appendChild(path);
     }
     return layer;
   }
