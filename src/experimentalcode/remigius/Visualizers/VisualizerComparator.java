4d163dc50230be43afa3d0754d803461da51f5f7
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-08-29 15:44:27 +0000

    Fix sorting of Visualizers.

--- src/experimentalcode/remigius/Visualizers/VisualizerComparator.java
+++ src/experimentalcode/remigius/Visualizers/VisualizerComparator.java
@@ -2,23 +2,21 @@ package experimentalcode.remigius.Visualizers;
 
 import java.util.Comparator;
 
+/**
+ * Compares Visualizers by level. <br>
+ * Note: this comparator imposes orderings that are inconsistent with equals.
+ * 
+ * @author Remigius Wojdanowski
+ * 
+ */
 public class VisualizerComparator implements Comparator<Visualizer> {
 
   @Override
   public int compare(Visualizer o1, Visualizer o2) {
-    if(o1.getLevel() < o2.getLevel()) {
+    if(o1.getLevel() < o2.getLevel())
       return -1;
-    }
-    else if(o1.getLevel() > o2.getLevel()) {
+    if(o1.getLevel() > o2.getLevel())
       return 1;
-    }
-    else {
-      if(o1.equals(o2)) {
-        return 0;
-      }
-      else {
-        return 1;
-      }
-    }
+    return 0;
   }
 }
