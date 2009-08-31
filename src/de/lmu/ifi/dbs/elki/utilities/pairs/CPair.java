b7681522c100b2a59b773664b751c2a50b2cdff8
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-08-31 11:58:03 +0000

    More refactoring of Pair code. PairUtil now stores all the external comparator classes in one place. This allows sorting e.g. Pair<F,C> objects easily when F or C or both are comparable or do have a Comparator<>.

--- src/de/lmu/ifi/dbs/elki/utilities/pairs/CPair.java
+++ src/de/lmu/ifi/dbs/elki/utilities/pairs/CPair.java
@@ -1,7 +1,5 @@
 package de.lmu.ifi.dbs.elki.utilities.pairs;
 
-import java.util.Comparator;
-
 import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
 
 /**
@@ -12,7 +10,7 @@ import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
  * @param <FIRST> first type
  * @param <SECOND> second type
  */
-public class CPair<FIRST extends Comparable<FIRST>, SECOND extends Comparable<SECOND>> extends Pair<FIRST, SECOND> implements Comparable<CPair<FIRST,SECOND>> {
+public class CPair<FIRST extends Comparable<? super FIRST>, SECOND extends Comparable<? super SECOND>> extends Pair<FIRST, SECOND> implements Comparable<CPair<FIRST,SECOND>> {
   /**
    * Initialize pair
    * 
@@ -60,42 +58,6 @@ public class CPair<FIRST extends Comparable<FIRST>, SECOND extends Comparable<SE
   }
 
   /**
-   * Generic derived compare function, with swapped components.
-   * 
-   * @param other Object to compare to
-   * @return comparison result
-   */
-  public int compareSwappedTo(CPair<FIRST, SECOND> other) {
-    // try comparing by second
-    if(this.second != null) {
-      if(other.second == null) {
-        return -1;
-      }
-      int delta2 = this.second.compareTo(other.second);
-      if(delta2 != 0) {
-        return delta2;
-      }
-    }
-    else if(other.second != null) {
-      return +1;
-    }
-    // try comparing by first
-    if(this.first != null) {
-      if(other.first == null) {
-        return -1;
-      }
-      int delta1 = this.first.compareTo(other.first);
-      if(delta1 != 0) {
-        return delta1;
-      }
-    }
-    else if(other.first != null) {
-      return +1;
-    }
-    return 0;
-  }
-
-  /**
    * Array constructor for generics
    * 
    * @param <F> First type
@@ -103,28 +65,8 @@ public class CPair<FIRST extends Comparable<FIRST>, SECOND extends Comparable<SE
    * @param size Size of array to be constructed
    * @return New array of requested size
    */
-  public static final <F extends Comparable<F>, S extends Comparable<S>> CPair<F, S>[] newArray(int size) {
-    Class<CPair<F,S>> paircls = ClassGenericsUtil.uglyCastIntoSubclass(CPair.class);
+  public static final <F extends Comparable<? super F>, S extends Comparable<? super S>> CPair<F, S>[] newArray(int size) {
+    Class<CPair<F,S>> paircls = ClassGenericsUtil.uglyCastIntoSubclass(CPair.class);    
     return ClassGenericsUtil.newArrayOfNull(size, paircls);
-  }
-
-  /**
-   * Class to do a canonical swapped comparison on this class.
-   * 
-   * @param <FIRST>
-   * @param <SECOND>
-   */
-  public final static class CompareSwapped<FIRST extends Comparable<FIRST>, SECOND extends Comparable<SECOND>> implements Comparator<CPair<FIRST, SECOND>> {
-    /**
-     * Compare by second component, using the ComparableSwapped interface.
-     * 
-     * @param o1 First object
-     * @param o2 Second object
-     */
-    @Override
-    public int compare(CPair<FIRST, SECOND> o1, CPair<FIRST, SECOND> o2) {
-      return o1.compareSwappedTo(o2);
-    }
-
   }
 }
