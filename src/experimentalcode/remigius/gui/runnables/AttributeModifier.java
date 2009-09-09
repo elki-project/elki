cae542b53afb064bab22415615f6d33c8616965f
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-09-09 14:14:45 +0000

    More Documentation

--- src/experimentalcode/remigius/gui/runnables/AttributeModifier.java
+++ src/experimentalcode/remigius/gui/runnables/AttributeModifier.java
@@ -2,12 +2,36 @@ package experimentalcode.remigius.gui.runnables;
 
 import org.w3c.dom.Element;
 
+/**
+ * Runnable wrapper for modifying XML-Attributes. 
+ * 
+ * @author Remigius Wojdanowski
+ *
+ */
 public class AttributeModifier implements Runnable {
   
+  /**
+   * Provides the attribute to be modified.
+   */
   private Element e;
+  
+  /**
+   * The name of the attribute to be modified.
+   */
   private String attribute;
+  
+  /**
+   * The new value of the attribute.
+   */
   private String newValue;
   
+  /**
+   * Trivial constructor.
+   * 
+   * @param e provides the attribute to be modified.
+   * @param attribute the name of the attribute to be modified.
+   * @param newValue the new value of the attribute.
+   */
   public AttributeModifier(Element e, String attribute, String newValue){
     this.e = e;
     this.attribute = attribute;
