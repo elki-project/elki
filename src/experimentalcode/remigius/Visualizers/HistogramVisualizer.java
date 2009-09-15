7f329f20a63028972ff58668436a38e624ffdc21
Remigius Wojdanowski <wojdanowski@cip.ifi.lmu.de>
2009-09-15 14:56:16 +0000

    -histogram.row produces stacked histograms now.

--- src/experimentalcode/remigius/Visualizers/HistogramVisualizer.java
+++ src/experimentalcode/remigius/Visualizers/HistogramVisualizer.java
@@ -37,7 +37,7 @@ import experimentalcode.remigius.ShapeLibrary;
  * @param <N>
  */
 public class HistogramVisualizer<NV extends NumberVector<NV, N>, N extends Number> extends ScalarVisualizer<NV, N> {
-  
+
   public static final OptionID STYLE_ROW_ID = OptionID.getOrCreateOptionID("histogram.row", "Alternative style: Rows.");
 
   private final Flag STYLE_ROW_PARAM = new Flag(STYLE_ROW_ID);
@@ -49,7 +49,7 @@ public class HistogramVisualizer<NV extends NumberVector<NV, N>, N extends Numbe
   private static final int BINS = 20;
 
   private Clustering<Model> clustering;
-  
+
   private double frac;
 
   private Result result;
@@ -73,22 +73,35 @@ public class HistogramVisualizer<NV extends NumberVector<NV, N>, N extends Numbe
     this.clustering = clustering;
   }
 
-private void setupCSS(SVGPlot svgp) {
-    
+  private void setupCSS(SVGPlot svgp) {
+
     // creating IDs manually because cluster often return a null-ID.
     int clusterID = 0;
-    
+
+    CSSClass allInOne = new CSSClass(svgp, ShapeLibrary.BIN);
     for (Cluster<Model> cluster : clustering.getAllClusters()){
-      
+
       CSSClass bin = new CSSClass(svgp, ShapeLibrary.BIN + clusterID);
-      
-      if (clustering.getAllClusters().size() == 1){
-        bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, "black");
+      String coloredElement;
+
+      if (row){
+        bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, 1.0);
+        coloredElement = SVGConstants.CSS_FILL_PROPERTY;
       } else {
-        bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, COLORS.getColor(clusterID));
-        bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, "0.5");
+        bin.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, 0.001);
+        bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, 0.0);
+        allInOne.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, 0.001);
+        allInOne.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, 0.0);
+        coloredElement = SVGConstants.CSS_STROKE_PROPERTY;
       }
       
+      allInOne.setStatement(coloredElement, "black");
+      if (clustering.getAllClusters().size() == 1){
+        bin.setStatement(coloredElement, "black");
+      } else {
+        bin.setStatement(coloredElement, COLORS.getColor(clusterID));
+      }
+
       try {
         svgp.getCSSClassManager().addClass(bin);
         svgp.updateStyleElement();
@@ -100,19 +113,19 @@ private void setupCSS(SVGPlot svgp) {
     }
   }
 
-//  private void setupCSS(SVGPlot svgp, int clusterID) {
-//    CSSClass bin = new CSSClass(svgp, ShapeLibrary.BIN + clusterID);
-//    bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, "0.5");
-//    bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, COLORS.getColor(clusterID));
-//
-//    try {
-//      svgp.getCSSClassManager().addClass(bin);
-//      svgp.updateStyleElement();
-//    }
-//    catch(CSSNamingConflict e) {
-//      LoggingUtil.exception("Equally-named CSSClass with different owner already exists", e);
-//    }
-//  }
+  //  private void setupCSS(SVGPlot svgp, int clusterID) {
+  //    CSSClass bin = new CSSClass(svgp, ShapeLibrary.BIN + clusterID);
+  //    bin.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, "0.5");
+  //    bin.setStatement(SVGConstants.CSS_FILL_PROPERTY, COLORS.getColor(clusterID));
+  //
+  //    try {
+  //      svgp.getCSSClassManager().addClass(bin);
+  //      svgp.updateStyleElement();
+  //    }
+  //    catch(CSSNamingConflict e) {
+  //      LoggingUtil.exception("Equally-named CSSClass with different owner already exists", e);
+  //    }
+  //  }
 
   @Override
   public Element visualize(SVGPlot svgp) {
@@ -124,24 +137,25 @@ private void setupCSS(SVGPlot svgp) {
     // Creating histograms
     int clusterID = 0;
     MinMax<Double> minmax = new MinMax<Double>();
-    
+
+    AggregatingHistogram<Double, Double> allInOne = AggregatingHistogram.DoubleSumHistogram(BINS, scales[dim].getMin(), scales[dim].getMax());
     for(Cluster<Model> cluster : clustering.getAllClusters()) {
       AggregatingHistogram<Double, Double> hist = AggregatingHistogram.DoubleSumHistogram(BINS, scales[dim].getMin(), scales[dim].getMax());
       for(int id : cluster.getIDs()) {
         hist.aggregate(database.get(id).getValue(dim).doubleValue(), frac);
+        allInOne.aggregate(database.get(id).getValue(dim).doubleValue(), frac);
       }
 
       for(int bin = 0; bin < hist.getNumBins(); bin++) {
-        double val = hist.get(bin * (scales[dim].getMax() - scales[dim].getMin()) / BINS);
-        minmax.put(val);
+        double valAllInOne = allInOne.get(bin * (scales[dim].getMax() - scales[dim].getMin()) / BINS);
+        minmax.put(valAllInOne);
       }
-     
       hists.put(clusterID, hist);
       clusterID += 1;
     }
-    
+
     LinearScale scale = new LinearScale(minmax.getMin(), minmax.getMax());
-    
+
     // Axis
     try {
       SVGSimpleLinearAxis.drawAxis(svgp, layer, scale, 0, 1, 0, 0, true, false);
@@ -151,27 +165,30 @@ private void setupCSS(SVGPlot svgp) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    
+
     // Visualizing
-    // TODO: Drawing centered instead of left-end values of bins.
-    for(int key = 0; key < hists.size(); key++) {
-      AggregatingHistogram<Double, Double> hist = hists.get(key);
-      if(row) {
-        for(int bin = 0; bin < hist.getNumBins(); bin++) {
-          // TODO: calculating the value *must* be simpler. Something is wrong
-          // here.
+
+    if (row){
+      for(int bin = 0; bin < BINS; bin++) {
+        double lastVal = 0;
+        for(int key = 0; key < hists.size(); key++) {
+          AggregatingHistogram<Double, Double> hist = hists.get(key);
+
           double val = hist.get((bin * (scales[dim].getMax() - scales[dim].getMin()) / BINS))/minmax.getMax();
-          layer.appendChild(ShapeLibrary.createRow(svgp.getDocument(), getPositioned(bin * hist.getBinsize(), dim), 1 - val, 1./BINS, val, dim, key, bin));
+          layer.appendChild(ShapeLibrary.createRow(svgp.getDocument(), getPositioned(bin * hist.getBinsize(), dim), 1-(val + lastVal), 1./BINS, val, dim, key, bin));
+          lastVal += val;
         }
       }
-      else {
-        // TODO: Path is still buggy.
-        Element path = ShapeLibrary.createPath(svgp.getDocument(), 1, 1, key, Integer.toString(key));
-        // Just a hack to ensure the path ends at its beginning.
-        ShapeLibrary.addLine(path, 0, 1);
+    } else {
+      for(int key = 0; key < hists.size(); key++) {
+        AggregatingHistogram<Double, Double> hist = hists.get(key);
+        Element path = ShapeLibrary.createPath(svgp.getDocument(), 0, 1, key, Integer.toString(key));
         for(int bin = 0; bin < hist.getNumBins(); bin++) {
           double val = hist.get((bin * (scales[dim].getMax() - scales[dim].getMin()) / BINS))/minmax.getMax();
-          ShapeLibrary.addLine(path, getPositioned(bin * hist.getBinsize(), dim), 1 - val);
+          double left = getPositioned(bin * hist.getBinsize(), dim);
+          double right = getPositioned(bin * hist.getBinsize(), dim) + (1./BINS);
+          ShapeLibrary.addLine(path, left, 1 - val);
+          ShapeLibrary.addLine(path, right, 1 - val);
         }
         layer.appendChild(path);
       }
