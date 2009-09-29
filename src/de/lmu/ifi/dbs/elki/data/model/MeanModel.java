d1a597c2ecae18598403a45fe556225e16886829
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-09-29 08:07:12 +0000

    Return means from kMeans.

---
+++ src/de/lmu/ifi/dbs/elki/data/model/MeanModel.java
@@ -0,0 +1,41 @@
+package de.lmu.ifi.dbs.elki.data.model;
+
+import de.lmu.ifi.dbs.elki.data.FeatureVector;
+
+/**
+ * Cluster model that stores a mean for the cluster.
+ * 
+ * @author Erich Schubert
+ *
+ * @param <V> Vector type
+ */
+public class MeanModel<V extends FeatureVector<V, ?>> extends BaseModel {
+  /**
+   * Cluster mean
+   */
+  private V mean;
+
+  /**
+   * Constructor with mean
+   * 
+   * @param mean Cluster mean
+   */
+  public MeanModel(V mean) {
+    super();
+    this.mean = mean;
+  }
+
+  /**
+   * @return mean
+   */
+  public V getMean() {
+    return mean;
+  }
+
+  /**
+   * @param mean
+   */
+  public void setMean(V mean) {
+    this.mean = mean;
+  }
+}
