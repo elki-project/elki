2a674510d49437c9951de39767477248985a2958
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-10-07 09:16:21 +0000

    Add new Double Parameter constructor with just an optional flag.

--- src/de/lmu/ifi/dbs/elki/utilities/optionhandling/NumberParameter.java
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/NumberParameter.java
@@ -6,59 +6,60 @@ import java.util.List;
 
 /**
  * Abstract class for defining a number parameter.
- *
+ * 
  * @author Steffi Wanka
  * @param <T> the type of a possible value (i.e., the type of the option)
  */
 public abstract class NumberParameter<T extends Number> extends Parameter<T, Number> {
+  /**
+   * Constructs a number parameter with the given optionID.
+   * 
+   * @param optionID the unique id of this parameter
+   */
+  public NumberParameter(OptionID optionID) {
+    super(optionID);
+  }
 
-    /**
-     * Constructs a number parameter with the given optionID.
-     *
-     * @param optionID the unique id of this parameter
-     */
-    public NumberParameter(OptionID optionID) {
-        super(optionID);
-    }
+  /**
+   * Constructs a number parameter with the given optionID, and constraint.
+   * 
+   * @param optionID the unique id of this parameter
+   * @param constraint the constraint of this parameter
+   */
+  public NumberParameter(OptionID optionID, ParameterConstraint<Number> constraint) {
+    super(optionID, constraint);
+  }
 
-    /**
-     * Constructs a number parameter with the given optionID, and constraint.
-     *
-     * @param optionID   the unique id of this parameter
-     * @param constraint the constraint of this parameter
-     */
-    public NumberParameter(OptionID optionID, ParameterConstraint<Number> constraint) {
-        super(optionID, constraint);
-    }
+  /**
+   * Constructs a number parameter with the given optionID, and constraint.
+   * 
+   * @param optionID the unique id of this parameter
+   * @param constraints the constraints of this parameter, may be empty if there
+   *        are no constraints
+   */
+  public NumberParameter(OptionID optionID, List<ParameterConstraint<Number>> constraints) {
+    super(optionID, constraints);
+  }
 
-    /**
-     * Constructs a number parameter with the given optionID, and constraint.
-     *
-     * @param optionID   the unique id of this parameter
-     * @param constraints the constraints of this parameter, may be empty if there are no constraints
-     */
-    public NumberParameter(OptionID optionID, List<ParameterConstraint<Number>> constraints) {
-        super(optionID, constraints);
-    }
+  /**
+   * Constructs a number parameter with the given optionID, constraint, and
+   * optional flag.
+   * 
+   * @param optionID the unique id of this parameter
+   * @param constraint the constraint of this parameter
+   * @param optional specifies if this parameter is an optional parameter
+   * @param defaultValue the default value for this parameter
+   */
+  public NumberParameter(OptionID optionID, ParameterConstraint<Number> constraint, boolean optional, T defaultValue) {
+    super(optionID, constraint, optional, defaultValue);
+  }
 
-    /**
-     * Constructs a number parameter with the given optionID, constraint, and optional flag.
-     *
-     * @param optionID     the unique id of this parameter
-     * @param constraint   the constraint of this parameter
-     * @param optional     specifies if this parameter is an optional parameter
-     * @param defaultValue the default value for this parameter
-     */
-    public NumberParameter(OptionID optionID, ParameterConstraint<Number> constraint, boolean optional, T defaultValue) {
-        super(optionID, constraint, optional, defaultValue);
-    }
-
-    /**
-     * Returns the number value of the parameter.
-     *
-     * @return number value of the parameter.
-     */
-    public T getNumberValue() {
-        return value;
-    }
+  /**
+   * Returns the number value of the parameter.
+   * 
+   * @return number value of the parameter.
+   */
+  public T getNumberValue() {
+    return value;
+  }
 }
