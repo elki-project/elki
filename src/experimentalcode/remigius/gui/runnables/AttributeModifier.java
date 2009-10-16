d332125706b61651672f8f7dc467f9aab22e91ba
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-10-16 11:03:10 +0000

    Allow removing attributes

--- src/experimentalcode/remigius/gui/runnables/AttributeModifier.java
+++ src/experimentalcode/remigius/gui/runnables/AttributeModifier.java
@@ -3,28 +3,28 @@ package experimentalcode.remigius.gui.runnables;
 import org.w3c.dom.Element;
 
 /**
- * Runnable wrapper for modifying XML-Attributes. 
+ * Runnable wrapper for modifying XML-Attributes.
  * 
  * @author Remigius Wojdanowski
- *
+ * 
  */
 public class AttributeModifier implements Runnable {
-  
+
   /**
    * Provides the attribute to be modified.
    */
   private Element e;
-  
+
   /**
    * The name of the attribute to be modified.
    */
   private String attribute;
-  
+
   /**
    * The new value of the attribute.
    */
   private String newValue;
-  
+
   /**
    * Trivial constructor.
    * 
@@ -32,14 +32,19 @@ public class AttributeModifier implements Runnable {
    * @param attribute the name of the attribute to be modified.
    * @param newValue the new value of the attribute.
    */
-  public AttributeModifier(Element e, String attribute, String newValue){
+  public AttributeModifier(Element e, String attribute, String newValue) {
     this.e = e;
     this.attribute = attribute;
     this.newValue = newValue;
   }
-  
+
   @Override
   public void run() {
-    e.setAttribute(attribute, newValue);
+    if(newValue != null) {
+      e.setAttribute(attribute, newValue);
+    }
+    else {
+      e.removeAttribute(attribute);
+    }
   }
 }
