742d3bc70914175acf1099c5ad17be9a2c062c09
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-10-27 09:17:57 +0000

    Add JavaDoc

--- src/de/lmu/ifi/dbs/elki/result/outlier/InvertedOutlierScoreMeta.java
+++ src/de/lmu/ifi/dbs/elki/result/outlier/InvertedOutlierScoreMeta.java
@@ -1,14 +1,42 @@
 package de.lmu.ifi.dbs.elki.result.outlier;
 
+/**
+ * Class to signal a value-inverted outlier score, i.e. low values are outliers.
+ * 
+ * @author Erich Schubert
+ */
 public class InvertedOutlierScoreMeta extends BasicOutlierScoreMeta {
+  /**
+   * Constructor with all values.
+   * 
+   * @param actualMinimum
+   * @param actualMaximum
+   * @param theoreticalMinimum
+   * @param theoreticalMaximum
+   * @param theoreticalBaseline
+   */
   public InvertedOutlierScoreMeta(double actualMinimum, double actualMaximum, double theoreticalMinimum, double theoreticalMaximum, double theoreticalBaseline) {
     super(actualMinimum, actualMaximum, theoreticalMinimum, theoreticalMaximum, theoreticalBaseline);
   }
 
+  /**
+   * Constructor with range values.
+   * 
+   * @param actualMinimum
+   * @param actualMaximum
+   * @param theoreticalMinimum
+   * @param theoreticalMaximum
+   */
   public InvertedOutlierScoreMeta(double actualMinimum, double actualMaximum, double theoreticalMinimum, double theoreticalMaximum) {
     super(actualMinimum, actualMaximum, theoreticalMinimum, theoreticalMaximum);
   }
 
+  /**
+   * Constructor with actual range only.
+   * 
+   * @param actualMinimum
+   * @param actualMaximum
+   */
   public InvertedOutlierScoreMeta(double actualMinimum, double actualMaximum) {
     super(actualMinimum, actualMaximum);
   }
--- src/de/lmu/ifi/dbs/elki/result/outlier/OutlierScoreMeta.java
+++ src/de/lmu/ifi/dbs/elki/result/outlier/OutlierScoreMeta.java
@@ -1,9 +1,46 @@
 package de.lmu.ifi.dbs.elki.result.outlier;
 
+/**
+ * Generic meta information about the value range of an outlier score.
+ * 
+ * All values can be {@link Double#NaN} if not specified by the algorithm or not computed (actual values).
+ * For theoretical values, it is explicitly allowed to return {@link Double#NEGATIVE_INFINITY} or {@link Double#POSITIVE_INFINITY}.
+ * 
+ * @author Erich Schubert
+ */
 public interface OutlierScoreMeta {
+  /**
+   * Get the actual minimum of the value range.
+   * 
+   * @return minimum or {@link Double#NaN}
+   */
   public double getActualMinimum();
+  /**
+   * Get the actual maximum of the value range.
+   * 
+   * @return maximum or {@link Double#NaN}
+   */
   public double getActualMaximum();
+  /**
+   * Get the theoretical minimum of the value range.
+   * 
+   * @return theoretical minimum or {@link Double#NaN}
+   */
   public double getTheoreticalMinimum();
+  /**
+   * Get the theoretical maximum of the value range.
+   *
+   * This value may be {@link Double#NEGATIVE_INFINITY} or {@link Double#NaN}.
+   * 
+   * @return theoretical maximum or {@link Double#NaN}
+   */
   public double getTheoreticalMaximum();
+  /**
+   * Get the theoretical baseline of the value range.
+   * 
+   * It will be common to see {@link Double#POSITIVE_INFINITY} here.
+   * 
+   * @return theoretical baseline or {@link Double#NaN}
+   */
   public double getTheoreticalBaseline();
 }
