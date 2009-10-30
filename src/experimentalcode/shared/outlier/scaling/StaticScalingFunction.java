e5bf62fd1e8b2ce6139aeb3947ef91100b88b717
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2009-10-30 13:32:32 +0000

    bracket in comment

--- src/experimentalcode/shared/outlier/scaling/StaticScalingFunction.java
+++ src/experimentalcode/shared/outlier/scaling/StaticScalingFunction.java
@@ -5,7 +5,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable;
 /**
  * Interface for Scaling functions that do NOT depend on analyzing the data set.
  * But will always map x to the same f(x) (given the same parameters when
- * implementing {@link Parameterizable}.
+ * implementing {@link Parameterizable}).
  * 
  * @author Erich Schubert
  * 
