5e85dabaab63939afbec9ccfb51fa42df0ea795b
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-11-18 15:52:17 +0000

    Work in progress: new visualization UI.

---
+++ src/experimentalcode/erich/visualization/VisualizersForResult.java
@@ -0,0 +1,113 @@
+package experimentalcode.erich.visualization;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.logging.Logging;
+import de.lmu.ifi.dbs.elki.result.MultiResult;
+import de.lmu.ifi.dbs.elki.utilities.InspectionUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizable;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import experimentalcode.remigius.Adapter.AlgorithmAdapter;
+import experimentalcode.remigius.Visualizers.Visualizer;
+import experimentalcode.remigius.Visualizers.VisualizerContext;
+
+/**
+ * Utility class to determine the visualizers for a result class.
+ * 
+ * @author Erich Schubert
+ */
+public class VisualizersForResult extends AbstractParameterizable {
+  /**
+   * Get a logger for this class.
+   */
+  protected final static Logging logger = Logging.getLogger(ResultHistogramVisualizer.class);
+
+  /**
+   * (Result-to-visualization) Adapters
+   */
+  private Collection<AlgorithmAdapter<?>> adapters;
+  
+  /**
+   * Visualizer instances.
+   */
+  private Collection<Visualizer> visualizers;
+  
+  /**
+   * Constructor. No parameters: AbstractParameterizable
+   */
+  public VisualizersForResult() {
+    super();
+    this.adapters = collectAlgorithmAdapters();
+  }
+  
+  /**
+   * Process a particular result.
+   * 
+   * @param db Database context
+   * @param result Result
+   */
+  public void processResult(Database<? extends DatabaseObject> db, MultiResult result) {
+    VisualizerContext context = new VisualizerContext(db, result);
+    
+    // Collect all visualizers.
+    for (AlgorithmAdapter<?> a: adapters){
+      if (a.canVisualize(result)){
+        // Note: this can throw an exception when setParameters() was not called!
+        a.init(context);        
+        Collection<Visualizer> avis = a.getUsableVisualizers();
+        logger.debug("Got "+avis.size()+" visualizers from "+a.getClass().getName());
+        this.visualizers.addAll(avis);
+      }
+    }
+  }
+  
+  /**
+   * Get the visualizers found.
+   * 
+   * @return Visualizers found for result
+   */
+  public Collection<Visualizer> getVisualizers() {
+    // TODO: copy? it's cheap because it's small.
+    return visualizers;
+  }
+
+  /**
+   * Collect and instantiate all adapters.
+   * 
+   * @return List of all adapters found.
+   */
+  private static Collection<AlgorithmAdapter<?>> collectAlgorithmAdapters() {
+    ArrayList<AlgorithmAdapter<?>> algorithmAdapters = new ArrayList<AlgorithmAdapter<?>>();
+    for (Class<?> c : InspectionUtil.findAllImplementations(AlgorithmAdapter.class, false)){
+      try {
+        AlgorithmAdapter<?> a = (AlgorithmAdapter<?>) c.newInstance();
+        algorithmAdapters.add(a);
+      } catch (Exception e) {
+        logger.exception("Error instantiating AlgorithmAdapter "+c.getName(),e);
+      }
+    }
+    return algorithmAdapters;
+  }
+
+  @Override
+  public List<String> setParameters(List<String> args) throws ParameterException {
+    List<String> remainingParameters = super.setParameters(args);
+    
+    for (AlgorithmAdapter<?> a : adapters){
+      a.setParameters(remainingParameters);
+      for (Visualizer v : a.getProvidedVisualizers()){
+        List<String> leftoverParameters = v.setParameters(remainingParameters);
+        remainingParameters.removeAll(leftoverParameters);
+        // TODO: collect the usable parameters somehow!
+        //addParameterizable(v);
+      }
+    }
+    
+    rememberParametersExcept(args, remainingParameters);
+    return remainingParameters;
+  }
+}
