a03091427d30d37fe18693cf9ac35da5dcffc608
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-12-01 17:17:57 +0000

    JavaDoc

--- src/experimentalcode/shared/algorithm/clustering/ReferencePointsHeuristic.java
+++ src/experimentalcode/shared/algorithm/clustering/ReferencePointsHeuristic.java
@@ -6,6 +6,19 @@ import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable;
 
+/**
+ * Simple Interface for an heuristic to pick reference points.
+ * 
+ * @author Erich Schubert
+ *
+ * @param <O> Object type
+ */
 public interface ReferencePointsHeuristic<O extends NumberVector<O,?>> extends Parameterizable {
+  /**
+   * Get the reference points for the given database.
+   * 
+   * @param db Database
+   * @return Collection of reference points.
+   */
   public Collection<O> getReferencePoints(Database<O> db);
 }
