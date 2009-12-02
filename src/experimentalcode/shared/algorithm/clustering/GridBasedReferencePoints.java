8ff47de41a9512f3b3201eb7287db69a3acf9b0d
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-12-02 10:05:27 +0000

    Add grid scaling, support grid size of 0 (mean).

--- src/experimentalcode/shared/algorithm/clustering/GridBasedReferencePoints.java
+++ src/experimentalcode/shared/algorithm/clustering/GridBasedReferencePoints.java
@@ -8,10 +8,11 @@ import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.utilities.DatabaseUtil;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizable;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.DoubleParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.IntParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
-import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterEqualConstraint;
 import de.lmu.ifi.dbs.elki.utilities.output.FormatUtil;
 import de.lmu.ifi.dbs.elki.utilities.pairs.Pair;
 
@@ -19,65 +20,94 @@ import de.lmu.ifi.dbs.elki.utilities.pairs.Pair;
  * Grid-based strategy to pick reference points.
  * 
  * @author Erich Schubert
- *
+ * 
  * @param <O> Object type
  */
 // TODO: allow picking cell centers as refpoints, not only cell corners.
 // TODO: allow deliberately moving reference points outside of the data space.
-public class GridBasedReferencePoints<O extends NumberVector<O,?>> extends AbstractParameterizable implements ReferencePointsHeuristic<O> {
+public class GridBasedReferencePoints<O extends NumberVector<O, ?>> extends AbstractParameterizable implements ReferencePointsHeuristic<O> {
   /**
    * OptionID for {@link #GRID_PARAM}
    */
-  public static final OptionID GRID_ID = OptionID.getOrCreateOptionID("grid.steps", "The number of splits to do.");
+  public static final OptionID GRID_ID = OptionID.getOrCreateOptionID("grid.size", "The number of partitions in each dimension. Points will be placed on the edges of the grid, except for a grid size of 0, where only the mean is generated as reference point.");
 
   /**
    * Parameter to specify the sample size.
    * <p>
-   * Key: {@code -grid.steps}
+   * Key: {@code -grid.size}
    * </p>
    */
-  private final IntParameter GRID_PARAM = new IntParameter(GRID_ID, new GreaterConstraint(0), 2);
+  private final IntParameter GRID_PARAM = new IntParameter(GRID_ID, new GreaterEqualConstraint(0), 1);
+
+  /**
+   * OptionID for {@link #GRID_SCALE_PARAM}
+   */
+  public static final OptionID GRID_SCALE_ID = OptionID.getOrCreateOptionID("grid.scale", "Scale the grid by the given factor. This can be used to obtain reference points outside the used data space.");
+
+  /**
+   * Parameter to specify the sample size.
+   * <p>
+   * Key: {@code -grid.oversize}
+   * </p>
+   */
+  private final DoubleParameter GRID_SCALE_PARAM = new DoubleParameter(GRID_SCALE_ID, new GreaterEqualConstraint(0.0), 1.0);
 
   /**
    * Holds the value of {@link #GRID_PARAM}.
    */
-  private int gridres;
-  
+  protected int gridres;
+
+  /**
+   * Holds the value of {@link #GRID_SCALE_PARAM}.
+   */
+  protected double gridscale;
+
   /**
    * Constructor, Parameterizable style.
    */
   public GridBasedReferencePoints() {
     super();
     addOption(GRID_PARAM);
+    addOption(GRID_SCALE_PARAM);
   }
 
   @Override
   public Collection<O> getReferencePoints(Database<O> db) {
     Pair<O, O> minmax = DatabaseUtil.computeMinMax(db);
-    
-    assert(gridres >= 2);
-    
+    O prototype = minmax.first;
+
     int dim = db.dimensionality();
-    int gridpoints = (int)Math.pow(gridres, dim);
     
+    // Compute mean from minmax.
+    double[] mean = new double[dim];
+    for (int d = 0; d < dim; d++) {
+      mean[d] = (minmax.first.getValue(d+1).doubleValue() + minmax.second.getValue(d+1).doubleValue()) / 2;
+    }
+
+    int gridpoints = Math.max(1,(int) Math.pow(gridres + 1, dim));
     ArrayList<O> result = new ArrayList<O>(gridpoints);
     double[] delta = new double[dim];
-    int div = (gridres > 1) ? (gridres - 1) : 1;
-    for (int d = 0; d < dim; d++) {
-      delta[d] = (minmax.second.getValue(d+1).doubleValue() - minmax.first.getValue(d+1).doubleValue()) / div;
-    }
-    
-    double[] vec = new double[dim];
-    for (int i = 0; i < gridpoints; i++) {
-      int acc = i;
-      for (int d = 0; d < dim; d++) {
-        int coord = acc % gridres;
-        acc = acc / gridres;
-        vec[d] = minmax.first.getValue(d+1).doubleValue() + coord * delta[d];
+    if(gridres > 0) {
+      double halfgrid = gridres / 2.0;
+      for(int d = 0; d < dim; d++) {
+        delta[d] = (minmax.second.getValue(d + 1).doubleValue() - minmax.first.getValue(d + 1).doubleValue()) / gridres;
       }
-      O newp = minmax.first.newInstance(vec);
-      logger.debug("New reference point: " + FormatUtil.format(vec));
-      result.add(newp);
+
+      double[] vec = new double[dim];
+      for(int i = 0; i < gridpoints; i++) {
+        int acc = i;
+        for(int d = 0; d < dim; d++) {
+          int coord = acc % (gridres + 1);
+          acc = acc / (gridres + 1);
+          vec[d] = mean[d] + (coord - halfgrid) * delta[d] * gridscale;
+        }
+        O newp = prototype.newInstance(vec);
+        logger.debug("New reference point: " + FormatUtil.format(vec));
+        result.add(newp);
+      }
+    } else {
+      result.add(prototype.newInstance(mean));
+      logger.debug("New reference point: " + FormatUtil.format(mean));
     }
 
     return result;
@@ -86,9 +116,10 @@ public class GridBasedReferencePoints<O extends NumberVector<O,?>> extends Abstr
   @Override
   public List<String> setParameters(List<String> args) throws ParameterException {
     List<String> remainingParameters = super.setParameters(args);
-    
+
     gridres = GRID_PARAM.getValue();
-    
+    gridscale = GRID_SCALE_PARAM.getValue();
+
     rememberParametersExcept(args, remainingParameters);
     return remainingParameters;
   }
