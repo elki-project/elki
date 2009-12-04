a76f0db4c2b4acfb0e5f0a61c4cc364d2c5dfaf5
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-12-04 18:58:46 +0000

    Add a minimalistic OPTICS plot

--- src/de/lmu/ifi/dbs/elki/visualization/visualizers/adapter/ClusterOrderAdapter.java
+++ src/de/lmu/ifi/dbs/elki/visualization/visualizers/adapter/ClusterOrderAdapter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
 import de.lmu.ifi.dbs.elki.logging.LoggingUtil;
 import de.lmu.ifi.dbs.elki.result.ClusterOrderResult;
 import de.lmu.ifi.dbs.elki.result.Result;
@@ -12,6 +13,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.Visualizer;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.vis2d.ClusterOrderVisualizer;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.visunproj.OPTICSPlotVisualizer;
 
 /**
  * Visualize a cluster order by connecting the points with arrows
@@ -23,6 +25,11 @@ public class ClusterOrderAdapter implements AlgorithmAdapter {
    * Prototype for parameterization
    */
   private ClusterOrderVisualizer<?> clusterorderVisualizer = new ClusterOrderVisualizer<DoubleVector>();
+  
+  /**
+   * Prototype for parameterization
+   */
+  private OPTICSPlotVisualizer<?> opticsplotVisualizer = new OPTICSPlotVisualizer<DoubleDistance>();
   
   /**
    * Constructor, following the {@link de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable} style.
@@ -40,17 +47,19 @@ public class ClusterOrderAdapter implements AlgorithmAdapter {
   @Override
   public Collection<Visualizer> getProvidedVisualizers() {
     // FIXME: parameter handling is not very nice here.
-    ArrayList<Visualizer> providedVisualizers = new ArrayList<Visualizer>(1);
+    ArrayList<Visualizer> providedVisualizers = new ArrayList<Visualizer>(2);
     providedVisualizers.add(clusterorderVisualizer);
+    providedVisualizers.add(opticsplotVisualizer);
     return providedVisualizers;
   }
 
   @Override
   public Collection<Visualizer> getUsableVisualizers(VisualizerContext context) {
-    Collection<ClusterOrderResult<?>> cos = ResultUtil.filterResults(context.getResult(), ClusterOrderResult.class);
-    ArrayList<Visualizer> usableVisualizers = new ArrayList<Visualizer>(cos.size());
+    Collection<ClusterOrderResult<DoubleDistance>> cos = ResultUtil.filterResults(context.getResult(), ClusterOrderResult.class);
+    ArrayList<Visualizer> usableVisualizers = new ArrayList<Visualizer>(cos.size() * 2);
     ArrayList<String> params = clusterorderVisualizer.getParameters();
-    for (ClusterOrderResult<?> co : cos) {
+    ArrayList<String> oparams = opticsplotVisualizer.getParameters();
+    for (ClusterOrderResult<DoubleDistance> co : cos) {
       ClusterOrderVisualizer<?> coVis = new ClusterOrderVisualizer<DoubleVector>();
       // setup parameters.
       try {
@@ -61,6 +70,16 @@ public class ClusterOrderAdapter implements AlgorithmAdapter {
       }
       coVis.init(context, co);
       usableVisualizers.add(coVis);
+      OPTICSPlotVisualizer<DoubleDistance> opVis = new OPTICSPlotVisualizer<DoubleDistance>();
+      // setup parameters.
+      try {
+        opVis.setParameters(oparams);
+      }
+      catch(ParameterException e) {
+        LoggingUtil.exception("Error setting parameters for optics plot visualizer.", e);
+      }
+      opVis.init(context, co);
+      usableVisualizers.add(opVis);
     }
     return usableVisualizers;
   }
