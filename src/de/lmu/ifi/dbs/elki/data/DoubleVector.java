b2da12cb6147c7d16024883bd9d28eb8770eedfc
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-12-08 09:36:09 +0000

    Performance tuning: less copies!

--- src/de/lmu/ifi/dbs/elki/data/DoubleVector.java
+++ src/de/lmu/ifi/dbs/elki/data/DoubleVector.java
@@ -9,33 +9,45 @@ import java.util.Random;
 
 /**
  * A DoubleVector is to store real values approximately as double values.
- *
+ * 
  * @author Arthur Zimek
  */
-public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
-
+public class DoubleVector extends AbstractNumberVector<DoubleVector, Double> {
   /**
    * Keeps the values of the real vector
    */
   private double[] values;
 
   /**
+   * Private constructor. NOT for public use.
+   */
+  private DoubleVector(double[] values, boolean nocopy) {
+    if(nocopy) {
+      this.values = values;
+    }
+    else {
+      this.values = new double[values.length];
+      System.arraycopy(values, 0, this.values, 0, values.length);
+    }
+  }
+
+  /**
    * Provides a feature vector consisting of double values according to the
    * given Double values.
-   *
+   * 
    * @param values the values to be set as values of the real vector
    */
   public DoubleVector(List<Double> values) {
     int i = 0;
     this.values = new double[values.size()];
-    for (Iterator<Double> iter = values.iterator(); iter.hasNext(); i++) {
+    for(Iterator<Double> iter = values.iterator(); iter.hasNext(); i++) {
       this.values[i] = (iter.next());
     }
   }
 
   /**
    * Provides a DoubleVector consisting of the given double values.
-   *
+   * 
    * @param values the values to be set as values of theDoubleVector
    */
   public DoubleVector(double[] values) {
@@ -45,25 +57,24 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
 
   /**
    * Provides a DoubleVector consisting of the given double values.
-   *
+   * 
    * @param values the values to be set as values of theDoubleVector
    */
   public DoubleVector(Double[] values) {
     this.values = new double[values.length];
-    //noinspection ManualArrayCopy
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       this.values[i] = values[i];
     }
   }
 
   /**
    * Expects a matrix of one column.
-   *
+   * 
    * @param columnMatrix a matrix of one column
    */
   public DoubleVector(Matrix columnMatrix) {
     values = new double[columnMatrix.getRowDimensionality()];
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       values[i] = columnMatrix.get(i, 0);
     }
   }
@@ -75,7 +86,7 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   public DoubleVector newInstance(Vector values) {
     return new DoubleVector(values);
   }
-  
+
   /**
    * @return a new DoubleVector with the specified values
    */
@@ -83,7 +94,7 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   public DoubleVector newInstance(Double[] values) {
     return new DoubleVector(values);
   }
-  
+
   /**
    * @return a new DoubleVector with the specified values
    */
@@ -91,8 +102,6 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   public DoubleVector newInstance(double[] values) {
     return new DoubleVector(values);
   }
-  
-  
 
   public DoubleVector newInstance(List<Double> values) {
     return new DoubleVector(values);
@@ -100,38 +109,38 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
 
   /**
    * Returns a new DoubleVector with random values between 0 and 1.
-   *
    */
   public DoubleVector randomInstance(Random random) {
     double[] randomValues = new double[getDimensionality()];
-    for (int i = 0; i < randomValues.length; i++) {
-      //int multiplier = random.nextBoolean() ? 1 : -1;
-      //randomValues[i] = random.nextDouble() * Double.MAX_VALUE * multiplier;
+    for(int i = 0; i < randomValues.length; i++) {
+      // int multiplier = random.nextBoolean() ? 1 : -1;
+      // randomValues[i] = random.nextDouble() * Double.MAX_VALUE * multiplier;
       randomValues[i] = random.nextDouble();
     }
-    return new DoubleVector(randomValues);
+    return new DoubleVector(randomValues, true);
   }
 
   public DoubleVector randomInstance(Double min, Double max, Random random) {
     double[] randomValues = new double[getDimensionality()];
-    for (int i = 0; i < randomValues.length; i++) {
+    for(int i = 0; i < randomValues.length; i++) {
       randomValues[i] = random.nextDouble() * (max - min) + min;
     }
-    return new DoubleVector(randomValues);
+    return new DoubleVector(randomValues, true);
   }
-  
+
   /**
    * 
-   * @see de.lmu.ifi.dbs.elki.data.NumberVector#randomInstance(de.lmu.ifi.dbs.elki.data.NumberVector, de.lmu.ifi.dbs.elki.data.NumberVector, java.util.Random)
+   * @see de.lmu.ifi.dbs.elki.data.NumberVector#randomInstance(de.lmu.ifi.dbs.elki.data.NumberVector,
+   *      de.lmu.ifi.dbs.elki.data.NumberVector, java.util.Random)
    */
   public DoubleVector randomInstance(DoubleVector min, DoubleVector max, Random random) {
     double[] randomValues = new double[getDimensionality()];
-    for (int i = 0; i < randomValues.length; i++) {
-      randomValues[i] = random.nextDouble() * (max.getValue(i+1) - min.getValue(i+1)) + min.getValue(i+1);
+    for(int i = 0; i < randomValues.length; i++) {
+      randomValues[i] = random.nextDouble() * (max.getValue(i + 1) - min.getValue(i + 1)) + min.getValue(i + 1);
     }
-    return new DoubleVector(randomValues);
+    return new DoubleVector(randomValues, true);
   }
-  
+
   public int getDimensionality() {
     return values.length;
   }
@@ -139,15 +148,19 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   /**
    * Returns the value of the specified attribute.
    * 
-   * @param dimension the selected attribute. Attributes are counted starting with 1.
+   * @param dimension the selected attribute. Attributes are counted starting
+   *        with 1.
    * 
-   * @throws IllegalArgumentException if the specified dimension is out of range of the possible attributes
+   * @throws IllegalArgumentException if the specified dimension is out of range
+   *         of the possible attributes
    */
   public Double getValue(int dimension) {
-    if (dimension < 1 || dimension > values.length) {
+    try {
+      return values[dimension - 1];
+    }
+    catch(IndexOutOfBoundsException e) {
       throw new IllegalArgumentException("Dimension " + dimension + " out of range.");
     }
-    return values[dimension - 1];
   }
 
   /**
@@ -166,33 +179,33 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   }
 
   public Matrix getRowVector() {
-    return new Matrix(new double[][]{values.clone()});
+    return new Matrix(new double[][] { values.clone() });
   }
-  
+
   public DoubleVector plus(DoubleVector fv) {
-    if (fv.getDimensionality() != this.getDimensionality()) {
+    if(fv.getDimensionality() != this.getDimensionality()) {
       throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
     }
     double[] values = new double[this.values.length];
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       values[i] = this.values[i] + fv.getValue(i + 1);
     }
-    return new DoubleVector(values);
+    return new DoubleVector(values, true);
   }
 
   public DoubleVector minus(DoubleVector fv) {
-    if (fv.getDimensionality() != this.getDimensionality()) {
+    if(fv.getDimensionality() != this.getDimensionality()) {
       throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
     }
     double[] values = new double[this.values.length];
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       values[i] = this.values[i] - fv.getValue(i + 1);
     }
-    return new DoubleVector(values);
+    return new DoubleVector(values, true);
   }
 
   public DoubleVector nullVector() {
-    return new DoubleVector(new double[this.values.length]);
+    return new DoubleVector(new double[this.values.length], true);
   }
 
   public DoubleVector negativeVector() {
@@ -201,24 +214,27 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
 
   public DoubleVector multiplicate(double k) {
     double[] values = new double[this.values.length];
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       values[i] = this.values[i] * k;
     }
-    return new DoubleVector(values);
+    return new DoubleVector(values, true);
   }
-  
+
   /**
-   * Provides the scalar product (inner product) of this and the given DoubleVector.
+   * Provides the scalar product (inner product) of this and the given
+   * DoubleVector.
+   * 
    * @param d the DoubleVector to compute the scalar product for
-   * @return the scalar product (inner product) of this and the given DoubleVector
+   * @return the scalar product (inner product) of this and the given
+   *         DoubleVector
    */
-  public Double scalarProduct(DoubleVector d){
-    if(this.getDimensionality()!=d.getDimensionality()){
+  public Double scalarProduct(DoubleVector d) {
+    if(this.getDimensionality() != d.getDimensionality()) {
       throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + d.getDimensionality() + ".");
     }
     double result = 0.0;
-    for(int i = 0; i < this.getDimensionality(); i++){
-      result += this.values[i]*d.values[i];
+    for(int i = 0; i < this.getDimensionality(); i++) {
+      result += this.values[i] * d.values[i];
     }
     return result;
   }
@@ -226,14 +242,12 @@ public class DoubleVector extends AbstractNumberVector<DoubleVector,Double> {
   @Override
   public String toString() {
     StringBuffer featureLine = new StringBuffer();
-    for (int i = 0; i < values.length; i++) {
+    for(int i = 0; i < values.length; i++) {
       featureLine.append(values[i]);
-      if (i + 1 < values.length) {
+      if(i + 1 < values.length) {
         featureLine.append(ATTRIBUTE_SEPARATOR);
       }
     }
     return featureLine.toString();
   }
-
-  
 }
--- src/de/lmu/ifi/dbs/elki/data/FloatVector.java
+++ src/de/lmu/ifi/dbs/elki/data/FloatVector.java
@@ -10,203 +10,218 @@ import java.util.Random;
 
 /**
  * A FloatVector is to store real values approximately as float values.
- *
+ * 
  * @author Elke Achtert
  */
 public class FloatVector extends AbstractNumberVector<FloatVector, Float> {
 
-    /**
-     * Keeps the values of the float vector
-     */
-    private float[] values;
+  /**
+   * Keeps the values of the float vector
+   */
+  private float[] values;
 
-    /**
-     * Provides a FloatVector consisting of float values according to the
-     * given Float values.
-     *
-     * @param values the values to be set as values of the float vector
-     */
-    public FloatVector(List<Float> values) {
-        int i = 0;
-        this.values = new float[values.size()];
-        for (Iterator<Float> iter = values.iterator(); iter.hasNext(); i++) {
-            this.values[i] = (iter.next());
-        }
+  /**
+   * Private constructor. NOT for public use.
+   */
+  private FloatVector(float[] values, boolean nocopy) {
+    if(nocopy) {
+      this.values = values;
     }
-
-    /**
-     * Provides a FloatVector consisting of the given float values.
-     *
-     * @param values the values to be set as values of the float vector
-     */
-    public FloatVector(float[] values) {
-        this.values = new float[values.length];
-        System.arraycopy(values, 0, this.values, 0, values.length);
+    else {
+      this.values = new float[values.length];
+      System.arraycopy(values, 0, this.values, 0, values.length);
     }
-
-    /**
-     * Provides a FloatVector consisting of the given float values.
-     *
-     * @param values the values to be set as values of the float vector
-     */
-    public FloatVector(Float[] values) {
-        this.values = new float[values.length];
-        for (int i = 0; i < values.length; i++) {
-            this.values[i] = values[i];
-        }
-    }
-
-    /**
-     * Expects a matrix of one column.
-     *
-     * @param columnMatrix a matrix of one column
-     */
-    public FloatVector(Matrix columnMatrix) {
-      values = new float[columnMatrix.getRowDimensionality()];
-      for (int i = 0; i < values.length; i++) {
-        values[i] = (float) columnMatrix.get(i, 0);
-      }
-    }
-
-    /**
-     * @return a new FloatVector with the specified values
-     */
-    @Override
-    public FloatVector newInstance(Vector values) {
-        return new FloatVector(values);
-    }
-    
-    /**
-     * @return a new FloatVector with the specified values
-     */
-    @Override
-    public FloatVector newInstance(double[] values) {
-        return new FloatVector(Util.convertToFloat(values));
-    }
-    
-    public FloatVector newInstance(Float[] values) {
-      return new FloatVector(values);
-    }
-
-    public FloatVector newInstance(List<Float> values) {
-      return new FloatVector(values);
-    }
-
-    /**
-     * @return a new FloatVector with random values between 0 and 1
-     */
-    public FloatVector randomInstance(Random random) {
-        float[] randomValues = new float[getDimensionality()];
-        for (int i = 0; i < randomValues.length; i++) {
-            randomValues[i] = random.nextFloat();
-        }
-        return new FloatVector(randomValues);
-    }
-
-    public FloatVector randomInstance(Float min, Float max, Random random) {
-        float[] randomValues = new float[getDimensionality()];
-        for (int i = 0; i < randomValues.length; i++) {
-            randomValues[i] = random.nextFloat() * (max - min) + min;
-        }
-        return new FloatVector(randomValues);
-    }
-
-    /**
-     * 
-     * @see de.lmu.ifi.dbs.elki.data.NumberVector#randomInstance(de.lmu.ifi.dbs.elki.data.NumberVector, de.lmu.ifi.dbs.elki.data.NumberVector, java.util.Random)
-     */
-    public FloatVector randomInstance(FloatVector min, FloatVector max, Random random) {
-        float[] randomValues = new float[getDimensionality()];
-        for (int i = 0; i < randomValues.length; i++) {
-            randomValues[i] = random.nextFloat() * (max.getValue(i+1) - min.getValue(i+1)) + min.getValue(i+1);
-        }
-        return new FloatVector(randomValues);
-    }
-    
-    public int getDimensionality() {
-        return values.length;
-    }
-
-    public Float getValue(int dimension) {
-        if (dimension < 1 || dimension > values.length) {
-            throw new IllegalArgumentException("Dimension " + dimension + " out of range.");
-        }
-        return values[dimension - 1];
-    }
-
-    public Vector getColumnVector() {
-        return new Vector(Util.convertToDoubles(values));
-    }
-
-    public Matrix getRowVector() {
-        return new Matrix(new double[][]{Util.convertToDoubles(values)});
-    }
-
-    public FloatVector plus(FloatVector fv) {
-        if (fv.getDimensionality() != this.getDimensionality()) {
-            throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
-        }
-        float[] values = new float[this.values.length];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = this.values[i] + fv.getValue(i + 1);
-        }
-        return new FloatVector(values);
-    }
-    
-    public FloatVector minus(FloatVector fv) {
-      if (fv.getDimensionality() != this.getDimensionality()) {
-          throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
-      }
-      float[] values = new float[this.values.length];
-      for (int i = 0; i < values.length; i++) {
-          values[i] = this.values[i] - fv.getValue(i + 1);
-      }
-      return new FloatVector(values);
   }
-  
-    /**
-     * Provides the scalar product (inner product) of this and the given FloatVector.
-     * @param f the FloatVector to compute the scalar product for
-     * @return the scalar product (inner product) of this and the given FloatVector
-     */
-    public Float scalarProduct(FloatVector f){
-      if(this.getDimensionality()!=f.getDimensionality()){
-        throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + f.getDimensionality() + ".");
+
+  /**
+   * Provides a FloatVector consisting of float values according to the given
+   * Float values.
+   * 
+   * @param values the values to be set as values of the float vector
+   */
+  public FloatVector(List<Float> values) {
+    int i = 0;
+    this.values = new float[values.size()];
+    for(Iterator<Float> iter = values.iterator(); iter.hasNext(); i++) {
+      this.values[i] = (iter.next());
+    }
+  }
+
+  /**
+   * Provides a FloatVector consisting of the given float values.
+   * 
+   * @param values the values to be set as values of the float vector
+   */
+  public FloatVector(float[] values) {
+    this.values = new float[values.length];
+    System.arraycopy(values, 0, this.values, 0, values.length);
+  }
+
+  /**
+   * Provides a FloatVector consisting of the given float values.
+   * 
+   * @param values the values to be set as values of the float vector
+   */
+  public FloatVector(Float[] values) {
+    this.values = new float[values.length];
+    for(int i = 0; i < values.length; i++) {
+      this.values[i] = values[i];
+    }
+  }
+
+  /**
+   * Expects a matrix of one column.
+   * 
+   * @param columnMatrix a matrix of one column
+   */
+  public FloatVector(Matrix columnMatrix) {
+    values = new float[columnMatrix.getRowDimensionality()];
+    for(int i = 0; i < values.length; i++) {
+      values[i] = (float) columnMatrix.get(i, 0);
+    }
+  }
+
+  /**
+   * @return a new FloatVector with the specified values
+   */
+  @Override
+  public FloatVector newInstance(Vector values) {
+    return new FloatVector(values);
+  }
+
+  /**
+   * @return a new FloatVector with the specified values
+   */
+  @Override
+  public FloatVector newInstance(double[] values) {
+    return new FloatVector(Util.convertToFloat(values));
+  }
+
+  public FloatVector newInstance(Float[] values) {
+    return new FloatVector(values);
+  }
+
+  public FloatVector newInstance(List<Float> values) {
+    return new FloatVector(values);
+  }
+
+  /**
+   * @return a new FloatVector with random values between 0 and 1
+   */
+  public FloatVector randomInstance(Random random) {
+    float[] randomValues = new float[getDimensionality()];
+    for(int i = 0; i < randomValues.length; i++) {
+      randomValues[i] = random.nextFloat();
+    }
+    return new FloatVector(randomValues, true);
+  }
+
+  public FloatVector randomInstance(Float min, Float max, Random random) {
+    float[] randomValues = new float[getDimensionality()];
+    for(int i = 0; i < randomValues.length; i++) {
+      randomValues[i] = random.nextFloat() * (max - min) + min;
+    }
+    return new FloatVector(randomValues, true);
+  }
+
+  /**
+   * 
+   * @see de.lmu.ifi.dbs.elki.data.NumberVector#randomInstance(de.lmu.ifi.dbs.elki.data.NumberVector,
+   *      de.lmu.ifi.dbs.elki.data.NumberVector, java.util.Random)
+   */
+  public FloatVector randomInstance(FloatVector min, FloatVector max, Random random) {
+    float[] randomValues = new float[getDimensionality()];
+    for(int i = 0; i < randomValues.length; i++) {
+      randomValues[i] = random.nextFloat() * (max.getValue(i + 1) - min.getValue(i + 1)) + min.getValue(i + 1);
+    }
+    return new FloatVector(randomValues, true);
+  }
+
+  public int getDimensionality() {
+    return values.length;
+  }
+
+  public Float getValue(int dimension) {
+    if(dimension < 1 || dimension > values.length) {
+      throw new IllegalArgumentException("Dimension " + dimension + " out of range.");
+    }
+    return values[dimension - 1];
+  }
+
+  public Vector getColumnVector() {
+    return new Vector(Util.convertToDoubles(values));
+  }
+
+  public Matrix getRowVector() {
+    return new Matrix(new double[][] { Util.convertToDoubles(values) });
+  }
+
+  public FloatVector plus(FloatVector fv) {
+    if(fv.getDimensionality() != this.getDimensionality()) {
+      throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
+    }
+    float[] values = new float[this.values.length];
+    for(int i = 0; i < values.length; i++) {
+      values[i] = this.values[i] + fv.getValue(i + 1);
+    }
+    return new FloatVector(values, true);
+  }
+
+  public FloatVector minus(FloatVector fv) {
+    if(fv.getDimensionality() != this.getDimensionality()) {
+      throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + fv.getDimensionality() + ".");
+    }
+    float[] values = new float[this.values.length];
+    for(int i = 0; i < values.length; i++) {
+      values[i] = this.values[i] - fv.getValue(i + 1);
+    }
+    return new FloatVector(values, true);
+  }
+
+  /**
+   * Provides the scalar product (inner product) of this and the given
+   * FloatVector.
+   * 
+   * @param f the FloatVector to compute the scalar product for
+   * @return the scalar product (inner product) of this and the given
+   *         FloatVector
+   */
+  public Float scalarProduct(FloatVector f) {
+    if(this.getDimensionality() != f.getDimensionality()) {
+      throw new IllegalArgumentException("Incompatible dimensionality: " + this.getDimensionality() + " - " + f.getDimensionality() + ".");
+    }
+    float result = 0.0f;
+    for(int i = 0; i < this.getDimensionality(); i++) {
+      result += this.values[i] * f.values[i];
+    }
+    return result;
+  }
+
+  public FloatVector nullVector() {
+    return new FloatVector(new float[this.values.length], true);
+  }
+
+  public FloatVector negativeVector() {
+    return multiplicate(-1);
+  }
+
+  public FloatVector multiplicate(double k) {
+    float[] values = new float[this.values.length];
+    for(int i = 0; i < values.length; i++) {
+      values[i] = (float) (this.values[i] * k);
+    }
+    return new FloatVector(values, true);
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer featureLine = new StringBuffer();
+    for(int i = 0; i < values.length; i++) {
+      featureLine.append(values[i]);
+      if(i + 1 < values.length) {
+        featureLine.append(ATTRIBUTE_SEPARATOR);
       }
-      float result = 0.0f;
-      for(int i = 0; i < this.getDimensionality(); i++){
-        result += this.values[i]*f.values[i];
-      }
-      return result;
     }
-
-    public FloatVector nullVector() {
-        return new FloatVector(new float[this.values.length]);
-    }
-
-    public FloatVector negativeVector() {
-        return multiplicate(-1);
-    }
-
-    public FloatVector multiplicate(double k) {
-        float[] values = new float[this.values.length];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = (float) (this.values[i] * k);
-        }
-        return new FloatVector(values);
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer featureLine = new StringBuffer();
-        for (int i = 0; i < values.length; i++) {
-            featureLine.append(values[i]);
-            if (i + 1 < values.length) {
-                featureLine.append(ATTRIBUTE_SEPARATOR);
-            }
-        }
-        return featureLine.toString();
-    }
-
-    
+    return featureLine.toString();
+  }
 }
