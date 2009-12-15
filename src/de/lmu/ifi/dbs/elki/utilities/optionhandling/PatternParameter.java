40194111c7b939ff40c71cb370aa9be8917b946f
Erich Schubert <schube@dbs.ifi.lmu.de>
2009-12-15 16:17:00 +0000

    Add optional boolean to string parameters

--- src/de/lmu/ifi/dbs/elki/utilities/optionhandling/PatternParameter.java
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/PatternParameter.java
@@ -8,78 +8,86 @@ import java.util.ArrayList;
 
 /**
  * Parameter class for a parameter specifying a pattern.
- *
+ * 
  * @author Steffi Wanka
  */
 public class PatternParameter extends Parameter<String, String> {
+  /**
+   * Constructs a pattern parameter with the given optionID.
+   * 
+   * @param optionID the unique id of the parameter
+   */
+  public PatternParameter(OptionID optionID) {
+    super(optionID);
+  }
 
-    /**
-     * Constructs a pattern parameter with the given optionID.
-     *
-     * @param optionID the unique id of the parameter
-     */
-    public PatternParameter(OptionID optionID) {
-        super(optionID);
-    }
+  /**
+   * Constructs a pattern parameter with the given optionID.
+   * 
+   * @param optionID the unique id of the parameter
+   * @param optional Flag to signal an optional parameter.
+   */
+  public PatternParameter(OptionID optionID, boolean optional) {
+    super(optionID, optional);
+  }
 
-    /**
-     * Constructs a pattern parameter with the given optionID, and default value.
-     *
-     * @param optionID the unique id of the parameter
-     * @param defaultValue the default value of the parameter
-     */
-    public PatternParameter(OptionID optionID, String defaultValue) {
-        super(optionID, new ArrayList<ParameterConstraint<String>>(), false, defaultValue);
-    }
+  /**
+   * Constructs a pattern parameter with the given optionID, and default value.
+   * 
+   * @param optionID the unique id of the parameter
+   * @param defaultValue the default value of the parameter
+   */
+  public PatternParameter(OptionID optionID, String defaultValue) {
+    super(optionID, new ArrayList<ParameterConstraint<String>>(), false, defaultValue);
+  }
 
-    /**
-     * Constructs a pattern parameter with the given optionID, constraints and default value.
-     *
-     * @param optionID the unique id of the parameter
-     * @param constraint parameter constraint
-     * @param defaultValue the default value of the parameter
-     */
-    public PatternParameter(OptionID optionID, ParameterConstraint<String> constraint, String defaultValue) {
-        this(optionID, defaultValue);
-        addConstraint(constraint);
-    }
+  /**
+   * Constructs a pattern parameter with the given optionID, constraints and
+   * default value.
+   * 
+   * @param optionID the unique id of the parameter
+   * @param constraint parameter constraint
+   * @param defaultValue the default value of the parameter
+   */
+  public PatternParameter(OptionID optionID, ParameterConstraint<String> constraint, String defaultValue) {
+    this(optionID, defaultValue);
+    addConstraint(constraint);
+  }
 
-    @Override
-    public void setValue(String value) throws ParameterException {
-        super.setValue(value);
-        if (isValid(value)) {
-            this.value = value;
-        }
+  @Override
+  public void setValue(String value) throws ParameterException {
+    super.setValue(value);
+    if(isValid(value)) {
+      this.value = value;
     }
+  }
 
-    @Override
-    public boolean isValid(String value) throws ParameterException {
-        try {
-            Pattern.compile(value);
-        }
-        catch (PatternSyntaxException e) {
-            throw new WrongParameterValueException("Given pattern \"" + value + "\" for parameter \"" + getName()
-                + "\" is no valid regular expression!");
-        }
-        try {
-            for (ParameterConstraint<String> con : this.constraints) {
-                con.test(value);
-            }
-        }
-        catch (ParameterException ex) {
-            throw new WrongParameterValueException("Parameter constraint error for pattern" +
-                " parameter " + getName() + ": " + ex.getMessage());
-        }
-        return true;
+  @Override
+  public boolean isValid(String value) throws ParameterException {
+    try {
+      Pattern.compile(value);
     }
+    catch(PatternSyntaxException e) {
+      throw new WrongParameterValueException("Given pattern \"" + value + "\" for parameter \"" + getName() + "\" is no valid regular expression!");
+    }
+    try {
+      for(ParameterConstraint<String> con : this.constraints) {
+        con.test(value);
+      }
+    }
+    catch(ParameterException ex) {
+      throw new WrongParameterValueException("Parameter constraint error for pattern" + " parameter " + getName() + ": " + ex.getMessage());
+    }
+    return true;
+  }
 
-    /**
-     * Returns a string representation of the parameter's type.
-     *
-     * @return &quot;&lt;pattern&gt;&quot;
-     */
-    @Override
-    public String getSyntax() {
-        return "<pattern>";
-    }
+  /**
+   * Returns a string representation of the parameter's type.
+   * 
+   * @return &quot;&lt;pattern&gt;&quot;
+   */
+  @Override
+  public String getSyntax() {
+    return "<pattern>";
+  }
 }
