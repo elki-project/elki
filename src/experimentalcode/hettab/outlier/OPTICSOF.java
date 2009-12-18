06af2c4174d57264d63cdc0540cfdc31c2ed0ceb
Ahmed Hettab <hettab@cip.ifi.lmu.de>
2009-12-18 10:27:12 +0000

    OPTICSOF

--- src/experimentalcode/hettab/outlier/OPTICSOF.java
+++ src/experimentalcode/hettab/outlier/OPTICSOF.java
@@ -1,20 +1,28 @@
 package experimentalcode.hettab.outlier;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-
 import de.lmu.ifi.dbs.elki.algorithm.DistanceBasedAlgorithm;
 import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.database.AssociationID;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.database.DistanceResultPair;
-import de.lmu.ifi.dbs.elki.distance.Distance;
-import de.lmu.ifi.dbs.elki.result.ClusterOrderResult;
+import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
+import de.lmu.ifi.dbs.elki.math.MinMax;
+import de.lmu.ifi.dbs.elki.result.AnnotationFromHashMap;
+import de.lmu.ifi.dbs.elki.result.AnnotationResult;
 import de.lmu.ifi.dbs.elki.result.MultiResult;
+import de.lmu.ifi.dbs.elki.result.OrderingFromHashMap;
+import de.lmu.ifi.dbs.elki.result.OrderingResult;
+import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
+import de.lmu.ifi.dbs.elki.result.outlier.OutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.result.outlier.QuotientOutlierScoreMeta;
 import de.lmu.ifi.dbs.elki.utilities.Description;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.IntParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
-import de.lmu.ifi.dbs.elki.utilities.optionhandling.PatternParameter;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterConstraint;
 
 /**
@@ -24,26 +32,9 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterConstrain
  * @param <O>
  * @param <D>
  */
-public class OPTICSOF<O extends DatabaseObject, D extends Distance<D>> extends DistanceBasedAlgorithm<O, D, MultiResult> {
+public class OPTICSOF<O extends DatabaseObject> extends DistanceBasedAlgorithm<O, DoubleDistance, MultiResult> {
 
-	 /**
-	   * OptionID for {@link #EPSILON_PARAM}
-	   */
-	  public static final OptionID EPSILON_ID = OptionID.getOrCreateOptionID("optics.epsilon", "The maximum radius of the neighborhood to be considered.");
-
-	  /**
-	   * Parameter to specify the maximum radius of the neighborhood to be
-	   * considered, must be suitable to the distance function specified.
-	   * <p>
-	   * Key: {@code -optics.epsilon}
-	   * </p>
-	   */
-	  private final PatternParameter EPSILON_PARAM = new PatternParameter(EPSILON_ID);
-
-	  /**
-	   * Hold the value of {@link #EPSILON_PARAM}.
-	   */
-	  private String epsilon;
+	 
 
 	  /**
 	   * OptionID for {@link #MINPTS_PARAM}
@@ -68,14 +59,21 @@ public class OPTICSOF<O extends DatabaseObject, D extends Distance<D>> extends D
 	   * Provides the result of the algorithm.
 	   */
 	  private MultiResult result;
+	  
+	 /**
+	  * 
+	  */
+	  public static final AssociationID<Double> OF_SCORE = AssociationID.getOrCreateAssociationID("of", Double.class);
 
-	  /**
-	   * Holds a set of processed ids.
-	   */
-	  private Set<Integer> processedIDs;
+
+    public OPTICSOF() {
+		super();
+	    // parameter minpts
+	    addOption(MINPTS_PARAM);
+	}
 
 
-    /**
+	/**
      *
      */
 	public Description getDescription() {
@@ -83,28 +81,93 @@ public class OPTICSOF<O extends DatabaseObject, D extends Distance<D>> extends D
 		return null;
 	}
 
-	/**
-	 *
-	 */
-	public ClusterOrderResult<D> getResult() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	/**
 	 *
 	 */
 	@Override
 	protected MultiResult runInTime(Database<O> database)
 			throws IllegalStateException {
-		HashMap<Integer, List<DistanceResultPair<D>>> nMinPts = new HashMap<Integer, List<DistanceResultPair<D>>>();
-		HashMap<Integer, Double> coreDist = new HashMap<Integer, Double>();
-
+		
+		 getDistanceFunction().setDatabase(database, isVerbose(), isTime());
+		 
+		HashMap<Integer,List<DistanceResultPair<DoubleDistance>>> nMinPts = new HashMap<Integer, List<DistanceResultPair<DoubleDistance>>>();
+		HashMap<Integer,Double> coreDistance = new HashMap<Integer,Double>();
+		
+		
+		// Pass 1 
+		//N_minpts(id) and core-distance(id)
 		for(Integer id : database){
-			List<DistanceResultPair<D>> neighbors = database.kNNQueryForID(id, minpts, getDistanceFunction());
-			nMinPts.put(id,neighbors);
-					}
-		return null;
+			 List<DistanceResultPair<DoubleDistance>> minptsNegibours =  database.kNNQueryForID(id, minpts, getDistanceFunction());
+			 double d  = minptsNegibours.get(minptsNegibours.size()-1).getDistance().doubleValue();
+		     nMinPts.put(id,minptsNegibours);
+		     coreDistance.put(id,d);
+		 }
+		
+		// Pass 2 
+		//
+		HashMap<Integer,List<Double>> reachDistance = new HashMap<Integer, List<Double>>();
+		HashMap<Integer,Double> lrds = new HashMap<Integer,Double>();
+		for(Integer id : database){
+			List<Double> core = new ArrayList<Double>();
+			double lrd = 0 ;
+			for(DistanceResultPair<DoubleDistance> neighPair : nMinPts.get(id)){
+				int idN = neighPair.getID();
+				double coreDist = coreDistance.get(idN);
+				double dist = getDistanceFunction().distance(id,idN).doubleValue();
+				Double rd = Math.max(coreDist, dist);
+				lrd = rd + lrd ;
+				core.add(rd);
+			}
+			lrd = (minpts/lrd);
+			reachDistance.put(id,core);
+			lrds.put(id, lrd);
+			
+		}
+		
+		//Pass 3 
+		 MinMax<Double> ofminmax = new MinMax<Double>();
+		HashMap<Integer,Double> ofs = new HashMap<Integer,Double>() ;
+		for(Integer id : database){
+			double of = 0 ;
+			for(DistanceResultPair<DoubleDistance> pair : nMinPts.get(id)){
+				int idN = pair.getID();
+				double lrd = lrds.get(id);
+				double lrdN = lrds.get(idN);
+				of = of + lrdN/lrd ;
+			}
+			of = of/minpts ;
+			ofs.put(id,of);
+			 // update minimum and maximum
+	        ofminmax.put(of);
+			
+		}
+		 // Build result representation.
+		AnnotationResult<Double> scoreResult = new AnnotationFromHashMap<Double>(OF_SCORE, ofs);
+	    OrderingResult orderingResult = new OrderingFromHashMap<Double>(ofs, false);
+	    OutlierScoreMeta scoreMeta = new QuotientOutlierScoreMeta(ofminmax.getMin(), ofminmax.getMax(), 0.0, Double.POSITIVE_INFINITY, 1.0);
+	    this.result = new OutlierResult(scoreMeta, scoreResult, orderingResult);
+
+	    return result;
 	}
+
+
+	@Override
+	public MultiResult getResult() {
+		return result;
+	}
+	
+	/**
+	   * Calls the super method and sets additionally the values of the parameters
+	   * {@link #EPSILON_PARAM} and {@link #MINPTS_PARAM}.
+	   */
+	  @Override
+	  public List<String> setParameters(List<String> args) throws ParameterException {
+	    List<String> remainingParameters = super.setParameters(args);
+	    // minpts
+	    minpts = MINPTS_PARAM.getValue();
+
+	    return remainingParameters;
+	  }
 
 }
