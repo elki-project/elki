235e50c6f7af0e3c51bf684a5c458790a24e6d2f
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-01-04 16:41:19 +0000

    view box calculation.

--- src/de/lmu/ifi/dbs/elki/visualization/visualizers/vis1d/Projection1DVisualizer.java
+++ src/de/lmu/ifi/dbs/elki/visualization/visualizers/vis1d/Projection1DVisualizer.java
@@ -13,7 +13,7 @@ import de.lmu.ifi.dbs.elki.visualization.visualizers.ProjectedVisualizer;
  * Produces visualizations of 1-dimensional projections.
  * 
  * @author Remigius Wojdanowski
- *
+ * 
  * @param <NV> Type of the NumberVector being visualized.
  */
 public abstract class Projection1DVisualizer<NV extends NumberVector<NV, ?>> extends AbstractVisualizer implements ProjectedVisualizer {
@@ -26,9 +26,13 @@ public abstract class Projection1DVisualizer<NV extends NumberVector<NV, ?>> ext
   public Element setupCanvas(SVGPlot svgp, double width, double height) {
     Element layer = SVGUtil.svgElement(svgp.getDocument(), SVGConstants.SVG_SVG_TAG);
     // Some default for the view box.
-    double xmargin = 0.2 * width;
-    double ymargin = 0.2 * height;
-    SVGUtil.setAtt(layer, SVGConstants.SVG_VIEW_BOX_ATTRIBUTE, "-1.2 -1.2 2.4 2.4");    
+    double xmargin = 0.1 * width;
+    double ymargin = 0.1 * height;
+    double left = -(width / 2 + xmargin);
+    double top = -(height / 2 + ymargin);
+    double vwidth = width + 2 * xmargin;
+    double vheight = height + 2 * ymargin;
+    SVGUtil.setAtt(layer, SVGConstants.SVG_VIEW_BOX_ATTRIBUTE, left + " " + top + " " + vwidth + " " + vheight);
     return layer;
   }
 }
