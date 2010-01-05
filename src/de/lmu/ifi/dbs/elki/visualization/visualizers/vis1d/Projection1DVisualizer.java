de0db2965b9f9a8b97fec08db4247734a6bbadf1
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-01-05 09:52:26 +0000

    fix histogram viewbox

--- src/de/lmu/ifi/dbs/elki/visualization/visualizers/vis1d/Projection1DVisualizer.java
+++ src/de/lmu/ifi/dbs/elki/visualization/visualizers/vis1d/Projection1DVisualizer.java
@@ -26,12 +26,12 @@ public abstract class Projection1DVisualizer<NV extends NumberVector<NV, ?>> ext
   public Element setupCanvas(SVGPlot svgp, double width, double height) {
     Element layer = SVGUtil.svgElement(svgp.getDocument(), SVGConstants.SVG_SVG_TAG);
     // Some default for the view box.
-    double xmargin = 0.1 * width;
-    double ymargin = 0.1 * height;
-    double left = -(width / 2 + xmargin);
-    double top = -(height / 2 + ymargin);
-    double vwidth = width + 2 * xmargin;
-    double vheight = height + 2 * ymargin;
+    double ratio = width / height;
+    double left = -1.2 * ratio;
+    double top = -1.1;
+    double vwidth = 2.3 * ratio;
+    double vheight = 2.2;
+    // TODO: use transform() instead.
     SVGUtil.setAtt(layer, SVGConstants.SVG_VIEW_BOX_ATTRIBUTE, left + " " + top + " " + vwidth + " " + vheight);
     return layer;
   }
