17f650e3ea6325605a21e177388b4844cdb5d554
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-02-08 16:21:17 +0000

    JavaDoc

--- src/experimentalcode/erich/newdblayer/DBID.java
+++ src/experimentalcode/erich/newdblayer/DBID.java
@@ -5,11 +5,11 @@ package experimentalcode.erich.newdblayer;
  * 
  * While this currently is just an Integer, it should be avoided to store the
  * object IDs in regular integers to reduce problems if this API ever changes
- * (for example if someone needs to support Long, it should not require changes
- * in too many places!)
+ * (for example if someone needs to support {@code long}, it should not require
+ * changes in too many places!)
  * 
  * In particular, a developer should not make any assumption of these IDs being
- * consistent.
+ * consistent across multiple results/databases.
  * 
  * @author Erich Schubert
  */
