ae01f3da8ea18612332e409307314f997886a6ea
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-02-15 17:09:59 +0000

    Add Javadoc package headers.

---
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/parameterization/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <p>Configruation managers</p> 
+ */
+package de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization;
---
+++ src/de/lmu/ifi/dbs/elki/utilities/optionhandling/parameters/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <p>Classes for various typed parameters.</p> 
+ */
+package de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters;
