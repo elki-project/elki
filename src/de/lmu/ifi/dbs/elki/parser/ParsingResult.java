407ecaf31a7afda59148a94d267b9feca5e1d8c8
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-03-02 16:55:23 +0000

    Reformat source code.

--- src/de/lmu/ifi/dbs/elki/parser/ParsingResult.java
+++ src/de/lmu/ifi/dbs/elki/parser/ParsingResult.java
@@ -8,58 +8,53 @@ import java.util.List;
 /**
  * Provides a list of database objects and labels associated with these objects.
  * 
- * @author Elke Achtert 
+ * @author Elke Achtert
  * @param <O> object type
  */
-public class ParsingResult<O extends DatabaseObject>
-{
-    /**
-     * The list of database objects and labels associated with these objects.
-     */
-    private final List<Pair<O,List<String>>> objectAndLabelList;
+public class ParsingResult<O extends DatabaseObject> {
+  /**
+   * The list of database objects and labels associated with these objects.
+   */
+  private final List<Pair<O, List<String>>> objectAndLabelList;
 
-    /**
-     * Provides a list of database objects and labels associated with these
-     * objects.
-     * 
-     * @param objectAndLabelList
-     *            the list of database objects and labels associated with these
-     *            objects
-     */
-    public ParsingResult(List<Pair<O,List<String>>> objectAndLabelList)
-    {
-        this.objectAndLabelList = objectAndLabelList;
-    }
+  /**
+   * Provides a list of database objects and labels associated with these
+   * objects.
+   * 
+   * @param objectAndLabelList the list of database objects and labels
+   *        associated with these objects
+   */
+  public ParsingResult(List<Pair<O, List<String>>> objectAndLabelList) {
+    this.objectAndLabelList = objectAndLabelList;
+  }
 
-    /**
-     * Returns the list of database objects and labels associated with these
-     * objects.
-     * 
-     * @return the list of database objects and labels associated with these
-     *         objects
-     */
-    public List<Pair<O,List<String>>> getObjectAndLabelList()
-    {
-        return objectAndLabelList;
-    }
+  /**
+   * Returns the list of database objects and labels associated with these
+   * objects.
+   * 
+   * @return the list of database objects and labels associated with these
+   *         objects
+   */
+  public List<Pair<O, List<String>>> getObjectAndLabelList() {
+    return objectAndLabelList;
+  }
 
-    /**
-     * Returns a string representation of the object.
-     * 
-     * @return a string representation of the object.
-     */
-    @Override
-    public String toString()
-    {
-        return objectAndLabelList.toString();
-    }
-    
-    /**
-     * Provides the number of objects listed in this parsing result.
-     * 
-     * @return the number of objects listed in this parsing result
-     */
-    public int size(){
-      return this.objectAndLabelList.size();
-    }
+  /**
+   * Returns a string representation of the object.
+   * 
+   * @return a string representation of the object.
+   */
+  @Override
+  public String toString() {
+    return objectAndLabelList.toString();
+  }
+
+  /**
+   * Provides the number of objects listed in this parsing result.
+   * 
+   * @return the number of objects listed in this parsing result
+   */
+  public int size() {
+    return this.objectAndLabelList.size();
+  }
 }
