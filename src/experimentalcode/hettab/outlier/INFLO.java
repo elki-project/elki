19c1a87f9310a4978313be55d1696e8abfb082d1
Ahmed Hettab <hettab@cip.ifi.lmu.de>
2010-03-10 16:25:59 +0000

    Fehlerbehebung: GreaterConstraint

--- src/experimentalcode/hettab/outlier/INFLO.java
+++ src/experimentalcode/hettab/outlier/INFLO.java
@@ -105,10 +105,10 @@ public class INFLO<O extends DatabaseObject> extends DistanceBasedAlgorithm<O, D
   public INFLO(Parameterization config) {
     super(config);
     // parameter minpts
-    if (config.grab(this,K_PARAM)) {
+    if (config.grab(K_PARAM)) {
       k = K_PARAM.getValue(); 
     }
-    if (config.grab(this,M_PARAM)) {
+    if (config.grab(M_PARAM)) {
       m = M_PARAM.getValue(); 
     }
   }
@@ -140,6 +140,7 @@ public class INFLO<O extends DatabaseObject> extends DistanceBasedAlgorithm<O, D
     }
 
     for(Integer id : database) {
+      // if not visited count=0
       int count = rnns.get(id).size();
       Vector<Integer> s;
       if(!processedIDs.contains(id)) {
