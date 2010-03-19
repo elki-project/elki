b0295c1d1b18534545b677730a991b1b1ebee3e7
Elke Achtert <achtert@dbs.ifi.lmu.de>
2010-03-19 21:07:44 +0000

    refactoring JUnitTests and adding new one

--- test/de/lmu/ifi/dbs/elki/algorithm/clustering/subspace/TestCLIQUEResults.java
+++ test/de/lmu/ifi/dbs/elki/algorithm/clustering/subspace/TestCLIQUEResults.java
@@ -14,7 +14,6 @@ import de.lmu.ifi.dbs.elki.data.model.Model;
 import de.lmu.ifi.dbs.elki.data.model.SubspaceModel;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
-import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
 import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
@@ -28,7 +27,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParamet
  * @author Elke Achtert
  * 
  */
-public class TestSUBCLUResults implements JUnit4Test {
+public class TestCLIQUEResults implements JUnit4Test {
   // the following values depend on the data set used!
   String dataset = "data/testdata/unittests/subspace-simple.csv";
 
@@ -36,18 +35,19 @@ public class TestSUBCLUResults implements JUnit4Test {
   int shoulds = 450;
 
   /**
-   * Run SUBCLU with fixed parameters and compare the result to a golden standard.
+   * Run CLIQUE with fixed parameters and compare the result to a golden
+   * standard.
    * 
    * @throws ParameterException
    */
   @Test
-  public void testSUBCLUResults() throws ParameterException {
+  public void testCLIQUEResults() throws ParameterException {
     ListParameterization params = new ListParameterization();
     params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
     // these parameters are not picked too smartly - room for improvement.
-    params.addParameter(SUBCLU.EPSILON_ID, "0.005");
-    params.addParameter(SUBCLU.MINPTS_ID, 20);
-    
+    params.addParameter(CLIQUE.TAU_ID, "0.1");
+    params.addParameter(CLIQUE.XSI_ID, 20);
+
     FileBasedDatabaseConnection<DoubleVector> dbconn = new FileBasedDatabaseConnection<DoubleVector>(params);
 
     // get database
@@ -57,22 +57,22 @@ public class TestSUBCLUResults implements JUnit4Test {
     assertEquals("Database size doesn't match expected size.", shoulds, db.size());
 
     // setup algorithm
-    SUBCLU<DoubleVector, DoubleDistance> subclu = new SUBCLU<DoubleVector, DoubleDistance>(params);
-    subclu.setVerbose(false);
+    CLIQUE<DoubleVector> clique = new CLIQUE<DoubleVector>(params);
+    clique.setVerbose(false);
 
     params.failOnErrors();
-    if (params.hasUnusedParameters()) {
-      fail("Unused parameters: "+params.getRemainingParameters());
+    if(params.hasUnusedParameters()) {
+      fail("Unused parameters: " + params.getRemainingParameters());
     }
-    // run SUBCLU on database
-    Clustering<SubspaceModel<DoubleVector>> result = subclu.run(db);
-    
+    // run CLIQUE on database
+    Clustering<SubspaceModel<DoubleVector>> result = clique.run(db);
+
     // run by-label as reference
     ByLabelHierarchicalClustering<DoubleVector> bylabel = new ByLabelHierarchicalClustering<DoubleVector>();
     Clustering<Model> rbl = bylabel.run(db);
 
     double score = PairCountingFMeasure.compareClusterings(result, rbl, 1.0);
-    assertTrue("SUBCLU score on test dataset too low: " + score, score > 0.5307);
-    System.out.println("SUBCLU score: " + score + " > " + 0.5307);
+    assertTrue("CLIQUE score on test dataset too low: " + score, score > 0.5356);
+    System.out.println("CLIQUE score: " + score + " > " + 0.5356);
   }
 }
