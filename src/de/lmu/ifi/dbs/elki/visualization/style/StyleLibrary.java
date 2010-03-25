64361cf3574d2db5223a02cf235d5c26281dd63f
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-03-25 16:40:06 +0000

    Add sizes for reference points

--- src/de/lmu/ifi/dbs/elki/visualization/style/StyleLibrary.java
+++ src/de/lmu/ifi/dbs/elki/visualization/style/StyleLibrary.java
@@ -76,6 +76,11 @@ public interface StyleLibrary {
   final static String DOTPLOT = "plot.dot";
 
   /**
+   * Reference points color and size
+   */
+  final static String REFERENCE_POINTS = "plot.referencepoints";
+
+  /**
    * Scaling constant. Keep in sync with
    * {@link de.lmu.ifi.dbs.elki.visualization.VisualizationProjection#SCALE}.
    */
