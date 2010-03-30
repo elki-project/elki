c84a208064dc6f639f1bd6969aa85791fcfb6646
Elke Achtert <achtert@dbs.ifi.lmu.de>
2010-03-30 19:21:45 +0000

    adding new JUnitTest for DiSH

---
+++ test/de/lmu/ifi/dbs/elki/algorithm/clustering/subspace/TestDiSHResults.java
@@ -0,0 +1,77 @@
+package de.lmu.ifi.dbs.elki.algorithm.clustering.subspace;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.JUnit4Test;
+import de.lmu.ifi.dbs.elki.algorithm.clustering.ByLabelHierarchicalClustering;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.data.model.SubspaceModel;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
+import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
+import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Performs a full DiSH run, and compares the result with a clustering derived
+ * from the data set labels. This test ensures that DiSH performance doesn't
+ * unexpectedly drop on this data set (and also ensures that the algorithms
+ * work, as a side effect).
+ * 
+ * @author Elke Achtert
+ * 
+ */
+public class TestDiSHResults implements JUnit4Test {
+  // the following values depend on the data set used!
+  String dataset = "data/testdata/unittests/subspace-hierarchy.csv";
+
+  // size of the data set
+  int shoulds = 450;
+
+  /**
+   * Run DiSH with fixed parameters and compare the result to a golden standard.
+   * 
+   * @throws ParameterException
+   */
+  @Test
+  public void testDiSHResults() throws ParameterException {
+    ListParameterization params = new ListParameterization();
+    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
+    params.addParameter(DiSH.EPSILON_ID, "0.005");
+    params.addParameter(DiSH.MU_ID, 50);
+    
+    FileBasedDatabaseConnection<DoubleVector> dbconn = new FileBasedDatabaseConnection<DoubleVector>(params);
+
+    // get database
+    Database<DoubleVector> db = dbconn.getDatabase(null);
+
+    // verify data set size.
+    assertEquals("Database size doesn't match expected size.", shoulds, db.size());
+
+    // setup algorithm
+    DiSH<DoubleVector> dish = new DiSH<DoubleVector>(params);
+    dish.setVerbose(false);
+
+    params.failOnErrors();
+    if (params.hasUnusedParameters()) {
+      fail("Unused parameters: "+params.getRemainingParameters());
+    }
+    // run DiSH on database
+    Clustering<SubspaceModel<DoubleVector>> result = dish.run(db);
+    
+    // run by-label as reference
+    ByLabelHierarchicalClustering<DoubleVector> bylabel = new ByLabelHierarchicalClustering<DoubleVector>();
+    Clustering<Model> rbl = bylabel.run(db);
+
+    double score = PairCountingFMeasure.compareClusterings(result, rbl, 1.0);
+    assertTrue("DiSH score on test dataset too low: " + score, score == 1.0);
+    System.out.println("DiSH score: " + score + " == " + 1.0);
+  }
+}
