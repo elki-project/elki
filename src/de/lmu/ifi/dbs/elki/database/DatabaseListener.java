1f02c479470562737c4c70e4783deb2fe061e804
Elke Achtert <achtert@dbs.ifi.lmu.de>
2010-04-08 13:32:45 +0000

    Refactoring DatabaseListener

--- src/de/lmu/ifi/dbs/elki/database/DatabaseListener.java
+++ src/de/lmu/ifi/dbs/elki/database/DatabaseListener.java
@@ -1,29 +1,32 @@
 package de.lmu.ifi.dbs.elki.database;
 
+import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+
 /**
  * Defines the interface for an object that listens to changes in a Database.
  *
  * @author Elke Achtert
+ * @param <O> the type of DatabaseObject as element of the database
  */
-public interface DatabaseListener {
+public interface DatabaseListener<O extends DatabaseObject> {
   /**
    * Invoked after objects of the database have been updated in some way.
    * Use <code>e.getObjects()</code> to get the updated database objects.
    * @param e the update event
    */
-  void objectsChanged(DatabaseEvent e);
+  void objectsChanged(DatabaseEvent<O> e);
 
   /**
    * Invoked after an object has been inserted into the database.
    * Use <code>e.getObjects()</code> to get the newly inserted database objects.
    * @param e the insertion event
    */
-  void objectsInserted(DatabaseEvent e);
+  void objectsInserted(DatabaseEvent<O> e);
 
   /**
    * Invoked after an object has been deleted from the database.
    * Use <code>e.getObjects()</code> to get the inserted database objects.
    * @param e the removal event 
    */
-  void objectsRemoved(DatabaseEvent e);
+  void objectsRemoved(DatabaseEvent<O> e);
 }
