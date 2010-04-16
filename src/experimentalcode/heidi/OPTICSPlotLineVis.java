ed6cdd81549645c7543b5865b4fefbfaf6477fcf
Heidi Kolb <kolbh@cip.ifi.lmu.de>
2010-04-16 14:58:47 +0000

    code umstrukturiert

--- src/experimentalcode/heidi/OPTICSPlotLineVis.java
+++ src/experimentalcode/heidi/OPTICSPlotLineVis.java
@@ -28,13 +28,21 @@ import de.lmu.ifi.dbs.elki.result.ClusterOrderResult;
 import de.lmu.ifi.dbs.elki.utilities.AnyMap;
 import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
 import de.lmu.ifi.dbs.elki.visualization.css.CSSClassManager.CSSNamingConflict;
+import de.lmu.ifi.dbs.elki.visualization.opticsplot.OPTICSPlot;
 import de.lmu.ifi.dbs.elki.visualization.scales.LinearScale;
 import de.lmu.ifi.dbs.elki.visualization.style.StyleLibrary;
 import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
 import de.lmu.ifi.dbs.elki.visualization.svg.SVGUtil;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.AbstractVisualizer;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
-
+/**
+ * Visualize a line in a OPTICS Plot to select an Epsilon value
+ * and generates a new result with the new epsilon
+ * 
+ * @author 
+ * 
+ * @param <D> distance type
+ */
 public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractVisualizer{
   /**
    * Generic tag to indicate the type of element. Used in IDs, CSS-Classes etc.
@@ -46,7 +54,7 @@ public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractV
    */
   public static final String CSS_RANGEMARKER = "opticsPlotRangeMarker";
 
-  public static final String CSS_EVENTRECTLINE = "opticsPlotEventrectLine";
+  public static final String CSS_EVENTRECT = "opticsPlotEventrect";
 
   public static final String CSS_LINE = "opticsPlotLine";
 
@@ -73,104 +81,47 @@ public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractV
   private SVGPlot svgp;
 
   List<ClusterOrderEntry<D>> order;
-  List<Double> imgratios;
   Double imgratio;
   LinearScale linscale;
-  Double sizey;
-  double yUp;
-  double yBottom;
-  double yAct;
   double scale;
   int plotInd;
+  OPTICSPlot<D> opticsplot;
   public Element ltag;
   
-  public void init(OPTICSPlotVis<D> opvis, SVGPlot svgp,VisualizerContext context, List<ClusterOrderEntry<D>> order, double yBottom, double yUp,List<Double> imgratios, Double imgratio, LinearScale linscale, int plotInd) {
+  public void init(OPTICSPlotVis<D> opvis, SVGPlot svgp,VisualizerContext context, List<ClusterOrderEntry<D>> order, int plotInd) {
     this.opvis = opvis;
     this.order = order;
     this.svgp = svgp;  
-    this.imgratios= imgratios;
-    this.imgratio=imgratio;
-    this.linscale=linscale;
     ltag = svgp.svgElement(SVGConstants.SVG_G_TAG);
-    this.yBottom = yBottom;
-    this.yUp = yUp;
-    this.plotInd =plotInd;
+    this.plotInd = plotInd;
+    opticsplot = opvis.opvisualizer.getOpticsplots().get(plotInd);
+    imgratio = 1. / (Double) opticsplot.getRatio();
+    linscale = opticsplot.getScale();
     scale = StyleLibrary.SCALE;
-    addCSSClasses(svgp);
-  }
-  /**
-   * Adds the required CSS-Classes
-   * 
-   * @param svgp SVG-Plot
-   */
-  private void addCSSClasses(SVGPlot svgp) {
-    // Class for the line
-    if(!svgp.getCSSClassManager().contains(CSS_LINE)) {
-      final CSSClass lcls = new CSSClass(this, CSS_LINE);
-      lcls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_BLACK_VALUE);
-      lcls.setStatement(SVGConstants.CSS_OPACITY_PROPERTY, "1");
-      try {
-        svgp.getCSSClassManager().addClass(lcls);
-      }
-      catch(CSSNamingConflict e) {
-        de.lmu.ifi.dbs.elki.logging.LoggingUtil.exception(e);
-      }
-    }
-    // Class for the rectangle to add eventListeners
-    if(!svgp.getCSSClassManager().contains(CSS_EVENTRECTLINE)) {
-      final CSSClass ecls = new CSSClass(this, CSS_EVENTRECTLINE);
-      ecls.setStatement(SVGConstants.CSS_OPACITY_PROPERTY, "0.05");
-      try {
-        svgp.getCSSClassManager().addClass(ecls);
-      }
-      catch(CSSNamingConflict e) {
-        de.lmu.ifi.dbs.elki.logging.LoggingUtil.exception(e);
-      }
-    }
-    // Class for the epsilon-value
-    //TODO: Hierher
-//    if(!svgp.getCSSClassManager().contains(CSS_EPSILON)) {
-//      final CSSClass label = new CSSClass(svgp, CSS_EPSILON);
-//      label.setStatement(SVGConstants.CSS_FONT_SIZE_PROPERTY, 1);
-//      label.setStatement(SVGConstants.CSS_FILL_PROPERTY, context.getStyleLibrary().getTextColor(StyleLibrary.AXIS_LABEL));
-//      label.setStatement(SVGConstants.CSS_FONT_FAMILY_PROPERTY, context.getStyleLibrary().getFontFamily(StyleLibrary.AXIS_LABEL));
-//      label.setStatement(SVGConstants.CSS_FONT_SIZE_PROPERTY, context.getStyleLibrary().getTextSize(StyleLibrary.AXIS_LABEL));
-//
-//      try {
-//        svgp.getCSSClassManager().addClass(label);
-//      }
-//      catch(CSSNamingConflict e) {
-//        de.lmu.ifi.dbs.elki.logging.LoggingUtil.exception(e);
-//      }
-//    }
   }
   /**
    * Creates an SVG-Element for the Line to select Epsilon-value
-
-   * @param ltag SVG-Tag to add Line
-   * @param svgp SVG-Plot
-   * @param y Y-Value
-   * @param plotInd Index of the ClusterOrderResult
-   * @param scale The scale
+   * 
+   * @param y Y-Value (relative Value to upper point of  layer)
    * @return SVG-Element
    */
 
-  protected Element drawLine(double yAct) {
-    LinearScale xscale = linscale;
-    //TODO: unterschiedliche Höhen der plots beachten
-    double plotHeight = yBottom-yUp;
+  protected Element visualize(double yAct) {
 
-//    Double scY = (linscale.getMax() - linscale.getMin()) + linscale.getMin();
-    Double scY = (((plotInd * plotHeight) + (scale * imgratio) - yAct) / (scale * imgratio)) * (xscale.getMax() - xscale.getMin()) + xscale.getMin();
- 
-//    if(scY < 0.0) {
-//      // logger.warning("scY: " + scY);
-//      scY = 0.0;
-//    }
-//    if(yAct - (plotInd * plotHeight) > scale * imgratio) {
-//      yAct = plotInd * plotHeight + scale * imgratio;
-//    }
+    double space = scale * OPTICSPlotVis.SPACEFACTOR;
+    double yValueLayer = opvis.getYValueOfPlot(plotInd);
 
+    Double heightPlot = scale * imgratio;
+    
+    if(yAct < 0 + space/2) {
+      yAct = space/2;
+    }
+    if(yAct> (scale * imgratio) + space/2) {
+      yAct = (scale * imgratio) + space/2;
+    }
+    // absolute value
+    yAct = yAct + yValueLayer;
+    Double scY = (((plotInd * heightPlot) + (scale * imgratio) - yAct-space/2) / (scale * imgratio)) * (linscale.getMax() - linscale.getMin()) + linscale.getMin();
 
     final Element ltagLine = svgp.svgRect(0, yAct, scale * 1.08, scale * 0.0004);
     SVGUtil.addCSSClass(ltagLine, CSS_LINE);
@@ -178,8 +129,8 @@ public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractV
     SVGUtil.addCSSClass(ltagPoint, CSS_LINE);
     Element ltagText = svgp.svgText(scale * 1.10, yAct, scY.toString());
     SVGUtil.setAtt(ltagText, SVGConstants.SVG_CLASS_ATTRIBUTE, CSS_EPSILON);
-    final Element ltagEventRect = svgp.svgRect(scale * 1.03,  yUp - plotHeight*(OPTICSPlotVis.SPACEFACTOR/2), scale * 0.2, plotHeight*OPTICSPlotVis.SPACEFACTOR);
-    SVGUtil.addCSSClass(ltagEventRect, CSS_EVENTRECTLINE);
+    final Element ltagEventRect = svgp.svgRect(scale * 1.03,  yValueLayer, scale * 0.2, heightPlot+space);
+    SVGUtil.addCSSClass(ltagEventRect, CSS_EVENTRECT);
 
     if(ltag.hasChildNodes()) {
       NodeList nodes = ltag.getChildNodes();
@@ -199,32 +150,34 @@ public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractV
 
   private void addEventTagLine(OPTICSPlotVis<D> opvisualizer, SVGPlot svgp, Element ltagEventRect) {
     EventTarget targ = (EventTarget) ltagEventRect;
+
+
     OPTICSPlotLineHandler oplhandler = new OPTICSPlotLineHandler(this, order, svgp, ltagEventRect, ltag, plotInd);
     targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEDOWN, oplhandler, false);
     targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEMOVE, oplhandler, false);
     targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEUP, oplhandler, false);
   }
   protected void handleLineMousedown(Event evt, SVGPoint cPt, int plotInd) {
-    drawLine(cPt.getY());
+    double yValueLayer = opvis.getYValueOfPlot(plotInd);
+    opvis.updateLines(cPt.getY()-yValueLayer);
     mouseDown = true;
   }
 
   protected void handleLineMousemove(Event evt, SVGPoint cPt, int plotInd) {
     if(mouseDown) {
-      drawLine(cPt.getY());
-    }
+      double yValueLayer = opvis.getYValueOfPlot(plotInd);
+      opvis.updateLines(cPt.getY()-yValueLayer);    }
   }
 
   protected void handleLineMouseup(Event evt, SVGPoint cPt, int plotInd) {
     if(mouseDown) {
-      drawLine(cPt.getY());
+      double yValueLayer = opvis.getYValueOfPlot(plotInd);
+      opvis.updateLines(cPt.getY()-yValueLayer);
       mouseDown = false;
     }
     float y = cPt.getY();
     double yValue = 0.0;
-    for (int j=0; j<plotInd; j++){
-      yValue += (scale * imgratios.get(j)*1.4);
-    }
+
     double scale = StyleLibrary.SCALE;
 
     Double epsilon = (((plotInd * yValue) + (scale * imgratio - y) / (scale * imgratio)) * (linscale.getMax() - linscale.getMin()) + linscale.getMin());
@@ -262,8 +215,8 @@ public class OPTICSPlotLineVis<D extends NumberDistance<D, ?>> extends AbstractV
     if(!res.isEmpty()) {
       resultList.add(res);
     }
-    logger.warning("resultList: " + resultList.toString());
-    logger.warning("noise: " + noise.toString());
+//    logger.warning("resultList: " + resultList.toString());
+//    logger.warning("noise: " + noise.toString());
 
     Clustering<Model> cl = newResult(resultList, noise);
     opvis.opvisualizer.onClusteringUpdated(cl);
---
+++ src/experimentalcode/heidi/OPTICSPlotPlotVis.java
@@ -0,0 +1,291 @@
+package experimentalcode.heidi;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.batik.dom.events.DOMKeyEvent;
+import org.apache.batik.util.SVGConstants;
+import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventTarget;
+
+import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.NumberDistance;
+import de.lmu.ifi.dbs.elki.result.ClusterOrderEntry;
+import de.lmu.ifi.dbs.elki.result.ClusterOrderResult;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClassManager.CSSNamingConflict;
+import de.lmu.ifi.dbs.elki.visualization.opticsplot.OPTICSPlot;
+import de.lmu.ifi.dbs.elki.visualization.scales.LinearScale;
+import de.lmu.ifi.dbs.elki.visualization.style.StyleLibrary;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGUtil;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.AbstractVisualizer;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
+/**
+ * Handle the marked items in an OPTICS plots.
+ * 
+ * @author 
+ * 
+ * @param <D> distance type
+ */
+public class OPTICSPlotPlotVis<D extends NumberDistance<D, ?>> extends AbstractVisualizer {
+  /**
+   * Generic tag to indicate the type of element. Used in IDs, CSS-Classes etc.
+   */
+  public static final String CSS_MARKER = "opticsPlotMarker";
+
+  /**
+   * CSS-Styles
+   */
+  public static final String CSS_RANGEMARKER = "opticsPlotRangeMarker";
+
+  public static final String CSS_EVENTRECTLINE = "opticsPlotEventrectLine";
+
+  public static final String CSS_LINE = "opticsPlotLine";
+
+  /**
+   * CSS-Styles
+   */
+
+  public static final String CSS_EVENTRECT = "opticsPlotEventrect";
+
+  /**
+   * Curves to visualize
+   */
+  ArrayList<ClusterOrderResult<D>> cos;
+
+  /**
+   * OpticsPlotVis
+   */
+  OPTICSPlotVis<D> opvis;
+
+  /**
+   * The SVGPlot
+   */
+  private SVGPlot svgp;
+
+  List<ClusterOrderEntry<D>> order;
+
+  Double imgratio;
+
+  LinearScale linscale;
+
+  Double sizey;
+
+  double yUp;
+
+  double yBottom;
+
+  double yAct;
+
+  double scale;
+
+  int plotInd;
+
+  OPTICSPlot<D> opticsplot;
+
+  public Element layer;
+
+  public Element etag;
+
+  public Element mtag;
+
+  public void init(OPTICSPlotVis<D> opvis, SVGPlot svgp, VisualizerContext context, List<ClusterOrderEntry<D>> order, int plotInd) {
+    this.opvis = opvis;
+    this.order = order;
+    this.svgp = svgp;
+    etag = svgp.svgElement(SVGConstants.SVG_G_TAG);
+    mtag = svgp.svgElement(SVGConstants.SVG_G_TAG);
+    layer = svgp.svgElement(SVGConstants.SVG_G_TAG);
+    this.plotInd = plotInd;
+    opticsplot = opvis.opvisualizer.getOpticsplots().get(plotInd);
+    imgratio = 1. / (Double) opticsplot.getRatio();
+    scale = StyleLibrary.SCALE;
+  }
+
+  /**
+   * Creates an SVG-Element
+   * 
+   * @param ltag SVG-Tag to add Line
+   * @param svgp SVG-Plot
+   * @param y Y-Value
+   * @param plotInd Index of the ClusterOrderResult
+   * @param scale The scale
+   * @return SVG-Element
+   */
+
+  protected Element visualize() {
+
+    double space = scale * OPTICSPlotVis.SPACEFACTOR;
+    double yValueLayerUp = opvis.getYValueOfPlot(plotInd);
+    Double heightPlot = scale * imgratio;
+
+    // rect greater than plot to mark ranges
+    etag = svgp.svgRect(0 - space, yValueLayerUp, scale + space, heightPlot + space / 2);
+    SVGUtil.addCSSClass(etag, CSS_EVENTRECT);
+    addEventTag(opvis, svgp, etag);
+
+    addMarker(svgp);
+    // mtag first !
+    layer.appendChild(mtag);
+    layer.appendChild(etag);
+    return layer;
+  }
+
+  private void addEventTag(OPTICSPlotVis<D> opvisualizer, SVGPlot svgp, Element etag) {
+    EventTarget targ = (EventTarget) etag;
+    OPTICSPlotHandler<D> ophandler = new OPTICSPlotHandler<D>(this, svgp, order, etag);
+    targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEDOWN, ophandler, false);
+    targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEUP, ophandler, false);
+    targ.addEventListener(SVGConstants.SVG_EVENT_MOUSEMOVE, ophandler, false);
+    targ.addEventListener(SVGConstants.SVG_EVENT_KEYDOWN, ophandler, false);
+    targ.addEventListener(SVGConstants.SVG_EVENT_KEYPRESS, ophandler, false);
+    targ.addEventListener(SVGConstants.SVG_EVENT_KEYUP, ophandler, false);
+  }
+
+  /**
+   * Adds the Markers to the given tag
+   * 
+   * @param svgp SVG-Plot
+   * @param mtag Tag to which the markers should be added
+   * @param cosIndex Index of the ClusterOrderResult
+   * @param scale The scale
+   */
+  public void addMarker(SVGPlot svgp) {
+    
+    while(mtag.hasChildNodes()) {
+      mtag.removeChild(mtag.getLastChild());
+    }
+    ArrayList<Integer> selection = opvis.opvisualizer.getSelection();
+
+    for(int i = 0; i < selection.size(); i++) {
+      Integer coeID = selection.get(i);
+      Integer elementNr = -1;
+      logger.warning("coeID" + coeID);
+
+      for(int j = 0; j < order.size(); j++) {
+        Integer orderID = order.get(j).getID();
+        if(coeID.equals(orderID)) {
+          elementNr = j;
+          break;
+        }
+      }
+      Double width = scale / order.size();
+      Double x1 = elementNr * width;
+      Element marker = addMarkerRect(svgp, x1, width);
+      SVGUtil.addCSSClass(marker, CSS_MARKER);
+      mtag.appendChild(marker);
+    }
+  }
+
+  /**
+   * Creates an SVG-Element for the marker (Marker higher than plot!)
+   * 
+   * @param svgp SVG-Plot
+   * @param cosIndex Index of the ClusterOrderResult
+   * @param x1 X-Value
+   * @param width Width
+   * @return SVG-Element
+   */
+  public Element addMarkerRect(SVGPlot svgp, Double x1, Double width) {
+
+    double yValueLayer = opvis.getYValueOfPlot(plotInd);
+
+    double space = scale * opvis.SPACEFACTOR;
+    Double heightPlot = scale * imgratio;
+    return svgp.svgRect(x1, yValueLayer, width, heightPlot + space / 2);
+  }
+
+  protected void handlePlotMouseDown(int mouseActIndex) {
+//    logger.warning("mouseDown - Index: " + mouseActIndex);
+    opvis.mouseDown = true;
+    opvis.mouseDownIndex = mouseActIndex;
+    if(mouseActIndex >= 0 && mouseActIndex < order.size()) {
+      Double width = scale / order.size();
+      Double x1 = mouseActIndex * width;
+      Element marker = addMarkerRect(svgp, x1, width);
+      SVGUtil.setCSSClass(marker, CSS_RANGEMARKER);
+      mtag.appendChild(marker);
+    }
+  }
+
+  protected void handlePlotMouseUp(int mouseActIndex) {
+//    logger.warning("mouseUp - Index: " + mouseActIndex);
+
+    if(!opvis.keyStrgPressed && !opvis.keyShiftPressed) {
+      opvis.opvisualizer.clearSelection();
+    }
+    if(opvis.mouseDownIndex != mouseActIndex) {
+      // Range selected
+      for(int i = Math.max(Math.min(opvis.mouseDownIndex, mouseActIndex), 0); i <= Math.min(Math.max(opvis.mouseDownIndex, mouseActIndex), order.size()); i++) {
+        opvis.updateSelection(order, i);
+      }
+    }
+    else {
+      // one item selected
+      if(mouseActIndex >= 0 && mouseActIndex < order.size()) {
+        opvis.updateSelection(order, mouseActIndex);
+      }
+    }
+
+    opvis.mouseDown = false;
+
+    opvis.updateMarker();
+    fireRedrawEvent();
+    opvis.opvisualizer.requestRedraw();
+  }
+
+  protected void handlePlotMouseMove(int mouseActIndex) {
+//    logger.warning("mouseUp - Index: " + mouseActIndex);
+
+    if(opvis.mouseDown) {
+      if(mouseActIndex >= 0 || mouseActIndex <= order.size() || opvis.mouseDownIndex >= 0 || opvis.mouseDownIndex <= order.size()) {
+        Double width = scale / order.size();
+        Double x1;
+        Double x2;
+        if(mouseActIndex < opvis.mouseDownIndex) {
+          x1 = mouseActIndex * width;
+          x2 = (opvis.mouseDownIndex * width) + width;
+        }
+        else {
+          x1 = opvis.mouseDownIndex * width;
+          x2 = mouseActIndex * width + width;
+        }
+        mtag.removeChild(mtag.getLastChild());
+        Element marker = addMarkerRect(svgp, x1, x2 - x1);
+        SVGUtil.setCSSClass(marker, CSS_RANGEMARKER);
+        mtag.appendChild(marker);
+      }
+    }
+  }
+
+  protected void handlePlotKeyDown(Event evt) {
+    DOMKeyEvent domke = (DOMKeyEvent) evt;
+    int keyCode = domke.getKeyCode();
+    if(keyCode == KeyEvent.VK_SHIFT) {
+      opvis.keyShiftPressed = true;
+      opvis.keyStrgPressed = false;
+    }
+    else if(keyCode == KeyEvent.VK_CONTROL) {
+      opvis.keyStrgPressed = true;
+      opvis.keyShiftPressed = false;
+    }
+    else {
+      opvis.keyStrgPressed = false;
+      opvis.keyShiftPressed = false;
+    }
+  }
+
+  protected void handlePlotKeyUp(Event evt) {
+    DOMKeyEvent domke = (DOMKeyEvent) evt;
+    int keyCode = domke.getKeyCode();
+    if(keyCode == KeyEvent.VK_SHIFT) {
+      opvis.keyShiftPressed = false;
+    }
+    if(keyCode == KeyEvent.VK_CONTROL) {
+      opvis.keyStrgPressed = false;
+    }
+  }
+}
