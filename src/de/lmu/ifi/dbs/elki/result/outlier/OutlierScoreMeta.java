78d4a5d7fd57b94ba1e28e051cd92f01207268a0
Elke Achtert <achtert@dbs.ifi.lmu.de>
2010-04-23 08:39:56 +0000

    format code

--- src/de/lmu/ifi/dbs/elki/result/outlier/OutlierScoreMeta.java
+++ src/de/lmu/ifi/dbs/elki/result/outlier/OutlierScoreMeta.java
@@ -3,8 +3,9 @@ package de.lmu.ifi.dbs.elki.result.outlier;
 /**
  * Generic meta information about the value range of an outlier score.
  * 
- * All values can be {@link Double#NaN} if not specified by the algorithm or not computed (actual values).
- * For theoretical values, it is explicitly allowed to return {@link Double#NEGATIVE_INFINITY} or {@link Double#POSITIVE_INFINITY}.
+ * All values can be {@link Double#NaN} if not specified by the algorithm or not
+ * computed (actual values). For theoretical values, it is explicitly allowed to
+ * return {@link Double#NEGATIVE_INFINITY} or {@link Double#POSITIVE_INFINITY}.
  * 
  * @author Erich Schubert
  */
@@ -15,26 +16,30 @@ public interface OutlierScoreMeta {
    * @return minimum or {@link Double#NaN}
    */
   public double getActualMinimum();
+
   /**
    * Get the actual maximum of the value range.
    * 
    * @return maximum or {@link Double#NaN}
    */
   public double getActualMaximum();
+
   /**
    * Get the theoretical minimum of the value range.
    * 
    * @return theoretical minimum or {@link Double#NaN}
    */
   public double getTheoreticalMinimum();
+
   /**
    * Get the theoretical maximum of the value range.
-   *
+   * 
    * This value may be {@link Double#NEGATIVE_INFINITY} or {@link Double#NaN}.
    * 
    * @return theoretical maximum or {@link Double#NaN}
    */
   public double getTheoreticalMaximum();
+
   /**
    * Get the theoretical baseline of the value range.
    * 
@@ -43,6 +48,7 @@ public interface OutlierScoreMeta {
    * @return theoretical baseline or {@link Double#NaN}
    */
   public double getTheoreticalBaseline();
+
   /**
    * Return a normalized value of the outlier score.
    * 
