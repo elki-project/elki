773f23e7cceb67e3c088b419baf312e265ac31a7
Heidi Kolb <kolbh@cip.ifi.lmu.de>
2010-06-05 19:53:48 +0000

    implementation of a tableModel

--- src/experimentalcode/heidi/DotSelectionWindow.java
+++ src/experimentalcode/heidi/DotSelectionWindow.java
@@ -1,112 +1,186 @@
 package experimentalcode.heidi;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
 import de.lmu.ifi.dbs.elki.data.ClassLabel;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.logging.Logging;
 import de.lmu.ifi.dbs.elki.visualization.VisualizationProjection;
 import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
 
-public class DotSelectionWindow extends JFrame implements MouseListener {
+public class DotSelectionWindow extends JFrame implements TableModelListener {
   /**
-   * 
+   * A short name characterizing this Visualizer.
    */
+  private static final String NAME = "Heidi DotSelectionWindow";
+
   private static final long serialVersionUID = 1L;
 
-  SelectionUpdateVisualizer<?> opsel;
+  private SelectionUpdateVisualizer<?> opsel;
 
   private JTable table;
 
-  String[][] data;
+  private JButton resetButton;
 
-  ArrayList<ArrayList<String>> tupels;
+  private JButton selectButton;
 
-  SVGPlot svgp;
+  private JButton changeButton;
 
-  VisualizationProjection proj;
+  private JButton deleteButton;
+
+  private String[][] dotArray;
+
+  private String[] colArray;
+
+  private DotTableModel dotTableModel;
+
+  private static Logging logger = Logging.getLogger(NAME);
+
+  private ArrayList<ArrayList<String>> tupels;
 
   public DotSelectionWindow(VisualizerContext<?> context, SelectionUpdateVisualizer<?> ops, ArrayList<DBID> dbIDs, ArrayList<ArrayList<String>> t, SVGPlot s, VisualizationProjection p) {
     super("Dot Selection");
-    svgp = s;
-    proj = p;
-    opsel = ops;
+    this.opsel = ops;
     tupels = t;
 
     Database<?> database = context.getDatabase();
 
-    data = new String[tupels.size()][tupels.get(0).size() + 3];
+    dotArray = new String[tupels.size()][tupels.get(0).size() + 3];
     for(int i = 0; i < tupels.size(); i++) {
 
       ClassLabel classLabel = database.getClassLabel(dbIDs.get(i));
-      data[i][0] = dbIDs.get(i).toString();
+      dotArray[i][0] = dbIDs.get(i).toString();
       if(classLabel == null) {
-        data[i][1] = "none";
+        dotArray[i][1] = "none";
       }
       else {
-        data[i][1] = classLabel.toString();
+        dotArray[i][1] = classLabel.toString();
       }
       String objectLabel = database.getObjectLabel(dbIDs.get(i));
-      data[i][2] = objectLabel;
-      
+      dotArray[i][2] = objectLabel;
+
       for(int j = 0; j < tupels.get(0).size(); j++) {
-        data[i][j+3] = (String) tupels.get(i).get(j);
+        dotArray[i][j + 3] = (String) tupels.get(i).get(j);
       }
     }
-    //TODO: besser machen
     JPanel panel = new JPanel();
-    String[] col = new String[tupels.get(0).size()+3];
-    col[0] = "ID";
-    col[1] = "ClassLabel";
-    col[2] = "ObjectLabel";
-    for(int j=1; j <= tupels.get(0).size(); j++) {
-      col[j+2] = "Dim " + j;
+    colArray = new String[tupels.get(0).size() + 3];
+    colArray[0] = "ID";
+    colArray[1] = "ClassLabel";
+    colArray[2] = "ObjectLabel";
+    for(int j = 1; j <= tupels.get(0).size(); j++) {
+      colArray[j + 2] = "Dim " + j;
     }
-    table = new JTable(data, col);
-    table.addMouseListener(this);
+
+    dotTableModel = new DotTableModel(dotArray, colArray);
+
+    table = new JTable(dotTableModel);
+//    table.addMouseListener(this);
+    table.getModel().addTableModelListener(this); 
+
+    table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
     JScrollPane pane = new JScrollPane(table);
     panel.add(pane);
-    add(panel);
+
+    selectButton = new JButton("select Point");
+    selectButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+      int row = table.getSelectedRow();
+      if(table.getSelectedRow() != -1) {
+        opsel.selectPoint(dotArray[row][0]);      }
+      }
+    });
+    changeButton = new JButton("change in DB");
+    changeButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent arg0) {
+        // TODO: change
+      }
+    });
+    resetButton = new JButton("reset");
+    resetButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent arg0) {
+        logger.warning("reset");
+        dotTableModel.setDotArray(dotArray);
+      }
+    });
+    deleteButton = new JButton("delete");
+    deleteButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent arg0) {
+        logger.warning("delete");
+        if(table.getSelectedRow() != -1) {
+//          dotTableModel.removeRow(table.getSelectedRow());
+        }
+      }
+    });
+    panel.add(selectButton);
+    panel.add(changeButton);
+    panel.add(resetButton);
+    panel.add(deleteButton);
+
     setSize(500, 500);
+    add(panel);
     // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
     setResizable(true);
   }
 
-  public void mouseClicked(MouseEvent e) {
-    int row = table.rowAtPoint(e.getPoint());
-    opsel.selectPoint(svgp, proj, data[row][0]);
+  public void tableChanged(TableModelEvent e) {
+    logger.warning("tableChanged");
+    int row = e.getFirstRow();
+    int column = e.getColumn();
+    logger.warning("row: " + row);
+    logger.warning("column: " + column);
+    int c = 2;
+    int r = 2;
+    logger.warning("Value (2,2): " + table.getModel().getValueAt(r, c));
   }
 
-  @Override
-  public void mouseEntered(MouseEvent e) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void mouseExited(MouseEvent e) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void mousePressed(MouseEvent e) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void mouseReleased(MouseEvent e) {
-    // TODO Auto-generated method stub
-
-  }
+//  public void mouseClicked(MouseEvent e) {
+//    int row = table.rowAtPoint(e.getPoint());
+//    opsel.selectPoint(dotArray[row][0]);
+//  }
+//
+//  @Override
+//  public void mouseEntered(MouseEvent e) {
+//    // TODO Auto-generated method stub
+//  }
+//
+//  @Override
+//  public void mouseExited(MouseEvent e) {
+//    // TODO Auto-generated method stub
+//
+//  }
+//
+//  @Override
+//  public void mousePressed(MouseEvent e) {
+//    // TODO Auto-generated method stub
+//
+//  }
+//
+//  @Override
+//  public void mouseReleased(MouseEvent e) {
+//    // TODO Auto-generated method stub
+//
+//  }
 }
