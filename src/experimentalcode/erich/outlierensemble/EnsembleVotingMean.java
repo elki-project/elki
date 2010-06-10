9b378d79262322b361a0c6dc34ffb5dff7f6cc19
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-06-10 15:10:20 +0000

    More ensemble modules

--- src/experimentalcode/erich/outlierensemble/EnsembleVotingMean.java
+++ src/experimentalcode/erich/outlierensemble/EnsembleVotingMean.java
@@ -1,42 +1,29 @@
 package experimentalcode.erich.outlierensemble;
 
-import java.util.Arrays;
 import java.util.List;
 
+import de.lmu.ifi.dbs.elki.math.MeanVariance;
+
 /**
- * Simple combination rule, by taking the median.
- * Note: median is similar to a majority voting!
+ * Simple combination rule, by taking the mean
  * 
  * @author Erich Schubert
  */
 public class EnsembleVotingMean implements EnsembleVoting {
   /**
-   * Quantile to use
-   */
-  private double quantile = 0.5;
-  
-  /**
    * Constructor, adhering to
    * {@link de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable}
    */
   public EnsembleVotingMean() {
-    // TODO make quantile configurable
+    // Empty.
   }
 
   @Override
   public double combine(List<Double> scores) {
-    Double[] s = scores.toArray(new Double[]{});
-    Arrays.sort(s);
-    double pos = quantile * s.length;
-    int u = (int) Math.ceil(pos);
-    int l = (int) Math.floor(pos);
-    if (u == l) {
-      return s[u];
-    } else {
-      // weighted, for quantiles != 0.5 this can be other than 0.5
-      double wu = u - pos;
-      double wl = pos - l;
-      return s[u] * wu + s[l] * wl;
+    MeanVariance mv = new MeanVariance();
+    for (double score : scores) {
+      mv.put(score);
     }
+    return mv.getMean();
   }
 }
--- src/experimentalcode/erich/outlierensemble/EnsembleVotingMedian.java
+++ src/experimentalcode/erich/outlierensemble/EnsembleVotingMedian.java
@@ -1,29 +1,62 @@
 package experimentalcode.erich.outlierensemble;
 
+import java.util.Arrays;
 import java.util.List;
 
-import de.lmu.ifi.dbs.elki.math.MeanVariance;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.IntervalConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.DoubleParameter;
 
 /**
- * Simple combination rule, by taking the mean
+ * Simple combination rule, by taking the median. Note: median is similar to a
+ * majority voting!
  * 
  * @author Erich Schubert
  */
 public class EnsembleVotingMedian implements EnsembleVoting {
   /**
+   * Option ID for the quantile
+   */
+  public final static OptionID QUANTILE_ID = OptionID.getOrCreateOptionID("ensemble.median.quantile", "Quantile to use in median voting.");
+
+  /**
+   * Quantile parameter
+   */
+  private final DoubleParameter QUANTILE_PARAM = new DoubleParameter(QUANTILE_ID, new IntervalConstraint(0.0, IntervalConstraint.IntervalBoundary.CLOSE, 1.0, IntervalConstraint.IntervalBoundary.CLOSE), 0.5);
+
+  /**
+   * Quantile to use
+   */
+  private double quantile = 0.5;
+
+  /**
    * Constructor, adhering to
    * {@link de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable}
+   * 
+   * @param config Parameterization
    */
-  public EnsembleVotingMedian() {
-    // Empty.
+  public EnsembleVotingMedian(Parameterization config) {
+    if(config.grab(QUANTILE_PARAM)) {
+      quantile = QUANTILE_PARAM.getValue();
+    }
   }
 
   @Override
   public double combine(List<Double> scores) {
-    MeanVariance mv = new MeanVariance();
-    for (double score : scores) {
-      mv.put(score);
+    Double[] s = scores.toArray(new Double[] {});
+    Arrays.sort(s);
+    double pos = quantile * s.length;
+    int u = Math.min(s.length, (int) Math.ceil(pos));
+    int l = Math.max(0, (int) Math.floor(pos));
+    if(u == l) {
+      return s[u];
     }
-    return mv.getMean();
+    else {
+      // weighted, for quantiles != 0.5 this can be other than 0.5
+      double wu = u - pos;
+      double wl = pos - l;
+      return s[u] * wu + s[l] * wl;
+    }
   }
 }
