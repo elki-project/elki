26b308453519c513fe19a79a27a82a200e5232d2
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-07-05 15:54:57 +0000

    fix unit test for ROC curves, add reverseIterator test to flexi histogram.

--- test/de/lmu/ifi/dbs/elki/math/TestFlexiHistogram.java
+++ test/de/lmu/ifi/dbs/elki/math/TestFlexiHistogram.java
@@ -8,17 +8,19 @@ import org.junit.Test;
 import de.lmu.ifi.dbs.elki.JUnit4Test;
 import de.lmu.ifi.dbs.elki.math.FlexiHistogram;
 import de.lmu.ifi.dbs.elki.math.ReplacingHistogram;
+import de.lmu.ifi.dbs.elki.utilities.datastructures.IterableIteratorAdapter;
 import de.lmu.ifi.dbs.elki.utilities.pairs.Pair;
 
 /**
  * JUnit test to test the {@link ReplacingHistogram} class.
+ * 
  * @author Erich Schubert
  */
 public class TestFlexiHistogram implements JUnit4Test {
   FlexiHistogram<Double, Double> hist;
 
   /**
-   * Test that adds some data to the histogram and compares results. 
+   * Test that adds some data to the histogram and compares results.
    */
   @Test
   public final void testHistogram() {
@@ -42,15 +44,22 @@ public class TestFlexiHistogram implements JUnit4Test {
     hist.aggregate(-.13, -1.23);
     hist.aggregate(1.13, -4.56);
     assertArrayEquals("Resized histogram doesn't match", resized, hist.getData().toArray(new Double[0]));
-    
+
     // compare results via Iterator.
     int off = 0;
-    for (Pair<Double, Double> pair : hist) {
+    for(Pair<Double, Double> pair : hist) {
       assertEquals("Array iterator bin position", -0.1 + 0.2 * off, pair.getFirst(), 0.00001);
       assertEquals("Array iterator bin contents", resized[off], pair.getSecond(), 0.00001);
       off++;
     }
-    
+    // backwards...
+    off--;
+    for(Pair<Double, Double> pair : new IterableIteratorAdapter<Pair<Double, Double>>(hist.reverseIterator())) {
+      assertEquals("Array iterator bin position", -0.1 + 0.2 * off, pair.getFirst(), 0.00001);
+      assertEquals("Array iterator bin contents", resized[off], pair.getSecond(), 0.00001);
+      off--;
+    }
+
     // totally break out of the data range
     hist.aggregate(-10., 1.);
     assertArrayEquals("Expanded histogram doesn't match", expanded, hist.getData().toArray(new Double[0]));
