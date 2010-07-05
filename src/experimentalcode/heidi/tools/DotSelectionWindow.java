4e00c0077fcea250c471fb1193459506750cc390
Heidi Kolb <kolbh@cip.ifi.lmu.de>
2010-07-05 20:36:15 +0000

    javaDoc

--- src/experimentalcode/heidi/tools/DotSelectionWindow.java
+++ src/experimentalcode/heidi/tools/DotSelectionWindow.java
@@ -21,6 +21,14 @@ import de.lmu.ifi.dbs.elki.logging.Logging;
 import de.lmu.ifi.dbs.elki.math.linearalgebra.Vector;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
 
+/**
+ * Visualizes selected Objects in a JTable.<br>
+ * Objects can be selected, changed and deleted
+ * 
+ * @author Heidi Kolb
+ * 
+ * @param <NV> Type of the NumberVector being visualized.
+ */
 public class DotSelectionWindow<NV extends NumberVector<NV, ?>> extends JFrame implements TableModelListener {
   /**
    * A short name characterizing this Visualizer.
@@ -29,33 +37,76 @@ public class DotSelectionWindow<NV extends NumberVector<NV, ?>> extends JFrame i
 
   private static final long serialVersionUID = 1L;
 
-  private ToolDBChangeVisualizer<?> opsel;
+  /**
+   * The ToolDBChangeVisualizer
+   */
+  private ToolDBChangeVisualizer<?> dbChanceVis;
 
+  /**
+   * The JTable
+   */
   private JTable table;
 
+  /**
+   * Button to reset changes
+   */
   private JButton resetButton;
 
+  /**
+   * Button to select the marked objects
+   */
   private JButton selectButton;
 
+  /**
+   * Button to write the changes in the database
+   */
   private JButton changeButton;
 
+  /**
+   * Button to delete the selected objects
+   */
   private JButton deleteButton;
 
+  /**
+   * DataVector for JTable
+   */
   private java.util.Vector<java.util.Vector<String>> dataVector;
+
+  /**
+   * DataVector before changes
+   */
   private java.util.Vector<java.util.Vector<String>> dataVectorOld;
 
+  /**
+   * Column identifiers for JTable
+   */
   private java.util.Vector<String> columnIdentifiers;
 
+  /**
+   * The table model
+   */
   private DefaultTableModel dotTableModel;
 
+  /**
+   * The logger
+   */
   private static Logging logger = Logging.getLogger(NAME);
 
+  /**
+   * The DBIDs to display
+   */
   private ArrayModifiableDBIDs dbids;
 
-
+  /**
+   * Constructor
+   * 
+   * @param context The Context
+   * @param toolDBChangeVisualizer The ToolDBChangeVisualizer
+   * @param dbIDs The DBIDs to display
+   */
   public DotSelectionWindow(VisualizerContext<? extends NV> context, ToolDBChangeVisualizer<NV> toolDBChangeVisualizer, final ArrayModifiableDBIDs dbIDs) {
     super(NAME);
-    this.opsel = toolDBChangeVisualizer;
+    this.dbChanceVis = toolDBChangeVisualizer;
     this.dbids = dbIDs;
 
     Database<? extends NV> database = context.getDatabase();
@@ -85,12 +136,12 @@ public class DotSelectionWindow<NV extends NumberVector<NV, ?>> extends JFrame i
         data.add(Double.toString(v.get(j)));
       }
       dataVector.add(data);
-      
+
       java.util.Vector<String> dataOld = new java.util.Vector<String>();
-      dataOld = (java.util.Vector<String>)data.clone();
+      dataOld = (java.util.Vector<String>) data.clone();
       dataVectorOld.add(dataOld);
     }
-    
+
     JPanel panel = new JPanel();
     columnIdentifiers.add("ID");
     columnIdentifiers.add("ClassLabel");
@@ -113,58 +164,28 @@ public class DotSelectionWindow<NV extends NumberVector<NV, ?>> extends JFrame i
     selectButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        if(table.getSelectedRowCount() > 0) {
-          int[] rowIDs = table.getSelectedRows();
-          ArrayModifiableDBIDs rows = DBIDUtil.newArray(rowIDs.length);
-          for(int i = 0; i < rowIDs.length; i++) {
-            rows.add(dbIDs.get(rowIDs[i]));
-          }
-          opsel.selectPoints(rows);
-        }
-        else {
-          opsel.deleteMarker();
-        }
-        opsel.closeDotWindow();
+        handleSelect();
       }
     });
     changeButton = new JButton("change in DB");
     changeButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent arg0) {
-        for(int i = 0; i < dataVector.size(); i++) {
-          if(dataVector.get(i).get(0).equals(dataVectorOld.get(i).get(0))) {
-            // dbids equal
-            if(!dataVector.get(i).equals(dataVectorOld.get(i))) {
-              opsel.updateDB(dbids.get(i), dataVector.get(i));
-            }
-          }
-          else {
-            // dbids not equal --> delete
-            logger.warning("Error: DBIDs not equal");
-          }
-        }
-        opsel.closeDotWindow();
-        opsel.deleteMarker();
+        handleChange();
       }
     });
     resetButton = new JButton("reset");
     resetButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent arg0) {
-        dotTableModel.setDataVector(dataVector, columnIdentifiers);
-        dotTableModel.fireTableDataChanged();
+        handleReset();
       }
     });
     deleteButton = new JButton("delete");
     deleteButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent arg0) {
-        int[] selRows = table.getSelectedRows();
-        for(int i = 0; i < selRows.length; i++) {
-          opsel.deleteInDB(dbids.get(selRows[i]));
-        }
-        opsel.closeDotWindow();
-        opsel.deleteMarker();
+        handleDelete();
       }
     });
     panel.add(selectButton);
@@ -179,6 +200,71 @@ public class DotSelectionWindow<NV extends NumberVector<NV, ?>> extends JFrame i
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
   }
 
-  public void tableChanged(TableModelEvent e) {
+  /**
+   * Handle select. <br>
+   * Select the marked objects in ToolDBChangeVisualizer.
+   */
+  private void handleSelect() {
+    if(table.getSelectedRowCount() > 0) {
+      int[] rowIDs = table.getSelectedRows();
+      ArrayModifiableDBIDs rows = DBIDUtil.newArray(rowIDs.length);
+      for(int i = 0; i < rowIDs.length; i++) {
+        rows.add(dbids.get(rowIDs[i]));
+      }
+      dbChanceVis.selectPoints(rows);
+    }
+    else {
+      dbChanceVis.deleteMarker();
+    }
+    dbChanceVis.closeDotWindow();
   }
+
+  /**
+   * Handle change. <br>
+   * Update the attributes of the modified objects in the database.
+   */
+  private void handleChange() {
+    for(int i = 0; i < dataVector.size(); i++) {
+      if(dataVector.get(i).get(0).equals(dataVectorOld.get(i).get(0))) {
+        // dbids equal
+        if(!dataVector.get(i).equals(dataVectorOld.get(i))) {
+          dbChanceVis.updateDB(dbids.get(i), dataVector.get(i));
+        }
+      }
+      else {
+        logger.warning("Error: DBIDs not equal");
+      }
+    }
+    dbChanceVis.closeDotWindow();
+    dbChanceVis.deleteMarker();
+  }
+
+  /**
+   * Handle reset. <br>
+   * Reset the modifications in the table.
+   */
+  private void handleReset() {
+    dotTableModel.setDataVector(dataVector, columnIdentifiers);
+    dotTableModel.fireTableDataChanged();
+  }
+
+  /**
+   * Handle delete. <br>
+   * Delete the marked objects in the database.
+   */
+  private void handleDelete() {
+    int[] selRows = table.getSelectedRows();
+    for(int i = 0; i < selRows.length; i++) {
+      dbChanceVis.deleteInDB(dbids.get(selRows[i]));
+    }
+    dbChanceVis.closeDotWindow();
+    dbChanceVis.deleteMarker();
+  }
+
+  @Override
+  public void tableChanged(TableModelEvent arg0) {
+    // TODO Auto-generated method stub
+    // nothing to do
+  }
+
 }
