679766d3870da52eb304127aa6154c91aab06348
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-08-06 16:10:38 +0000

    Add Package for Florian

---
+++ src/experimentalcode/frankenb/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>The base-package of Florian's experimental code for the ELKI framework.</p>
+ * 
+ * 
+ *  <p>Classes not scheduled for a release.</p>
+ *  
+ */
+package experimentalcode.frankenb;
