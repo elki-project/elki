25b7ad4019f4eea7d7d1830effdcc1e2f82c81ad
Florian Frankenberger <frankenb@cip.ifi.lmu.de>
2010-09-20 14:46:39 +0000

    + just realized that this implementation was crap - so implemented it again ;)

--- src/experimentalcode/frankenb/main/KnnDataDivider.java
+++ src/experimentalcode/frankenb/main/KnnDataDivider.java
@@ -4,10 +4,28 @@
 package experimentalcode.frankenb.main;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import de.lmu.ifi.dbs.elki.application.StandAloneApplication;
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
@@ -20,6 +38,7 @@ import de.lmu.ifi.dbs.elki.utilities.exceptions.UnableToComplyException;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
+import de.lmu.ifi.dbs.elki.utilities.pairs.CPair;
 
 /**
  * This application divides a given database into
@@ -40,7 +59,7 @@ public class KnnDataDivider extends StandAloneApplication {
    * OptionID for {@link #PACKAGES_PARAM}
    */
   public static final OptionID PACKAGES_ID = OptionID.getOrCreateOptionID("packagequantity", "");
-
+  
   /**
    * Parameter that specifies the number of segments to create (= # of computers)
    * <p>
@@ -105,46 +124,89 @@ public class KnnDataDivider extends StandAloneApplication {
       
       Random random = new Random(System.currentTimeMillis());
       
+      //create and fill the segments
+      List<Set<DBID>> segments = new ArrayList<Set<DBID>>();
       for (int i = 0; i < segmentQuantity; ++i) {
-        int itemsToWrite = (i == segmentQuantity - 1 ? ids.size() : itemsPerSegment);
+        int itemsToWrite = (i == segmentQuantity - 1 ? ids.size() : itemsPerSegment); // the last one gets the rest
         
-        String filename = String.format("%03d.bin", i);
-        System.out.print(String.format("Writing %7s (package %03d of %03d / %05d items remain to be distributed) ... ", filename, i + 1, segmentQuantity, ids.size()));
+        Set<DBID> segment = new HashSet<DBID>();
+        segments.add(segment);
         
-        File file = new File(outputDir, filename);
-        if (file.exists()) {
-          if (!file.delete()) throw new UnableToComplyException("File " + filename + " already exists and could not be removed.");
+        for (int j = 0; j < itemsToWrite; ++j) { 
+          int id = random.nextInt(ids.size());
+          segment.add(ids.remove(id));
+        }        
+        
+      }
+      
+      //create permutations
+      Set<CPair<Integer, Integer>> segmentPermutations = permutateSegments(segmentQuantity);
+      
+      System.out.println(segmentPermutations);
+      
+      int i = 0;
+      for (CPair<Integer, Integer> segmentPermutation : segmentPermutations) {
+        
+        System.out.println(String.format("Writing package %03d of %03d", i + 1, segmentPermutations.size()));
+        String filenamePrefix = String.format("p%03d_", i);
+        
+        Set<DBID> segmentOne = segments.get(segmentPermutation.getFirst());
+        Set<DBID> segmentTwo = segments.get(segmentPermutation.getSecond());
+        
+        List<Set<DBID>> selectedSegments = new ArrayList<Set<DBID>>();
+        selectedSegments.add(segmentOne);
+        if (!segmentPermutation.getFirst().equals(segmentPermutation.getSecond())) {
+          selectedSegments.add(segmentTwo);
         }
         
-        OnDiskArray onDiskArray = new OnDiskArray(
-              file, 
+        List<String> segmentFilenames = new ArrayList<String>();
+        int segmentCounter = 0;
+        for (Set<DBID> segment : selectedSegments) {
+          String segmentFilename = filenamePrefix + String.format("s%01d.dat", segmentCounter++);
+          segmentFilenames.add(segmentFilename);
+          
+          File segmentFile = new File(outputDir, segmentFilename);
+          System.out.print(String.format("\tsegment %1d of %1d ... ", segmentCounter, selectedSegments.size()));
+          deleteAlreadyExistingFile(segmentFile);
+          
+          OnDiskArray onDiskArray = new OnDiskArray(
+              segmentFile, 
               MAGIC_NUMBER, 
               4, // = 4 byte header = 1 int (32bit)
-              database.dimensionality() * 8, // = 64bit of a double * dimensionality 
-              itemsToWrite
+              database.dimensionality() * 8 + 4, // = 64bit of a double * dimensionality + 1 int id
+              segment.size()
             );
-        
-        ByteBuffer header = onDiskArray.getExtraHeader();
-        
-        //save dimensionality
-        header.putInt(database.dimensionality());
-        
-        for (int j = 0; j < itemsToWrite; ++j) { // the last one gets the rest
-          
-          int id = random.nextInt(ids.size());
-          ByteBuffer buffer = onDiskArray.getRecordBuffer(j);
-          DoubleVector vector = database.get(ids.remove(id));
 
-          // at this point we assume that all elements have the same
-          // dimensionality within a database
-          for (int k = 1; k <= database.dimensionality(); ++k) {
-            buffer.putDouble(vector.getValue(k));
+          //saving dimensionality
+          onDiskArray.getExtraHeader().putInt(database.dimensionality());
+          
+          int dataPointer = 0;
+          for (DBID segmentDataID : segment) { // the last one gets the rest
+            
+            ByteBuffer buffer = onDiskArray.getRecordBuffer(dataPointer++);
+            DoubleVector vector = database.get(segmentDataID);
+
+            //first we write the id
+            buffer.putInt(segmentDataID.getIntegerID());
+            
+            // at this point we assume that all elements have the same
+            // dimensionality within a database
+            for (int k = 1; k <= database.dimensionality(); ++k) {
+              buffer.putDouble(vector.getValue(k));
+            }
+            
           }
           
+          onDiskArray.close();
+          System.out.println("done.");
         }
         
-        onDiskArray.close();
-        System.out.println("done.");
+        File headerFile = new File(outputDir, filenamePrefix + "header.xml");
+        
+        //write xml header
+        writeHeaderFile(headerFile, segmentFilenames, database.dimensionality());
+        
+        i++;
       }
       
     } catch (RuntimeException e) {
@@ -154,6 +216,31 @@ public class KnnDataDivider extends StandAloneApplication {
     } catch (Exception e) {
       throw new UnableToComplyException(e);
     }
+  }
+  
+  private static void deleteAlreadyExistingFile(File file) throws UnableToComplyException {
+    if (file.exists()) {
+      if (!file.delete()) throw new UnableToComplyException("File " + file.getName() + " already exists and could not be removed.");
+    }
+    
+  }
+  
+  /**
+   * Returns all possible permutations
+   * 
+   * @param i
+   * @param segmentQuantity
+   * @return
+   */
+  private static Set<CPair<Integer, Integer>> permutateSegments(int segmentQuantity) {
+    Set<CPair<Integer, Integer>> permutations = new HashSet<CPair<Integer, Integer>>();
+    for (int i = 0; i < segmentQuantity; ++i) {
+      for (int j = i; j < segmentQuantity; ++j) {
+        permutations.add(new CPair<Integer, Integer>(i, j));
+      }
+    }
+    
+    return permutations;
   }
   
   /**
@@ -167,7 +254,55 @@ public class KnnDataDivider extends StandAloneApplication {
     if (packageQuantity < 3) {
       throw new UnableToComplyException("Minimum is 3 packages");
     }
-    return (int)Math.floor(Math.sqrt(1 + packageQuantity * 8) - 1 / 2.0);
+    return (int)Math.floor((Math.sqrt(1 + packageQuantity * 8) - 1) / 2.0);
+  }
+  
+  private static void writeHeaderFile(File headerFile, List<String> segmentFilenames, int dimensionality) throws ParserConfigurationException, TransformerException, IOException {
+    DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+    DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+    Document doc = docBuilder.newDocument();
+
+    Comment comment = doc.createComment("KNN cluster precalculation data");
+    doc.appendChild(comment);
+    
+    Element rootElement = doc.createElement("package");
+    doc.appendChild(rootElement);
+    
+    Element dimensionalityElement = doc.createElement("dimensionality");
+    rootElement.appendChild(dimensionalityElement);
+    dimensionalityElement.setTextContent(String.valueOf(dimensionality));
+    
+    Element segmentsElement = doc.createElement("segments");
+    rootElement.appendChild(segmentsElement);
+
+    if (segmentFilenames.size() == 1) {
+      segmentFilenames.add(segmentFilenames.get(0));
+    }
+    
+    for (String segmentFilename : segmentFilenames) {
+      Element segmentElement = doc.createElement("segment");
+      segmentsElement.appendChild(segmentElement);
+      segmentElement.setTextContent(segmentFilename);
+    }
+    
+    TransformerFactory transfac = TransformerFactory.newInstance();
+    Transformer trans = transfac.newTransformer();
+    trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+    trans.setOutputProperty(OutputKeys.INDENT, "yes");
+
+    //create xml file
+    FileWriter fileWriter = null;
+    try {
+      fileWriter = new FileWriter(headerFile);
+      
+      StreamResult result = new StreamResult(fileWriter);
+      DOMSource source = new DOMSource(doc);
+      trans.transform(source, result);
+    } finally {
+      if (fileWriter != null) {
+        fileWriter.close();
+      }
+    }
   }
   
   public static void main(String[] args) {
