36452b9bc81a7da50a04d7ea986598891315a1cc
Erich Schubert <schube@dbs.ifi.lmu.de>
2010-10-14 12:36:28 +0000

    Move ToolBox2D into main.

--- src/de/lmu/ifi/dbs/elki/visualization/visualizers/adapter/DefaultAdapter.java
+++ src/de/lmu/ifi/dbs/elki/visualization/visualizers/adapter/DefaultAdapter.java
@@ -13,6 +13,7 @@ import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerTree;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.vis1d.Projection1DHistogramVisualizer;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.vis2d.AxisVisualizer;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.vis2d.ToolBox2D;
 import de.lmu.ifi.dbs.elki.visualization.visualizers.visunproj.SettingsVisualizer;
 
 /**
@@ -39,6 +40,11 @@ public class DefaultAdapter<NV extends NumberVector<NV, ?>> implements Algorithm
   private Projection1DHistogramVisualizer<NV> histoVisualizer;
 
   /**
+   * Visualizer for drawing a tool box in the 2D projections
+   */
+  private ToolBox2D<NV> toolBoxVisualizer;
+
+  /**
    * Track parameters for subclasses for "replay".
    */
   private MergedParameterization reconfig;
@@ -56,6 +62,7 @@ public class DefaultAdapter<NV extends NumberVector<NV, ?>> implements Algorithm
     axisVisualizer = new AxisVisualizer<NV>();
     histoVisualizer = new Projection1DHistogramVisualizer<NV>(reconfig);
     settingsVisualizer = new SettingsVisualizer(null);
+    toolBoxVisualizer = new ToolBox2D<NV>();
   }
 
   @Override
@@ -66,25 +73,34 @@ public class DefaultAdapter<NV extends NumberVector<NV, ?>> implements Algorithm
 
   @Override
   public Collection<Visualizer> getProvidedVisualizers() {
-    ArrayList<Visualizer> providedVisualizers = new ArrayList<Visualizer>(3);
+    ArrayList<Visualizer> providedVisualizers = new ArrayList<Visualizer>(4);
     providedVisualizers.add(axisVisualizer);
     providedVisualizers.add(histoVisualizer);
     providedVisualizers.add(settingsVisualizer);
+    providedVisualizers.add(toolBoxVisualizer);
     return providedVisualizers;
   }
 
   @Override
   public void addVisualizers(VisualizerContext<? extends NV> context, VisualizerTree<? extends NV> vistree) {
-    axisVisualizer.init(context);
-    histoVisualizer.init(context);
+    // Create and init new visualizers.
+    AxisVisualizer<NV> aVis = new AxisVisualizer<NV>();
+    aVis.init(context);
+    vistree.addVisualization(context.getDatabase(), aVis);
+
+    reconfig.rewind();
+    Projection1DHistogramVisualizer<NV> hVis = new Projection1DHistogramVisualizer<NV>(reconfig);
+    hVis.init(context);
+    vistree.addVisualization(context.getDatabase(), hVis);
+
+    ToolBox2D<NV> tbVis = new ToolBox2D<NV>();
+    tbVis.init(context);
+    vistree.addVisualization(context.getDatabase(), tbVis);
 
     for(SettingsResult sr : ResultUtil.getSettingsResults(context.getResult())) {
       SettingsVisualizer v = new SettingsVisualizer(sr);
       v.init(context);
       vistree.addVisualization(sr, v);
     }
-    
-    vistree.addVisualization(context.getDatabase(), axisVisualizer);
-    vistree.addVisualization(context.getDatabase(), histoVisualizer);
   }
 }
