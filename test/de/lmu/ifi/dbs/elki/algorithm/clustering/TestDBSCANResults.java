c662b106215127f5b6e6f511f6c4e7750552a4e4
Elke Achtert <achtert@dbs.ifi.lmu.de>
2010-11-11 13:12:06 +0000

    Unit test for DBSCAN

---
+++ test/de/lmu/ifi/dbs/elki/algorithm/clustering/TestDBSCANResults.java
@@ -0,0 +1,76 @@
+package de.lmu.ifi.dbs.elki.algorithm.clustering;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.JUnit4Test;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
+import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Performs a full DiSH run, and compares the result with a clustering derived
+ * from the data set labels. This test ensures that DiSH performance doesn't
+ * unexpectedly drop on this data set (and also ensures that the algorithms
+ * work, as a side effect).
+ * 
+ * @author Elke Achtert
+ * 
+ */
+public class TestDBSCANResults implements JUnit4Test {
+  // the following values depend on the data set used!
+  String dataset = "data/testdata/unittests/3clusters-and-noise-2d.csv";
+
+  // size of the data set
+  int shoulds = 330;
+
+  /**
+   * Run DBSCAN with fixed parameters and compare the result to a golden
+   * standard.
+   * 
+   * @throws ParameterException
+   */
+  @Test
+  public void testDBSCANResults() throws ParameterException {
+    ListParameterization params = new ListParameterization();
+    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
+    params.addParameter(FileBasedDatabaseConnection.IDSTART_ID, 1);
+    params.addParameter(DBSCAN.EPSILON_ID, "0.04");
+    params.addParameter(DBSCAN.MINPTS_ID, 20);
+
+    FileBasedDatabaseConnection<DoubleVector> dbconn = new FileBasedDatabaseConnection<DoubleVector>(params);
+
+    // get database
+    Database<DoubleVector> db = dbconn.getDatabase(null);
+
+    // verify data set size.
+    assertEquals("Database size doesn't match expected size.", shoulds, db.size());
+
+    // setup algorithm
+    DBSCAN<DoubleVector, DoubleDistance> dbscan = DBSCAN.parameterize(params);
+
+    params.failOnErrors();
+    if(params.hasUnusedParameters()) {
+      fail("Unused parameters: " + params.getRemainingParameters());
+    }
+    // run DBSCAN on database
+    Clustering<Model> result = dbscan.run(db);
+
+    // run by-label as reference
+    ByLabelClustering<DoubleVector> bylabel = new ByLabelClustering<DoubleVector>();
+    Clustering<Model> rbl = bylabel.run(db);
+
+    double score = PairCountingFMeasure.compareClusterings(result, rbl, 0.98);
+    assertTrue("DBSACN score on test dataset too low: " + score, score > 0.98);
+    System.out.println("DBSACN score: " + score + " > " + 0.98);
+  }
+}
