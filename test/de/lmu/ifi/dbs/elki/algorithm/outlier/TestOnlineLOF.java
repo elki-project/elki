b8903cb58b00f6c9cce3ec7422709405f8cad95b
Elke Achtert <achtert@dbs.ifi.lmu.de>
2011-01-05 10:02:04 +0000

    documentation

--- test/de/lmu/ifi/dbs/elki/algorithm/outlier/TestOnlineLOF.java
+++ test/de/lmu/ifi/dbs/elki/algorithm/outlier/TestOnlineLOF.java
@@ -1,6 +1,7 @@
 package de.lmu.ifi.dbs.elki.algorithm.outlier;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +10,6 @@ import java.util.Random;
 import org.junit.Test;
 
 import de.lmu.ifi.dbs.elki.JUnit4Test;
-import de.lmu.ifi.dbs.elki.algorithm.outlier.LOF;
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
 import de.lmu.ifi.dbs.elki.database.Database;
 import de.lmu.ifi.dbs.elki.database.DatabaseObjectMetadata;
@@ -17,10 +17,11 @@ import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
 import de.lmu.ifi.dbs.elki.database.ids.DBID;
 import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
 import de.lmu.ifi.dbs.elki.distance.distancefunction.CosineDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction;
 import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
 import de.lmu.ifi.dbs.elki.result.AnnotationResult;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
-import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
 import de.lmu.ifi.dbs.elki.utilities.exceptions.UnableToComplyException;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
 import de.lmu.ifi.dbs.elki.utilities.pairs.Pair;
@@ -38,20 +39,43 @@ public class TestOnlineLOF implements JUnit4Test {
   // the following values depend on the data set used!
   static String dataset = "data/testdata/unittests/3clusters-and-noise-2d.csv";
 
+  // parameter k for LOF and OnlineLOF
   static int k = 5;
 
-  static int size = 50;
+  // neighborhood distance function for LOF and OnlineLOF
+  @SuppressWarnings("rawtypes")
+  static DistanceFunction neighborhoodDistanceFunction = EuclideanDistanceFunction.STATIC;
 
+  // reachability distance function for LOF and OnlineLOF
+  @SuppressWarnings("rawtypes")
+  static DistanceFunction reachabilityDistanceFunction = new CosineDistanceFunction();
+
+  // seed for the generator
   static int seed = 5;
 
+  // size of the data set
+  static int size = 50;
+
+  /**
+   * First, run the {@link LOF} algorithm on the database. Second, run the
+   * {@link OnlineLOF} algorithm on the database, insert new objects and
+   * afterwards delete them. Then, compare the two results for equality.
+   * 
+   * @throws UnableToComplyException
+   */
+  @SuppressWarnings("unchecked")
   @Test
   public void testOnlineLOF() throws UnableToComplyException {
     Database<DoubleVector> db = getDatabase();
 
-    // run first LOF, then OnlineLOF (with delete and insert) on database
-    OutlierResult result1 = runLOF(db);
+    // 1. Run LOF
+    LOF<DoubleVector, DoubleDistance> lof = new LOF<DoubleVector, DoubleDistance>(k, neighborhoodDistanceFunction, reachabilityDistanceFunction);
+    OutlierResult result1 = lof.run(db);
+
+    // 2. Run OnlineLOF (with insertions and removals) on database
     OutlierResult result2 = runOnlineLOF(db);
 
+    // 3. Compare results
     AnnotationResult<Double> scores1 = result1.getScores();
     AnnotationResult<Double> scores2 = result2.getScores();
 
@@ -62,32 +86,13 @@ public class TestOnlineLOF implements JUnit4Test {
     }
   }
 
-  private static ListParameterization lofParameter() {
-    ListParameterization params = new ListParameterization();
-    params.addParameter(LOF.K_ID, k);
-    params.addParameter(LOF.REACHABILITY_DISTANCE_FUNCTION_ID, CosineDistanceFunction.class.getName());
-    return params;
-  }
-
-  private static OutlierResult runLOF(Database<DoubleVector> db) {
-    // setup algorithm
-    ListParameterization params = lofParameter();
-    LOF<DoubleVector, DoubleDistance> lof = null;
-    Class<LOF<DoubleVector, DoubleDistance>> lofcls = ClassGenericsUtil.uglyCastIntoSubclass(LOF.class);
-    lof = params.tryInstantiate(lofcls, lofcls);
-    params.failOnErrors();
-
-    // run LOF on database
-    return lof.run(db);
-  }
-
+  /**
+   * Run OnlineLOF (with insertions and removals) on database.
+   */
+  @SuppressWarnings("unchecked")
   private static OutlierResult runOnlineLOF(Database<DoubleVector> db) throws UnableToComplyException {
     // setup algorithm
-    ListParameterization params = lofParameter();
-    OnlineLOF<DoubleVector, DoubleDistance> lof = null;
-    Class<OnlineLOF<DoubleVector, DoubleDistance>> lofcls = ClassGenericsUtil.uglyCastIntoSubclass(OnlineLOF.class);
-    lof = params.tryInstantiate(lofcls, lofcls);
-    params.failOnErrors();
+    OnlineLOF<DoubleVector, DoubleDistance> lof = new OnlineLOF<DoubleVector, DoubleDistance>(k, neighborhoodDistanceFunction, reachabilityDistanceFunction);
 
     // run OnlineLOF on database
     OutlierResult result = lof.run(db);
@@ -108,6 +113,9 @@ public class TestOnlineLOF implements JUnit4Test {
     return result;
   }
 
+  /**
+   * Returns the database.
+   */
   private static Database<DoubleVector> getDatabase() {
     ListParameterization params = new ListParameterization();
     params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
