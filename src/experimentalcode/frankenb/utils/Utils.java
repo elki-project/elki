9390560665f0b25c4af9ee17c4448e843d382036
Florian Frankenberger <frankenb@cip.ifi.lmu.de>
2011-02-08 20:54:50 +0000

    <empty commit message>

---
+++ src/experimentalcode/frankenb/utils/Utils.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package experimentalcode.frankenb.utils;
+
+/**
+ * No description given.
+ * 
+ * @author Florian Frankenberger
+ */
+public class Utils {
+  private Utils() {}
+  
+
+  public static long sumFormular(final long i) {
+    return ((i - 1L) * i) / 2;
+  }
+  
+}
