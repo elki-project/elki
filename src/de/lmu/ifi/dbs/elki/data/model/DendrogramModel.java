03f6d2f8f0ae2398a835e7ae0f52393d12612c36
Elke Achtert <achtert@dbs.ifi.lmu.de>
2011-02-18 12:30:49 +0000

    new cluster model for dendrograms, stores the distance to the child clusters

---
+++ src/de/lmu/ifi/dbs/elki/data/model/DendrogramModel.java
@@ -0,0 +1,42 @@
+package de.lmu.ifi.dbs.elki.data.model;
+
+import de.lmu.ifi.dbs.elki.distance.distancevalue.Distance;
+import de.lmu.ifi.dbs.elki.result.textwriter.TextWriteable;
+import de.lmu.ifi.dbs.elki.result.textwriter.TextWriterStream;
+
+/**
+ * Model for dendrograms, provides the distance to the child cluster.
+ * 
+ * @author @author Elke Achtert
+ * 
+ */
+public class DendrogramModel<D extends Distance<D>> extends BaseModel {
+
+  private D distance;
+
+  public DendrogramModel(D distance) {
+    super();
+    this.distance = distance;
+  }
+
+  /**
+   * @return the distance
+   */
+  public D getDistance() {
+    return distance;
+  }
+
+  /**
+   * Implementation of {@link TextWriteable} interface.
+   */
+  @Override
+  public void writeToText(TextWriterStream out, String label) {
+    super.writeToText(out, label);
+    out.commentPrintLn("Distance to children: " + (distance != null ? distance.toString() : "null"));
+  }
+
+  public String toString() {
+    return "Distance to children: " + (distance != null ? distance.toString() : "null");
+  }
+
+}
