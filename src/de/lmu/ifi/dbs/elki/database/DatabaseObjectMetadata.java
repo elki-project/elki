e73e3f4cc304893d28393fb4a183df71bbdb245c
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-03-30 14:05:12 +0000

    Database metadata toString

--- src/de/lmu/ifi/dbs/elki/database/DatabaseObjectMetadata.java
+++ src/de/lmu/ifi/dbs/elki/database/DatabaseObjectMetadata.java
@@ -5,14 +5,14 @@ import de.lmu.ifi.dbs.elki.data.ClassLabel;
 /**
  * Temporary class, containing the remaining associations.
  * 
- * FIXME: remove this, use the new DB layer.
- * This is only used in a few convenience places.
+ * FIXME: remove this, use the new DB layer. This is only used in a few
+ * convenience places.
  * 
  * @author Erich Schubert
  */
 public class DatabaseObjectMetadata {
   public String objectlabel = null;
-  
+
   public String externalId = null;
 
   public ClassLabel classlabel = null;
@@ -26,5 +26,26 @@ public class DatabaseObjectMetadata {
 
   public DatabaseObjectMetadata() {
     super();
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer buf = new StringBuffer();
+    if(objectlabel != null) {
+      buf.append("label=").append(objectlabel);
+    }
+    if(classlabel != null) {
+      if(buf.length() > 0) {
+        buf.append(", ");
+      }
+      buf.append("class=").append(classlabel.toString());
+    }
+    if(externalId != null) {
+      if(buf.length() > 0) {
+        buf.append(", ");
+      }
+      buf.append("externalid=").append(externalId);
+    }
+    return buf.toString();
   }
 }
