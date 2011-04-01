180a87c9f3c8cffb6ec590d703235ebdfa2235bc
Katharina Rausch <rausch@cip.ifi.lmu.de>
2011-04-01 06:37:43 +0000

    <empty commit message>

---
+++ src/experimentalcode/katharina/TestEMResults.java
@@ -0,0 +1,77 @@
+package experimentalcode.katharina;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.JUnit4Test;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.EMModel;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
+import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Performs a full EM run, and compares the result with a clustering derived
+ * from the data set labels. This test ensures that EM performance doesn't
+ * unexpectedly drop on this data set (and also ensures that the algorithms
+ * work, as a side effect).
+ * 
+ * @author Katharina Rausch
+ * 
+ */
+public class TestEMResults implements JUnit4Test {
+  // the following values depend on the data set used!
+  String dataset = "data/katharina/generiert/1hierarchischesCluster.csv";
+
+  // size of the data set
+  int shoulds = 710;
+
+  /**
+   * Run EM with fixed parameters and compare the result to a golden
+   * standard.
+   * 
+   * @throws ParameterException
+   */
+  @Test
+  public void testEMResults() throws ParameterException {
+    ListParameterization params = new ListParameterization();
+    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
+    params.addParameter(FileBasedDatabaseConnection.IDSTART_ID, 1);
+    params.addParameter(EM.SEED_ID, "1");
+    params.addParameter(EM.K_ID, "5");
+
+    FileBasedDatabaseConnection<DoubleVector> dbconn = FileBasedDatabaseConnection.parameterize(params);
+
+    // get database
+    Database<DoubleVector> db = dbconn.getDatabase(null);
+
+    // verify data set size.
+    assertEquals("Database size doesn't match expected size.", shoulds, db.size());
+
+    // setup algorithm
+    EM<DoubleVector> em = ClassGenericsUtil.parameterizeOrAbort(EM.class, params);
+
+    params.failOnErrors();
+    if(params.hasUnusedParameters()) {
+      fail("Unused parameters: " + params.getRemainingParameters());
+    }
+    // run EM on database
+    Clustering<EMModel<DoubleVector>> result = em.run(db);
+
+    // run by-label as reference
+    ByLabelClustering<DoubleVector> bylabel = new ByLabelClustering<DoubleVector>();
+    Clustering<Model> rbl = bylabel.run(db);
+
+    double score = PairCountingFMeasure.compareClusterings(result, rbl, 1.0);
+    assertTrue("EM score on test dataset too low: " + score, score > 0.96);
+    System.out.println("EM score: " + score + " > " + 0.96);
+  }
+}
