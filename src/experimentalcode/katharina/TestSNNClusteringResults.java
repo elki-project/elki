496abc971708d6d6821072e27130e3f5157f0fa4
Katharina Rausch <rausch@cip.ifi.lmu.de>
2011-04-01 06:37:43 +0000

    <empty commit message>

---
+++ src/experimentalcode/katharina/TestSNNClusteringResults.java
@@ -0,0 +1,80 @@
+package experimentalcode.katharina;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.JUnit4Test;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.index.preprocessed.snn.SharedNearestNeighborPreprocessor;
+import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
+import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Performs a full SNNClustering run, and compares the result with a clustering derived
+ * from the data set labels. This test ensures that SNNClustering performance doesn't
+ * unexpectedly drop on this data set (and also ensures that the algorithms
+ * work, as a side effect).
+ * 
+ * @author Katharina Rausch
+ * 
+ */
+public class TestSNNClusteringResults implements JUnit4Test {
+  // the following values depend on the data set used!
+  String dataset = "data/katharina/1dbscan_failure.ascii";
+
+  // size of the data set
+  int shoulds = 1200;
+
+  /**
+   * Run SNNClustering with fixed parameters and compare the result to a golden
+   * standard.
+   * 
+   * @throws ParameterException
+   */
+  @Test
+  public void testSNNClusteringResults() throws ParameterException {
+    ListParameterization params = new ListParameterization();
+    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
+    params.addParameter(FileBasedDatabaseConnection.IDSTART_ID, 1);
+    params.addParameter(SNNClustering.EPSILON_ID, "77");
+    params.addParameter(SNNClustering.MINPTS_ID, "28");
+    params.addParameter(SharedNearestNeighborPreprocessor.Factory.NUMBER_OF_NEIGHBORS_ID, "100");
+
+
+    FileBasedDatabaseConnection<DoubleVector> dbconn = FileBasedDatabaseConnection.parameterize(params);
+
+    // get database
+    Database<DoubleVector> db = dbconn.getDatabase(null);
+
+    // verify data set size.
+    assertEquals("Database size doesn't match expected size.", shoulds, db.size());
+
+    // setup algorithm
+    SNNClustering<DoubleVector, DoubleDistance> snn = ClassGenericsUtil.parameterizeOrAbort(SNNClustering.class, params);
+
+    params.failOnErrors();
+    if(params.hasUnusedParameters()) {
+      fail("Unused parameters: " + params.getRemainingParameters());
+    }
+    // run EM on database
+    Clustering<Model> result = snn.run(db);
+
+    // run by-label as reference
+    ByLabelClustering<DoubleVector> bylabel = new ByLabelClustering<DoubleVector>();
+    Clustering<Model> rbl = bylabel.run(db);
+
+    double score = PairCountingFMeasure.compareClusterings(result, rbl, 1.0);
+    assertTrue("SNNClustering score on test dataset too low: " + score, score > 0.82);
+    System.out.println("SNNClustering score: " + score + " > " + 0.82);
+  }
+}
