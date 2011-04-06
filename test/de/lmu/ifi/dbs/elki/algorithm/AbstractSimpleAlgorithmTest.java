3baf12524b12f07159db75ad0596a1e96bbdc4c1
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-04-06 15:04:56 +0000

    Refactor Katharinas Unit tests

---
+++ test/de/lmu/ifi/dbs/elki/algorithm/AbstractSimpleAlgorithmTest.java
@@ -0,0 +1,139 @@
+package de.lmu.ifi.dbs.elki.algorithm;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import de.lmu.ifi.dbs.elki.algorithm.clustering.ByLabelClustering;
+import de.lmu.ifi.dbs.elki.data.Cluster;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
+import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
+import de.lmu.ifi.dbs.elki.logging.Logging;
+import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Abstract base class useful for testing various algorithms.
+ * 
+ * @author Erich Schubert
+ */
+public abstract class AbstractSimpleAlgorithmTest {
+  /**
+   * Notice: this is okay for tests - don't use this for frequently used
+   * objects, use a static instance instead!
+   */
+  protected Logging logger = Logging.getLogger(this.getClass());
+
+  /**
+   * Validate that parameterization succeeded: no parameters left, no
+   * parameterization errors.
+   * 
+   * @param config Parameterization to test
+   */
+  protected void testParameterizationOk(ListParameterization config) {
+    if(config.hasUnusedParameters()) {
+      fail("Unused parameters: " + config.getRemainingParameters());
+    }
+    if(config.hasErrors()) {
+      config.logAndClearReportedErrors();
+      fail("Parameterization errors.");
+    }
+  }
+
+  /**
+   * Generate a simple DoubleVector database from a file.
+   * 
+   * @param filename File to load
+   * @param expectedSize Expected size in records
+   * @return Database
+   */
+  protected Database<DoubleVector> makeSimpleDatabase(String filename, int expectedSize) {
+    org.junit.Assert.assertTrue("Test data set not found: " + filename, (new File(filename)).exists());
+    ListParameterization params = new ListParameterization();
+    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, filename);
+    params.addParameter(FileBasedDatabaseConnection.IDSTART_ID, 1);
+    FileBasedDatabaseConnection<DoubleVector> dbconn = ClassGenericsUtil.parameterizeOrAbort(FileBasedDatabaseConnection.class, params);
+
+    testParameterizationOk(params);
+
+    Database<DoubleVector> db = dbconn.getDatabase(null);
+    org.junit.Assert.assertEquals("Database size does not match.", expectedSize, db.size());
+    return db;
+  }
+
+  /**
+   * Find a clustering result, fail if there is more than one or none.
+   * 
+   * @param result Base result
+   * @return Clustering
+   */
+  protected Clustering<?> findSingleClustering(Result result) {
+    List<Clustering<? extends Model>> clusterresults = ResultUtil.getClusteringResults(result);
+    assertTrue("No unique clustering found in result.", clusterresults.size() == 1);
+    Clustering<? extends Model> clustering = clusterresults.get(0);
+    return clustering;
+  }
+
+  /**
+   * Test the clustering result by comparing the score with an expected value.
+   * 
+   * @param database Database to test
+   * @param clustering Clustering result
+   * @param expected Expected score
+   */
+  protected <O extends DatabaseObject> void testFMeasure(Database<O> database, Clustering<?> clustering, double expected) {
+    // Run by-label as reference
+    ByLabelClustering<O> bylabel = new ByLabelClustering<O>();
+    Clustering<Model> rbl = bylabel.run(database);
+
+    double score = PairCountingFMeasure.compareClusterings(clustering, rbl, 1.0);
+    if(logger.isVerbose()) {
+      logger.verbose(this.getClass().getSimpleName() + " score: " + score + " expect: " + expected);
+    }
+    org.junit.Assert.assertEquals(this.getClass().getSimpleName() + ": Score does not match. " + score, expected, score, 0.0001);
+  }
+
+  /**
+   * Validate the cluster sizes with an expected result.
+   * 
+   * @param clustering Clustering to test
+   * @param expected Expected cluster sizes
+   */
+  protected void testClusterSizes(Clustering<?> clustering, int[] expected) {
+    List<Integer> sizes = new java.util.ArrayList<Integer>();
+    for(Cluster<?> cl : clustering.getAllClusters()) {
+      sizes.add(cl.size());
+    }
+    // Sort both
+    Collections.sort(sizes);
+    Arrays.sort(expected);
+    // Report
+    if(logger.isVerbose()) {
+      StringBuffer buf = new StringBuffer();
+      buf.append("Cluster sizes: [");
+      for(int i = 0; i < sizes.size(); i++) {
+        if(i > 0) {
+          buf.append(", ");
+        }
+        buf.append(sizes.get(i));
+      }
+      buf.append("]");
+    }
+    // Test
+    org.junit.Assert.assertEquals("Number of clusters does not match expectations.", expected.length, sizes.size());
+    for(int i = 0; i < expected.length; i++) {
+      org.junit.Assert.assertEquals("Cluster size does not match at position " + i, expected[i], (int) sizes.get(i));
+    }
+  }
+}
--- test/de/lmu/ifi/dbs/elki/algorithm/clustering/TestDBSCANResults.java
+++ test/de/lmu/ifi/dbs/elki/algorithm/clustering/TestDBSCANResults.java
@@ -1,19 +1,14 @@
 package de.lmu.ifi.dbs.elki.algorithm.clustering;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 import de.lmu.ifi.dbs.elki.JUnit4Test;
+import de.lmu.ifi.dbs.elki.algorithm.AbstractSimpleAlgorithmTest;
 import de.lmu.ifi.dbs.elki.data.Clustering;
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
 import de.lmu.ifi.dbs.elki.data.model.Model;
 import de.lmu.ifi.dbs.elki.database.Database;
-import de.lmu.ifi.dbs.elki.database.connection.FileBasedDatabaseConnection;
 import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
-import de.lmu.ifi.dbs.elki.evaluation.paircounting.PairCountingFMeasure;
 import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
@@ -25,9 +20,9 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParamet
  * work, as a side effect).
  * 
  * @author Elke Achtert
- * 
+ * @author Erich Schubert
  */
-public class TestDBSCANResults implements JUnit4Test {
+public class TestDBSCANResults extends AbstractSimpleAlgorithmTest implements JUnit4Test {
   // the following values depend on the data set used!
   String dataset = "data/testdata/unittests/3clusters-and-noise-2d.csv";
 
@@ -42,36 +37,18 @@ public class TestDBSCANResults implements JUnit4Test {
    */
   @Test
   public void testDBSCANResults() throws ParameterException {
-    ListParameterization params = new ListParameterization();
-    params.addParameter(FileBasedDatabaseConnection.INPUT_ID, dataset);
-    params.addParameter(FileBasedDatabaseConnection.IDSTART_ID, 1);
-    params.addParameter(DBSCAN.EPSILON_ID, "0.04");
-    params.addParameter(DBSCAN.MINPTS_ID, 20);
-
-    FileBasedDatabaseConnection<DoubleVector> dbconn = FileBasedDatabaseConnection.parameterize(params);
-
-    // get database
-    Database<DoubleVector> db = dbconn.getDatabase(null);
-
-    // verify data set size.
-    assertEquals("Database size doesn't match expected size.", shoulds, db.size());
-
+    Database<DoubleVector> db = makeSimpleDatabase(dataset, shoulds);
+    
     // setup algorithm
+    ListParameterization params = new ListParameterization();
+    params.addParameter(DBSCAN.EPSILON_ID, 0.04);
+    params.addParameter(DBSCAN.MINPTS_ID, 20);
     DBSCAN<DoubleVector, DoubleDistance> dbscan = ClassGenericsUtil.parameterizeOrAbort(DBSCAN.class, params);
-
-    params.failOnErrors();
-    if(params.hasUnusedParameters()) {
-      fail("Unused parameters: " + params.getRemainingParameters());
-    }
+    testParameterizationOk(params);
+    
     // run DBSCAN on database
     Clustering<Model> result = dbscan.run(db);
 
-    // run by-label as reference
-    ByLabelClustering<DoubleVector> bylabel = new ByLabelClustering<DoubleVector>();
-    Clustering<Model> rbl = bylabel.run(db);
-
-    double score = PairCountingFMeasure.compareClusterings(result, rbl, 1.0);
-    assertTrue("DBSACN score on test dataset too low: " + score, score > 0.996);
-    System.out.println("DBSCAN score: " + score + " > " + 0.996);
+    testFMeasure(db, result, 0.996413);
   }
 }
