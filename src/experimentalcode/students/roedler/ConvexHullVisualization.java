c75f7ea5325b4965fac7dd4db54e5fdaa4db1991
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-04-12 12:45:28 +0000

    Move robert into students package

---
+++ src/experimentalcode/students/roedler/ConvexHullVisualization.java
@@ -0,0 +1,247 @@
+package experimentalcode.students.roedler;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.batik.util.SVGConstants;
+import org.w3c.dom.Element;
+
+import de.lmu.ifi.dbs.elki.data.Cluster;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.model.MeanModel;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.math.linearalgebra.Vector;
+import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
+import de.lmu.ifi.dbs.elki.visualization.colors.ColorLibrary;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
+import de.lmu.ifi.dbs.elki.visualization.projections.Projection;
+import de.lmu.ifi.dbs.elki.visualization.projections.Projection2D;
+import de.lmu.ifi.dbs.elki.visualization.style.StyleLibrary;
+import de.lmu.ifi.dbs.elki.visualization.style.marker.MarkerLibrary;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGPath;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGUtil;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.AbstractVisFactory;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.Visualization;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizationTask;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerUtil;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.vis2d.P2DVisualization;
+import experimentalcode.students.roedler.utils.convexhull.ConvexHull2D;
+
+
+/**
+ * Visualizer for generating an SVG-Element containing the convex hull of each cluster
+ * 
+ * @author Robert Rödler
+ * 
+ * @apiviz.has MeanModel oneway - - visualizes
+ * 
+ * @param <NV> Type of the NumberVector being visualized.
+ */
+public class ConvexHullVisualization<NV extends NumberVector<NV, ?>>  extends P2DVisualization<NV>{
+
+  /**
+   * A short name characterizing this Visualizer.
+   */
+  private static final String NAME = "convex Hull Visualization";
+  
+  /**
+   * Generic tags to indicate the type of element. Used in IDs, CSS-Classes etc.
+   */
+  public static final String CONVEXHULL = "convexHull";
+
+  /**
+   * The result we work on
+   */
+  Clustering<MeanModel<NV>> clustering;
+
+  /**
+   * The hulls
+   */
+  Element hulls;
+/**
+ *   Constructor
+ *   @param task VisualizationTask
+ */
+  public ConvexHullVisualization(VisualizationTask task) {
+    super(task);
+    this.clustering = task.getResult();
+    context.addContextChangeListener(this);
+    incrementalRedraw();
+  }
+  
+  @Override
+  protected void redraw() {
+    
+  //  addCSSClasses(svgp);
+    //SVGPath path = new SVGPath();
+    ConvexHull2D ch;
+     
+    double opacity = 0.25;
+     Vector[] chres;
+     Vector[] means = new Vector[clustering.getAllClusters().size()];
+     Vector[] meansproj = new Vector[clustering.getAllClusters().size()];
+     
+    Iterator<Cluster<MeanModel<NV>>> ci = clustering.getAllClusters().iterator();
+    int clusterID = 0;
+    
+    for(int cnum = 0; cnum < clustering.getAllClusters().size(); cnum++) {
+      SVGPath path = new SVGPath();
+      Cluster<MeanModel<NV>> clus = ci.next();
+      double[] mean = proj.fastProjectDataToRenderSpace(clus.getModel().getMean());
+      meansproj[cnum] = new Vector(mean);
+      means[cnum] = clus.getModel().getMean().getColumnVector();
+      
+      Vector[] clsPoints = new Vector[clustering.getAllClusters().get(cnum).getIDs().size()];
+      Iterator<DBID> clp = clustering.getAllClusters().get(cnum).getIDs().iterator();
+      
+      Database<? extends NV> database = context.getDatabase();
+      
+      for (int i = 0; i < clsPoints.length; i++) {
+        DBID clpnum = clp.next();
+        double[] projP = proj.fastProjectDataToRenderSpace(database.get(clpnum).getColumnVector());
+        clsPoints[i] = new Vector(projP);
+      }
+      ch = new ConvexHull2D(clsPoints);
+      chres = ch.start();
+      
+      double minX, maxX, minY, maxY;
+      
+      if (chres != null){
+        minX = chres[0].get(0); maxX = chres[0].get(0);
+        minY = chres[1].get(1); maxY = chres[1].get(1);
+        
+        for (int i = 0; i < chres.length; i++) {
+          if (chres[i].get(0) > maxX) {maxX = chres[i].get(0); }
+          if (chres[i].get(0) < minX) {minX = chres[i].get(0); }
+          if (chres[i].get(1) > maxY) {maxY = chres[i].get(1); }
+          if (chres[i].get(1) < minY) {minY = chres[i].get(1); }
+          
+          path.drawTo(chres[i].get(0), chres[i].get(1));
+        }
+        double hullarea = (Math.abs(maxX - minX) * Math.abs(maxY - minY));
+        double projarea = (proj.estimateViewport().getFirst().getMax() - proj.estimateViewport().getFirst().getMin()) *
+                          (proj.estimateViewport().getSecond().getMax() - proj.estimateViewport().getSecond().getMin());
+        
+        opacity =  Math.sqrt(((double) clsPoints.length / database.size()) * ((projarea - hullarea) / projarea));
+        
+        path.close();
+      }
+
+      hulls = path.makeElement(svgp);
+      addCSSClasses(svgp, cnum, opacity);
+      
+      SVGUtil.addCSSClass(hulls, CONVEXHULL + cnum);
+      layer.appendChild(hulls);
+      clusterID++;
+    }
+  }
+  
+  /**
+   * Adds the required CSS-Classes
+   * 
+   * @param svgp SVG-Plot
+   */
+  private void addCSSClasses(SVGPlot svgp, int clusterID, double opac) {
+    ColorLibrary colors = context.getStyleLibrary().getColorSet(StyleLibrary.PLOT);
+    
+    CSSClass cls = new CSSClass(this, CONVEXHULL + clusterID);
+    cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, context.getStyleLibrary().getLineWidth(StyleLibrary.PLOT));
+
+    String color;
+
+    if(clustering.getAllClusters().size() == 1) {
+      color = "black";
+    }
+    else {
+      color = colors.getColor(clusterID);
+    }
+    cls.setStatement(SVGConstants.CSS_STROKE_PROPERTY, color);
+    cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, color);
+    cls.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, opac);
+    
+
+    svgp.addCSSClassOrLogError(cls);
+  }
+  
+/*  @Override
+  public void resultChanged(Result current) {
+    if (current instanceof SelectionResult) {
+      synchronizedRedraw();
+      return;
+    }
+    super.resultChanged(current);
+  }
+*/
+  
+  /**
+   * Factory for visualizers to generate an SVG-Element containing 
+   * the lines between kMeans clusters
+   * 
+   * @author Heidi Kolb
+   * 
+   * @apiviz.stereotype factory
+   * @apiviz.uses kMeansVisualisation oneway - - «create»
+   * 
+   * @param <NV> Type of the NumberVector being visualized.
+   */
+  
+  public static class Factory<NV extends NumberVector<NV, ?>> extends AbstractVisFactory<NV> {
+    /**
+     * Constructor
+     */
+    public Factory() {
+      super();
+    }
+
+    @Override
+    public Visualization makeVisualization(VisualizationTask task) {
+      return new ConvexHullVisualization<NV>(task);
+    }
+
+    @Override
+    public void addVisualizers(VisualizerContext<? extends NV> context, Result result) {
+      if(!VisualizerUtil.isNumberVectorDatabase(context.getDatabase())) {
+        return;
+      }
+      // Find clusterings we can visualize:
+      Collection<Clustering<?>> clusterings = ResultUtil.filterResults(result, Clustering.class);
+      for(Clustering<?> c : clusterings) {
+        if(c.getAllClusters().size() > 0) {
+          // Does the cluster have a model with cluster means?
+          Clustering<MeanModel<NV>> mcls = findMeanModel(c);
+          if(mcls != null) {
+            final VisualizationTask task = new VisualizationTask(NAME, context, c, this, P2DVisualization.class);
+            task.put(VisualizationTask.META_LEVEL, VisualizationTask.LEVEL_DATA + 3);
+            context.addVisualizer(c, task);
+          }
+        }
+      }
+    }
+
+    /**
+     * Test if the given clustering has a mean model.
+     * 
+     * @param <NV> Vector type
+     * @param c Clustering to inspect
+     * @return the clustering cast to return a mean model, null otherwise.
+     */
+    @SuppressWarnings("unchecked")
+    private static <NV extends NumberVector<NV, ?>> Clustering<MeanModel<NV>> findMeanModel(Clustering<?> c) {
+      if(c.getAllClusters().get(0).getModel() instanceof MeanModel<?>) {
+        return (Clustering<MeanModel<NV>>) c;
+      }
+      return null;
+    }
+
+    @Override
+    public Class<? extends Projection> getProjectionType() {
+      return Projection2D.class;
+    }
+  }
+}
+
---
+++ src/experimentalcode/students/roedler/DistanceFunctionVisualization.java
@@ -0,0 +1,292 @@
+package experimentalcode.students.roedler;
+
+import java.util.ArrayList;
+
+import org.apache.batik.util.SVGConstants;
+import org.w3c.dom.Element;
+
+import de.lmu.ifi.dbs.elki.data.DatabaseObject;
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.datastore.DataStoreEvent;
+import de.lmu.ifi.dbs.elki.database.datastore.DataStoreListener;
+import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
+import de.lmu.ifi.dbs.elki.database.query.DistanceResultPair;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.ArcCosineDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.CosineDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.LPNormDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.ManhattanDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.NumberDistance;
+import de.lmu.ifi.dbs.elki.index.preprocessed.knn.MaterializeKNNPreprocessor;
+import de.lmu.ifi.dbs.elki.result.DBIDSelection;
+import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
+import de.lmu.ifi.dbs.elki.result.SelectionResult;
+import de.lmu.ifi.dbs.elki.utilities.exceptions.ObjectNotFoundException;
+import de.lmu.ifi.dbs.elki.visualization.css.CSSClass;
+import de.lmu.ifi.dbs.elki.visualization.projections.Projection;
+import de.lmu.ifi.dbs.elki.visualization.projections.Projection2D;
+import de.lmu.ifi.dbs.elki.visualization.style.StyleLibrary;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGHyperSphere;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
+import de.lmu.ifi.dbs.elki.visualization.svg.SVGUtil;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.AbstractVisFactory;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.Visualization;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizationTask;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.VisualizerContext;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.events.ContextChangeListener;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.thumbs.ThumbnailVisualization;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.vis2d.P2DVisualization;
+import experimentalcode.students.roedler.utils.DistanceFunctionDrawUtils;
+
+/**
+ * Visualizer for generating an SVG-Element containing dots as markers
+ * representing the kNN of the selected Database objects.
+ * 
+ * @author Robert Rödler
+ * 
+ * @apiviz.has SelectionResult oneway - - visualizes
+ * @apiviz.has DBIDSelection oneway - - visualizes
+ * 
+ * @param <NV> Type of the NumberVector being visualized.
+ */
+public class DistanceFunctionVisualization<NV extends NumberVector<NV, ?>, D extends NumberDistance<D, ?>> extends P2DVisualization<NV> implements ContextChangeListener, DataStoreListener<NV> {
+
+  /**
+   * A short name characterizing this Visualizer.
+   */
+  private static final String NAME = "kNN + Distancefunction Visualization";
+
+  /**
+   * Generic tags to indicate the type of element. Used in IDs, CSS-Classes etc.
+   */
+  public static final String KNNMARKER = "kNNMarker";
+
+  public static final String KNNDIST = "kNNDist";
+
+  public static final String DISTANCEFUNCTION = "distancefunction";
+
+  /**
+   * The selection result we work on
+   */
+  private MaterializeKNNPreprocessor<NV, DoubleDistance> result;
+
+  /**
+   * p[0] type of a Norm p[1] value of a Lp Norm
+   */
+  private double[] p;
+
+  /**
+   * Constructor
+   * 
+   * @param task VisualizationTask
+   */
+  public DistanceFunctionVisualization(VisualizationTask task) {
+    super(task);
+    this.result = task.getResult();
+    context.addContextChangeListener(this);
+    context.addDataStoreListener(this);
+    context.addResultListener(this);
+    incrementalRedraw();
+  }
+
+  @Override
+  protected void redraw() {
+    addCSSClasses(svgp);
+    final double size = context.getStyleLibrary().getSize(StyleLibrary.SELECTION);
+    DBIDSelection selContext = context.getSelection();
+    p = getLPNormP(result);
+
+    if(selContext != null) {
+      Database<? extends NV> database = context.getDatabase();
+      DBIDs selection = selContext.getSelectedIds();
+      int counter;
+      Element dist = null;
+
+      for(DBID i : selection) {
+        counter = 1;
+
+        drawDistancefunction: for(DistanceResultPair<DoubleDistance> j : result.get(i)) {
+          try {
+            double[] v = proj.fastProjectDataToRenderSpace(database.get(j.second));
+            Element dot = svgp.svgCircle(v[0], v[1], size);
+            SVGUtil.addCSSClass(dot, KNNMARKER);
+            layer.appendChild(dot);
+
+            Element lbl = svgp.svgText(v[0] + size, v[1] + size, j.getDistance().toString());
+            SVGUtil.addCSSClass(lbl, KNNDIST);
+            layer.appendChild(lbl);
+
+            if(result.getK() == counter) {
+              switch((int) p[0]){
+              case 1: {
+                dist = SVGHyperSphere.drawManhattan(svgp, proj, (NV) database.get(i), j.getDistance());
+                break;
+              }
+              case 2: {
+                dist = SVGHyperSphere.drawEuclidean(svgp, proj, (NV) database.get(i), j.getDistance());
+                break;
+              }
+              case 3: {
+                dist = SVGHyperSphere.drawLp(svgp, proj, (NV) database.get(i), j.getDistance(), p[1]);
+                break;
+              }
+              case 4: {
+                dist = DistanceFunctionDrawUtils.drawCosine(svgp, proj, database.get(i), database.get(j.second));
+                break;
+              }
+              default: {
+                break drawDistancefunction;
+              }
+              }
+
+              SVGUtil.addCSSClass(dist, DISTANCEFUNCTION);
+              layer.appendChild(dist);
+            }
+            counter++;
+          }
+          catch(ObjectNotFoundException e) {
+            // ignore
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Adds the required CSS-Classes
+   * 
+   * @param svgp SVG-Plot
+   */
+  private void addCSSClasses(SVGPlot svgp) {
+    final StyleLibrary style = context.getStyleLibrary();
+    // Class for the distance markers
+    if(!svgp.getCSSClassManager().contains(KNNMARKER)) {
+      CSSClass cls = new CSSClass(this, KNNMARKER);
+      cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_DARKGREEN_VALUE);
+      cls.setStatement(SVGConstants.CSS_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
+      svgp.addCSSClassOrLogError(cls);
+    }
+    // Class for the distance function
+    if(!svgp.getCSSClassManager().contains(DISTANCEFUNCTION)) {
+      CSSClass cls = new CSSClass(this, DISTANCEFUNCTION);
+      cls.setStatement(SVGConstants.CSS_STROKE_PROPERTY, SVGConstants.CSS_RED_VALUE);
+      cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, style.getLineWidth(StyleLibrary.PLOT));
+      cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_NONE_VALUE);
+      cls.setStatement(SVGConstants.CSS_STROKE_LINECAP_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
+      cls.setStatement(SVGConstants.CSS_STROKE_LINEJOIN_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
+      svgp.addCSSClassOrLogError(cls);
+    }
+    // Class for the distance label
+    if(!svgp.getCSSClassManager().contains(KNNDIST)) {
+      CSSClass cls = new CSSClass(this, KNNDIST);
+      cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_BLACK_VALUE);
+      cls.setStatement(SVGConstants.CSS_FONT_SIZE_PROPERTY, style.getTextSize(StyleLibrary.PLOT));
+      cls.setStatement(SVGConstants.CSS_FONT_FAMILY_PROPERTY, style.getFontFamily(StyleLibrary.PLOT));
+      svgp.addCSSClassOrLogError(cls);
+    }
+  }
+
+  /**
+   * Get the "p" value of an Lp norm.
+   * 
+   * @param kNN MaterializeKNNPreprocessor<?,?>
+   * @return p[0] indicates the type of the Distancefunction, p[0] = 1 ->
+   *         Manhattan = 2 -> Euclidean = 3 -> Lp Norm = 4 -> ArcCosine and
+   *         Cosine =-1 -> visualization not implemented so far p[1] the p value
+   *         itself
+   */
+  public static double[] getLPNormP(MaterializeKNNPreprocessor<?, ?> kNN) {
+    double[] p = new double[2];
+    // Note: we deliberately lose generics here, so the compilers complain less
+    // on the next typecheck and cast!
+    DistanceFunction<?, ?> distanceFunction = kNN.getDistanceQuery().getDistanceFunction();
+    if(ManhattanDistanceFunction.class.isInstance(distanceFunction)) {
+      p[0] = 1.0;
+    }
+    else if(EuclideanDistanceFunction.class.isInstance(distanceFunction)) {
+      p[0] = 2.0;
+    }
+    else if(LPNormDistanceFunction.class.isInstance(distanceFunction)) {
+      p[1] = ((LPNormDistanceFunction) distanceFunction).getP();
+      p[0] = 3.0;
+    }
+    else if(CosineDistanceFunction.class.isInstance(distanceFunction) || ArcCosineDistanceFunction.class.isInstance(distanceFunction)) {
+      p[0] = 4.0;
+    }
+    else {
+      p[0] = -1.0;
+    }
+    return p;
+  }
+
+  @Override
+  public void contentChanged(@SuppressWarnings("unused") DataStoreEvent<NV> e) {
+    synchronizedRedraw();
+  }
+
+  @Override
+  public void resultChanged(Result current) {
+    if(current instanceof SelectionResult) {
+      synchronizedRedraw();
+      return;
+    }
+    super.resultChanged(current);
+  }
+
+  @Override
+  public void destroy() {
+    super.destroy();
+    context.removeContextChangeListener(this);
+    context.removeDataStoreListener(this);
+  }
+
+  /**
+   * Factory for visualizers to generate an SVG-Element containing dots as
+   * markers representing the kNN of the selected Database objects.
+   * 
+   * @author Robert Rödler
+   * 
+   * @apiviz.stereotype factory
+   * @apiviz.uses DistanceFunctionVisualisation oneway - - «create»
+   * 
+   * @param <NV> Type of the NumberVector being visualized.
+   */
+  public static class Factory<NV extends NumberVector<NV, ?>> extends AbstractVisFactory<NV> {
+    /**
+     * Constructor
+     */
+    public Factory() {
+      super();
+    }
+
+    @Override
+    public Visualization makeVisualization(VisualizationTask task) {
+      return new DistanceFunctionVisualization<NV, DoubleDistance>(task);
+    }
+
+    @Override
+    public Visualization makeVisualizationOrThumbnail(VisualizationTask task) {
+      return new ThumbnailVisualization<DatabaseObject>(this, task, ThumbnailVisualization.ON_DATA | ThumbnailVisualization.ON_SELECTION);
+    }
+
+    @Override
+    public void addVisualizers(VisualizerContext<? extends NV> context, Result result) {
+      final ArrayList<MaterializeKNNPreprocessor<NV, DoubleDistance>> kNNIndex = ResultUtil.filterResults(result, MaterializeKNNPreprocessor.class);
+      for(MaterializeKNNPreprocessor<NV, DoubleDistance> kNN : kNNIndex) {
+        final VisualizationTask task = new VisualizationTask(NAME, context, kNN, this, P2DVisualization.class);
+        task.put(VisualizationTask.META_LEVEL, VisualizationTask.LEVEL_DATA - 1);
+        context.addVisualizer(kNN, task);
+      }
+    }
+
+    @Override
+    public Class<? extends Projection> getProjectionType() {
+      return Projection2D.class;
+    }
+  }
+}
