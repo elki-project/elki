aa8bb5d81e35f9a44e2c463a68f9abada45afd38
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-04-19 16:59:58 +0000

    Make alias for ResultHandler

--- src/de/lmu/ifi/dbs/elki/evaluation/Evaluator.java
+++ src/de/lmu/ifi/dbs/elki/evaluation/Evaluator.java
@@ -1,8 +1,6 @@
 package de.lmu.ifi.dbs.elki.evaluation;
 
-import de.lmu.ifi.dbs.elki.database.Database;
-import de.lmu.ifi.dbs.elki.result.Result;
-import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable;
+import de.lmu.ifi.dbs.elki.result.ResultHandler;
 
 /**
  * Interface for post-algorithm evaluations, such as histograms, outlier score
@@ -10,12 +8,6 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable;
  * 
  * @author Erich Schubert
  */
-public interface Evaluator extends Parameterizable {
-  /**
-   * Process a result.
-   * 
-   * @param database Database the result is for
-   * @param result Result object
-   */
-  public abstract void processResult(Database database, Result result);
+public interface Evaluator extends ResultHandler {
+  // Empty now - uses ResultHandler API. Remove?
 }
