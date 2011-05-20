43e28f40f05e8e5f60c7d9358130ba6363f27be2
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-05-20 06:38:51 +0000

    add a by-label filter for data import. some spelling corrections.

---
+++ src/de/lmu/ifi/dbs/elki/datasource/filter/FilterByLabelFilter.java
@@ -0,0 +1,123 @@
+package de.lmu.ifi.dbs.elki.datasource.filter;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import de.lmu.ifi.dbs.elki.data.LabelList;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.datasource.bundle.MultipleObjectsBundle;
+import de.lmu.ifi.dbs.elki.logging.Logging;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.PatternParameter;
+
+/**
+ * A filter to sort the data set by some label.
+ * 
+ * @author Erich Schubert
+ */
+public class FilterByLabelFilter implements ObjectFilter {
+  /**
+   * Class logger
+   */
+  private static final Logging logger = Logging.getLogger(FilterByLabelFilter.class);
+
+  /**
+   * The filter pattern
+   */
+  private final Pattern pattern;
+
+  /**
+   * Constructor.
+   * 
+   * @param pattern Filter pattern
+   */
+  public FilterByLabelFilter(Pattern pattern) {
+    super();
+    this.pattern = pattern;
+  }
+
+  @Override
+  public MultipleObjectsBundle filter(final MultipleObjectsBundle objects) {
+    if(logger.isDebugging()) {
+      logger.debug("Filtering the data set");
+    }
+
+    // Identify a label column
+    final int lblcol;
+    {
+      int lblc = -1;
+      for(int i = 0; i < objects.metaLength(); i++) {
+        if(TypeUtil.GUESSED_LABEL.isAssignableFromType(objects.meta(i))) {
+          lblc = i;
+          break;
+        }
+      }
+      lblcol = lblc; // make static
+    }
+
+    MultipleObjectsBundle bundle = new MultipleObjectsBundle();
+    for(int j = 0; j < objects.metaLength(); j++) {
+      bundle.appendColumn(objects.meta(j), new ArrayList<Object>());
+    }
+    for(int i = 0; i < objects.dataLength(); i++) {
+      Object l = objects.data(i, lblcol);
+      if(l instanceof LabelList) {
+        boolean good = false;
+        for(String label : (LabelList) l) {
+          if(pattern.matcher(label).matches()) {
+            good = true;
+            break;
+          }
+        }
+        if(!good) {
+          continue;
+        }
+      }
+      else {
+        if(!pattern.matcher(l.toString()).matches()) {
+          continue;
+        }
+      }
+      bundle.appendSimple(objects.getRow(i));
+    }
+    return bundle;
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    /**
+     * Parameter that specifies the filter pattern (regular expression).
+     * <p>
+     * Key: {@code -patternfilter}
+     * </p>
+     */
+    public static final OptionID LABELFILTER_PATTERN_ID = OptionID.getOrCreateOptionID("patternfilter", "The filter pattern to use.");
+
+    /**
+     * The pattern configured.
+     */
+    Pattern pattern = null;
+
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+      final PatternParameter patternP = new PatternParameter(LABELFILTER_PATTERN_ID);
+      if(config.grab(patternP)) {
+        pattern = patternP.getValue();
+      }
+    }
+
+    @Override
+    protected Object makeInstance() {
+      return new FilterByLabelFilter(pattern);
+    }
+  }
+}
