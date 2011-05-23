67115498287ef11907c7ee0825f027bf8f2d77c5
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-05-23 17:15:36 +0000

    Simplify UpdatableDatabase API.

--- src/de/lmu/ifi/dbs/elki/database/UpdatableDatabase.java
+++ src/de/lmu/ifi/dbs/elki/database/UpdatableDatabase.java
@@ -1,11 +1,7 @@
 package de.lmu.ifi.dbs.elki.database;
 
-import java.util.List;
-
-import de.lmu.ifi.dbs.elki.database.ids.DBID;
 import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
-import de.lmu.ifi.dbs.elki.datasource.bundle.MultipleObjectsBundle;
-import de.lmu.ifi.dbs.elki.datasource.bundle.SingleObjectBundle;
+import de.lmu.ifi.dbs.elki.datasource.bundle.ObjectBundle;
 import de.lmu.ifi.dbs.elki.utilities.exceptions.UnableToComplyException;
 
 /**
@@ -16,31 +12,13 @@ import de.lmu.ifi.dbs.elki.utilities.exceptions.UnableToComplyException;
  */
 public interface UpdatableDatabase extends Database {
   /**
-   * Inserts the given objects and their associations into the database.
+   * Inserts the given object(s) and their associations into the database.
    * 
    * @param objpackages the objects to be inserted
    * @return the IDs assigned to the inserted objects
    * @throws UnableToComplyException if insertion is not possible
    */
-  DBIDs insert(MultipleObjectsBundle objpackages) throws UnableToComplyException;
-
-  /**
-   * Inserts the given object package into the database.
-   * 
-   * @param objpackage the packaged object
-   * @return the ID assigned to the inserted object
-   * @throws UnableToComplyException if insertion is not possible
-   */
-  DBID insert(SingleObjectBundle objpackage) throws UnableToComplyException;
-
-  /**
-   * Removes and returns the object with the given id from the database.
-   * 
-   * @param id the id of an object to be removed from the database
-   * @return the object that has been removed
-   * @throws UnableToComplyException if deletion is not possible
-   */
-  SingleObjectBundle delete(DBID id);
+  DBIDs insert(ObjectBundle objpackages) throws UnableToComplyException;
 
   /**
    * Removes and returns the specified objects with the given ids from the
@@ -50,5 +28,5 @@ public interface UpdatableDatabase extends Database {
    * @return the objects that have been removed
    * @throws UnableToComplyException if deletion is not possible
    */
-  List<SingleObjectBundle> delete(DBIDs ids);
+  ObjectBundle delete(DBIDs ids);
 }
