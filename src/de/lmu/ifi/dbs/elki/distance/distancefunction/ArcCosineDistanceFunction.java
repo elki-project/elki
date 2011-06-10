7799adb7526f4b4d94c48bb902d032c04927919d
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-06-10 13:10:36 +0000

    Turn cosine distances into double distances to allow optimizations.

--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/ArcCosineDistanceFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/ArcCosineDistanceFunction.java
@@ -1,13 +1,6 @@
 package de.lmu.ifi.dbs.elki.distance.distancefunction;
 
-import java.util.BitSet;
-
 import de.lmu.ifi.dbs.elki.data.NumberVector;
-import de.lmu.ifi.dbs.elki.data.SparseNumberVector;
-import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
-import de.lmu.ifi.dbs.elki.data.type.VectorFieldTypeInformation;
-import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
-import de.lmu.ifi.dbs.elki.math.linearalgebra.Vector;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 
 /**
@@ -18,7 +11,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
  * 
  * @author Arthur Zimek
  */
-public class ArcCosineDistanceFunction extends AbstractPrimitiveDistanceFunction<NumberVector<?,?>, DoubleDistance> {
+public class ArcCosineDistanceFunction extends AbstractCosineDistanceFunction {
   /**
    * Static instance
    */
@@ -45,68 +38,12 @@ public class ArcCosineDistanceFunction extends AbstractPrimitiveDistanceFunction
    * @return the cosine distance for two given feature vectors v1 and v2
    */
   @Override
-  public DoubleDistance distance(NumberVector<?,?> v1, NumberVector<?,?> v2) {
-    if (v1 instanceof SparseNumberVector<?,?> && v2 instanceof SparseNumberVector<?, ?>) {
-      return sparseDistance((SparseNumberVector<?,?>)v1, (SparseNumberVector<?,?>)v2);
-    }
-    Vector m1 = v1.getColumnVector();
-    m1.normalize();
-    Vector m2 = v2.getColumnVector();
-    m2.normalize();
-
-    double d = Math.acos(m1.transposeTimes(m2));
+  public double doubleDistance(NumberVector<?,?> v1, NumberVector<?,?> v2) {
+    double d = Math.acos(angle(v1, v2));
     if(d < 0) {
       d = 0;
     }
-    return new DoubleDistance(d);
-  }
-
-  /**
-   * Compute the cosine distance for sparse vectors.
-   * 
-   * @param v1 First vector
-   * @param v2 Second vector
-   * @return Cosine distance
-   */
-  private DoubleDistance sparseDistance(SparseNumberVector<?, ?> v1, SparseNumberVector<?, ?> v2) {
-    BitSet b1 = v1.getNotNullMask();
-    BitSet b2 = v2.getNotNullMask();
-    BitSet both = (BitSet) b1.clone();
-    both.and(b2);
-    
-    // Length of first vector
-    double l1 = 0.0;
-    for (int i = b1.nextSetBit(0); i >= 0; i = b1.nextSetBit(i+1)) {
-      final double val = v1.doubleValue(i);
-      l1 += val * val;
-    }
-    l1 = Math.sqrt(l1);
-    
-    // Length of second vector
-    double l2 = 0.0;
-    for (int i = b2.nextSetBit(0); i >= 0; i = b2.nextSetBit(i+1)) {
-      final double val = v2.doubleValue(i);
-      l2 += val * val;
-    }
-    l2 = Math.sqrt(l2);
-    
-    // Cross product
-    double cross = 0.0;
-    for (int i = both.nextSetBit(0); i >= 0; i = both.nextSetBit(i+1)) {
-      cross += v1.doubleValue(i) * v2.doubleValue(i);
-    }
-    
-    return new DoubleDistance(Math.acos(cross / (l1*l2)));
-  }
-
-  @Override
-  public VectorFieldTypeInformation<? super NumberVector<?, ?>> getInputTypeRestriction() {
-    return TypeUtil.NUMBER_VECTOR_FIELD;
-  }
-
-  @Override
-  public DoubleDistance getDistanceFactory() {
-    return DoubleDistance.FACTORY;
+    return d;
   }
 
   @Override
--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/CosineDistanceFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/CosineDistanceFunction.java
@@ -1,13 +1,7 @@
 package de.lmu.ifi.dbs.elki.distance.distancefunction;
 
-import java.util.BitSet;
 
 import de.lmu.ifi.dbs.elki.data.NumberVector;
-import de.lmu.ifi.dbs.elki.data.SparseNumberVector;
-import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
-import de.lmu.ifi.dbs.elki.data.type.VectorFieldTypeInformation;
-import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
-import de.lmu.ifi.dbs.elki.math.linearalgebra.Vector;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 
 /**
@@ -18,12 +12,12 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
  * 
  * @author Arthur Zimek
  */
-public class CosineDistanceFunction extends AbstractPrimitiveDistanceFunction<NumberVector<?,?>, DoubleDistance> {
+public class CosineDistanceFunction extends AbstractCosineDistanceFunction {
   /**
    * Static instance
    */
-  public static final CosineDistanceFunction STATIC = new CosineDistanceFunction();
-  
+  public static final AbstractCosineDistanceFunction STATIC = new CosineDistanceFunction();
+
   /**
    * Provides a CosineDistanceFunction.
    * 
@@ -45,68 +39,12 @@ public class CosineDistanceFunction extends AbstractPrimitiveDistanceFunction<Nu
    * @return the cosine distance for two given feature vectors v1 and v2
    */
   @Override
-  public DoubleDistance distance(NumberVector<?,?> v1, NumberVector<?,?> v2) {
-    if (v1 instanceof SparseNumberVector<?,?> && v2 instanceof SparseNumberVector<?, ?>) {
-      return sparseDistance((SparseNumberVector<?,?>)v1, (SparseNumberVector<?,?>)v2);
-    }
-    Vector m1 = v1.getColumnVector();
-    m1.normalize();
-    Vector m2 = v2.getColumnVector();
-    m2.normalize();
-
-    double d = 1 - m1.transposeTimes(m2);
+  public double doubleDistance(NumberVector<?, ?> v1, NumberVector<?, ?> v2) {
+    double d = 1 - angle(v1, v2);
     if(d < 0) {
       d = 0;
     }
-    return new DoubleDistance(d);
-  }
-
-  /**
-   * Compute the cosine distance for sparse vectors.
-   * 
-   * @param v1 First vector
-   * @param v2 Second vector
-   * @return Cosine distance
-   */
-  private DoubleDistance sparseDistance(SparseNumberVector<?, ?> v1, SparseNumberVector<?, ?> v2) {
-    BitSet b1 = v1.getNotNullMask();
-    BitSet b2 = v2.getNotNullMask();
-    BitSet both = (BitSet) b1.clone();
-    both.and(b2);
-    
-    // Length of first vector
-    double l1 = 0.0;
-    for (int i = b1.nextSetBit(0); i >= 0; i = b1.nextSetBit(i+1)) {
-      final double val = v1.doubleValue(i);
-      l1 += val * val;
-    }
-    l1 = Math.sqrt(l1);
-    
-    // Length of second vector
-    double l2 = 0.0;
-    for (int i = b2.nextSetBit(0); i >= 0; i = b2.nextSetBit(i+1)) {
-      final double val = v2.doubleValue(i);
-      l2 += val * val;
-    }
-    l2 = Math.sqrt(l2);
-    
-    // Cross product
-    double cross = 0.0;
-    for (int i = both.nextSetBit(0); i >= 0; i = both.nextSetBit(i+1)) {
-      cross += v1.doubleValue(i) * v2.doubleValue(i);
-    }
-    
-    return new DoubleDistance(1. - cross / (l1*l2));
-  }
-
-  @Override
-  public VectorFieldTypeInformation<? super NumberVector<?, ?>> getInputTypeRestriction() {
-    return TypeUtil.NUMBER_VECTOR_FIELD;
-  }
-
-  @Override
-  public DoubleDistance getDistanceFactory() {
-    return DoubleDistance.FACTORY;
+    return d;
   }
 
   @Override
@@ -123,7 +61,7 @@ public class CosineDistanceFunction extends AbstractPrimitiveDistanceFunction<Nu
    */
   public static class Parameterizer extends AbstractParameterizer {
     @Override
-    protected CosineDistanceFunction makeInstance() {
+    protected AbstractCosineDistanceFunction makeInstance() {
       return CosineDistanceFunction.STATIC;
     }
   }
