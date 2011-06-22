4b757421119180d983c639921bf6cb5491dd14eb
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-06-22 11:03:19 +0000

    A rather obscure normalization, mostly for meanvariance normalized data.

---
+++ src/de/lmu/ifi/dbs/elki/datasource/filter/AttributeWiseErfNormalization.java
@@ -0,0 +1,43 @@
+package de.lmu.ifi.dbs.elki.datasource.filter;
+
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.type.SimpleTypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.math.ErrorFunctions;
+
+/**
+ * Attribute-wise Normalization using the error function. This mostly makes
+ * sense when you have data that has been mean-variance normalized before.
+ * 
+ * @author Erich Schubert
+ * 
+ * @param <O> Object type
+ */
+public class AttributeWiseErfNormalization<O extends NumberVector<O, ?>> extends AbstractNormalization<O> {
+  /**
+   * Constructor.
+   */
+  public AttributeWiseErfNormalization() {
+    super();
+  }
+
+  @SuppressWarnings("unused")
+  @Override
+  public O restore(O featureVector) throws NonNumericFeaturesException {
+    throw new UnsupportedOperationException("Not implemented yet.");
+  }
+
+  @Override
+  protected O filterSingleObject(O obj) {
+    double[] val = new double[obj.getDimensionality()];
+    for(int i = 0; i < val.length; i++) {
+      val[i] = ErrorFunctions.erf(obj.doubleValue(i + 1));
+    }
+    return obj.newInstance(val);
+  }
+
+  @Override
+  protected SimpleTypeInformation<? super O> getInputTypeRestriction() {
+    return TypeUtil.NUMBER_VECTOR_FIELD;
+  }
+}
