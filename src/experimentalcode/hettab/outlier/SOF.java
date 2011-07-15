53d1724d1f7dbcbd0634dd330eb775fffe6975da
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-07-15 13:21:31 +0000

    <empty commit message>

--- src/experimentalcode/hettab/outlier/SOF.java
+++ src/experimentalcode/hettab/outlier/SOF.java
@@ -1,6 +1,5 @@
 package experimentalcode.hettab.outlier;
 
-import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.data.type.TypeInformation;
 import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
 import de.lmu.ifi.dbs.elki.database.AssociationID;
@@ -20,23 +19,53 @@ import de.lmu.ifi.dbs.elki.result.AnnotationResult;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierScoreMeta;
 import de.lmu.ifi.dbs.elki.result.outlier.QuotientOutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Title;
 import experimentalcode.shared.outlier.generalized.neighbors.NeighborSetPredicate;
 
 /**
+ * The Spatial Outlier Factor (SOF) is a spatial
+ * {@link de.lmu.ifi.dbs.elki.algorithms.outlier.LOF LOF} variation.
+ * 
+ * Since the "reachability distance" of LOF cannot be used canonically in the
+ * bichromatic case, this part of LOF is dropped and the exact distance is used
+ * instead.
+ * 
+ * <p>
+ * Huang, T., Qin, X.<br>
+ * Detecting outliers in spatial database.<br>
+ * In: Proceedings of the 3rd International Conference on Image and Graphics,
+ * Hong Kong, China.
+ * </p>
+ * 
+ * A LOF variation simplified with reachDist(o,p) == dist(o,p).
  * 
  * @author Ahmed Hettab
  * 
- * @param <V>
- * @param <D> reachDist(o,p) = dist(o,p)
+ * @param <N> Neighborhood type
+ * @param <V> Object type
+ * @param <D> Distance type
  */
-public class SOF<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedSpatialOutlier<N, V, D> {
+@Title("Spatial Outlier Factor")
+@Reference(authors = "Huang, T., Qin, X.", title = "Detecting outliers in spatial database", booktitle = "Proceedings of the 3rd International Conference on Image and Graphics", url = "http://dx.doi.org/10.1109/ICIG.2004.53")
+public class SOF<N, V, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedSpatialOutlier<N, V, D> {
   /**
    * The logger for this class.
    */
   private static final Logging logger = Logging.getLogger(SOF.class);
 
-  public static final AssociationID<Double> LOF_SCORE = AssociationID.getOrCreateAssociationID("lof", Double.class);
+  /**
+   * Association ID used for output
+   */
+  public static final AssociationID<Double> SOF_SCORE = AssociationID.getOrCreateAssociationID("sof", Double.class);
 
+  /**
+   * Constructor.
+   * 
+   * @param npred Neighborhood predicate
+   * @param nonSpatialDistanceFunction Distance function on non-spatial
+   *        attributes
+   */
   public SOF(NeighborSetPredicate.Factory<N> npred, PrimitiveDistanceFunction<V, D> nonSpatialDistanceFunction) {
     super(npred, nonSpatialDistanceFunction);
   }
@@ -46,13 +75,22 @@ public class SOF<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>
     return logger;
   }
 
-  public OutlierResult run(Database database, Relation<N> spatial, Relation<V> relation) throws IllegalStateException {
+  /**
+   * The main run method
+   * 
+   * @param database Database to use (actually unused)
+   * @param spatial Relation for neighborhood
+   * @param relation Attributes to evaluate
+   * @return Outlier result
+   */
+  public OutlierResult run(Database database, Relation<N> spatial, Relation<V> relation) {
     final NeighborSetPredicate npred = getNeighborSetPredicateFactory().instantiate(spatial);
 
     WritableDataStore<Double> lrds = DataStoreUtil.makeStorage(relation.getDBIDs(), DataStoreFactory.HINT_TEMP | DataStoreFactory.HINT_HOT, Double.class);
     WritableDataStore<Double> lofs = DataStoreUtil.makeStorage(relation.getDBIDs(), DataStoreFactory.HINT_STATIC, Double.class);
     DoubleMinMax lofminmax = new DoubleMinMax();
 
+    // Compute densities
     for(DBID id : relation.iterDBIDs()) {
       DBIDs neighbors = npred.getNeighborDBIDs(id);
       double avg = 0;
@@ -62,6 +100,7 @@ public class SOF<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>
       lrds.put(id, 1 / (avg / neighbors.size()));
     }
 
+    // Compute density quotients
     for(DBID id : relation.iterDBIDs()) {
       DBIDs neighbors = npred.getNeighborDBIDs(id);
       double avg = 0;
@@ -74,7 +113,7 @@ public class SOF<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>
     }
 
     // Build result representation.
-    AnnotationResult<Double> scoreResult = new AnnotationFromDataStore<Double>("Local Outlier Factor", "lof-outlier", LOF_SCORE, lofs);
+    AnnotationResult<Double> scoreResult = new AnnotationFromDataStore<Double>("Spatial Outlier Factor", "sof-outlier", SOF_SCORE, lofs);
     OutlierScoreMeta scoreMeta = new QuotientOutlierScoreMeta(lofminmax.getMin(), lofminmax.getMax(), 0.0, Double.POSITIVE_INFINITY, 1.0);
     OutlierResult result = new OutlierResult(scoreMeta, scoreResult);
     return result;
@@ -85,7 +124,16 @@ public class SOF<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>
     return TypeUtil.array(getNeighborSetPredicateFactory().getInputTypeRestriction(), TypeUtil.NUMBER_VECTOR_FIELD);
   }
 
-  public static class Parameterizer<N, V extends NumberVector<?, ?>, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedSpatialOutlier.Parameterizer<N, V, D> {
+  /**
+   * Parameterization class
+   * 
+   * @author Ahmed Hettab
+   * 
+   * @param <N> Neighborhood type
+   * @param <V> Attribute type
+   * @param <D> Distance type
+   */
+  public static class Parameterizer<N, V, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedSpatialOutlier.Parameterizer<N, V, D> {
     @Override
     protected SOF<N, V, D> makeInstance() {
       return new SOF<N, V, D>(npredf, distanceFunction);
