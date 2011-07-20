de49f4053b8b7bc9f066275c60b548bf700075df
Ahmed Hettab <hettab@cip.ifi.lmu.de>
2011-07-20 13:50:52 +0000

    Fix

--- src/experimentalcode/hettab/outlier/MoranScatterPlotOutlier.java
+++ src/experimentalcode/hettab/outlier/MoranScatterPlotOutlier.java
@@ -21,14 +21,28 @@ import de.lmu.ifi.dbs.elki.result.AnnotationFromDataStore;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierScoreMeta;
 import de.lmu.ifi.dbs.elki.result.outlier.QuotientOutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Description;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Title;
 
 /**
- * FIXME: Documentation, Reference
+ * <p>
+ * Reference: <br>
+ * S. Shekhar and C.-T. Lu and P. Zhang <br>
+ * A Unified Approach to Detecting Spatial Outliers <br>
+ * in GeoInformatica 7-2, 2003
  * 
- * @author hettab
+ *<p> Moran scatterplot is a plot of normalized attribute values 
+ * against the neighborhood average of normalized attribute values. 
+ * Spatial Objects on the upper left or lower right are Spatial Outliers.
+ * 
+ * @author Ahmed Hettab
  * 
  * @param <N> Neighborhood type
  */
+@Title("A Unified Approach to Spatial Outliers Detection")
+@Description("Spatial Outlier Detection Algorithm")
+@Reference(authors = "S. Shekhar and C.-T. Lu and P. Zhang", title = "A Unified Approach to Detecting Spatial Outliers", booktitle = "GeoInformatica 7-2, 2003")
 public class MoranScatterPlotOutlier<N> extends AbstractNeighborhoodOutlier<N> {
   /**
    * The logger for this class.
@@ -36,8 +50,8 @@ public class MoranScatterPlotOutlier<N> extends AbstractNeighborhoodOutlier<N> {
   private static final Logging logger = Logging.getLogger(MoranScatterPlotOutlier.class);
 
   /**
-   * FIXME: Documentation
-   * The association id to associate the SLOM_SCORE of an object for the SLOM
+   * 
+   * The association id to associate the SCORE of an object for the MoranScatterplotOutlier
    * algorithm.
    */
   public static final AssociationID<Double> SCORE = AssociationID.getOrCreateAssociationID("Moran-Outlier", TypeUtil.DOUBLE);
@@ -63,30 +77,32 @@ public class MoranScatterPlotOutlier<N> extends AbstractNeighborhoodOutlier<N> {
     final NeighborSetPredicate npred = getNeighborSetPredicateFactory().instantiate(nrel);
     WritableDataStore<Double> stdZ = DataStoreUtil.makeStorage(relation.getDBIDs(), DataStoreFactory.HINT_TEMP, Double.class);
     WritableDataStore<Double> neighborStdZ = DataStoreUtil.makeStorage(relation.getDBIDs(), DataStoreFactory.HINT_TEMP, Double.class);
-    MeanVariance stdZMV = new MeanVariance();
-
+    
+    // add non-spatial value to MeanVariance 
+    MeanVariance stdZMV = new MeanVariance();    
     for(DBID id : relation.getDBIDs()) {
       stdZMV.put(relation.get(id).doubleValue(1));
     }
 
-    //System.out.println(stdZMV.getMean());
-    //System.out.println(stdZMV.getSampleVariance());
-
+    
+    // calculate normalized attribute values 
+    //calculate neighborhood average of normalized attribute values.
     for(DBID id : relation.getDBIDs()) {
-      double zValue = (relation.get(id).doubleValue(1) - stdZMV.getMean()) / stdZMV.getSampleVariance();
+      double zValue = (relation.get(id).doubleValue(1) - stdZMV.getMean()) / stdZMV.getNaiveStddev();
       stdZ.put(id, zValue);
       double neighborZValue = 0;
       for(DBID n : npred.getNeighborDBIDs(id)) {
-        neighborZValue += (relation.get(n).doubleValue(1) - stdZMV.getMean()) / stdZMV.getSampleVariance();
+        neighborZValue += (relation.get(n).doubleValue(1) - stdZMV.getMean()) / stdZMV.getNaiveStddev();
       }
       neighborStdZ.put(id, neighborZValue / npred.getNeighborDBIDs(id).size());
     }
-
+    
+    // compute score
+    //Spatial Object with score=1 are Spatial Outlier
     DoubleMinMax minmax = new DoubleMinMax();
     WritableDataStore<Double> scores = DataStoreUtil.makeStorage(relation.getDBIDs(), DataStoreFactory.HINT_STATIC, Double.class);
     for(DBID id : relation.getDBIDs()) {
       double score = stdZ.get(id) * neighborStdZ.get(id);
-      // System.out.println(score);
       if(score < 0) {
         minmax.put(1.0);
         scores.put(id, 1.0);
