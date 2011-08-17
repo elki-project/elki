abcf1db364ca14c598bca5bae4c9c4bef0549b4b
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-08-17 09:23:48 +0000

    add AGPL copyright headers.

--- src/de/lmu/ifi/dbs/elki/math/linearalgebra/package-info.java
+++ src/de/lmu/ifi/dbs/elki/math/linearalgebra/package-info.java
@@ -39,4 +39,26 @@ can be found at <a href="http://math.nist.gov/javanumerics/jama/">http://math.ni
    Here, for the adaption some classes and methods convenient for data mining applications within ELKI were added.
    Furthermore some erroneous comments were corrected and the coding-style was subtly changed to a more Java-typical style.
 */
+/*
+This file is part of ELKI:
+Environment for Developing KDD-Applications Supported by Index-Structures
+
+Copyright (C) 2011
+Ludwig-Maximilians-Universität München
+Lehr- und Forschungseinheit für Datenbanksysteme
+ELKI Development Team
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package de.lmu.ifi.dbs.elki.math.linearalgebra;
