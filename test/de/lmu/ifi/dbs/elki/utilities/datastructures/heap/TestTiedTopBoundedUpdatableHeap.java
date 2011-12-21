04a0c828485ecce8f8d560047f78d08b126c0ef3
Erich Schubert <schube@dbs.ifi.lmu.de>
2011-12-21 17:52:54 +0000

    <empty commit message>

--- test/de/lmu/ifi/dbs/elki/utilities/datastructures/heap/TestTiedTopBoundedUpdatableHeap.java
+++ test/de/lmu/ifi/dbs/elki/utilities/datastructures/heap/TestTiedTopBoundedUpdatableHeap.java
@@ -38,8 +38,8 @@ public class TestTiedTopBoundedUpdatableHeap implements JUnit4Test {
   public void testTiedTopBoundedUpdatableHeap() {
     final int iters = 100;
     final int maxid = 5000;
-    final int bsize = 100;
-    final int limit = 50;
+    final int bsize = 1000;
+    final int limit = 200;
     final Random r = new Random(1);
     ArrayList<IntegerPriorityObject<Integer>> simulate = new ArrayList<IntegerPriorityObject<Integer>>(1000);
     UpdatableHeap<IntegerPriorityObject<Integer>> heap = new TiedTopBoundedUpdatableHeap<IntegerPriorityObject<Integer>>(limit);
@@ -55,7 +55,6 @@ public class TestTiedTopBoundedUpdatableHeap implements JUnit4Test {
         for(IntegerPriorityObject<Integer> ent : simulate) {
           if(ent.getObject().equals(id)) {
             if(score > ent.priority) {
-              System.err.println("Updating in ArrayList: " + ent + " to " + score);
               ent.priority = score;
             }
             found = true;
@@ -72,15 +71,18 @@ public class TestTiedTopBoundedUpdatableHeap implements JUnit4Test {
             break;
           }
           else {
-            assert(simulate.get(simulate.size() - 1).priority > max);
+            // assertTrue(simulate.get(simulate.size() - 1).priority > max);
             simulate.remove(simulate.size() - 1);
           }
         }
-        System.err.println("Sim: " + simulate.get(simulate.size() - 1) + " <=> Heap: " + heap.peek());
+        if(simulate.size() != heap.size()) {
+          System.err.println("Lost synchronization: " + id + " to " + score);
+          System.err.println("Sim: " + simulate.get(simulate.size() - 1) + " <=> Heap: " + heap.peek());
+        }
+        assertEquals("Sizes don't match!", simulate.size(), heap.size());
       }
-      System.err.println("Sim: " + simulate.get(simulate.size() - 1) + " <=> Heap: " + heap.peek());
-      System.err.println(simulate.size() + " <=> " + heap.size());
-      assertEquals("Sizes don't match!", simulate.size(), heap.size());
+      // System.err.println("Sim: " + simulate.get(simulate.size() - 1) + " <=> Heap: " + heap.peek());
+      // System.err.println(simulate.size() + " <=> " + heap.size());
       int remove = r.nextInt(simulate.size());
       for(int j = 0; j < remove; j++) {
         // System.out.println(heap.toString());
@@ -88,10 +90,16 @@ public class TestTiedTopBoundedUpdatableHeap implements JUnit4Test {
         IntegerPriorityObject<Integer> fromsim = simulate.remove(simulate.size() - 1);
         // System.out.println(fromheap+" <=> "+fromsim);
         assertEquals("Priority doesn't agree.", fromheap.priority, fromsim.priority);
-        if(fromheap.getObject() != fromsim.getObject()) {
-          // Ties
-          if(j + 1 == remove && heap.size() > 0) {
-            j++;
+        if(j + 1 == remove) {
+          while(heap.size() > 0) {
+            assertEquals("Priority doesn't agree.", heap.peek().priority, simulate.get(simulate.size() - 1).priority);
+            if(heap.peek().priority == fromheap.priority) {
+              fromheap = heap.poll();
+              fromsim = simulate.remove(simulate.size() - 1);
+            }
+            else {
+              break;
+            }
           }
         }
       }
