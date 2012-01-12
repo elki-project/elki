2092376e86327237b247d9cce913d6be89b7c8e6
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-01-12 17:51:43 +0000

    Add constant pseudo-distribution.

---
+++ src/de/lmu/ifi/dbs/elki/math/statistics/distribution/ConstantDistribution.java
@@ -0,0 +1,61 @@
+package de.lmu.ifi.dbs.elki.math.statistics.distribution;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Pseudo distribution, that has a unique constant value.
+ * 
+ * @author Erich Schubert
+ */
+public class ConstantDistribution implements Distribution {
+  /**
+   * The constant
+   */
+  final double c;
+
+  /**
+   * Constructor.
+   * 
+   * @param c Constant
+   */
+  public ConstantDistribution(double c) {
+    super();
+    this.c = c;
+  }
+
+  @Override
+  public double nextRandom() {
+    return c;
+  }
+
+  @Override
+  public double pdf(double val) {
+    return (val == c) ? 1 : 0;
+  }
+
+  @Override
+  public double cdf(double val) {
+    return (val >= c) ? 1.0 : 0.0;
+  }
+}
