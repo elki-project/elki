55a27bbf4491ec6ed1f2d55e44f53340aaac9ac8
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-02-01 17:31:33 +0000

    Unit test for BitsUtil

---
+++ src/experimentalcode/erich/TestBitsUtil.java
@@ -0,0 +1,107 @@
+package experimentalcode.erich;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class TestBitsUtil {
+  @Test
+  public void testAgainstBigInteger() {
+    BigInteger bigint = new BigInteger("123");
+    long[] bituti = BitsUtil.make(Long.SIZE, 123);
+    assertEquals("Bit strings do not agree.", bigint.toString(2), BitsUtil.toString(bituti));
+
+    bigint = bigint.shiftLeft(13);
+    BitsUtil.shiftLeftI(bituti, 13);
+    assertEquals("Bit strings do not agree.", bigint.toString(2), BitsUtil.toString(bituti));
+
+    bigint = bigint.shiftRight(15);
+    BitsUtil.shiftRightI(bituti, 15);
+    assertEquals("Bit strings do not agree.", bigint.toString(2), BitsUtil.toString(bituti));
+  }
+
+  @Test
+  public void testSimpleOperations() {
+    long[] test = BitsUtil.zero(128);
+    BitsUtil.setI(test, 5);
+    BitsUtil.setI(test, 7);
+    assertEquals(BitsUtil.toString(test), "10100000");
+    assertEquals(BitsUtil.numberOfTrailingZeros(test), 5);
+    BitsUtil.truncateI(test, 7);
+    assertEquals(BitsUtil.toString(test), "100000");
+    assertEquals(BitsUtil.numberOfTrailingZeros(test), 5);
+    BitsUtil.setI(test, 7);
+    assertEquals(BitsUtil.toString(test), "10100000");
+    assertEquals(BitsUtil.numberOfTrailingZeros(test), 5);
+    BitsUtil.cycleRightI(test, 6, 8);
+    assertEquals(BitsUtil.toString(test), "10000010");
+    assertEquals(BitsUtil.numberOfTrailingZeros(test), 1);
+  }
+
+  @Test
+  public void testSorting() {
+    final Random r = new Random(0);
+    final int cnt = 100;
+    long[] rnds = new long[cnt];
+    long[][] bits = new long[cnt][];
+    for(int i = 0; i < cnt; i++) {
+      rnds[i] = Math.abs(r.nextLong());
+      bits[i] = BitsUtil.make(Long.SIZE, rnds[i]);
+    }
+
+    for(int i = 0; i < cnt; i++) {
+      for(int j = 0; j < cnt; j++) {
+        assertEquals(Long.compare(rnds[i], rnds[j]), BitsUtil.compare(bits[i], bits[j]));
+      }
+    }
+
+    for(int i = 0; i < cnt; i++) {
+      long[] btmp = BitsUtil.copy(bits[i], 64 + r.nextInt(500));
+      assertEquals(BitsUtil.compare(btmp, bits[i]), 0);
+      for(int j = 0; j < cnt; j++) {
+        assertEquals(Long.compare(rnds[i], rnds[j]), BitsUtil.compare(btmp, bits[j]));
+      }
+    }
+
+    for(int i = 0; i < cnt; i++) {
+      long[] btmp = BitsUtil.truncateI(BitsUtil.copy(bits[i]), 47);
+      for(int j = 0; j < cnt; j++) {
+        assertEquals(Long.compare(rnds[i] & ((1 << 48) - 1), rnds[j]), BitsUtil.compare(btmp, bits[j]));
+      }
+    }
+
+    for(int i = 0; i < cnt; i++) {
+      long[] btmp = BitsUtil.cycleRightI(BitsUtil.copy(bits[i]), 13, Long.SIZE - 32);
+      long ltmp = BitsUtil.cycleRightC(rnds[i], 13, Long.SIZE - 32);
+      for(int j = 0; j < cnt; j++) {
+        assertEquals(Long.compare(ltmp, rnds[j]), BitsUtil.compare(btmp, bits[j]));
+      }
+    }
+  }
+}
