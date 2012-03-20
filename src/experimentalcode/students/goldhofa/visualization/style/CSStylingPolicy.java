97bd5371d693fe4bdf3bc7f31bd29bf79b97cd5e
Sascha Goldhofer <goldhofa@cip.ifi.lmu.de>
2012-03-20 17:41:29 +0000

    - cs style policy stores selection of objects and returns color id above 0 for selected objects, else 0

--- src/experimentalcode/students/goldhofa/visualization/style/CSStylingPolicy.java
+++ src/experimentalcode/students/goldhofa/visualization/style/CSStylingPolicy.java
@@ -1,6 +1,9 @@
 package experimentalcode.students.goldhofa.visualization.style;
 
+import java.util.TreeMap;
+
 import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
 import de.lmu.ifi.dbs.elki.visualization.style.StylingPolicy;
 
 /** 
@@ -8,17 +11,36 @@ import de.lmu.ifi.dbs.elki.visualization.style.StylingPolicy;
  * Genau so ist das gedacht. Das erzeugt eine eigene styling policy, und setzt diese dann im StylingResult als aktiv.
  * Dann löst sie ein "ResultChanged" für das StylingResult aus, und die anderen Visualizer zeichnen neu
  * 
- * @author Gott
+ * @author Sascha Goldhofer
  *
  */
 public class CSStylingPolicy implements StylingPolicy {
+  
+  TreeMap<DBID, Integer> selection;
+  
+  public CSStylingPolicy() {
+    selection = new TreeMap<DBID, Integer>();
+  }
+  
+  public void deselectObject(DBID id) {
+    //System.out.println("removing object "+id.toString());
+    this.selection.remove(id);
+  }
+  
+  public void selectObject(DBID id, int color) {
+    //System.out.println("adding object "+id.toString()+" ("+color+")");
+    this.selection.put(id, color);
+  }
+  
+  public void setSelectedDBIDs(TreeMap<String, DBIDs> objectSelection) {
+  }
 
   @Override
   public int getColorForDBID(DBID id) {
-    
-    
-    
+    //System.out.println("CSStylingPolicy distributing color");
+    if (this.selection.containsKey(id)) {
+      return this.selection.get(id);
+    }
     return 0;
   }
-
 }
