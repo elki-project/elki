197ad5f53930124b701fa8b67bc57dceb1c8c191
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-03-23 10:55:01 +0000

    Code formatting.

--- src/experimentalcode/students/goldhofa/SegmentID.java
+++ src/experimentalcode/students/goldhofa/SegmentID.java
@@ -4,168 +4,155 @@ import java.util.ArrayList;
 import java.lang.Comparable;
 
 /**
- * Identifies a CircleSegment by its clusterings and cluster. Can be stored as and
- * retrieved from a String.
+ * Identifies a CircleSegment by its clusterings and cluster. Can be stored as
+ * and retrieved from a String.
  * 
- * A segmentID String consists of the cluster id of each clustering, ordered by clustering
- * and separated by a character. Thus a segment ID describes the common pairs in all
- * clusterings and cluster.
+ * A segmentID String consists of the cluster id of each clustering, ordered by
+ * clustering and separated by a character. Thus a segment ID describes the
+ * common pairs in all clusterings and cluster.
  * 
- * i.e. clusteringID 0 & clusterID 2, clusteringID 1 & clusterID 0 => segmentID: 2-0
+ * i.e. clusteringID 0 & clusterID 2, clusteringID 1 & clusterID 0 => segmentID:
+ * 2-0
  */
 public class SegmentID implements Comparable<SegmentID> {
-  
   private static final String SEPARATOR = "-";
-  
-  // number of segment. if -1 this segment id is a unclustered pair segment, having no
-  // corresponding object segment
+
+  // number of segment. if -1 this segment id is a unclustered pair segment,
+  // having no corresponding object segment
   private int index = -1;
-  
-  // 
+
+  //
   public ArrayList<Integer> ids;
-  
+
   public SegmentID() {
     ids = new ArrayList<Integer>(2);
   }
-  
+
   public void setIndex(int index) {
     this.index = index;
   }
-  
+
   public int getIndex() {
     return index;
   }
-  
+
   public SegmentID(int clusterings) {
-    
     ids = new ArrayList<Integer>(clusterings);
   }
-  
+
   /**
-   * Creates a SegmentID by its String representation, extracting
-   * clustering and corresponding clusterIDs.
+   * Creates a SegmentID by its String representation, extracting clustering and
+   * corresponding clusterIDs.
    * 
-   * @param segmentID   String representation of SegmentID
+   * @param segmentID String representation of SegmentID
    */
   public SegmentID(String segmentID) {
-    
     String[] id = segmentID.split(SEPARATOR);
-    
     ids = new ArrayList<Integer>(id.length);
-    
-    for (int i=0; i<id.length; i++) {
-      
+    for(int i = 0; i < id.length; i++) {
       add(Integer.valueOf(id[i]).intValue());
     }
   }
-  
+
   public SegmentID add(int nextClusteringCluster) {
-    
     ids.add(nextClusteringCluster);
-    
     return this;
   }
-  
+
   public int size() {
-    
     return ids.size();
   }
-  
+
   /**
-   * Checks if the segment has a cluster with unpaired objects.
-   * Unpaired clusters are represented by "0" (0 = all).
+   * Checks if the segment has a cluster with unpaired objects. Unpaired
+   * clusters are represented by "0" (0 = all).
    * 
    * @return
    */
   public boolean isUnpaired() {
-    
-    for (int id: ids) {
-      if (id == 0) return true;
+    for(int id : ids) {
+      if(id == 0) {
+        return true;
+      }
     }
-    
+
     return false;
   }
-  
+
   /**
-   * Check if this segment contains the pairs that are never
-   * clustered by any of the clusterings (all 0).
+   * Check if this segment contains the pairs that are never clustered by any of
+   * the clusterings (all 0).
    * 
    * @return
    */
   public boolean isNone() {
-    
-    for (int id: ids) {
-      if (id != 0) return false;
+    for(int id : ids) {
+      if(id != 0) {
+        return false;
+      }
     }
-    
+
     return true;
   }
-  
+
   /**
-   * Returns the index of the first clustering having an unpaired cluster,
-   * or -1 no unpaired cluster exists.  
+   * Returns the index of the first clustering having an unpaired cluster, or -1
+   * no unpaired cluster exists.
    * 
-   * @return  clustering id or -1
+   * @return clustering id or -1
    */
   public int getUnpairedClusteringIndex() {
-    
     int index = 0;
-    for (int id: ids) {
-      if (id == 0) return index;
+    for(int id : ids) {
+      if(id == 0) {
+        return index;
+      }
       index++;
     }
-    
+
     return -1;
   }
-  
+
   /**
    * Get the cluster IDs of the segment as array. index representing clustering.
    * 
    * @return IDs of cluster by clustering
    */
   public ArrayList<Integer> getIDs() {
-    
     return this.ids;
   }
-  
+
   public int get(int clusteringsIndex) {
-    
     return ids.get(clusteringsIndex);
   }
-  
+
   public int get(String clusteringsIndex) {
-    
     return ids.get(Integer.valueOf(clusteringsIndex).intValue());
   }
-  
+
   @Override
   public String toString() {
-    
     String string = "";
-    
-    for (Integer id : ids) {
-      string += id+SEPARATOR;
+
+    for(Integer id : ids) {
+      string += id + SEPARATOR;
     }
-    
-    return string.substring(0, string.length()-SEPARATOR.length());
+
+    return string.substring(0, string.length() - SEPARATOR.length());
   }
-  
+
   @Override
   public boolean equals(Object obj) {
-    
     if(!(SegmentID.class.isInstance(obj))) {
-      
       return false;
     }
-    
+
     SegmentID other = (SegmentID) obj;
-        
-    if (this.compareTo(other) == 0) {
-      
+
+    if(this.compareTo(other) == 0) {
       return true;
-      
-    } else {
-     
+    }
+    else {
       return false;
     }
   }
@@ -173,37 +160,31 @@ public class SegmentID implements Comparable<SegmentID> {
   // TODO performance?
   @Override
   public int hashCode() {
-
     int result = 0;
-    for (Integer id : ids) {
+    for(Integer id : ids) {
       result += id.hashCode();
     }
 
     return result;
   }
-  
+
   @Override
   public int compareTo(SegmentID sid) {
-    
-    if (this.size() < sid.size()) {
+    if(this.size() < sid.size()) {
       return -1;
     }
-    if (this.size() > sid.size()) {
+    if(this.size() > sid.size()) {
       return 1;
     }
-    
-    for (int i=0; i<this.size(); i++) {
-      
-      if (this.get(i) < sid.ids.get(i)) {
-        
+
+    for(int i = 0; i < this.size(); i++) {
+      if(this.get(i) < sid.ids.get(i)) {
         return -1;
-        
-      } else if (this.get(i) > sid.ids.get(i)) {
-        
+      }
+      else if(this.get(i) > sid.ids.get(i)) {
         return 1;
       }
     }
-    
     return 0;
   }
 }
