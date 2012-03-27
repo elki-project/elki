b1641b6af3802a843dd5dcfb3ad4d4268c051f10
Sascha Goldhofer <goldhofa@cip.ifi.lmu.de>
2012-03-27 16:48:42 +0000

    - nicer presentation of measure results in bar charts

--- src/experimentalcode/students/goldhofa/visualization/batikutil/BarChart.java
+++ src/experimentalcode/students/goldhofa/visualization/batikutil/BarChart.java
@@ -3,6 +3,7 @@ package experimentalcode.students.goldhofa.visualization.batikutil;
 import org.apache.batik.util.SVGConstants;
 import org.w3c.dom.Element;
 
+import de.lmu.ifi.dbs.elki.utilities.FormatUtil;
 import de.lmu.ifi.dbs.elki.visualization.svg.SVGPlot;
 
 public class BarChart implements UIInterfaceElement {
@@ -21,6 +22,9 @@ public class BarChart implements UIInterfaceElement {
   protected boolean horizontal;
 
   protected double width;
+  
+  protected double x = 0.0;
+  protected double y = 0.0;
 
   protected double length;
 
@@ -30,30 +34,42 @@ public class BarChart implements UIInterfaceElement {
 
   protected double fill;
 
+  public BarChart(SVGPlot svgp, double x, double y, double width, double height, boolean isHorizontal) {
+    this.svgp   = svgp;
+    this.x      = x;
+    this.y      = y;
+    build(width, height, isHorizontal);
+  }
+  
   public BarChart(SVGPlot svgp, double width, double height, boolean isHorizontal) {
-    this.svgp = svgp;
+    this.svgp   = svgp;
+    build(width, height, isHorizontal);
+  }
+  
+  protected void build(double width, double height, boolean isHorizontal) {
     horizontal = isHorizontal;
     barchart = svgp.svgElement(SVGConstants.SVG_G_TAG);
 
-    bar = svgp.svgRect(0.0, 0.0, width, height);
+    bar = svgp.svgRect(x, y, width, height);
+    //bar.setAttribute(SVGConstants.SVG_TRANSFORM_ATTRIBUTE, SVGConstants.SVG_TRANSLATE_VALUE+"("+x+","+y+")");
     bar.setAttribute(SVGConstants.SVG_FILL_ATTRIBUTE, "#a0a0a0");
     bar.setAttribute(SVGConstants.SVG_STROKE_ATTRIBUTE, "#a0a0a0");
-    bar.setAttribute(SVGConstants.SVG_STROKE_WIDTH_ATTRIBUTE, "0.5");
+    bar.setAttribute(SVGConstants.SVG_STROKE_WIDTH_ATTRIBUTE, ""+height*0.01);
 
     if(isHorizontal) {
       length = width;
       this.width = height;
-      chart = svgp.svgRect(1, 1, 0, height - 2);
+      chart = svgp.svgRect(x+0.02*height, y+0.02*height, 0, height - 0.04*height);
     }
     else {
       length = height;
       this.width = width;
-      chart = svgp.svgRect(1, 1, width - 2, 0);
+      chart = svgp.svgRect(x+0.02*width, y+0.02*width, width - 0.04*width, 0);
     }
 
     chart.setAttribute(SVGConstants.SVG_FILL_ATTRIBUTE, "#d4e4f1");
     chart.setAttribute(SVGConstants.SVG_STROKE_ATTRIBUTE, "#a0a0a0");
-    chart.setAttribute(SVGConstants.SVG_STROKE_WIDTH_ATTRIBUTE, "0.5");
+    chart.setAttribute(SVGConstants.SVG_STROKE_WIDTH_ATTRIBUTE, ""+height*0.01);
 
     barchart = svgp.svgElement(SVGConstants.SVG_G_TAG);
     barchart.appendChild(bar);
@@ -66,27 +82,29 @@ public class BarChart implements UIInterfaceElement {
 
   public void setFill(double newfill) {
     this.fill = newfill;
-    this.fillLength = (fill / size) * length;
+    this.fillLength = (fill / size) * length - 0.04*width;
     this.chart.setAttribute(SVGConstants.SVG_WIDTH_ATTRIBUTE, "" + fillLength);
   }
 
   public void addLabel(String text) {
     if(horizontal) {
-      label = svgp.svgText(length + 5, width / 2 + 5, text);
+      label = svgp.svgText(x + length + 0.2*width, y + 0.75*width, text);
     }
     else {
-      label = svgp.svgText(0, length + 5, text);
+      label = svgp.svgText(x, y + length + 0.25*width, text);
     }
+    label.setAttribute(SVGConstants.SVG_STYLE_ATTRIBUTE, "font-size: "+0.75*width+"; font-weight: normal");
     barchart.appendChild(label);
   }
 
   public void showValues() {
     if(horizontal) {
-      valueLabel = svgp.svgText(2, width / 2 + 5, "" + Math.round(fill) + " / " + Math.round(size));
+      valueLabel = svgp.svgText(x + 0.2*width, y + 0.75*width, "" + FormatUtil.format(fill, FormatUtil.NF4));// + " / " + Math.round(size));
     }
     else {
-      valueLabel = svgp.svgText(width + 5, 10, "" + Math.round(fill) + " / " + Math.round(size));
+      valueLabel = svgp.svgText(width + 0.25*width, y, "" + FormatUtil.format(fill, FormatUtil.NF4));// + " / " + Math.round(size));
     }
+    valueLabel.setAttribute(SVGConstants.SVG_STYLE_ATTRIBUTE, "font-size: "+0.75*width+"; font-weight: bold");
     barchart.appendChild(valueLabel);
   }
 
