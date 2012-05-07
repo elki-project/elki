16c89975ef66a495b38cc43d1db39fee90ba7aca
Arthur Zimek <zimek@dbs.ifi.lmu.de>
2012-05-07 20:52:44 +0000

    spelling

--- src/experimentalcode/arthur/PerturbationFilter.java
+++ src/experimentalcode/arthur/PerturbationFilter.java
@@ -47,7 +47,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.LongParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.Parameter;
 
 /**
- * A filter to perturbe the values by adding Gaussian micro-noise.
+ * A filter to perturb the values by adding Gaussian micro-noise.
  * 
  * The added noise is generated, attribute-wise, by a Gaussian with mean=0 and a standard deviation.
  * The standard deviation can be scaled, attribute-wise, to a given percentage of the original standard deviation in the data distribution (assuming a Gaussian distribution there), or to a percentage of the extension in each attribute ({@code maximumValue - minimumValue}).
