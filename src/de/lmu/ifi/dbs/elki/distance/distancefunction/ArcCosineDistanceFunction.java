87330334989c5e2ab04d49f4129788e26e764f7e
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-05-11 08:42:32 +0000

    Make Cosine and Arccosine distances use spatial indexes. This is a rather ad-hoc mindist implementation, but should be correct...

--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/ArcCosineDistanceFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/ArcCosineDistanceFunction.java
@@ -25,6 +25,11 @@ package de.lmu.ifi.dbs.elki.distance.distancefunction;
 
 import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.data.VectorUtil;
+import de.lmu.ifi.dbs.elki.data.spatial.SpatialComparable;
+import de.lmu.ifi.dbs.elki.database.query.distance.SpatialDistanceQuery;
+import de.lmu.ifi.dbs.elki.database.query.distance.SpatialPrimitiveDistanceQuery;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 
 /**
@@ -35,12 +40,12 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
  * 
  * @author Arthur Zimek
  */
-public class ArcCosineDistanceFunction extends AbstractVectorDoubleDistanceFunction {
+public class ArcCosineDistanceFunction extends AbstractVectorDoubleDistanceFunction implements SpatialPrimitiveDoubleDistanceFunction<NumberVector<?, ?>> {
   /**
    * Static instance
    */
   public static final ArcCosineDistanceFunction STATIC = new ArcCosineDistanceFunction();
-  
+
   /**
    * Provides a CosineDistanceFunction.
    * 
@@ -62,12 +67,39 @@ public class ArcCosineDistanceFunction extends AbstractVectorDoubleDistanceFunct
    * @return the cosine distance for two given feature vectors v1 and v2
    */
   @Override
-  public double doubleDistance(NumberVector<?,?> v1, NumberVector<?,?> v2) {
+  public double doubleDistance(NumberVector<?, ?> v1, NumberVector<?, ?> v2) {
     double d = Math.acos(VectorUtil.angle(v1, v2));
     if(d < 0) {
       d = 0;
     }
     return d;
+  }
+
+  @Override
+  public double doubleMinDist(SpatialComparable mbr1, SpatialComparable mbr2) {
+    // Essentially, we want to compute this:
+    // min(v1.transposeTimes(v2)) / (max(v1.euclideanLength()) *
+    // max(v2.euclideanLength()));
+    // We can just compute all three in parallel.
+    final int dim = mbr1.getDimensionality();
+    double s = 0, e1 = 0, e2 = 0;
+    for(int k = 0; k < dim; k++) {
+      s += mbr1.getMin(k + 1) * mbr2.getMin(k + 1);
+      final double r1 = mbr1.getMax(k + 1);
+      final double r2 = mbr2.getMax(k + 1);
+      e1 += r1 * r1;
+      e2 += r2 * r2;
+    }
+    double d = 1 - Math.acos(Math.sqrt((s / e1) * (s / e2)));
+    if(d < 0) {
+      d = 0;
+    }
+    return d;
+  }
+
+  @Override
+  public DoubleDistance minDist(SpatialComparable mbr1, SpatialComparable mbr2) {
+    return new DoubleDistance(doubleMinDist(mbr1, mbr2));
   }
 
   @Override
@@ -84,6 +116,11 @@ public class ArcCosineDistanceFunction extends AbstractVectorDoubleDistanceFunct
       return true;
     }
     return this.getClass().equals(obj.getClass());
+  }
+
+  @Override
+  public <T extends NumberVector<?, ?>> SpatialDistanceQuery<T, DoubleDistance> instantiate(Relation<T> relation) {
+    return new SpatialPrimitiveDistanceQuery<T, DoubleDistance>(relation, this);
   }
 
   /**
