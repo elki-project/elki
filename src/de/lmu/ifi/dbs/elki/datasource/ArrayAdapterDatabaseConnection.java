51dd6d1e3da691db8a2f3b1cf9aeb63d6d4c2909
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-06-04 14:19:37 +0000

    Fix (DOH, obvious bug) and enhance ArrayAdapterDatabaseConnection

--- src/de/lmu/ifi/dbs/elki/datasource/ArrayAdapterDatabaseConnection.java
+++ src/de/lmu/ifi/dbs/elki/datasource/ArrayAdapterDatabaseConnection.java
@@ -24,12 +24,17 @@ package de.lmu.ifi.dbs.elki.datasource;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
 import de.lmu.ifi.dbs.elki.data.type.SimpleTypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
 import de.lmu.ifi.dbs.elki.data.type.VectorFieldTypeInformation;
+import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
 import de.lmu.ifi.dbs.elki.datasource.bundle.MultipleObjectsBundle;
+import de.lmu.ifi.dbs.elki.utilities.exceptions.AbortException;
 
 /**
  * Import an existing data matrix (<code>double[rows][cols]</code>) into an ELKI
@@ -46,17 +51,59 @@ public class ArrayAdapterDatabaseConnection implements DatabaseConnection {
   double[][] data;
 
   /**
+   * Object labels
+   */
+  String[] labels;
+
+  /**
+   * Starting ID for fixed object ids.
+   */
+  Integer startid = null;
+
+  /**
    * Constructor.
    * 
    * @param data Existing data matrix
    */
   public ArrayAdapterDatabaseConnection(double[][] data) {
+    this(data, null, null);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param data Existing data matrix
+   * @param labels Object labels
+   */
+  public ArrayAdapterDatabaseConnection(double[][] data, String[] labels) {
+    this(data, labels, null);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param data Existing data matrix
+   * @param labels Object labels
+   * @param startid Starting object ID
+   */
+  public ArrayAdapterDatabaseConnection(double[][] data, String[] labels, Integer startid) {
     super();
+    this.data = data;
+    this.labels = labels;
+    this.startid = startid;
   }
 
   @Override
   public MultipleObjectsBundle loadData() {
     MultipleObjectsBundle b = new MultipleObjectsBundle();
+    if(startid != null) {
+      List<DBID> ids = new ArrayList<DBID>(data.length);
+      for(int i = 0; i < data.length; i++) {
+        ids.add(DBIDUtil.importInteger(startid + i));
+      }
+      b.appendColumn(TypeUtil.DBID, Arrays.asList(labels));
+    }
+
     int mind = Integer.MAX_VALUE;
     int maxd = 0;
     List<DoubleVector> vecs = new ArrayList<DoubleVector>(data.length);
@@ -73,6 +120,12 @@ public class ArrayAdapterDatabaseConnection implements DatabaseConnection {
       type = new SimpleTypeInformation<DoubleVector>(DoubleVector.class);
     }
     b.appendColumn(type, vecs);
+    if(labels != null) {
+      if(labels.length != data.length) {
+        throw new AbortException("Label and DBID columns must have the same size.");
+      }
+      b.appendColumn(TypeUtil.STRING, Arrays.asList(labels));
+    }
     return b;
   }
 }
