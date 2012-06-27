9049d8e93d21f5f88d05d69a7811c6031aa9b79d
Robert Roedler <roedler@cip.ifi.lmu.de>
2012-06-27 14:11:31 +0000

    <empty commit message>

---
+++ src/experimentalcode/students/roedler/parallelCoordinates/visualizer/algorithm/HiCSDimensionOrder.java
@@ -0,0 +1,518 @@
+package experimentalcode.students.roedler.parallelCoordinates.visualizer.algorithm;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.VectorUtil;
+import de.lmu.ifi.dbs.elki.data.VectorUtil.SortDBIDsBySingleDimension;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.ids.ArrayDBIDs;
+import de.lmu.ifi.dbs.elki.database.ids.ArrayModifiableDBIDs;
+import de.lmu.ifi.dbs.elki.database.ids.DBID;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.math.linearalgebra.Matrix;
+import de.lmu.ifi.dbs.elki.math.statistics.tests.GoodnessOfFitTest;
+import de.lmu.ifi.dbs.elki.math.statistics.tests.KolmogorovSmirnovTest;
+import de.lmu.ifi.dbs.elki.result.HierarchicalResult;
+import de.lmu.ifi.dbs.elki.result.Result;
+import de.lmu.ifi.dbs.elki.result.ResultUtil;
+import de.lmu.ifi.dbs.elki.utilities.DatabaseUtil;
+import de.lmu.ifi.dbs.elki.utilities.datastructures.heap.TopBoundedHeap;
+import de.lmu.ifi.dbs.elki.visualization.VisualizationTask;
+import de.lmu.ifi.dbs.elki.visualization.projector.ParallelPlotProjector;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.AbstractVisFactory;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.Visualization;
+import de.lmu.ifi.dbs.elki.visualization.visualizers.parallel.AbstractParallelVisualization;
+import experimentalcode.students.roedler.parallelCoordinates.gui.MenuOwner;
+import experimentalcode.students.roedler.parallelCoordinates.gui.SubMenu;
+
+/**
+ * Arrange dimensions
+ * 
+ * @author Robert Rödler
+ */
+public class HiCSDimensionOrder extends AbstractParallelVisualization<NumberVector<?, ?>> implements MenuOwner {
+  /**
+   * Generic tags to indicate the type of element. Used in IDs, CSS-Classes etc.
+   */
+  public static final String CLUSTERORDER = "HiCS Dimension Order";
+
+  private Clustering<Model> clustering;
+
+  
+  /**
+   * Maximum number of retries.
+   */
+  private static final int MAX_RETRIES = 100;
+  
+  /**
+   * Monte-Carlo iterations
+   */
+  private int m = 10;
+
+  /**
+   * Alpha threshold
+   */
+  private double alpha = 0.1;
+
+  /**
+   * Statistical test to use
+   */
+  private GoodnessOfFitTest statTest;
+
+  /**
+   * Candidates limit
+   */
+  private int cutoff = 400;
+  
+  /**
+   * Random generator
+   */
+  private Random random;
+  
+  
+  /**
+   * Constructor.
+   * 
+   * @param task VisualizationTask
+   */
+  public HiCSDimensionOrder(VisualizationTask task) {
+    super(task);
+    clustering = task.getResult();
+    this.statTest = new KolmogorovSmirnovTest();
+    this.random = new Random();
+    incrementalRedraw();
+  }
+
+  @Override
+  protected void redraw() {
+  }
+
+  private void arrange(int par) {
+    int dim = DatabaseUtil.dimensionality(relation);
+    Matrix hicsmat = new Matrix(dim, dim, 0.);
+
+    DBIDs ids = null;
+    
+    switch(par){
+      case -2: {
+        ids = context.getSelection().getSelectedIds();
+        break;
+      }
+      case -1: {
+        ids = ResultUtil.getSamplingResult(relation).getSample();
+        break;
+      }
+      default: {
+        ids = clustering.getAllClusters().get(par).getIDs();
+      }
+    }
+    
+    ArrayList<ArrayDBIDs> subspaceIndex = buildOneDimIndexes(relation, ids);
+    Set<HiCSSubspace> subspaces = calculateSubspaces(relation, subspaceIndex);
+    
+    for (HiCSSubspace hics : subspaces){
+      hicsmat.set(hics.nextSetBit(0), hics.nextSetBit(hics.nextSetBit(0) + 1), hics.contrast);
+      hicsmat.set(hics.nextSetBit(hics.nextSetBit(0) + 1), hics.nextSetBit(0), hics.contrast);
+    }
+    
+    ArrayList<Integer> arrange = new ArrayList<Integer>();
+
+    int[] first = getMax(hicsmat);
+    arrange.add(first[0]);
+    arrange.add(1, first[1]);
+    int[] pos;
+    pos = first.clone();
+
+    blank(hicsmat, pos[0]);
+    blank(hicsmat, pos[1]);
+    int tmp1, tmp2;
+
+    for(int i = 2; i < dim; i++) {
+      tmp1 = getMax(hicsmat, pos[0]);
+      tmp2 = getMax(hicsmat, pos[1]);
+
+      if(Math.abs(hicsmat.get(pos[0], tmp1)) > Math.abs(hicsmat.get(pos[1], tmp2))) {
+        arrange.add(0, tmp1);
+        blank(hicsmat, tmp1);
+        pos[0] = tmp1;
+      }
+      else {
+        arrange.add(arrange.size(), tmp2);
+        blank(hicsmat, tmp2);
+        pos[1] = tmp2;
+      }
+    }
+    
+    for(int i = 0; i < arrange.size(); i++) {
+      System.out.print("" + arrange.get(i) + "  ");
+    }
+
+    for(int i = 0; i < dim; i++) {
+      proj.moveAxis(proj.getAxisForDim(arrange.get(i)), i);
+    }
+  }
+
+  private int[] getMax(Matrix mat) {
+    int[] pos = new int[2];
+    double max = 0.;
+
+    for(int i = 0; i < mat.getColumnDimensionality(); i++) {
+      for(int j = 0; j < mat.getColumnDimensionality(); j++) {
+        if(Math.abs(mat.get(i, j)) > max) {
+          max = Math.abs(mat.get(i, j));
+          pos[0] = i;
+          pos[1] = j;
+        }
+      }
+    }
+    mat.set(pos[0], pos[1], 0.);
+    mat.set(pos[1], pos[0], 0.);
+    return pos;
+  }
+
+  private int getMax(Matrix mat, int row) {
+    int pos = 0;
+    double max = 0.;
+
+    for(int i = 0; i < mat.getColumnDimensionality(); i++) {
+      if(Math.abs(mat.get(row, i)) > max) {
+        max = Math.abs(mat.get(row, i));
+        pos = i;
+      }
+    }
+    
+    return pos;
+  }
+
+  private void blank(Matrix mat, int dim) {
+    for(int i = 0; i < mat.getColumnDimensionality(); i++) {
+      mat.set(i, dim, 0.);
+    }
+  }
+  
+  
+  /**
+   * Identifies high contrast subspaces in a given full-dimensional database
+   * 
+   * @param relation the relation the HiCS should be evaluated for
+   * @param subspaceIndex Subspace indexes
+   * @return a set of high contrast subspaces
+   */
+  private Set<HiCSSubspace> calculateSubspaces(Relation<? extends NumberVector<?, ?>> relation, ArrayList<ArrayDBIDs> subspaceIndex) {
+    final int dbdim = DatabaseUtil.dimensionality(relation);
+
+    TreeSet<HiCSSubspace> subspaceList = new TreeSet<HiCSSubspace>(HiCSSubspace.SORT_BY_SUBSPACE);
+    TopBoundedHeap<HiCSSubspace> dDimensionalList = new TopBoundedHeap<HiCSSubspace>(cutoff, HiCSSubspace.SORT_BY_CONTRAST_ASC);
+    
+    // compute two-element sets of subspaces
+    for(int i = 0; i < dbdim; i++) {
+      for(int j = i + 1; j < dbdim; j++) {
+        HiCSSubspace ts = new HiCSSubspace();
+        ts.set(i);
+        ts.set(j);
+        calculateContrast(relation, ts, subspaceIndex);
+        dDimensionalList.add(ts);
+
+      }
+    }
+
+    for(int d = 3; !dDimensionalList.isEmpty(); d++) {
+
+      subspaceList.addAll(dDimensionalList);
+      // result now contains all d-dimensional sets of subspaces
+
+      ArrayList<HiCSSubspace> candidateList = new ArrayList<HiCSSubspace>(dDimensionalList);
+      dDimensionalList.clear();
+      // candidateList now contains the *m* best d-dimensional sets
+      Collections.sort(candidateList, HiCSSubspace.SORT_BY_SUBSPACE);
+
+      // TODO: optimize APRIORI style, by not even computing the bit set or?
+      for(int i = 0; i < candidateList.size() - 1; i++) {
+        for(int j = i + 1; j < candidateList.size(); j++) {
+          HiCSSubspace set1 = candidateList.get(i);
+          HiCSSubspace set2 = candidateList.get(j);
+
+          HiCSSubspace joinedSet = new HiCSSubspace();
+          joinedSet.or(set1);
+          joinedSet.or(set2);
+          if(joinedSet.cardinality() != d) {
+            continue;
+          }
+
+          calculateContrast(relation, joinedSet, subspaceIndex);
+          dDimensionalList.add(joinedSet);
+        }
+      }  
+    }
+ 
+    return subspaceList;
+  }
+  
+  
+  /**
+   * Calculates "index structures" for every attribute, i.e. sorts a
+   * ModifiableArray of every DBID in the database for every dimension and
+   * stores them in a list
+   * 
+   * @param relation Relation to index
+   * @return List of sorted objects
+   */
+  private ArrayList<ArrayDBIDs> buildOneDimIndexes(Relation<? extends NumberVector<?, ?>> relation, DBIDs ids) {
+    final int dim = DatabaseUtil.dimensionality(relation);
+    ArrayList<ArrayDBIDs> subspaceIndex = new ArrayList<ArrayDBIDs>(dim + 1);
+
+    SortDBIDsBySingleDimension comp = new VectorUtil.SortDBIDsBySingleDimension(relation);
+    for(int i = 1; i <= dim; i++) {
+    //  ArrayModifiableDBIDs amDBIDs = DBIDUtil.newArray(relation.getDBIDs());
+      ArrayModifiableDBIDs amDBIDs = DBIDUtil.newArray(ids);
+      comp.setDimension(i);
+      amDBIDs.sort(comp);
+      subspaceIndex.add(amDBIDs);
+    }
+
+    return subspaceIndex;
+  }
+  
+  /**
+   * Calculates the actual contrast of a given subspace
+   * 
+   * @param relation
+   * @param subspace
+   * @param subspaceIndex Subspace indexes
+   */
+  private void calculateContrast(Relation<? extends NumberVector<?, ?>> relation, HiCSSubspace subspace, ArrayList<ArrayDBIDs> subspaceIndex) {
+    final int card = subspace.cardinality();
+    final double alpha1 = Math.pow(alpha, (1.0 / card));
+    final int windowsize = (int) (relation.size() * alpha1);
+
+    int retries = 0;
+    double deviationSum = 0.0;
+    for(int i = 0; i < m; i++) {
+      // Choose a random set bit.
+      int chosen = -1;
+      for(int tmp = random.nextInt(card); tmp >= 0; tmp--) {
+        chosen = subspace.nextSetBit(chosen + 1);
+      }
+      // initialize sample
+      DBIDs conditionalSample = relation.getDBIDs();
+
+      for(int j = subspace.nextSetBit(0); j >= 0; j = subspace.nextSetBit(j + 1)) {
+        if(j == chosen) {
+          continue;
+        }
+        ArrayDBIDs sortedIndices = subspaceIndex.get(j);
+        ArrayModifiableDBIDs indexBlock = DBIDUtil.newArray();
+        // initialize index block
+        int start = random.nextInt(relation.size() - windowsize);
+        for(int k = start; k < start + windowsize; k++) {
+          indexBlock.add(sortedIndices.get(k)); // select index block
+        }
+
+        conditionalSample = DBIDUtil.intersection(conditionalSample, indexBlock);
+      }
+      if(conditionalSample.size() < 10) {
+        retries++;
+ 
+        if(retries < MAX_RETRIES) {
+          i--;
+          continue;
+        }
+      }
+      // Project conditional set
+      double[] sampleValues = new double[conditionalSample.size()];
+      {
+        int l = 0;
+        for(DBID id : conditionalSample) {
+          sampleValues[l] = relation.get(id).doubleValue(chosen + 1);
+          l++;
+        }
+      }
+      // Project full set
+      double[] fullValues = new double[relation.size()];
+      {
+        int l = 0;
+        for(DBID id : subspaceIndex.get(chosen)) {
+          fullValues[l] = relation.get(id).doubleValue(chosen + 1);
+          l++;
+        }
+      }
+      double contrast = statTest.deviation(fullValues, sampleValues);
+      if(Double.isNaN(contrast)) {
+        i--;
+        continue;
+      }
+      deviationSum += contrast;
+    }
+    subspace.contrast = deviationSum / m;
+  }
+
+  /**
+   * BitSet that holds a contrast value as field. Used for the representation of
+   * a subspace in HiCS
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class HiCSSubspace extends BitSet {
+    /**
+     * Serial version
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The HiCS contrast value
+     */
+    protected double contrast;
+
+    /**
+     * Constructor.
+     */
+    public HiCSSubspace() {
+      super();
+    }
+
+    @Override
+    public String toString() {
+      StringBuffer buf = new StringBuffer();
+      buf.append("[contrast=").append(contrast);
+      for(int i = nextSetBit(0); i >= 0; i = nextSetBit(i + 1)) {
+        buf.append(" ").append(i + 1);
+      }
+      buf.append("]");
+      return buf.toString();
+    }
+    
+    /**
+     * Sort subspaces by their actual subspace.
+     */
+    public static Comparator<HiCSSubspace> SORT_BY_CONTRAST_ASC = new Comparator<HiCSSubspace>() {
+      @Override
+      public int compare(HiCSSubspace o1, HiCSSubspace o2) {
+        if(o1.contrast == o2.contrast) {
+          return 0;
+        }
+        return o1.contrast > o2.contrast ? 1 : -1;
+      }
+    };
+
+    /**
+     * Sort subspaces by their actual subspace.
+     */
+    public static Comparator<HiCSSubspace> SORT_BY_CONTRAST_DESC = new Comparator<HiCSSubspace>() {
+      @Override
+      public int compare(HiCSSubspace o1, HiCSSubspace o2) {
+        if(o1.contrast == o2.contrast) {
+          return 0;
+        }
+        return o1.contrast < o2.contrast ? 1 : -1;
+      }
+    };
+
+    /**
+     * Sort subspaces by their actual subspace.
+     */
+    public static Comparator<HiCSSubspace> SORT_BY_SUBSPACE = new Comparator<HiCSSubspace>() {
+      @Override
+      public int compare(HiCSSubspace o1, HiCSSubspace o2) {
+        int dim1 = o1.nextSetBit(0);
+        int dim2 = o2.nextSetBit(0);
+        while(dim1 >= 0 && dim2 >= 0) {
+          if(dim1 < dim2) {
+            return -1;
+          }
+          else if(dim1 > dim2) {
+            return 1;
+          }
+          dim1 = o1.nextSetBit(dim1 + 1);
+          dim2 = o2.nextSetBit(dim2 + 1);
+        }
+        return 0;
+      }
+    };
+  }
+    
+  
+  @Override
+  public SubMenu getMenu() {
+    SubMenu myMenu = new SubMenu(CLUSTERORDER, this);
+
+    myMenu.addItem("arrange", Integer.toString(-1));
+
+    int clus = clustering.getAllClusters().size();
+    if(clus > 0) {
+      for(int num = 0; num < clus; num++) {
+        myMenu.addItem("arrange Cluster " + num, Integer.toString(num));
+      }
+    }
+    myMenu.addItem("arrange selected", "-2");
+
+    return myMenu;
+  }
+  
+  
+
+  @Override
+  public void menuPressed(String id, boolean checked) {
+    int iid = Integer.parseInt(id);
+    arrange(iid);
+    context.contentChanged(null);
+  }
+
+  /**
+   * Factory
+   * 
+   * @author Robert Rödler
+   * 
+   * @apiviz.stereotype factory
+   * @apiviz.uses AxisVisualization oneway - - «create»
+   * 
+   */
+  public static class Factory extends AbstractVisFactory {
+    /**
+     * A short name characterizing this Visualizer.
+     */
+    private static final String NAME = "HiCS Dimension Order";
+
+    /**
+     * Constructor, adhering to
+     * {@link de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable}
+     */
+    public Factory() {
+      super();
+    }
+
+    @Override
+    public Visualization makeVisualization(VisualizationTask task) {
+      return new HiCSDimensionOrder(task);
+    }
+
+    @Override
+    public void processNewResult(HierarchicalResult baseResult, Result result) {
+      // Find clusterings we can visualize:
+      Collection<Clustering<?>> clusterings = ResultUtil.filterResults(result, Clustering.class);
+      for(Clustering<?> c : clusterings) {
+        if(c.getAllClusters().size() > 0) {
+          Collection<ParallelPlotProjector<?>> ps = ResultUtil.filterResults(baseResult, ParallelPlotProjector.class);
+          for(ParallelPlotProjector<?> p : ps) {
+            final VisualizationTask task = new VisualizationTask(NAME, c, p.getRelation(), this);
+            task.put(VisualizationTask.META_LEVEL, VisualizationTask.LEVEL_DATA + 3);
+            baseResult.getHierarchy().add(c, task);
+            baseResult.getHierarchy().add(p, task);
+          }
+        }
+      }
+    }
+  }
+}
--- src/experimentalcode/students/roedler/parallelCoordinates/visualizer/algorithm/SURFINGDimensionOrder.java
+++ src/experimentalcode/students/roedler/parallelCoordinates/visualizer/algorithm/SURFINGDimensionOrder.java
@@ -26,27 +26,22 @@ import experimentalcode.students.roedler.parallelCoordinates.gui.SubMenu;
  * 
  * @author Robert Rödler
  */
-public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVector<?, ?>> implements MenuOwner {
+public class SURFINGDimensionOrder extends AbstractParallelVisualization<NumberVector<?, ?>> implements MenuOwner {
   /**
    * Generic tags to indicate the type of element. Used in IDs, CSS-Classes etc.
    */
-  public static final String CLUSTERORDER = "HSM Dimension Order";
+  public static final String CLUSTERORDER = "SURFING Dimension Order";
 
   private Clustering<Model> clustering;
-  
-  private int sum;
-  
-  private int mode = 3;
 
   /**
    * Constructor.
    * 
    * @param task VisualizationTask
    */
-  public HSMDimensionOrder(VisualizationTask task) {
+  public SURFINGDimensionOrder(VisualizationTask task) {
     super(task);
     clustering = task.getResult();
-    sum = 0;
     incrementalRedraw();
   }
 
@@ -56,8 +51,7 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
 
   private void arrange(int par) {
     int dim = DatabaseUtil.dimensionality(relation);
-    Matrix hsmmat = new Matrix(dim, dim, 0.);
-    Matrix pic = new Matrix(500, 500);
+    Matrix surfmat = new Matrix(dim, dim, 0.);
 
     DBIDs ids = null;
     
@@ -75,63 +69,77 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
       }
     }
     
-    int progress = 0;
-    int max = ((dim * dim) - dim) / 2;
-    
     for (int i = 1; i < dim; i++){
-  //    if (!proj.isAxisVisible(i - 1)) { 
-  //      continue; 
-  //    }
-      
       for (int j = i + 1; j <= dim; j++){
-  //      if (!proj.isAxisVisible(j - 1)) { continue; }
-        
-        for (DBID id : ids){
-          double[] yPos = proj.fastProjectDataToRenderSpace(relation.get(id));
-          line(0, (int)(5. * yPos[i-1]), 499, (int)(5. * yPos[j-1]), pic);
+        double[] knns = new double[ids.size()];
+        double sum = 0.;
+        int knn = 0;
+        for (DBID id1 : ids){
+          double idsum = 0.;
+          double x, y;
+          
+          for (DBID id2 : ids){
+            if (id1.getIntegerID() == id2.getIntegerID()) { break; }
+            x = relation.get(id1).doubleValue(i) - relation.get(id2).doubleValue(i);
+            y = relation.get(id1).doubleValue(j) - relation.get(id2).doubleValue(j);
+            idsum += Math.sqrt(x * x + y * y);
+          }
+          
+          sum += idsum  / (double) ids.size();
+          knns[knn] = idsum  / (double) ids.size();
+          knn++;
         }
-
-        Matrix hough = houghTransformation(pic);
+        double median = sum / (double) ids.size();
+        double diff = 0.;
         
-        double median;
+        for (int k = 0; k < knns.length; k++){
+          diff += Math.abs(median - knns[k]);
+        }
+        diff *= 0.5;
         
-        median = (double)sum / (double)(hough.getColumnDimensionality() * hough.getRowDimensionality());
-        
-        double bigCells = (double) sumMatrix(splitMatrixIntoCells(hough, mode, median));
-        
-        hsmmat.set(i - 1, j - 1, 1. - (bigCells / 2500.));
-        hsmmat.set(j - 1, i - 1, 1. - (bigCells / 2500.));
-        
-        progress++;
-        System.out.println("HSM Progress " + progress + " von " + max);
+        int below = 0;
+        for (int l = 0; l < knns.length; l++){
+          if (knns[l] < median) { 
+            below++;
+          }
+        }
+        double quality;
+        if (below == 0){
+          quality = 0.;
+        }
+        else {
+          quality = diff / ((double)below * median);
+        }
+        surfmat.set(i - 1, j - 1, quality);
+        surfmat.set(j - 1, i - 1, quality);
       }
     }
-    System.out.println(hsmmat.toString());
+    
     
     ArrayList<Integer> arrange = new ArrayList<Integer>();
 
-    int[] first = getMax(hsmmat);
+    int[] first = getMax(surfmat);
     arrange.add(first[0]);
     arrange.add(1, first[1]);
     int[] pos;
     pos = first.clone();
 
-    blank(hsmmat, pos[0]);
-    blank(hsmmat, pos[1]);
+    blank(surfmat, pos[0]);
+    blank(surfmat, pos[1]);
     int tmp1, tmp2;
 
     for(int i = 2; i < dim; i++) {
-      tmp1 = getMax(hsmmat, pos[0]);
-      tmp2 = getMax(hsmmat, pos[1]);
+      tmp1 = getMax(surfmat, pos[0]);
+      tmp2 = getMax(surfmat, pos[1]);
 
-      if(Math.abs(hsmmat.get(pos[0], tmp1)) > Math.abs(hsmmat.get(pos[1], tmp2))) {
+      if(Math.abs(surfmat.get(pos[0], tmp1)) > Math.abs(surfmat.get(pos[1], tmp2))) {
         arrange.add(0, tmp1);
-        blank(hsmmat, tmp1);
+        blank(surfmat, tmp1);
         pos[0] = tmp1;
       }
       else {
         arrange.add(arrange.size(), tmp2);
-        blank(hsmmat, tmp2);
+        blank(surfmat, tmp2);
         pos[1] = tmp2;
       }
     }
@@ -142,113 +150,6 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
 
     for(int i = 0; i < dim; i++) {
       proj.moveAxis(proj.getAxisForDim(arrange.get(i)), i);
-    }
-  }
-  
-  private int sumMatrix(Matrix mat){
-    int ret = 0;
-    for (int i = 0; i < mat.getColumnDimensionality(); i++){
-      for (int j = 0; j < mat.getRowDimensionality(); j++){
-        if (mat.get(i, j) == 1.){
-          ret++;
-        }
-      }
-    }
-    return ret;
-  }
-  
-  private Matrix splitMatrixIntoCells(Matrix mat, int mode, double median){
-    
-    Matrix ret = new Matrix(50, 50, 0.);
-    
-    double stepX = mat.getColumnDimensionality() / 50.;
-    double stepY = mat.getRowDimensionality() / 50.;
-    
-    for (int i = 0; i < 50; i++){
-      for (int j = 0; j < 50; j++){
-        
-        double sum = 0.;
-        int cells = 0;
-        boolean bigger = true;
-        
-        for (int k = (int)(i * stepY); k < (int)((i + 1) * stepY); k++){
-          for (int l = (int) (j * stepX); l < (int)((j + 1) * stepX); l++){
-            
-            if (mode == 1 && mat.get(k, l) > median){
-              ret.set(i, j, 1.);
-              break;
-            }
-            if (mode == 2 && mat.get(k, l) <= median){
-              bigger = false;
-              break;
-            }
-            sum += mat.get(k, l);
-            cells++;
-          }
-        }
-        if (mode == 2 && bigger){
-          ret.set(i, j, 1.);
-        }
-        if (mode == 3 && ((sum /(double)cells) > median)){
-          ret.set(i, j, 1.);
-        }
-      }
-    } 
-    return ret;
-  }
-  
-  private Matrix houghTransformation(Matrix mat){
-    
-    Matrix ret;
-    double theta;
-    int d;
-    //int max = (int) Math.sqrt(Math.pow((double) mat.getRowDimensionality() / 2., 2.) + Math.pow((double) mat.getColumnDimensionality() / 2., 2.));
-    int max = (int) Math.sqrt(Math.pow((double) mat.getRowDimensionality(), 2.) + Math.pow((double) mat.getColumnDimensionality(), 2.));
-    
-    ret = new Matrix(max, 360, 0.);
-    sum = 0;
-    
-    for (int x = 0; x < mat.getRowDimensionality(); x++){
-      
-      for (int y = 0; y < mat.getColumnDimensionality(); y++){
-       
-        if (mat.get(x, y) > 0){
-          
-          for (int ang = 0; ang < 360; ang++){
-            
-            theta = Math.toRadians(ang);
-            
-          //  d = (int)(i * Math.cos(theta) + j * Math.sin(theta));
-            d = (int)(x * Math.cos(theta) + y * Math.sin(theta));
-            
-            if (d > 0 && d < max){
-              ret.set(d, ang, ret.get(d, ang) + 1.);
-              sum++;
-            }
-          }
-        }
-      }
-    }
-    
-    return ret;
-  }
-  
-  //Bresenham algorithm, copied from Wikipedia
-  private void line(int x0, int y0, int x1, int y1, Matrix mat)
-  {
-    if(y0 == 500){y0--;}
-    if(y1 == 500){y1--;}
-    int dx =  Math.abs(x1-x0), sx = x0<x1 ? 1 : -1;
-    int dy = -Math.abs(y1-y0), sy = y0<y1 ? 1 : -1; 
-    int err = dx+dy, e2; /* error value e_xy */
-   
-    for(;;) { 
-      mat.set(x0, y0, 1.);
-      if (x0==x1 && y0==y1) break;
-      
-      e2 = 2*err;
-      if (e2 > dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
-      if (e2 < dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
     }
   }
 
@@ -327,7 +228,7 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
     /**
      * A short name characterizing this Visualizer.
      */
-    private static final String NAME = "HSM Dimension Order";
+    private static final String NAME = "SURFING Dimension Order";
 
     /**
      * Constructor, adhering to
@@ -339,7 +240,7 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
 
     @Override
     public Visualization makeVisualization(VisualizationTask task) {
-      return new HSMDimensionOrder(task);
+      return new SURFINGDimensionOrder(task);
     }
 
     @Override
@@ -360,4 +261,3 @@ public class HSMDimensionOrder extends AbstractParallelVisualization<NumberVecto
     }
   }
 }
-
