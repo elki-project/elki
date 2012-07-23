d97dd713b905213927e74f0dea19db5c00094c3d
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-07-23 17:02:35 +0000

    Reorganize parameteterization / factory / instance split in visualizers.

--- src/de/lmu/ifi/dbs/elki/visualization/visualizers/scatterplot/selection/SelectionCubeVisualization.java
+++ src/de/lmu/ifi/dbs/elki/visualization/visualizers/scatterplot/selection/SelectionCubeVisualization.java
@@ -59,141 +59,179 @@ import de.lmu.ifi.dbs.elki.visualization.visualizers.thumbs.ThumbnailVisualizati
  * 
  * @author Heidi Kolb
  * 
- * @apiviz.has SelectionResult oneway - - visualizes
- * @apiviz.has RangeSelection oneway - - visualizes
- * @apiviz.uses SVGHyperCube
+ * @apiviz.stereotype factory
+ * @apiviz.uses Instance oneway - - «create»
  */
-public class SelectionCubeVisualization extends AbstractScatterplotVisualization {
+public class SelectionCubeVisualization extends AbstractVisFactory {
   /**
    * A short name characterizing this Visualizer.
    */
   private static final String NAME = "Selection Range";
 
   /**
-   * Generic tag to indicate the type of element. Used in IDs, CSS-Classes etc.
+   * Settings
    */
-  public static final String MARKER = "selectionCubeMarker";
+  protected Parameterizer settings;
 
   /**
-   * CSS class for the filled cube
+   * Constructor.
+   * 
+   * @param settings Settings
    */
-  public static final String CSS_CUBE = "selectionCube";
-
-  /**
-   * CSS class for the cube frame
-   */
-  public static final String CSS_CUBEFRAME = "selectionCubeFrame";
-
-  /**
-   * Fill parameter.
-   */
-  protected boolean nofill = false;
-
-  public SelectionCubeVisualization(VisualizationTask task, boolean nofill) {
-    super(task);
-    this.nofill = nofill;
-    addCSSClasses(svgp);
-    context.addResultListener(this);
-    incrementalRedraw();
+  public SelectionCubeVisualization(Parameterizer settings) {
+    super();
+    this.settings = settings;
+    thumbmask |= ThumbnailVisualization.ON_DATA | ThumbnailVisualization.ON_SELECTION;
   }
 
   @Override
-  public void destroy() {
-    context.removeResultListener(this);
-    super.destroy();
-  }
-
-  /**
-   * Adds the required CSS-Classes
-   * 
-   * @param svgp SVG-Plot
-   */
-  private void addCSSClasses(SVGPlot svgp) {
-    final StyleLibrary style = context.getStyleLibrary();
-    // Class for the cube
-    if(!svgp.getCSSClassManager().contains(CSS_CUBE)) {
-      CSSClass cls = new CSSClass(this, CSS_CUBE);
-      cls.setStatement(SVGConstants.CSS_STROKE_VALUE, style.getColor(StyleLibrary.SELECTION));
-      cls.setStatement(SVGConstants.CSS_STROKE_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
-      cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, style.getLineWidth(StyleLibrary.PLOT));
-      cls.setStatement(SVGConstants.CSS_STROKE_LINECAP_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
-      cls.setStatement(SVGConstants.CSS_STROKE_LINEJOIN_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
-      if(nofill) {
-        cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_NONE_VALUE);
-      }
-      else {
-        cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, style.getColor(StyleLibrary.SELECTION));
-        cls.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
-      }
-      svgp.addCSSClassOrLogError(cls);
-    }
-    // Class for the cube frame
-    if(!svgp.getCSSClassManager().contains(CSS_CUBEFRAME)) {
-      CSSClass cls = new CSSClass(this, CSS_CUBEFRAME);
-      cls.setStatement(SVGConstants.CSS_STROKE_VALUE, style.getColor(StyleLibrary.SELECTION));
-      cls.setStatement(SVGConstants.CSS_STROKE_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
-      cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, style.getLineWidth(StyleLibrary.SELECTION));
-
-      svgp.addCSSClassOrLogError(cls);
-    }
-  }
-
-  /**
-   * Generates a cube and a frame depending on the selection stored in the
-   * context
-   * 
-   * @param svgp The plot
-   * @param proj The projection
-   */
-  private void setSVGRect(SVGPlot svgp, Projection2D proj) {
-    DBIDSelection selContext = context.getSelection();
-    if(selContext instanceof RangeSelection) {
-      DoubleDoublePair[] ranges = ((RangeSelection) selContext).getRanges();
-      int dim = DatabaseUtil.dimensionality(rel);
-
-      double[] min = new double[dim];
-      double[] max = new double[dim];
-      for(int d = 0; d < dim; d++) {
-        if(ranges != null && ranges[d] != null) {
-          min[d] = ranges[d].first;
-          max[d] = ranges[d].second;
-        }
-        else {
-          min[d] = proj.getScale(d).getMin();
-          max[d] = proj.getScale(d).getMax();
-        }
-      }
-      if(nofill) {
-        Element r = SVGHyperCube.drawFrame(svgp, proj, min, max);
-        SVGUtil.setCSSClass(r, CSS_CUBEFRAME);
-        layer.appendChild(r);
-      }
-      else {
-        Element r = SVGHyperCube.drawFilled(svgp, CSS_CUBE, proj, min, max);
-        layer.appendChild(r);
-      }
-
-    }
+  public Visualization makeVisualization(VisualizationTask task) {
+    return new Instance(task);
   }
 
   @Override
-  protected void redraw() {
-    DBIDSelection selContext = context.getSelection();
-    if(selContext != null && selContext instanceof RangeSelection) {
-      setSVGRect(svgp, proj);
+  public void processNewResult(HierarchicalResult baseResult, Result result) {
+    Collection<SelectionResult> selectionResults = ResultUtil.filterResults(result, SelectionResult.class);
+    for(SelectionResult selres : selectionResults) {
+      Collection<ScatterPlotProjector<?>> ps = ResultUtil.filterResults(baseResult, ScatterPlotProjector.class);
+      for(ScatterPlotProjector<?> p : ps) {
+        final VisualizationTask task = new VisualizationTask(NAME, selres, p.getRelation(), this);
+        task.put(VisualizationTask.META_LEVEL, VisualizationTask.LEVEL_DATA - 2);
+        baseResult.getHierarchy().add(selres, task);
+        baseResult.getHierarchy().add(p, task);
+      }
     }
   }
 
   /**
-   * Factory for visualizers to generate an SVG-Element containing a cube as
-   * marker representing the selected range for each dimension
+   * Instance.
    * 
    * @author Heidi Kolb
    * 
-   * @apiviz.stereotype factory
-   * @apiviz.uses SelectionCubeVisualization oneway - - «create»
+   * @apiviz.has SelectionResult oneway - - visualizes
+   * @apiviz.has RangeSelection oneway - - visualizes
+   * @apiviz.uses SVGHyperCube
    */
-  public static class Factory extends AbstractVisFactory {
+  public class Instance extends AbstractScatterplotVisualization {
+    /**
+     * Generic tag to indicate the type of element. Used in IDs, CSS-Classes
+     * etc.
+     */
+    public static final String MARKER = "selectionCubeMarker";
+
+    /**
+     * CSS class for the filled cube
+     */
+    public static final String CSS_CUBE = "selectionCube";
+
+    /**
+     * CSS class for the cube frame
+     */
+    public static final String CSS_CUBEFRAME = "selectionCubeFrame";
+
+    public Instance(VisualizationTask task) {
+      super(task);
+      addCSSClasses(svgp);
+      context.addResultListener(this);
+      incrementalRedraw();
+    }
+
+    @Override
+    public void destroy() {
+      context.removeResultListener(this);
+      super.destroy();
+    }
+
+    /**
+     * Adds the required CSS-Classes
+     * 
+     * @param svgp SVG-Plot
+     */
+    private void addCSSClasses(SVGPlot svgp) {
+      final StyleLibrary style = context.getStyleLibrary();
+      // Class for the cube
+      if(!svgp.getCSSClassManager().contains(CSS_CUBE)) {
+        CSSClass cls = new CSSClass(this, CSS_CUBE);
+        cls.setStatement(SVGConstants.CSS_STROKE_VALUE, style.getColor(StyleLibrary.SELECTION));
+        cls.setStatement(SVGConstants.CSS_STROKE_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
+        cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, style.getLineWidth(StyleLibrary.PLOT));
+        cls.setStatement(SVGConstants.CSS_STROKE_LINECAP_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
+        cls.setStatement(SVGConstants.CSS_STROKE_LINEJOIN_PROPERTY, SVGConstants.CSS_ROUND_VALUE);
+        if(settings.nofill) {
+          cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, SVGConstants.CSS_NONE_VALUE);
+        }
+        else {
+          cls.setStatement(SVGConstants.CSS_FILL_PROPERTY, style.getColor(StyleLibrary.SELECTION));
+          cls.setStatement(SVGConstants.CSS_FILL_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
+        }
+        svgp.addCSSClassOrLogError(cls);
+      }
+      // Class for the cube frame
+      if(!svgp.getCSSClassManager().contains(CSS_CUBEFRAME)) {
+        CSSClass cls = new CSSClass(this, CSS_CUBEFRAME);
+        cls.setStatement(SVGConstants.CSS_STROKE_VALUE, style.getColor(StyleLibrary.SELECTION));
+        cls.setStatement(SVGConstants.CSS_STROKE_OPACITY_PROPERTY, style.getOpacity(StyleLibrary.SELECTION));
+        cls.setStatement(SVGConstants.CSS_STROKE_WIDTH_PROPERTY, style.getLineWidth(StyleLibrary.SELECTION));
+
+        svgp.addCSSClassOrLogError(cls);
+      }
+    }
+
+    /**
+     * Generates a cube and a frame depending on the selection stored in the
+     * context
+     * 
+     * @param svgp The plot
+     * @param proj The projection
+     */
+    private void setSVGRect(SVGPlot svgp, Projection2D proj) {
+      DBIDSelection selContext = context.getSelection();
+      if(selContext instanceof RangeSelection) {
+        DoubleDoublePair[] ranges = ((RangeSelection) selContext).getRanges();
+        int dim = DatabaseUtil.dimensionality(rel);
+
+        double[] min = new double[dim];
+        double[] max = new double[dim];
+        for(int d = 0; d < dim; d++) {
+          if(ranges != null && ranges[d] != null) {
+            min[d] = ranges[d].first;
+            max[d] = ranges[d].second;
+          }
+          else {
+            min[d] = proj.getScale(d).getMin();
+            max[d] = proj.getScale(d).getMax();
+          }
+        }
+        if(settings.nofill) {
+          Element r = SVGHyperCube.drawFrame(svgp, proj, min, max);
+          SVGUtil.setCSSClass(r, CSS_CUBEFRAME);
+          layer.appendChild(r);
+        }
+        else {
+          Element r = SVGHyperCube.drawFilled(svgp, CSS_CUBE, proj, min, max);
+          layer.appendChild(r);
+        }
+
+      }
+    }
+
+    @Override
+    protected void redraw() {
+      DBIDSelection selContext = context.getSelection();
+      if(selContext != null && selContext instanceof RangeSelection) {
+        setSVGRect(svgp, proj);
+      }
+    }
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
     /**
      * Flag for half-transparent filling of selection cubes.
      * 
@@ -206,61 +244,20 @@ public class SelectionCubeVisualization extends AbstractScatterplotVisualization
     /**
      * Fill parameter.
      */
-    protected boolean nofill = false;
+    protected boolean nofill;
 
-    /**
-     * Constructor.
-     * 
-     * @param nofill
-     */
-    public Factory(boolean nofill) {
-      super();
-      this.nofill = nofill;
-      thumbmask |= ThumbnailVisualization.ON_DATA | ThumbnailVisualization.ON_SELECTION;
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+      Flag nofillF = new Flag(NOFILL_ID);
+      if(config.grab(nofillF)) {
+        nofill = nofillF.getValue();
+      }
     }
 
     @Override
-    public Visualization makeVisualization(VisualizationTask task) {
-      return new SelectionCubeVisualization(task, nofill);
-    }
-
-    @Override
-    public void processNewResult(HierarchicalResult baseResult, Result result) {
-      Collection<SelectionResult> selectionResults = ResultUtil.filterResults(result, SelectionResult.class);
-      for(SelectionResult selres : selectionResults) {
-        Collection<ScatterPlotProjector<?>> ps = ResultUtil.filterResults(baseResult, ScatterPlotProjector.class);
-        for(ScatterPlotProjector<?> p : ps) {
-          final VisualizationTask task = new VisualizationTask(NAME, selres, p.getRelation(), this);
-          task.put(VisualizationTask.META_LEVEL, VisualizationTask.LEVEL_DATA - 2);
-          baseResult.getHierarchy().add(selres, task);
-          baseResult.getHierarchy().add(p, task);
-        }
-      }
-    }
-
-    /**
-     * Parameterization class.
-     * 
-     * @author Erich Schubert
-     * 
-     * @apiviz.exclude
-     */
-    public static class Parameterizer extends AbstractParameterizer {
-      protected boolean nofill;
-
-      @Override
-      protected void makeOptions(Parameterization config) {
-        super.makeOptions(config);
-        Flag nofillF = new Flag(NOFILL_ID);
-        if(config.grab(nofillF)) {
-          nofill = nofillF.getValue();
-        }
-      }
-
-      @Override
-      protected Factory makeInstance() {
-        return new Factory(nofill);
-      }
+    protected SelectionCubeVisualization makeInstance() {
+      return new SelectionCubeVisualization(this);
     }
   }
 }
