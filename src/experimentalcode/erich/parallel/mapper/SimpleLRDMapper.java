88dc2aded768f3c1273e8b833eada4f0381adb63
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-10-24 11:30:58 +0000

    Add Simple-LOF as parallel implementation.

---
+++ src/experimentalcode/erich/parallel/mapper/SimpleLRDMapper.java
@@ -0,0 +1,124 @@
+package experimentalcode.erich.parallel.mapper;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.database.datastore.DataStore;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDRef;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.DistanceDBIDResultIter;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.KNNResult;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.NumberDistance;
+import experimentalcode.erich.parallel.MapExecutor;
+import experimentalcode.erich.parallel.SharedDouble;
+
+/**
+ * Mapper for the "local reachability density" of LOF.
+ * 
+ * Note: we compute 1/lrd, the local reachability distance.
+ * 
+ * @author Erich Schubert
+ */
+public class SimpleLRDMapper<D extends NumberDistance<D, ?>> implements Mapper {
+  /**
+   * KNN store
+   */
+  private DataStore<? extends KNNResult<D>> knns;
+
+  /**
+   * Output variable
+   */
+  private SharedDouble output;
+
+  /**
+   * Constructor.
+   * 
+   * @param knns k nearest neighbors
+   */
+  public SimpleLRDMapper(DataStore<? extends KNNResult<D>> knns) {
+    super();
+    this.knns = knns;
+  }
+
+  /**
+   * Connect the output variable.
+   * 
+   * @param output Output variable
+   */
+  public void connectOutput(SharedDouble output) {
+    this.output = output;
+  }
+
+  @Override
+  public Instance instantiate(MapExecutor mapper) {
+    return new Instance(output.instantiate(mapper));
+  }
+
+  /**
+   * Mapper instance
+   * 
+   * @author Erich Schubert
+   */
+  private class Instance implements Mapper.Instance {
+    /**
+     * Output variable
+     */
+    private SharedDouble.Instance output;
+
+    /**
+     * Constructor.
+     * 
+     * @param output Output variable
+     */
+    public Instance(SharedDouble.Instance output) {
+      super();
+      this.output = output;
+    }
+
+    @Override
+    public void map(DBIDRef id) {
+      KNNResult<D> knn = knns.get(id);
+      final int size = knn.size() - 1;
+      double lrd = 0.0;
+      int dup = 0;
+      for(DistanceDBIDResultIter<D> n = knn.iter(); n.valid(); n.advance()) {
+        // Do not include the query object
+        if(DBIDUtil.equal(n, id)) {
+          dup++;
+          continue;
+        }
+        lrd += n.getDistance().doubleValue() / size;
+      }
+      // This shouldn't occur, but we can handle it:
+      if(dup != 1) {
+        lrd = lrd * (size / (knn.size() - (double) dup));
+      }
+      output.set(lrd > 0 ? 1 / lrd : 0);
+    }
+
+    @Override
+    public void cleanup() {
+      // Nothing to do.
+    }
+  }
+}
