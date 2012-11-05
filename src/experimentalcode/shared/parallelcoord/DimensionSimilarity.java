b95e09aa128783d3eacfcfa67910a90a5bdef2f8
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-11-05 10:33:02 +0000

    Start cleaning and refactoring Roberts code.

---
+++ src/experimentalcode/shared/parallelcoord/DimensionSimilarity.java
@@ -0,0 +1,47 @@
+package experimentalcode.shared.parallelcoord;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.visualization.projections.ProjectionParallel;
+
+/**
+ * Interface for computing pairwise dimension similarities, used for arranging
+ * dimensions in parallel coordinate plots.
+ * 
+ * @author Erich Schubert
+ * 
+ * @param <V> Object type
+ */
+public interface DimensionSimilarity<V> {
+  /**
+   * Compute the dimension similarity matrix
+   * 
+   * @param relation Relation
+   * @param proj Projection
+   * @param subset DBID subset (for sampling / selection)
+   * @return similarity matrix
+   */
+  public double[][] computeDimensionSimilarites(Relation<? extends V> relation, ProjectionParallel proj, DBIDs subset);
+}
