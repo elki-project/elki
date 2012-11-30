494c98f3cfd281b3f91281d12eb401cb8ccfd86f
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-11-30 15:29:20 +0000

    Add SimpleKernelDensityLOF

---
+++ src/de/lmu/ifi/dbs/elki/algorithm/outlier/SimpleKernelDensityLOF.java
@@ -0,0 +1,287 @@
+package de.lmu.ifi.dbs.elki.algorithm.outlier;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.algorithm.AbstractDistanceBasedAlgorithm;
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.type.CombinedTypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.database.QueryUtil;
+import de.lmu.ifi.dbs.elki.database.datastore.DataStoreFactory;
+import de.lmu.ifi.dbs.elki.database.datastore.DataStoreUtil;
+import de.lmu.ifi.dbs.elki.database.datastore.WritableDoubleDataStore;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDIter;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDs;
+import de.lmu.ifi.dbs.elki.database.query.DatabaseQuery;
+import de.lmu.ifi.dbs.elki.database.query.distance.DistanceQuery;
+import de.lmu.ifi.dbs.elki.database.query.knn.KNNQuery;
+import de.lmu.ifi.dbs.elki.database.query.knn.PreprocessorKNNQuery;
+import de.lmu.ifi.dbs.elki.database.relation.MaterializedRelation;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.database.relation.RelationUtil;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.DistanceDBIDResultIter;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.DoubleDistanceDBIDResultIter;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.DoubleDistanceKNNList;
+import de.lmu.ifi.dbs.elki.distance.distanceresultlist.KNNResult;
+import de.lmu.ifi.dbs.elki.distance.distancevalue.NumberDistance;
+import de.lmu.ifi.dbs.elki.index.preprocessed.knn.MaterializeKNNPreprocessor;
+import de.lmu.ifi.dbs.elki.logging.Logging;
+import de.lmu.ifi.dbs.elki.logging.progress.FiniteProgress;
+import de.lmu.ifi.dbs.elki.logging.progress.StepProgress;
+import de.lmu.ifi.dbs.elki.math.DoubleMinMax;
+import de.lmu.ifi.dbs.elki.math.statistics.EpanechnikovKernelDensityFunction;
+import de.lmu.ifi.dbs.elki.math.statistics.KernelDensityFunction;
+import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
+import de.lmu.ifi.dbs.elki.result.outlier.OutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.result.outlier.QuotientOutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.ObjectParameter;
+
+/**
+ * A simple variant of the LOF algorithm, which uses a simple kernel density
+ * estimation instead of the local reachability density.
+ * 
+ * @author Erich Schubert
+ * 
+ * @apiviz.has KNNQuery
+ * @apiviz.has KernelDensityFunction
+ * 
+ * @param <O> the type of objects handled by this Algorithm
+ * @param <D> Distance type
+ */
+public class SimpleKernelDensityLOF<O extends NumberVector<?>, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedAlgorithm<O, D, OutlierResult> implements OutlierAlgorithm {
+  /**
+   * The logger for this class.
+   */
+  private static final Logging LOG = Logging.getLogger(SimpleKernelDensityLOF.class);
+
+  /**
+   * Parameter k.
+   */
+  protected int k;
+
+  /**
+   * Kernel density function
+   */
+  private KernelDensityFunction kernel;
+
+  /**
+   * Constructor.
+   * 
+   * @param k the value of k
+   * @param kernel Kernel function
+   */
+  public SimpleKernelDensityLOF(int k, DistanceFunction<? super O, D> distance, KernelDensityFunction kernel) {
+    super(distance);
+    this.k = k + 1;
+    this.kernel = kernel;
+  }
+
+  /**
+   * Run the naive kernel density LOF algorithm.
+   * 
+   * @param relation Data to process
+   * @return LOF outlier result
+   */
+  public OutlierResult run(Relation<O> relation) {
+    StepProgress stepprog = LOG.isVerbose() ? new StepProgress("KernelDensityLOF", 3) : null;
+
+    final int dim = RelationUtil.dimensionality(relation);
+
+    DBIDs ids = relation.getDBIDs();
+
+    // "HEAVY" flag for KNN Query since it is used more than once
+    KNNQuery<O, D> knnq = QueryUtil.getKNNQuery(relation, getDistanceFunction(), k, DatabaseQuery.HINT_HEAVY_USE, DatabaseQuery.HINT_OPTIMIZED_ONLY, DatabaseQuery.HINT_NO_CACHE);
+    // No optimized kNN query - use a preprocessor!
+    if (!(knnq instanceof PreprocessorKNNQuery)) {
+      if (stepprog != null) {
+        stepprog.beginStep(1, "Materializing neighborhoods w.r.t. distance function.", LOG);
+      }
+      MaterializeKNNPreprocessor<O, D> preproc = new MaterializeKNNPreprocessor<O, D>(relation, getDistanceFunction(), k);
+      relation.getDatabase().addIndex(preproc);
+      DistanceQuery<O, D> rdq = relation.getDatabase().getDistanceQuery(relation, getDistanceFunction());
+      knnq = preproc.getKNNQuery(rdq, k);
+    }
+
+    // Compute LRDs
+    if (stepprog != null) {
+      stepprog.beginStep(2, "Computing densities.", LOG);
+    }
+    WritableDoubleDataStore dens = DataStoreUtil.makeDoubleStorage(ids, DataStoreFactory.HINT_HOT | DataStoreFactory.HINT_TEMP);
+    FiniteProgress densProgress = LOG.isVerbose() ? new FiniteProgress("Densities", ids.size(), LOG) : null;
+    for (DBIDIter it = ids.iter(); it.valid(); it.advance()) {
+      final KNNResult<D> neighbors = knnq.getKNNForDBID(it, k);
+      final double max;
+      double sum = 0.0;
+      if (neighbors instanceof DoubleDistanceKNNList) {
+        max = ((DoubleDistanceKNNList) neighbors).doubleKNNDistance();
+        // Fast version for double distances
+        for (DoubleDistanceDBIDResultIter neighbor = ((DoubleDistanceKNNList) neighbors).iter(); neighbor.valid(); neighbor.advance()) {
+          if (DBIDUtil.equal(neighbor, it)) {
+            continue;
+          }
+          final double v = neighbor.doubleDistance() / max;
+          sum += kernel.density(v);
+        }
+      } else {
+        max = neighbors.getKNNDistance().doubleValue();
+        for (DistanceDBIDResultIter<D> neighbor = neighbors.iter(); neighbor.valid(); neighbor.advance()) {
+          if (DBIDUtil.equal(neighbor, it)) {
+            continue;
+          }
+          final double v = neighbor.getDistance().doubleValue() / max;
+          sum += kernel.density(v);
+        }
+      }
+      final double density = sum / Math.pow(max, dim);
+      dens.putDouble(it, density);
+      if (densProgress != null) {
+        densProgress.incrementProcessed(LOG);
+      }
+    }
+    if (densProgress != null) {
+      densProgress.ensureCompleted(LOG);
+    }
+
+    // compute LOF_SCORE of each db object
+    if (stepprog != null) {
+      stepprog.beginStep(3, "Computing KLOFs.", LOG);
+    }
+    WritableDoubleDataStore lofs = DataStoreUtil.makeDoubleStorage(ids, DataStoreFactory.HINT_STATIC);
+    // track the maximum value for normalization.
+    DoubleMinMax lofminmax = new DoubleMinMax();
+
+    FiniteProgress progressLOFs = LOG.isVerbose() ? new FiniteProgress("KLOF_SCORE for objects", ids.size(), LOG) : null;
+    for (DBIDIter it = ids.iter(); it.valid(); it.advance()) {
+      final double lrdp = dens.doubleValue(it);
+      final double lof;
+      if (lrdp > 0) {
+        final KNNResult<D> neighbors = knnq.getKNNForDBID(it, k);
+        double sum = 0.0;
+        int count = 0;
+        for (DBIDIter neighbor = neighbors.iter(); neighbor.valid(); neighbor.advance()) {
+          // skip the point itself
+          if (DBIDUtil.equal(neighbor, it)) {
+            continue;
+          }
+          sum += dens.doubleValue(neighbor);
+          count++;
+        }
+        lof = sum / (count * lrdp);
+      } else {
+        lof = 1.0;
+      }
+      lofs.putDouble(it, lof);
+      // update minimum and maximum
+      lofminmax.put(lof);
+
+      if (progressLOFs != null) {
+        progressLOFs.incrementProcessed(LOG);
+      }
+    }
+    if (progressLOFs != null) {
+      progressLOFs.ensureCompleted(LOG);
+    }
+
+    if (stepprog != null) {
+      stepprog.setCompleted(LOG);
+    }
+
+    // Build result representation.
+    Relation<Double> scoreResult = new MaterializedRelation<Double>("Kernel Density Local Outlier Factor", "kernel-density-slof-outlier", TypeUtil.DOUBLE, lofs, ids);
+    OutlierScoreMeta scoreMeta = new QuotientOutlierScoreMeta(lofminmax.getMin(), lofminmax.getMax(), 0.0, Double.POSITIVE_INFINITY, 1.0);
+    OutlierResult result = new OutlierResult(scoreMeta, scoreResult);
+
+    return result;
+  }
+
+  @Override
+  public TypeInformation[] getInputTypeRestriction() {
+    return TypeUtil.array(new CombinedTypeInformation(getDistanceFunction().getInputTypeRestriction(), TypeUtil.NUMBER_VECTOR_FIELD));
+  }
+
+  @Override
+  protected Logging getLogger() {
+    return LOG;
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   * 
+   * @param <O> vector type
+   * @param <D> distance type
+   */
+  public static class Parameterizer<O extends NumberVector<?>, D extends NumberDistance<D, ?>> extends AbstractDistanceBasedAlgorithm.Parameterizer<O, D> {
+    /**
+     * Option ID for kernel density LOF.
+     */
+    public static final OptionID KERNEL_ID = OptionID.getOrCreateOptionID("kernellof.kernel", "Kernel to use for kernel density LOF.");
+
+    /**
+     * The neighborhood size to use.
+     */
+    protected int k = 2;
+
+    /**
+     * Neighborhood distance function.
+     */
+    protected DistanceFunction<O, D> neighborhoodDistanceFunction = null;
+
+    /**
+     * Kernel density function parameter
+     */
+    KernelDensityFunction kernel;
+
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+
+      final IntParameter pK = new IntParameter(LOF.K_ID);
+      pK.addConstraint(new GreaterConstraint(1));
+      if (config.grab(pK)) {
+        k = pK.getValue();
+      }
+
+      ObjectParameter<KernelDensityFunction> kernelP = new ObjectParameter<KernelDensityFunction>(KERNEL_ID, KernelDensityFunction.class, EpanechnikovKernelDensityFunction.class);
+      if (config.grab(kernelP)) {
+        kernel = kernelP.instantiateClass(config);
+      }
+    }
+
+    @Override
+    protected SimpleKernelDensityLOF<O, D> makeInstance() {
+      return new SimpleKernelDensityLOF<O, D>(k, distanceFunction, kernel);
+    }
+  }
+}
