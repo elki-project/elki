8002110811113436d720bbf13d59fb85f5c0728f
Erich Schubert <schube@dbs.ifi.lmu.de>
2012-12-07 10:09:33 +0000

    Move simple outlier ensemble code to trunk.

--- src/de/lmu/ifi/dbs/elki/algorithm/outlier/meta/SimpleOutlierEnsemble.java
+++ src/de/lmu/ifi/dbs/elki/algorithm/outlier/meta/SimpleOutlierEnsemble.java
@@ -1,28 +1,30 @@
-package experimentalcode.shared.outlier.ensemble;
+package de.lmu.ifi.dbs.elki.algorithm.outlier.meta;
+
 /*
-This file is part of ELKI:
-Environment for Developing KDD-Applications Supported by Index-Structures
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
 
-Copyright (C) 2012
-Ludwig-Maximilians-Universität München
-Lehr- und Forschungseinheit für Datenbanksysteme
-ELKI Development Team
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.lmu.ifi.dbs.elki.algorithm.AbstractAlgorithm;
@@ -48,13 +50,14 @@ import de.lmu.ifi.dbs.elki.result.ResultUtil;
 import de.lmu.ifi.dbs.elki.result.outlier.BasicOutlierScoreMeta;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
 import de.lmu.ifi.dbs.elki.result.outlier.OutlierScoreMeta;
+import de.lmu.ifi.dbs.elki.utilities.ensemble.EnsembleVoting;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ChainedParameterization;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.ObjectListParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.ObjectParameter;
-import experimentalcode.shared.outlier.ensemble.voting.EnsembleVoting;
 
 /**
  * Simple outlier ensemble method.
@@ -64,34 +67,17 @@ import experimentalcode.shared.outlier.ensemble.voting.EnsembleVoting;
  * @apiviz.composedOf EnsembleVoting
  * @apiviz.uses OutlierResult oneway - - reads
  * @apiviz.uses OutlierResult oneway - - «create»
- * 
- * @param <O> object type
  */
-public class OutlierVotingEnsemble<O> extends AbstractAlgorithm<OutlierResult> {
+public class SimpleOutlierEnsemble extends AbstractAlgorithm<OutlierResult> implements OutlierAlgorithm {
   /**
    * The logger for this class.
    */
-  private static final Logging LOG = Logging.getLogger(OutlierVotingEnsemble.class);
-  
-  /**
-   * Parameter for the individual algorithms
-   */
-  private ObjectListParameter<OutlierAlgorithm> ALGORITHMS_PARAM = new ObjectListParameter<OutlierAlgorithm>(OptionID.ALGORITHM, OutlierAlgorithm.class);
+  private static final Logging LOG = Logging.getLogger(SimpleOutlierEnsemble.class);
 
   /**
-   * The actual algorithms
+   * The algorithms to run.
    */
   private List<OutlierAlgorithm> algorithms;
-
-  /**
-   * Voting strategy to use in the ensemble.
-   */
-  public static final OptionID VOTING_ID = new OptionID("ensemble.voting", "Voting strategy to use in the ensemble.");
-
-  /**
-   * Voting strategy parameter
-   */
-  private ObjectParameter<EnsembleVoting> VOTING_PARAM = new ObjectParameter<EnsembleVoting>(VOTING_ID, EnsembleVoting.class);
 
   /**
    * The voting in use.
@@ -99,24 +85,14 @@ public class OutlierVotingEnsemble<O> extends AbstractAlgorithm<OutlierResult> {
   private EnsembleVoting voting;
 
   /**
-   * Constructor, adhering to
-   * {@link de.lmu.ifi.dbs.elki.utilities.optionhandling.Parameterizable}
+   * Constructor.
    * 
-   * @param config Parameterization
+   * @param algorithms Algorithms to run
+   * @param voting Voting method
    */
-  public OutlierVotingEnsemble(Parameterization config) {
-    super();
-    config = config.descend(this);
-    if(config.grab(ALGORITHMS_PARAM)) {
-      ListParameterization subconfig = new ListParameterization();
-      ChainedParameterization chain = new ChainedParameterization(subconfig, config);
-      chain.errorsTo(config);
-      algorithms = ALGORITHMS_PARAM.instantiateClasses(chain);
-      subconfig.logAndClearReportedErrors();
-    }
-    if(config.grab(VOTING_PARAM)) {
-      voting = VOTING_PARAM.instantiateClass(config);
-    }
+  public SimpleOutlierEnsemble(List<OutlierAlgorithm> algorithms, EnsembleVoting voting) {
+    this.algorithms = algorithms;
+    this.voting = voting;
   }
 
   @Override
@@ -127,18 +103,18 @@ public class OutlierVotingEnsemble<O> extends AbstractAlgorithm<OutlierResult> {
     ArrayList<OutlierResult> results = new ArrayList<OutlierResult>(num);
     {
       FiniteProgress prog = LOG.isVerbose() ? new FiniteProgress("Inner outlier algorithms", num, LOG) : null;
-      for(Algorithm alg : algorithms) {
+      for (Algorithm alg : algorithms) {
         Result res = alg.run(database);
         List<OutlierResult> ors = ResultUtil.getOutlierResults(res);
-        for(OutlierResult or : ors) {
+        for (OutlierResult or : ors) {
           results.add(or);
           ids.addDBIDs(or.getScores().getDBIDs());
         }
-        if(prog != null) {
+        if (prog != null) {
           prog.incrementProcessed(LOG);
         }
       }
-      if(prog != null) {
+      if (prog != null) {
         prog.ensureCompleted(LOG);
       }
     }
@@ -147,35 +123,39 @@ public class OutlierVotingEnsemble<O> extends AbstractAlgorithm<OutlierResult> {
     DoubleMinMax minmax = new DoubleMinMax();
     {
       FiniteProgress cprog = LOG.isVerbose() ? new FiniteProgress("Combining results", ids.size(), LOG) : null;
-      for(DBIDIter id = ids.iter(); id.valid(); id.advance()) {
-        ArrayList<Double> scores = new ArrayList<Double>(num);
-        for(OutlierResult r : results) {
+      for (DBIDIter id = ids.iter(); id.valid(); id.advance()) {
+        double[] scores = new double[num];
+        int i = 0;
+        for (OutlierResult r : results) {
           Double score = r.getScores().get(id);
-          if(score != null) {
-            scores.add(score);
-          }
-          else {
+          if (score != null) {
+            scores[i] = score;
+            i++;
+          } else {
             LOG.warning("DBID " + id + " was not given a score by result " + r);
           }
         }
-        if(scores.size() > 0) {
+        if (i > 0) {
+          // Shrink array if necessary.
+          if (i < scores.length) {
+            scores = Arrays.copyOf(scores, i);
+          }
           double combined = voting.combine(scores);
           sumscore.putDouble(id, combined);
           minmax.put(combined);
-        }
-        else {
+        } else {
           LOG.warning("DBID " + id + " was not given any score at all.");
         }
-        if(cprog != null) {
+        if (cprog != null) {
           cprog.incrementProcessed(LOG);
         }
       }
-      if(cprog != null) {
+      if (cprog != null) {
         cprog.ensureCompleted(LOG);
       }
     }
     OutlierScoreMeta meta = new BasicOutlierScoreMeta(minmax.getMin(), minmax.getMax());
-    Relation<Double> scores = new MaterializedRelation<Double>("Outlier Ensemble", "ensemble-outlier", TypeUtil.DOUBLE, sumscore, ids);
+    Relation<Double> scores = new MaterializedRelation<Double>("Simple Outlier Ensemble", "ensemble-outlier", TypeUtil.DOUBLE, sumscore, ids);
     return new OutlierResult(meta, scores);
   }
 
@@ -188,9 +168,55 @@ public class OutlierVotingEnsemble<O> extends AbstractAlgorithm<OutlierResult> {
   public TypeInformation[] getInputTypeRestriction() {
     TypeInformation[] trs = new TypeInformation[algorithms.size()];
     for (int i = 0; i < trs.length; i++) {
-      // FIXME: only allow single-input algorithms?
+      // FIXME: what if an algorithm needs more than one input data source?
       trs[i] = algorithms.get(i).getInputTypeRestriction()[0];
     }
     return TypeUtil.array(new CombinedTypeInformation(trs));
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    /**
+     * Voting strategy to use in the ensemble.
+     */
+    public static final OptionID VOTING_ID = new OptionID("ensemble.voting", "Voting strategy to use in the ensemble.");
+
+    /**
+     * The algorithms to run.
+     */
+    private List<OutlierAlgorithm> algorithms;
+
+    /**
+     * The voting in use.
+     */
+    private EnsembleVoting voting;
+
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+      ObjectListParameter<OutlierAlgorithm> algP = new ObjectListParameter<OutlierAlgorithm>(OptionID.ALGORITHM, OutlierAlgorithm.class);
+      if (config.grab(algP)) {
+        ListParameterization subconfig = new ListParameterization();
+        ChainedParameterization chain = new ChainedParameterization(subconfig, config);
+        chain.errorsTo(config);
+        algorithms = algP.instantiateClasses(chain);
+        subconfig.logAndClearReportedErrors();
+      }
+      ObjectParameter<EnsembleVoting> votingP = new ObjectParameter<EnsembleVoting>(VOTING_ID, EnsembleVoting.class);
+      if (config.grab(votingP)) {
+        voting = votingP.instantiateClass(config);
+      }
+    }
+
+    @Override
+    protected SimpleOutlierEnsemble makeInstance() {
+      return new SimpleOutlierEnsemble(algorithms, voting);
+    }
   }
 }
---
+++ src/de/lmu/ifi/dbs/elki/utilities/ensemble/EnsembleVotingMedian.java
@@ -0,0 +1,95 @@
+package de.lmu.ifi.dbs.elki.utilities.ensemble;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.utilities.datastructures.QuickSelect;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterEqualConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.LessEqualConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.DoubleParameter;
+
+/**
+ * Simple combination rule, by taking the median.
+ * 
+ * Note: median is very similar to a <em>majority voting</em>!
+ * 
+ * @author Erich Schubert
+ */
+public class EnsembleVotingMedian implements EnsembleVoting {
+  /**
+   * Quantile to use
+   */
+  private double quantile = 0.5;
+
+  /**
+   * Constructor.
+   * 
+   * @param quantile Quantile
+   */
+  public EnsembleVotingMedian(double quantile) {
+    this.quantile = quantile;
+  }
+
+  @Override
+  public double combine(double[] scores) {
+    return QuickSelect.quantile(scores, quantile);
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    /**
+     * Option ID for the quantile
+     */
+    public static final OptionID QUANTILE_ID = new OptionID("ensemble.median.quantile", "Quantile to use in median voting.");
+
+    /**
+     * Quantile to use
+     */
+    private double quantile = 0.5;
+
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+      DoubleParameter quantileP = new DoubleParameter(QUANTILE_ID, 0.5);
+      quantileP.addConstraint(new GreaterEqualConstraint(0.0));
+      quantileP.addConstraint(new LessEqualConstraint(1.0));
+      if (config.grab(quantileP)) {
+        quantile = quantileP.getValue();
+      }
+    }
+
+    @Override
+    protected EnsembleVotingMedian makeInstance() {
+      return new EnsembleVotingMedian(quantile);
+    }
+  }
+}
