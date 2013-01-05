1a61a631730594c1f57b054ca5fa839bc2a1077b
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-01-05 13:30:55 +0000

    Test sliding window variance support.

---
+++ test/de/lmu/ifi/dbs/elki/math/TestSlidingVariance.java
@@ -0,0 +1,67 @@
+package de.lmu.ifi.dbs.elki.math;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.JUnit4Test;
+
+/**
+ * Unit test if negative weights in {@link MeanVariance} work as desired.
+ * 
+ * @author Erich Schubert
+ */
+public class TestSlidingVariance implements JUnit4Test {
+  /**
+   * Size of test data set.
+   */
+  private static final int SIZE = 100000;
+
+  /**
+   * Sliding window size.
+   */
+  private static final int WINDOWSIZE = 100;
+
+  @Test
+  public void testSlidingWindowVariance() {
+    MeanVariance mv = new MeanVariance();
+    MeanVariance mc = new MeanVariance();
+
+    Random r = new Random();
+    double[] data = new double[SIZE];
+    for(int i = 0; i < data.length; i++) {
+      data[i] = r.nextDouble();
+    }
+
+    // Pre-roll:
+    for(int i = 0; i < WINDOWSIZE; i++) {
+      mv.put(data[i]);
+    }
+    // Compare to window approach
+    for(int i = WINDOWSIZE; i < data.length; i++) {
+      mv.put(data[i - WINDOWSIZE], -1.); // Remove
+      mv.put(data[i]);
+
+      mc.reset(); // Reset statistics
+      for(int j = i + 1 - WINDOWSIZE; j <= i; j++) {
+        mc.put(data[j]);
+      }
+      // Fully manual statistics, exact two-pass algorithm:
+      double mean = 0.0;
+      for(int j = i + 1 - WINDOWSIZE; j <= i; j++) {
+        mean += data[j];
+      }
+      mean /= WINDOWSIZE;
+      double var = 0.0;
+      for(int j = i + 1 - WINDOWSIZE; j <= i; j++) {
+        double v = data[j] - mean;
+        var += v * v;
+      }
+      var /= (WINDOWSIZE - 1);
+      assertEquals("Variance does not agree.", mv.getSampleVariance(), mc.getSampleVariance(), 1e-14);
+      assertEquals("Variance does not agree.", mv.getSampleVariance(), var, 1e-14);
+    }
+  }
+}
