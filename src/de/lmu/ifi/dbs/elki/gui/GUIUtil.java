eebbaf6b17fce09b92fb2325334d4d4cf011da41
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-02-06 16:53:32 +0000

    Bounce copyright year number to 2013

--- src/de/lmu/ifi/dbs/elki/gui/GUIUtil.java
+++ src/de/lmu/ifi/dbs/elki/gui/GUIUtil.java
@@ -4,7 +4,7 @@ package de.lmu.ifi.dbs.elki.gui;
  This file is part of ELKI:
  Environment for Developing KDD-Applications Supported by Index-Structures
 
- Copyright (C) 2012
+ Copyright (C) 2013
  Ludwig-Maximilians-Universität München
  Lehr- und Forschungseinheit für Datenbanksysteme
  ELKI Development Team
