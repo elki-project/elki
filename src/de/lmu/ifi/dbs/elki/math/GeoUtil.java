8d692ae06181b9501b9d70ff8f8d64d92717c11b
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-03-04 14:36:48 +0000

    Comments.

--- src/de/lmu/ifi/dbs/elki/math/GeoUtil.java
+++ src/de/lmu/ifi/dbs/elki/math/GeoUtil.java
@@ -135,6 +135,8 @@ public final class GeoUtil {
     final double slon = Math.sin((lon1 - lon2) * .5);
     final double a = slat * slat + slon * slon * Math.cos(lat1) * Math.cos(lat2);
     final double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    // Alternate version - TODO: which one is faster, more precise?
+    // final double c = 2 * Math.asin(Math.sqrt(a));
     return EARTH_RADIUS * c;
   }
 
