28275bca7ee6d3b0006df8e4831f482f6ce34958
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-03-13 17:58:02 +0000

    PINN is now achievable using the ProjectedIndex class

--- src/experimentalcode/students/frankenb/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * <p>The base-package of Florian's experimental code for the ELKI framework.</p>
- * 
- * 
- *  <p>Classes not scheduled for a release.</p>
- *  
- */
-package experimentalcode.students.frankenb;
