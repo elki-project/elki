b61aa6f08237658e22c18c1ee43fa3c720729a9f
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-03-31 13:04:27 +0000

    Add DoubleLongHeaps.

---
+++ src/de/lmu/ifi/dbs/elki/utilities/datastructures/heap/DoubleLongHeap.java
@@ -0,0 +1,127 @@
+package de.lmu.ifi.dbs.elki.utilities.datastructures.heap;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2012
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.utilities.datastructures.iterator.Iter;
+
+/**
+ * Basic in-memory heap interface, for double keys and long values.
+ * 
+ * @author Erich Schubert
+ * 
+ * @apiviz.has UnsortedIter
+ */
+public interface DoubleLongHeap {
+  /**
+   * Add a key-value pair to the heap
+   * 
+   * @param key Key
+   * @param val Value
+   */
+  void add(double key, long val);
+
+  /**
+   * Add a key-value pair to the heap if it improves the top.
+   * 
+   * @param key Key
+   * @param val Value
+   * @param k Desired maximum size
+   */
+  void add(double key, long val, int k);
+
+  /**
+   * Combined operation that removes the top element, and inserts a new element
+   * instead.
+   * 
+   * @param key Key of new element
+   * @param val Value of new element
+   */
+  void replaceTopElement(double key, long val);
+
+  /**
+   * Get the current top key
+   * 
+   * @return Top key
+   */
+  double peekKey();
+
+  /**
+   * Get the current top value
+   * 
+   * @return Value
+   */
+  long peekValue();
+
+  /**
+   * Remove the first element
+   */
+  void poll();
+
+  /**
+   * Clear the heap contents.
+   */
+  void clear();
+
+  /**
+   * Query the size
+   * 
+   * @return Size
+   */
+  public int size();
+  
+  /**
+   * Is the heap empty?
+   * 
+   * @return {@code true} when the size is 0.
+   */
+  public boolean isEmpty();
+
+  /**
+   * Get an unsorted iterator to inspect the heap.
+   * 
+   * @return Iterator
+   */
+  UnsortedIter unsortedIter();
+
+  /**
+   * Unsorted iterator - in heap order. Does not poll the heap.
+   * 
+   * @author Erich Schubert
+   */
+  public static interface UnsortedIter extends Iter {
+    /**
+     * Get the current key
+     * 
+     * @return Current key
+     */
+    double getKey();
+
+    /**
+     * Get the current value
+     * 
+     * @return Current value
+     */
+    long getValue();
+  }
+}
---
+++ src/de/lmu/ifi/dbs/elki/utilities/datastructures/heap/DoubleLongMaxHeap.java
@@ -0,0 +1,478 @@
+package de.lmu.ifi.dbs.elki.utilities.datastructures.heap;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+
+import de.lmu.ifi.dbs.elki.math.MathUtil;
+
+/**
+ * Advanced priority queue class, based on a binary heap (for small sizes),
+ * which will for larger heaps be accompanied by a 4-ary heap (attached below
+ * the root of the two-ary heap, making the root actually 3-ary).
+ * 
+ * This code was automatically instantiated for the types: Double and Long
+ * 
+ * This combination was found to work quite well in benchmarks, but YMMV.
+ * 
+ * Some other observations from benchmarking:
+ * <ul>
+ * <li>Bulk loading did not improve things</li>
+ * <li>Primitive heaps are substantially faster.</li>
+ * <li>Since an array in Java has an overhead of 12 bytes, odd-sized object and
+ * integer arrays are actually well aligned both for 2-ary and 4-ary heaps.</li>
+ * <li>Workload makes a huge difference. A load-once, poll-until-empty priority
+ * queue is something different than e.g. a top-k heap, which will see a lot of
+ * top element replacements.</li>
+ * <li>Random vs. increasing vs. decreasing vs. sawtooth insertion patterns for
+ * top-k make a difference.</li>
+ * <li>Different day, different benchmark results ...</li>
+ * </ul>
+ * 
+ * @author Erich Schubert
+ * 
+ * @apiviz.has UnsortedIter
+ */
+public class DoubleLongMaxHeap implements DoubleLongHeap {
+  /**
+   * Base heap.
+   */
+  protected double[] twoheap;
+
+  /**
+   * Base heap values.
+   */
+  protected long[] twovals;
+
+  /**
+   * Extension heap.
+   */
+  protected double[] fourheap;
+
+  /**
+   * Extension heapvalues.
+   */
+  protected long[] fourvals;
+
+  /**
+   * Current size of heap.
+   */
+  protected int size;
+
+  /**
+   * (Structural) modification counter. Used to invalidate iterators.
+   */
+  protected int modCount = 0;
+
+  /**
+   * Maximum size of the 2-ary heap. A complete 2-ary heap has (2^k-1) elements.
+   */
+  private final static int TWO_HEAP_MAX_SIZE = (1 << 9) - 1;
+
+  /**
+   * Initial size of the 2-ary heap.
+   */
+  private final static int TWO_HEAP_INITIAL_SIZE = (1 << 5) - 1;
+
+  /**
+   * Initial size of 4-ary heap when initialized.
+   * 
+   * 21 = 4-ary heap of height 2: 1 + 4 + 4*4
+   * 
+   * 85 = 4-ary heap of height 3: 21 + 4*4*4
+   * 
+   * 341 = 4-ary heap of height 4: 85 + 4*4*4*4
+   * 
+   * Since we last grew by 255 (to 511), let's use 341.
+   */
+  private final static int FOUR_HEAP_INITIAL_SIZE = 341;
+
+  /**
+   * Constructor, with default size.
+   */
+  public DoubleLongMaxHeap() {
+    super();
+    double[] twoheap = new double[TWO_HEAP_INITIAL_SIZE];
+    long[] twovals = new long[TWO_HEAP_INITIAL_SIZE];
+
+    this.twoheap = twoheap;
+    this.twovals = twovals;
+    this.fourheap = null;
+    this.fourvals = null;
+    this.size = 0;
+    this.modCount = 0;
+  }
+
+  /**
+   * Constructor, with given minimum size.
+   * 
+   * @param minsize Minimum size
+   */
+  public DoubleLongMaxHeap(int minsize) {
+    super();
+    if (minsize < TWO_HEAP_MAX_SIZE) {
+      final int size = MathUtil.nextPow2Int(minsize + 1) - 1;
+      double[] twoheap = new double[size];
+      long[] twovals = new long[size];
+      
+      this.twoheap = twoheap;
+      this.twovals = twovals;
+      this.fourheap = null;
+      this.fourvals = null;
+    } else {
+      double[] twoheap = new double[TWO_HEAP_INITIAL_SIZE];
+      long[] twovals = new long[TWO_HEAP_INITIAL_SIZE];
+      double[] fourheap = new double[minsize - TWO_HEAP_MAX_SIZE];
+      long[] fourvals = new long[minsize - TWO_HEAP_MAX_SIZE];
+      this.twoheap = twoheap;
+      this.twovals = twovals;
+      this.fourheap = fourheap;
+      this.fourvals = fourvals;
+    }
+    this.size = 0;
+    this.modCount = 0;
+  }
+
+  @Override
+  public void clear() {
+    size = 0;
+    ++modCount;
+    fourheap = null;
+    fourvals = null;
+    Arrays.fill(twoheap, 0.0);
+    Arrays.fill(twovals, 0);
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+
+  @Override
+  public void add(double o, long v) {
+    final double co = o;
+    final long cv = v;
+    // System.err.println("Add: " + o);
+    if (size < TWO_HEAP_MAX_SIZE) {
+      if (size >= twoheap.length) {
+        // Grow by one layer.
+        twoheap = Arrays.copyOf(twoheap, twoheap.length + twoheap.length + 1);
+        twovals = Arrays.copyOf(twovals, twovals.length + twovals.length + 1);
+      }
+      final int twopos = size;
+      twoheap[twopos] = co;
+      twovals[twopos] = cv;
+      ++size;
+      heapifyUp2(twopos, co, cv);
+      ++modCount;
+    } else {
+      final int fourpos = size - TWO_HEAP_MAX_SIZE;
+      if (fourheap == null) {
+        fourheap = new double[FOUR_HEAP_INITIAL_SIZE];
+        fourvals = new long[FOUR_HEAP_INITIAL_SIZE];
+      } else if (fourpos >= fourheap.length) {
+        // Grow extension heap by half.
+        fourheap = Arrays.copyOf(fourheap, fourheap.length + (fourheap.length >> 1));
+        fourvals = Arrays.copyOf(fourvals, fourvals.length + (fourvals.length >> 1));
+      }
+      fourheap[fourpos] = co;
+      fourvals[fourpos] = cv;
+      ++size;
+      heapifyUp4(fourpos, co, cv);
+      ++modCount;
+    }
+  }
+
+  @Override
+  public void add(double key, long val, int max) {
+    if (size < max) {
+      add(key, val);
+    } else if (twoheap[0] >= key) {
+      replaceTopElement(key, val);
+    }
+  }
+
+  @Override
+  public void replaceTopElement(double reinsert, long val) {
+    heapifyDown(reinsert, val);
+    ++modCount;
+  }
+
+  /**
+   * Heapify-Up method for 2-ary heap.
+   * 
+   * @param twopos Position in 2-ary heap.
+   * @param cur Current object
+   * @param val Current value
+   */
+  private void heapifyUp2(int twopos, double cur, long val) {
+    while (twopos > 0) {
+      final int parent = (twopos - 1) >>> 1;
+      double par = twoheap[parent];
+      if (cur <= par) {
+        break;
+      }
+      twoheap[twopos] = par;
+      twovals[twopos] = twovals[parent];
+      twopos = parent;
+    }
+    twoheap[twopos] = cur;
+    twovals[twopos] = val;
+  }
+
+  /**
+   * Heapify-Up method for 4-ary heap.
+   * 
+   * @param fourpos Position in 4-ary heap.
+   * @param cur Current object
+   * @param val Current value
+   */
+  private void heapifyUp4(int fourpos, double cur, long val) {
+    while (fourpos > 0) {
+      final int parent = (fourpos - 1) >> 2;
+      double par = fourheap[parent];
+      if (cur <= par) {
+        break;
+      }
+      fourheap[fourpos] = par;
+      fourvals[fourpos] = fourvals[parent];
+      fourpos = parent;
+    }
+    if (fourpos == 0 && twoheap[0] < cur) {
+      fourheap[0] = twoheap[0];
+      fourvals[0] = twovals[0];
+      twoheap[0] = cur;
+      twovals[0] = val;
+    } else {
+      fourheap[fourpos] = cur;
+      fourvals[fourpos] = val;
+    }
+  }
+
+  @Override
+  public void poll() {
+    --size;
+    // Replacement object:
+    if (size >= TWO_HEAP_MAX_SIZE) {
+      final int last = size - TWO_HEAP_MAX_SIZE;
+      final double reinsert = fourheap[last];
+      final long reinsertv = fourvals[last];
+      fourheap[last] = 0.0;
+      fourvals[last] = 0;
+      heapifyDown(reinsert, reinsertv);
+    } else if (size > 0) {
+      final double reinsert = twoheap[size];
+      final long reinsertv = twovals[size];
+      twoheap[size] = 0.0;
+      twovals[size] = 0;
+      heapifyDown(reinsert, reinsertv);
+    } else {
+      twoheap[0] = 0.0;
+      twovals[0] = 0;
+    }
+    ++modCount;
+  }
+
+  /**
+   * Invoke heapify-down for the root object.
+   * 
+   * @param reinsert Object to insert.
+   * @param val Value to reinsert.
+   */
+  private void heapifyDown(double reinsert, long val) {
+    if (size > TWO_HEAP_MAX_SIZE) {
+      // Special case: 3-ary situation.
+      final int best = (twoheap[1] >= twoheap[2]) ? 1 : 2;
+      if (fourheap[0] > twoheap[best]) {
+        twoheap[0] = fourheap[0];
+        twovals[0] = fourvals[0];
+        heapifyDown4(0, reinsert, val);
+      } else {
+        twoheap[0] = twoheap[best];
+        twovals[0] = twovals[best];
+        heapifyDown2(best, reinsert, val);
+      }
+      return;
+    }
+    heapifyDown2(0, reinsert, val);
+  }
+
+  /**
+   * Heapify-Down for 2-ary heap.
+   * 
+   * @param twopos Position in 2-ary heap.
+   * @param cur Current object
+   * @param val Value to reinsert.
+   */
+  private void heapifyDown2(int twopos, double cur, long val) {
+    final int stop = Math.min(size, TWO_HEAP_MAX_SIZE) >>> 1;
+    while (twopos < stop) {
+      int bestchild = (twopos << 1) + 1;
+      double best = twoheap[bestchild];
+      final int right = bestchild + 1;
+      if (right < size && best < twoheap[right]) {
+        bestchild = right;
+        best = twoheap[right];
+      }
+      if (cur >= best) {
+        break;
+      }
+      twoheap[twopos] = best;
+      twovals[twopos] = twovals[bestchild];
+      twopos = bestchild;
+    }
+    twoheap[twopos] = cur;
+    twovals[twopos] = val;
+  }
+
+  /**
+   * Heapify-Down for 4-ary heap.
+   * 
+   * @param fourpos Position in 4-ary heap.
+   * @param cur Current object
+   * @param val Value to reinsert.
+   */
+  private void heapifyDown4(int fourpos, double cur, long val) {
+    final int stop = (size - TWO_HEAP_MAX_SIZE + 2) >>> 2;
+    while (fourpos < stop) {
+      final int child = (fourpos << 2) + 1;
+      double best = fourheap[child];
+      int bestchild = child, candidate = child + 1, minsize = candidate + TWO_HEAP_MAX_SIZE;
+      if (size > minsize) {
+        double nextchild = fourheap[candidate];
+        if (best < nextchild) {
+          bestchild = candidate;
+          best = nextchild;
+        }
+
+        minsize += 2;
+        if (size >= minsize) {
+          nextchild = fourheap[++candidate];
+          if (best < nextchild) {
+            bestchild = candidate;
+            best = nextchild;
+          }
+
+          if (size > minsize) {
+            nextchild = fourheap[++candidate];
+            if (best < nextchild) {
+              bestchild = candidate;
+              best = nextchild;
+            }
+          }
+        }
+      }
+      if (cur >= best) {
+        break;
+      }
+      fourheap[fourpos] = best;
+      fourvals[fourpos] = fourvals[bestchild];
+      fourpos = bestchild;
+    }
+    fourheap[fourpos] = cur;
+    fourvals[fourpos] = val;
+  }
+
+  @Override
+  public double peekKey() {
+    return twoheap[0];
+  }
+
+  @Override
+  public long peekValue() {
+    return twovals[0];
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder buf = new StringBuilder();
+    buf.append(DoubleLongMaxHeap.class.getSimpleName()).append(" [");
+    for (UnsortedIter iter = new UnsortedIter(); iter.valid(); iter.advance()) {
+      buf.append(iter.getKey()).append(':').append(iter.getValue()).append(',');
+    }
+    buf.append(']');
+    return buf.toString();
+  }
+
+  @Override
+  public UnsortedIter unsortedIter() {
+    return new UnsortedIter();
+  }
+
+  /**
+   * Unsorted iterator - in heap order. Does not poll the heap.
+   * 
+   * Use this class as follows:
+   * 
+   * <pre>
+   * {@code
+   * for (DoubleLongHeap.UnsortedIter iter = heap.unsortedIter(); iter.valid(); iter.next()) {
+   *   doSomething(iter.get());
+   * }
+   * }
+   * </pre>
+   * 
+   * @author Erich Schubert
+   */
+  private class UnsortedIter implements DoubleLongHeap.UnsortedIter {
+    /**
+     * Iterator position.
+     */
+    protected int pos = 0;
+
+    /**
+     * Modification counter we were initialized at.
+     */
+    protected final int myModCount = modCount;
+
+    @Override
+    public boolean valid() {
+      if (modCount != myModCount) {
+        throw new ConcurrentModificationException();
+      }
+      return pos < size;
+    }
+
+    @Override
+    public void advance() {
+      pos++;
+    }
+
+    @Override
+    public double getKey() {
+      return ((pos < TWO_HEAP_MAX_SIZE) ? twoheap[pos] : fourheap[pos - TWO_HEAP_MAX_SIZE]);
+    }
+
+    @Override
+    public long getValue() {
+      return ((pos < TWO_HEAP_MAX_SIZE) ? twovals[pos] : fourvals[pos - TWO_HEAP_MAX_SIZE]);
+    }
+  }
+}
