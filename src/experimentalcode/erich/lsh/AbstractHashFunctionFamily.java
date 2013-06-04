4424eaabe65a96e47180b2599593c7014aed4301
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-06-04 09:53:16 +0000

    More LSH work. TODO: queries!

---
+++ src/experimentalcode/erich/lsh/AbstractHashFunctionFamily.java
@@ -0,0 +1,161 @@
+package experimentalcode.erich.lsh;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.util.ArrayList;
+
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.data.type.TypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.database.relation.RelationUtil;
+import de.lmu.ifi.dbs.elki.math.linearalgebra.Matrix;
+import de.lmu.ifi.dbs.elki.math.linearalgebra.randomprojections.RandomProjectionFamily;
+import de.lmu.ifi.dbs.elki.utilities.RandomFactory;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.GreaterConstraint;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.DoubleParameter;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.RandomParameter;
+
+/**
+ * Abstract base class for projection based hash functions.
+ * 
+ * @author Erich Schubert
+ */
+public class AbstractHashFunctionFamily implements LocalitySensitiveHashFunctionFamily<NumberVector<?>> {
+  /**
+   * Random generator to use.
+   */
+  protected RandomFactory random;
+
+  /**
+   * Projection family to use.
+   */
+  protected RandomProjectionFamily proj;
+
+  /**
+   * Width of each bin.
+   */
+  protected double width;
+
+  /**
+   * The number of projections to use for each hash function.
+   */
+  protected int l;
+
+  /**
+   * Constructor.
+   * 
+   * @param random Random generator
+   * @param proj Projection family
+   * @param width Bin width
+   * @param l Number of projections for each hash function.
+   */
+  public AbstractHashFunctionFamily(RandomFactory random, RandomProjectionFamily proj, double width, int l) {
+    super();
+    this.random = random;
+    this.proj = proj;
+    this.width = width;
+    this.l = l;
+  }
+
+  @Override
+  public ArrayList<? extends LocalitySensitiveHashFunction<? super NumberVector<?>>> generateHashFunctions(Relation<? extends NumberVector<?>> relation, int k) {
+    int dim = RelationUtil.dimensionality(relation);
+    ArrayList<LocalitySensitiveHashFunction<? super NumberVector<?>>> ps = new ArrayList<>(k);
+    for (int i = 0; i < k; i++) {
+      Matrix mat = proj.generateProjectionMatrix(dim, l);
+      ps.add(new MultipleProjectionsLocalitySensitiveHashFunction(mat, width, random));
+    }
+    return ps;
+  }
+
+  @Override
+  public TypeInformation getInputTypeRestriction() {
+    return TypeUtil.NUMBER_VECTOR_FIELD;
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public abstract static class Parameterizer extends AbstractParameterizer {
+    /**
+     * Parameter for fixing the random seed.
+     */
+    public static final OptionID RANDOM_ID = new OptionID("lsh.projection.random", "Random seed for generating the projections.");
+
+    /**
+     * Parameter for choosing the bin width.
+     */
+    public static final OptionID WIDTH_ID = new OptionID("lsh.projection.width", "Bin width for random projections.");
+
+    /**
+     * Number of projections to use in each hash function.
+     */
+    public static final OptionID NUMPROJ_ID = new OptionID("lsh.projection.l", "Number of projections to use for each hash function.");
+
+    /**
+     * Random generator to use.
+     */
+    RandomFactory random;
+
+    /**
+     * Width of each bin.
+     */
+    double width;
+
+    /**
+     * The number of projections to use for each hash function.
+     */
+    int l;
+
+    @Override
+    protected void makeOptions(Parameterization config) {
+      super.makeOptions(config);
+      RandomParameter randP = new RandomParameter(RANDOM_ID, RandomFactory.DEFAULT);
+      if (config.grab(randP)) {
+        random = randP.getValue();
+      }
+
+      DoubleParameter widthP = new DoubleParameter(WIDTH_ID);
+      widthP.addConstraint(new GreaterConstraint(0.0));
+      if (config.grab(widthP)) {
+        width = widthP.doubleValue();
+      }
+
+      IntParameter lP = new IntParameter(NUMPROJ_ID);
+      lP.addConstraint(new GreaterConstraint(0));
+      if (config.grab(lP)) {
+        l = lP.intValue();
+      }
+    }
+  }
+}
