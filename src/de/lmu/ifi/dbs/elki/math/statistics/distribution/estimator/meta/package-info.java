bceff1917a75fd85bcb454bdd0ec5e11c7af9f9c
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-07-31 07:49:48 +0000

    Add another estimator for uniform data, and a meta estimator using trimmed data.

---
+++ src/de/lmu/ifi/dbs/elki/math/statistics/distribution/estimator/meta/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Meta estimators: estimators that do not actually estimate themselves, but instead use other estimators, e.g. on a trimmed data set, or as an ensemble.
+ * 
+ * @author Erich Schubert
+ */
+package de.lmu.ifi.dbs.elki.math.statistics.distribution.estimator.meta;
