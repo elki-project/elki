9c8abb6e0de1e679b2a8ee0a63a7eb7444363cad
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-08-17 15:32:53 +0000

    Add log-logistic and wald distributions (untested).

---
+++ src/de/lmu/ifi/dbs/elki/math/statistics/distribution/LogLogisticDistribution.java
@@ -0,0 +1,137 @@
+package de.lmu.ifi.dbs.elki.math.statistics.distribution;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.util.Random;
+
+/**
+ * Log-Logistic distribution also known as Fisk distribution.
+ * 
+ * @author Erich Schubert
+ */
+public class LogLogisticDistribution implements DistributionWithRandom {
+  /**
+   * Parameters: scale and shape
+   */
+  double scale, shape;
+
+  /**
+   * Random number generator
+   */
+  Random random;
+
+  /**
+   * Constructor.
+   * 
+   * @param scale Scale
+   * @param shape Shape
+   */
+  public LogLogisticDistribution(double scale, double shape) {
+    this(scale, shape, null);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param scale Scale
+   * @param shape Shape
+   * @param random Random number generator
+   */
+  public LogLogisticDistribution(double scale, double shape, Random random) {
+    super();
+    this.scale = scale;
+    this.shape = shape;
+    this.random = random;
+  }
+
+  /**
+   * Probability density function.
+   * 
+   * @param val Value
+   * @param scale Scale
+   * @param shape Shape
+   * @return PDF
+   */
+  public static double pdf(double val, double scale, double shape) {
+    if(val < 0) {
+      return 0;
+    }
+    val = Math.abs(val / scale);
+    double f = shape / scale * Math.pow(val, shape - 1.);
+    double d = 1. + Math.pow(val, shape);
+    return f / (d * d);
+  }
+
+  @Override
+  public double pdf(double val) {
+    return pdf(val, scale, shape);
+  }
+
+  /**
+   * Cumulative density function.
+   * 
+   * @param val Value
+   * @param scale Scale
+   * @param shape Shape
+   * @return CDF
+   */
+  public static double cdf(double val, double scale, double shape) {
+    if(val < 0) {
+      return 0;
+    }
+    return 1. / (1. + Math.pow(val / scale, -shape));
+  }
+
+  @Override
+  public double cdf(double val) {
+    return cdf(val, scale, shape);
+  }
+
+  /**
+   * Quantile function.
+   * 
+   * @param val Value
+   * @param scale Scale
+   * @param shape Shape
+   * @return Quantile
+   */
+  public static double quantile(double val, double scale, double shape) {
+    return scale * Math.pow(val / (1. - val), 1. / shape);
+  }
+
+  @Override
+  public double quantile(double val) {
+    return quantile(val, scale, shape);
+  }
+
+  @Override
+  public double nextRandom() {
+    double u = random.nextDouble();
+    return scale * Math.pow(u / (1. - u), 1. / shape);
+  }
+
+  @Override
+  public String toString() {
+    return "LogLogisticDistribution(scale=" + scale + ", shape=" + shape + ")";
+  }
+}
---
+++ src/de/lmu/ifi/dbs/elki/math/statistics/distribution/WaldDistribution.java
@@ -0,0 +1,170 @@
+package de.lmu.ifi.dbs.elki.math.statistics.distribution;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.util.Random;
+
+import de.lmu.ifi.dbs.elki.math.MathUtil;
+import de.lmu.ifi.dbs.elki.utilities.Alias;
+import de.lmu.ifi.dbs.elki.utilities.exceptions.ExceptionMessages;
+import de.lmu.ifi.dbs.elki.utilities.exceptions.NotImplementedException;
+
+/**
+ * Inverse Gaussian distribution aka Wald distribution
+ * 
+ * @author Erich Schubert
+ */
+@Alias({ "InverseGaussianDistribution", "invgauss" })
+public class WaldDistribution implements DistributionWithRandom {
+  /**
+   * Mean value
+   */
+  private double mean;
+
+  /**
+   * Shape parameter
+   */
+  private double shape;
+
+  /**
+   * The random generator.
+   */
+  private Random random;
+
+  /**
+   * Constructor for wald distribution
+   * 
+   * @param mean Mean
+   * @param shape Shape parameter
+   * @param random Random generator
+   */
+  public WaldDistribution(double mean, double shape, Random random) {
+    super();
+    this.mean = mean;
+    this.shape = shape;
+    this.random = random;
+  }
+
+  /**
+   * Constructor for Gaussian distribution
+   * 
+   * @param mean Mean
+   * @param shape Shape parameter
+   */
+  public WaldDistribution(double mean, double shape) {
+    this(mean, shape, null);
+  }
+
+  @Override
+  public double pdf(double val) {
+    return pdf(val, mean, shape);
+  }
+
+  @Override
+  public double cdf(double val) {
+    return cdf(val, mean, shape);
+  }
+
+  /**
+   * @deprecated NOT YET IMPLEMENTED.
+   */
+  @Override
+  @Deprecated
+  public double quantile(double q) {
+    return quantile(q, mean, shape);
+  }
+
+  @Override
+  public double nextRandom() {
+    double v = random.nextGaussian();
+    v *= v;
+    double x = mean + mean * .5 / shape * (mean * v - Math.sqrt(4. * mean * shape * v + mean * mean * v * v));
+    double u = random.nextDouble();
+    if(u * (mean + x) <= mean) {
+      return x;
+    }
+    else {
+      return mean * mean / x;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "WaldDistribution(mean=" + mean + ", shape=" + shape + ")";
+  }
+
+  /**
+   * Probability density function of the Wald distribution.
+   * 
+   * 
+   * @param x The value.
+   * @param mu The mean.
+   * @param shape Shape parameter
+   * @return PDF of the given Wald distribution at x.
+   */
+  public static double pdf(double x, double mu, double shape) {
+    if(!(x > 0)) {
+      return 0;
+    }
+    final double v = (x - mu);
+    return Math.sqrt(shape / (MathUtil.TWOPI * x * x * x)) * Math.exp(-shape * v * v / (2. * mu * mu * x));
+  }
+
+  /**
+   * Cumulative probability density function (CDF) of a Wald distribution.
+   * 
+   * @param x value to evaluate CDF at
+   * @param mu Mean value
+   * @param shape Shape parameter
+   * @return The CDF of the given Wald distribution at x.
+   */
+  public static double cdf(double x, double mu, double shape) {
+    if(!(x > 0)) {
+      return 0;
+    }
+    // TODO: accelerate by caching exp(2 * shape / mu).
+    final double v0 = x / mu;
+    final double v1 = Math.sqrt(shape / x);
+    double c1 = NormalDistribution.standardNormalCDF(v1 * (v0 - 1.));
+    double c2 = NormalDistribution.standardNormalCDF(-v1 * (v0 + 1.));
+    return c1 + Math.exp(2 * shape / mu) * c2;
+  }
+
+  /**
+   * Inverse cumulative probability density function (probit) of a Wald
+   * distribution.
+   * 
+   * @param x value to evaluate probit function at
+   * @param mu Mean value
+   * @param shape Shape parameter
+   * @return The probit of the given Wald distribution at x.
+   * 
+   * @deprecated NOT YET IMPLEMENTED.
+   */
+  @Deprecated
+  public static double quantile(double x, double mu, double shape) {
+    // FIXME: implement!
+    throw new NotImplementedException(ExceptionMessages.UNSUPPORTED_NOT_YET);
+  }
+}
