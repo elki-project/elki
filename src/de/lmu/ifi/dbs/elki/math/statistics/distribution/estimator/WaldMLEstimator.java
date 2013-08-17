8a47ead7e89a28e2a3a6f98a630cb3af899e4107
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-08-17 15:49:16 +0000

    Add estimator for Wald, untested.

---
+++ src/de/lmu/ifi/dbs/elki/math/statistics/distribution/estimator/WaldMLEstimator.java
@@ -0,0 +1,80 @@
+package de.lmu.ifi.dbs.elki.math.statistics.distribution.estimator;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import de.lmu.ifi.dbs.elki.math.statistics.distribution.WaldDistribution;
+import de.lmu.ifi.dbs.elki.utilities.datastructures.arraylike.NumberArrayAdapter;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+
+/**
+ * Estimate parameter of the Wald distribution.
+ * 
+ * @author Erich Schubert
+ * 
+ * @apiviz.has WaldDistribution
+ */
+public class WaldMLEstimator implements DistributionEstimator<WaldDistribution> {
+  /**
+   * Static instance.
+   */
+  public static final WaldMLEstimator STATIC = new WaldMLEstimator();
+
+  @Override
+  public <A> WaldDistribution estimate(A data, NumberArrayAdapter<?, A> adapter) {
+    final int len = adapter.size(data);
+    double mean = 0.;
+    for(int i = 0; i < len; i++) {
+      double v = adapter.getDouble(data, i);
+      mean += v;
+    }
+    mean /= len;
+    double invmean = len / mean;
+    double invdev = 0.;
+    for(int i = 0; i < len; i++) {
+      double v = adapter.getDouble(data, i);
+      if(v > 0.) {
+        invdev += len / v - invmean;
+      }
+    }
+    return new WaldDistribution(mean, 1. / invdev);
+  }
+
+  @Override
+  public Class<? super WaldDistribution> getDistributionClass() {
+    return WaldDistribution.class;
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    @Override
+    protected WaldMLEstimator makeInstance() {
+      return STATIC;
+    }
+  }
+}
