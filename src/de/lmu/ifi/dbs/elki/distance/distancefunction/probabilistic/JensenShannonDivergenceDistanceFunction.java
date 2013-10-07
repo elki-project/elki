ce4bf84bda0bcaad5396fe4536bd8575edf4f736
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-10-07 14:20:16 +0000

    Move distances from probability theory into own package.

---
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/probabilistic/JensenShannonDivergenceDistanceFunction.java
@@ -0,0 +1,74 @@
+package de.lmu.ifi.dbs.elki.distance.distancefunction.probabilistic;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2011
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.data.NumberVector;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+
+/**
+ * Jensen-Shannon Divergence is essentially the same as Jeffrey divergence, only
+ * scaled by half.
+ * 
+ * @author Erich Schubert
+ */
+public class JensenShannonDivergenceDistanceFunction extends JeffreyDivergenceDistanceFunction {
+  /**
+   * Static instance. Use this!
+   */
+  public static final JensenShannonDivergenceDistanceFunction STATIC = new JensenShannonDivergenceDistanceFunction();
+
+  /**
+   * Constructor for the Jensen-Shannon divergence.
+   *
+   * @deprecated Use static instance!
+   */
+  @Deprecated
+  public JensenShannonDivergenceDistanceFunction() {
+    super();
+  }
+
+  @Override
+  public double doubleDistance(NumberVector<?> v1, NumberVector<?> v2) {
+    return .5 * super.doubleDistance(v1, v2);
+  }
+
+  @Override
+  public String toString() {
+    return "JensenShannonDivergenceDistance";
+  }
+
+  /**
+   * Parameterization class, using the static instance.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    @Override
+    protected JensenShannonDivergenceDistanceFunction makeInstance() {
+      return STATIC;
+    }
+  }
+}
---
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/probabilistic/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Distance from probability theory, mostly divergences such as K-L-divergence, J-divergence.
+ * 
+ * @author Erich Schubert
+ */
+package de.lmu.ifi.dbs.elki.distance.distancefunction.probabilistic;
