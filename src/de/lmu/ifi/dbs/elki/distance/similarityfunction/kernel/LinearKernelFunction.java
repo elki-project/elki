040ba7f3f5e583ab1fb4a70a4e79a15c77d1c5ef
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-11-06 14:50:59 +0000

    Simplifications.

--- src/de/lmu/ifi/dbs/elki/distance/similarityfunction/kernel/LinearKernelFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/similarityfunction/kernel/LinearKernelFunction.java
@@ -29,7 +29,7 @@ import de.lmu.ifi.dbs.elki.data.type.VectorFieldTypeInformation;
 import de.lmu.ifi.dbs.elki.database.query.DistanceSimilarityQuery;
 import de.lmu.ifi.dbs.elki.database.query.distance.PrimitiveDistanceSimilarityQuery;
 import de.lmu.ifi.dbs.elki.database.relation.Relation;
-import de.lmu.ifi.dbs.elki.distance.distancefunction.PrimitiveDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.PrimitiveDoubleDistanceFunction;
 import de.lmu.ifi.dbs.elki.distance.distancevalue.DoubleDistance;
 import de.lmu.ifi.dbs.elki.distance.similarityfunction.AbstractPrimitiveSimilarityFunction;
 
@@ -40,7 +40,7 @@ import de.lmu.ifi.dbs.elki.distance.similarityfunction.AbstractPrimitiveSimilari
  * @author Simon Paradies
  * @param <O> vector type
  */
-public class LinearKernelFunction<O extends NumberVector<?>> extends AbstractPrimitiveSimilarityFunction<O, DoubleDistance> implements PrimitiveDistanceFunction<O, DoubleDistance> {
+public class LinearKernelFunction<O extends NumberVector<?>> extends AbstractPrimitiveSimilarityFunction<O, DoubleDistance> implements PrimitiveDoubleDistanceFunction<O> {
   /**
    * Provides a linear Kernel function that computes a similarity between the
    * two vectors V1 and V2 defined by V1^T*V2.
@@ -49,30 +49,30 @@ public class LinearKernelFunction<O extends NumberVector<?>> extends AbstractPri
     super();
   }
 
-  /**
-   * Provides a linear Kernel function that computes a similarity between the
-   * two feature vectors V1 and V2 definded by V1^T*V2.
-   * 
-   * @param o1 first feature vector
-   * @param o2 second feature vector
-   * @return the linear kernel similarity between the given two vectors as an
-   *         instance of {@link DoubleDistance DoubleDistance}.
-   */
   @Override
   public DoubleDistance similarity(final O o1, final O o2) {
-    if(o1.getDimensionality() != o2.getDimensionality()) {
+    return new DoubleDistance(doubleSimilarity(o1, o2));
+  }
+
+  protected double doubleSimilarity(final O o1, final O o2) {
+    if (o1.getDimensionality() != o2.getDimensionality()) {
       throw new IllegalArgumentException("Different dimensionality of Feature-Vectors" + "\n  first argument: " + o1.toString() + "\n  second argument: " + o2.toString());
     }
     double sim = 0;
-    for(int i = 0; i < o1.getDimensionality(); i++) {
+    for (int i = 0; i < o1.getDimensionality(); i++) {
       sim += o1.doubleValue(i) * o2.doubleValue(i);
     }
-    return new DoubleDistance(sim);
+    return sim;
+  }
+
+  @Override
+  public double doubleDistance(final O fv1, final O fv2) {
+    return Math.sqrt(doubleSimilarity(fv1, fv1) + doubleSimilarity(fv2, fv2) - 2 * doubleSimilarity(fv1, fv2));
   }
 
   @Override
   public DoubleDistance distance(final O fv1, final O fv2) {
-    return new DoubleDistance(Math.sqrt(similarity(fv1, fv1).doubleValue() + similarity(fv2, fv2).doubleValue() - 2 * similarity(fv1, fv2).doubleValue()));
+    return new DoubleDistance(doubleDistance(fv1, fv2));
   }
 
   @Override
