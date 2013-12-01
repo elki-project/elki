de9c09bc6139ef6454226654dc2a5604c5acc8bf
Erich Schubert <schube@dbs.ifi.lmu.de>
2013-12-01 14:18:54 +0000

    Cosine for vectors of different length.

--- src/de/lmu/ifi/dbs/elki/data/VectorUtil.java
+++ src/de/lmu/ifi/dbs/elki/data/VectorUtil.java
@@ -69,7 +69,7 @@ public final class VectorUtil {
   public static DoubleMinMax getRangeDouble(NumberVector<?> vec) {
     DoubleMinMax minmax = new DoubleMinMax();
 
-    for (int i = 0; i < vec.getDimensionality(); i++) {
+    for(int i = 0; i < vec.getDimensionality(); i++) {
       minmax.put(vec.doubleValue(i));
     }
 
@@ -116,28 +116,28 @@ public final class VectorUtil {
 
     // TODO: add precomputed length to data type!
     // Length of first vector
-    double l1 = 0.0;
-    for (int i = b1.nextSetBit(0); i >= 0; i = b1.nextSetBit(i + 1)) {
+    double l1 = 0.;
+    for(int i = b1.nextSetBit(0); i >= 0; i = b1.nextSetBit(i + 1)) {
       final double val = v1.doubleValue(i);
       l1 += val * val;
     }
     l1 = Math.sqrt(l1);
 
     // Length of second vector
-    double l2 = 0.0;
-    for (int i = b2.nextSetBit(0); i >= 0; i = b2.nextSetBit(i + 1)) {
+    double l2 = 0.;
+    for(int i = b2.nextSetBit(0); i >= 0; i = b2.nextSetBit(i + 1)) {
       final double val = v2.doubleValue(i);
       l2 += val * val;
     }
     l2 = Math.sqrt(l2);
 
     // Cross product
-    double cross = 0.0;
-    for (int i = both.nextSetBit(0); i >= 0; i = both.nextSetBit(i + 1)) {
+    double cross = 0.;
+    for(int i = both.nextSetBit(0); i >= 0; i = both.nextSetBit(i + 1)) {
       cross += v1.doubleValue(i) * v2.doubleValue(i);
     }
     final double a = cross / (l1 * l2);
-    return (a > 1.0) ? 1.0 : a;
+    return (a > 1.) ? 1. : a;
   }
 
   /**
@@ -149,22 +149,34 @@ public final class VectorUtil {
    * @return Angle
    */
   public static double angle(NumberVector<?> v1, NumberVector<?> v2, Vector o) {
+    final int dim1 = v1.getDimensionality(), dim2 = v1.getDimensionality(), dimo = v1.getDimensionality();
+    final int mindim = (dim1 <= dim2) ? dim1 : dim2;
     // Essentially, we want to compute this:
     // v1' = v1 - o, v2' = v2 - o
     // v1'.transposeTimes(v2') / (v1'.euclideanLength()*v2'.euclideanLength());
     // We can just compute all three in parallel.
     double[] oe = o.getArrayRef();
-    final int dim = v1.getDimensionality();
     double s = 0, e1 = 0, e2 = 0;
-    for (int k = 0; k < dim; k++) {
-      final double r1 = v1.doubleValue(k) - oe[k];
-      final double r2 = v2.doubleValue(k) - oe[k];
+    for(int k = 0; k < mindim; k++) {
+      final double dk = k < dimo ? oe[k] : 0.;
+      final double r1 = v1.doubleValue(k) - dk;
+      final double r2 = v2.doubleValue(k) - dk;
       s += r1 * r2;
       e1 += r1 * r1;
       e2 += r2 * r2;
     }
+    for(int k = mindim; k < dim1; k++) {
+      final double dk = k < dimo ? oe[k] : 0.;
+      final double r1 = v1.doubleValue(k) - dk;
+      e1 += r1 * r1;
+    }
+    for(int k = mindim; k < dim2; k++) {
+      final double dk = k < dimo ? oe[k] : 0.;
+      final double r2 = v2.doubleValue(k) - dk;
+      e2 += r2 * r2;
+    }
     final double a = Math.sqrt((s / e1) * (s / e2));
-    return (a > 1.0) ? 1.0 : a;
+    return (a > 1.) ? 1. : a;
   }
 
   /**
@@ -176,21 +188,33 @@ public final class VectorUtil {
    * @return Angle
    */
   public static double angle(NumberVector<?> v1, NumberVector<?> v2, NumberVector<?> o) {
+    final int dim1 = v1.getDimensionality(), dim2 = v1.getDimensionality(), dimo = o.getDimensionality();
+    final int mindim = (dim1 <= dim2) ? dim1 : dim2;
     // Essentially, we want to compute this:
     // v1' = v1 - o, v2' = v2 - o
     // v1'.transposeTimes(v2') / (v1'.euclideanLength()*v2'.euclideanLength());
     // We can just compute all three in parallel.
-    final int dim = v1.getDimensionality();
     double s = 0, e1 = 0, e2 = 0;
-    for (int k = 0; k < dim; k++) {
-      final double r1 = v1.doubleValue(k) - o.doubleValue(k);
+    for(int k = 0; k < mindim; k++) {
+      final double ok = k < dimo ? o.doubleValue(k) : 0.;
+      final double r1 = v1.doubleValue(k) - ok;
       final double r2 = v2.doubleValue(k) - o.doubleValue(k);
       s += r1 * r2;
       e1 += r1 * r1;
       e2 += r2 * r2;
     }
+    for(int k = mindim; k < dim1; k++) {
+      final double ok = k < dimo ? o.doubleValue(k) : 0.;
+      final double r1 = v1.doubleValue(k) - ok;
+      e1 += r1 * r1;
+    }
+    for(int k = mindim; k < dim2; k++) {
+      final double ok = k < dimo ? o.doubleValue(k) : 0.;
+      final double r2 = v2.doubleValue(k) - ok;
+      e2 += r2 * r2;
+    }
     final double a = Math.sqrt((s / e1) * (s / e2));
-    return (a > 1.0) ? 1.0 : a;
+    return (a > 1.) ? 1. : a;
   }
 
   /**
@@ -203,33 +227,32 @@ public final class VectorUtil {
    * @return Angle
    */
   public static double cosAngle(NumberVector<?> v1, NumberVector<?> v2) {
-    if (v1 instanceof SparseNumberVector<?> && v2 instanceof SparseNumberVector<?>) {
+    if(v1 instanceof SparseNumberVector<?> && v2 instanceof SparseNumberVector<?>) {
       return angleSparse((SparseNumberVector<?>) v1, (SparseNumberVector<?>) v2);
     }
+    final int dim1 = v1.getDimensionality(), dim2 = v1.getDimensionality();
+    final int mindim = (dim1 <= dim2) ? dim1 : dim2;
     // Essentially, we want to compute this:
     // v1.transposeTimes(v2) / (v1.euclideanLength() * v2.euclideanLength());
     // We can just compute all three in parallel.
-    final int d1 = v1.getDimensionality();
-    final int d2 = v2.getDimensionality();
-    final int dim = Math.min(d1, d2);
     double s = 0, e1 = 0, e2 = 0;
-    for (int k = 0; k < dim; k++) {
+    for(int k = 0; k < mindim; k++) {
       final double r1 = v1.doubleValue(k);
       final double r2 = v2.doubleValue(k);
       s += r1 * r2;
       e1 += r1 * r1;
       e2 += r2 * r2;
     }
-    for (int k = dim; k < d1; k++) {
+    for(int k = mindim; k < dim1; k++) {
       final double r1 = v1.doubleValue(k);
       e1 += r1 * r1;
     }
-    for (int k = dim; k < d2; k++) {
+    for(int k = mindim; k < dim2; k++) {
       final double r2 = v2.doubleValue(k);
       e2 += r2 * r2;
     }
     final double a = Math.sqrt((s / e1) * (s / e2));
-    return (a > 1.0) ? 1.0 : a;
+    return (a > 1.) ? 1. : a;
   }
 
   // TODO: add more precise but slower O(n^2) angle computation according to:
@@ -244,35 +267,56 @@ public final class VectorUtil {
    * @return Angle
    */
   public static double minCosAngle(SpatialComparable v1, SpatialComparable v2) {
-    if (v1 instanceof NumberVector<?> && v2 instanceof NumberVector<?>) {
+    if(v1 instanceof NumberVector<?> && v2 instanceof NumberVector<?>) {
       return cosAngle((NumberVector<?>) v1, (NumberVector<?>) v2);
     }
+    final int dim1 = v1.getDimensionality(), dim2 = v1.getDimensionality();
+    final int mindim = (dim1 <= dim2) ? dim1 : dim2;
     // Essentially, we want to compute this:
     // absmax(v1.transposeTimes(v2))/(min(v1.euclideanLength())*min(v2.euclideanLength()));
     // We can just compute all three in parallel.
-    final int dim = v1.getDimensionality();
     double s1 = 0, s2 = 0, e1 = 0, e2 = 0;
-    for (int k = 0; k < dim; k++) {
+    for(int k = 0; k < mindim; k++) {
       final double min1 = v1.getMin(k), max1 = v1.getMax(k);
       final double min2 = v2.getMin(k), max2 = v2.getMax(k);
       final double p1 = min1 * min2, p2 = min1 * max2;
       final double p3 = max1 * min2, p4 = max1 * max2;
       s1 += Math.max(Math.max(p1, p2), Math.max(p3, p4));
       s2 += Math.min(Math.min(p1, p2), Math.min(p3, p4));
-      if (max1 < 0) {
+      if(max1 < 0) {
         e1 += max1 * max1;
-      } else if (min1 > 0) {
+      }
+      else if(min1 > 0) {
         e1 += min1 * min1;
       } // else: 0
-      if (max2 < 0) {
+      if(max2 < 0) {
         e2 += max2 * max2;
-      } else if (min2 > 0) {
+      }
+      else if(min2 > 0) {
+        e2 += min2 * min2;
+      } // else: 0
+    }
+    for(int k = mindim; k < dim1; k++) {
+      final double min1 = v1.getMin(k), max1 = v1.getMax(k);
+      if(max1 < 0.) {
+        e1 += max1 * max1;
+      }
+      else if(min1 > 0.) {
+        e1 += min1 * min1;
+      } // else: 0
+    }
+    for(int k = mindim; k < dim2; k++) {
+      final double min2 = v2.getMin(k), max2 = v2.getMax(k);
+      if(max2 < 0.) {
+        e2 += max2 * max2;
+      }
+      else if(min2 > 0.) {
         e2 += min2 * min2;
       } // else: 0
     }
     final double s = Math.max(s1, Math.abs(s2));
     final double a = Math.sqrt((s / e1) * (s / e2));
-    return (a > 1.0) ? 1.0 : a;
+    return (a > 1.) ? 1. : a;
   }
 
   /**
@@ -286,8 +330,8 @@ public final class VectorUtil {
    */
   public static double scalarProduct(NumberVector<?> d1, NumberVector<?> d2) {
     final int dim = d1.getDimensionality();
-    double result = 0.0;
-    for (int i = 0; i < dim; i++) {
+    double result = 0.;
+    for(int i = 0; i < dim; i++) {
       result += d1.doubleValue(i) * d2.doubleValue(i);
     }
     return result;
@@ -305,7 +349,7 @@ public final class VectorUtil {
     ArrayModifiableDBIDs mids = DBIDUtil.newArray(sample);
     SortDBIDsBySingleDimension s = new SortDBIDsBySingleDimension(relation);
     Vector medoid = new Vector(dim);
-    for (int d = 0; d < dim; d++) {
+    for(int d = 0; d < dim; d++) {
       s.setDimension(d);
       medoid.set(d, relation.get(QuickSelect.median(mids, s)).doubleValue(d));
     }
@@ -326,10 +370,10 @@ public final class VectorUtil {
     final int cdim = mat.getColumnDimensionality();
     final double[] X = new double[elements.length];
     // multiply it with each row from A
-    for (int i = 0; i < elements.length; i++) {
+    for(int i = 0; i < elements.length; i++) {
       final double[] Arowi = elements[i];
       double s = 0;
-      for (int k = 0; k < cdim; k++) {
+      for(int k = 0; k < cdim; k++) {
         s += Arowi[k] * v.doubleValue(k);
       }
       X[i] = s;
@@ -465,11 +509,11 @@ public final class VectorUtil {
    * @return a new NumberVector as a projection on the specified attributes
    */
   public static <V extends NumberVector<?>> V project(V v, BitSet selectedAttributes, NumberVector.Factory<V, ?> factory) {
-    if (factory instanceof SparseNumberVector.Factory) {
+    if(factory instanceof SparseNumberVector.Factory) {
       final SparseNumberVector.Factory<?, ?> sfactory = (SparseNumberVector.Factory<?, ?>) factory;
       TIntDoubleHashMap values = new TIntDoubleHashMap(selectedAttributes.cardinality(), 1);
-      for (int d = selectedAttributes.nextSetBit(0); d >= 0; d = selectedAttributes.nextSetBit(d + 1)) {
-        if (v.doubleValue(d) != 0.0) {
+      for(int d = selectedAttributes.nextSetBit(0); d >= 0; d = selectedAttributes.nextSetBit(d + 1)) {
+        if(v.doubleValue(d) != 0.0) {
           values.put(d, v.doubleValue(d));
         }
       }
@@ -478,10 +522,11 @@ public final class VectorUtil {
       @SuppressWarnings("unchecked")
       V projectedVector = (V) sfactory.newNumberVector(values, selectedAttributes.cardinality());
       return projectedVector;
-    } else {
+    }
+    else {
       double[] newAttributes = new double[selectedAttributes.cardinality()];
       int i = 0;
-      for (int d = selectedAttributes.nextSetBit(0); d >= 0; d = selectedAttributes.nextSetBit(d + 1)) {
+      for(int d = selectedAttributes.nextSetBit(0); d >= 0; d = selectedAttributes.nextSetBit(d + 1)) {
         newAttributes[i] = v.doubleValue(d);
         i++;
       }
