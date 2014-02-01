f371a708b55b99e800e551385431b5f4e1ff58b5
Erich Schubert <schube@dbs.ifi.lmu.de>
2014-02-01 22:20:58 +0000

    Some simple similarity measures for clusters.

---
+++ src/de/lmu/ifi/dbs/elki/distance/similarityfunction/cluster/ClusterJaccardSimilarityFunction.java
@@ -0,0 +1,103 @@
+package de.lmu.ifi.dbs.elki.distance.similarityfunction.cluster;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2014
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import de.lmu.ifi.dbs.elki.data.Cluster;
+import de.lmu.ifi.dbs.elki.data.type.SimpleTypeInformation;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
+import de.lmu.ifi.dbs.elki.database.query.DistanceSimilarityQuery;
+import de.lmu.ifi.dbs.elki.database.query.distance.PrimitiveDistanceSimilarityQuery;
+import de.lmu.ifi.dbs.elki.database.relation.Relation;
+import de.lmu.ifi.dbs.elki.distance.distancefunction.PrimitiveDistanceFunction;
+import de.lmu.ifi.dbs.elki.distance.similarityfunction.AbstractPrimitiveSimilarityFunction;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
+
+/**
+ * Measure the similarity of clusters via the Jaccard coefficient.
+ * 
+ * <p>
+ * P. Jaccard<br />
+ * Étude comparative de la distribution florale dans une portion des Alpes et
+ * des Jura<br />
+ * Bulletin del la Société Vaudoise des Sciences Naturelles
+ * </p>
+ * 
+ * @author Erich Schubert
+ */
+@Reference(authors = "P. Jaccard", title = "Étude comparative de la distribution florale dans une portion des Alpes et des Jura", booktitle = "Bulletin del la Société Vaudoise des Sciences Naturelles")
+public class ClusterJaccardSimilarityFunction extends AbstractPrimitiveSimilarityFunction<Cluster<?>> implements PrimitiveDistanceFunction<Cluster<?>> {
+  /**
+   * Static instance.
+   */
+  public static final ClusterJaccardSimilarityFunction STATIC = new ClusterJaccardSimilarityFunction();
+
+  /**
+   * Constructor - use the static instance {@link #STATIC}!
+   */
+  public ClusterJaccardSimilarityFunction() {
+    super();
+  }
+
+  @Override
+  public double similarity(Cluster<?> o1, Cluster<?> o2) {
+    int i = DBIDUtil.intersectionSize(o1.getIDs(), o2.getIDs());
+    int union = o1.size() + o2.size() - i;
+    return i / (double) union;
+  }
+
+  @Override
+  public double distance(Cluster<?> o1, Cluster<?> o2) {
+    int i = DBIDUtil.intersectionSize(o1.getIDs(), o2.getIDs());
+    int union = o1.size() + o2.size() - i;
+    return 1. - i / (double) union;
+  }
+
+  @Override
+  public boolean isMetric() {
+    return true;
+  }
+
+  @Override
+  public <T extends Cluster<?>> DistanceSimilarityQuery<T> instantiate(Relation<T> relation) {
+    return new PrimitiveDistanceSimilarityQuery<T>(relation, this, this);
+  }
+
+  @Override
+  public SimpleTypeInformation<? super Cluster<?>> getInputTypeRestriction() {
+    return new SimpleTypeInformation<>(Cluster.class);
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    @Override
+    protected ClusterJaccardSimilarityFunction makeInstance() {
+      return STATIC;
+    }
+  }
+}
