026102a1edc1883733b2cbeb53c1f750befcdec9
Erich Schubert <schube@dbs.ifi.lmu.de>
2014-06-12 14:34:09 +0000

    Add JavaDoc.

--- src/de/lmu/ifi/dbs/elki/distance/distancefunction/set/HammingDistanceFunction.java
+++ src/de/lmu/ifi/dbs/elki/distance/distancefunction/set/HammingDistanceFunction.java
@@ -1,13 +1,56 @@
 package de.lmu.ifi.dbs.elki.distance.distancefunction.set;
 
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2014
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.lmu.ifi.dbs.elki.data.BitVector;
 import de.lmu.ifi.dbs.elki.data.FeatureVector;
 import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.data.type.SimpleTypeInformation;
 import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
 import de.lmu.ifi.dbs.elki.distance.distancefunction.NumberVectorDistanceFunction;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 
-/*CFP*/
+/**
+ * Computes the Hamming distance of arbitrary vectors - i.e. counting, on how
+ * many places they differ.
+ * 
+ * Reference:
+ * <p>
+ * R. W. Hamming<br />
+ * Error detecting and error correcting codes<br />
+ * Bell System technical journal, 29(2)
+ * </p>
+ * 
+ * TODO: add a sparse (but not binary) optimized version?
+ * 
+ * @author Erich Schubert
+ */
+@Reference(authors = "R. W. Hamming", //
+title = "Error detecting and error correcting codes", //
+booktitle = "Bell System technical journal, 29(2)", //
+url = "http://dx.doi.org/10.1002/j.1538-7305.1950.tb00463.x")
 public class HammingDistanceFunction extends AbstractSetDistanceFunction<FeatureVector<?>> implements NumberVectorDistanceFunction<FeatureVector<?>> {
   /**
    * Static instance.
@@ -114,5 +157,19 @@ public class HammingDistanceFunction extends AbstractSetDistanceFunction<Feature
   @Override
   public SimpleTypeInformation<? super FeatureVector<?>> getInputTypeRestriction() {
     return TypeUtil.FEATURE_VECTORS;
+  }
+
+  /**
+   * Parameterization class.
+   * 
+   * @author Erich Schubert
+   * 
+   * @apiviz.exclude
+   */
+  public static class Parameterizer extends AbstractParameterizer {
+    @Override
+    protected HammingDistanceFunction makeInstance() {
+      return STATIC;
+    }
   }
 }
