e54bcc232ca396dd8269f96ce55c784999f6eef0
Erich Schubert <schube@dbs.ifi.lmu.de>
2014-06-30 17:06:05 +0000

    Cleanup and refactor classification code.

--- src/experimentalcode/shared/evaluation/classifier/ConfusionMatrixBasedEvaluation.java
+++ src/experimentalcode/shared/evaluation/classifier/ConfusionMatrixBasedEvaluation.java
@@ -1,96 +1,94 @@
 package experimentalcode.shared.evaluation.classifier;
+
 /*
-This file is part of ELKI:
-Environment for Developing KDD-Applications Supported by Index-Structures
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
 
-Copyright (C) 2013
-Ludwig-Maximilians-Universität München
-Lehr- und Forschungseinheit für Datenbanksysteme
-ELKI Development Team
+ Copyright (C) 2013
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import java.io.PrintStream;
 
 import de.lmu.ifi.dbs.elki.data.ClassLabel;
-import de.lmu.ifi.dbs.elki.database.Database;
 import experimentalcode.shared.algorithm.classifier.Classifier;
-import experimentalcode.shared.evaluation.classifier.procedure.EvaluationProcedure;
 
 /**
- * Provides the prediction performance measures for a classifier
- * based on the confusion matrix.
+ * Provides the prediction performance measures for a classifier based on the
+ * confusion matrix.
  *
  * @author Arthur Zimek
  */
-public class ConfusionMatrixBasedEvaluation<O, L extends ClassLabel, C extends Classifier<O, L>> extends AbstractClassifierEvaluation<O, L, C> {
-    /**
-     * Holds the confusion matrix.
-     */
-    private ConfusionMatrix confusionmatrix;
+public class ConfusionMatrixBasedEvaluation<O, L extends ClassLabel, C extends Classifier<O, L>> implements EvaluationResult {
+  /**
+   * Holds the confusion matrix.
+   */
+  private ConfusionMatrix confusionmatrix;
 
-    /**
-     * Holds the used EvaluationProcedure.
-     */
-    private EvaluationProcedure<O, L, C> evaluationProcedure;
+  /**
+   * Holds the used EvaluationProcedure.
+   */
+  private String evaluationName;
 
-    /**
-     * Provides an evaluation based on the given confusion matrix.
-     *
-     * @param confusionmatrix     the confusion matrix to provide
-     *                            the prediction performance measures for
-     * @param classifier          the classifier this evaluation is based on
-     * @param rep            the training set this evaluation is based on
-     * @param testset             the test set this evaluation is based on
-     * @param evaluationProcedure the evaluation procedure used
-     */
-    public ConfusionMatrixBasedEvaluation(ConfusionMatrix confusionmatrix, C classifier, Database database, Database testset, EvaluationProcedure<O, L, C> evaluationProcedure) {
-        super(database, testset, classifier);
-        this.confusionmatrix = confusionmatrix;
-        this.evaluationProcedure = evaluationProcedure;
-    }
+  /**
+   * Provides an evaluation based on the given confusion matrix.
+   *
+   * @param confusionmatrix the confusion matrix to provide the prediction
+   *        performance measures for
+   * @param classifier the classifier this evaluation is based on
+   * @param rep the training set this evaluation is based on
+   * @param testset the test set this evaluation is based on
+   * @param evaluationName name of the evaluation procedure used
+   */
+  public ConfusionMatrixBasedEvaluation(ConfusionMatrix confusionmatrix, String evaluationName) {
+    super();
+    this.confusionmatrix = confusionmatrix;
+    this.evaluationName = evaluationName;
+  }
 
-    @Override
-    public void outputEvaluationResult(PrintStream out) {
-        out.println("\nEvaluation:");
-        out.println(evaluationProcedure.getClass().getName());
-        //out.println(evaluationProcedure.setting());
-        out.print("\nAccuracy: \n  correctly classified instances: ");
-        out.println(confusionmatrix.truePositives());
-        out.print("true positive rate:         ");
-        double tpr = confusionmatrix.truePositiveRate();
-        out.println(tpr);
-        out.print("false positive rate:        ");
-        out.println(confusionmatrix.falsePositiveRate());
-        out.print("positive predicted value:   ");
-        double ppv = confusionmatrix.positivePredictedValue();
-        out.println(ppv);
-        out.print("F1-measure:                 ");
-        out.println((2 * ppv * tpr) / (ppv + tpr));
-        out.println("\nconfusion matrix:\n");
-        out.println(confusionmatrix.toString());
-    }
+  @Override
+  public void outputEvaluationResult(PrintStream out) {
+    out.println("\nEvaluation:");
+    out.println(evaluationName);
+    // out.println(evaluationProcedure.setting());
+    out.print("\nAccuracy: \n  correctly classified instances: ");
+    out.println(confusionmatrix.truePositives());
+    out.print("true positive rate:         ");
+    double tpr = confusionmatrix.truePositiveRate();
+    out.println(tpr);
+    out.print("false positive rate:        ");
+    out.println(confusionmatrix.falsePositiveRate());
+    out.print("positive predicted value:   ");
+    double ppv = confusionmatrix.positivePredictedValue();
+    out.println(ppv);
+    out.print("F1-measure:                 ");
+    out.println((2 * ppv * tpr) / (ppv + tpr));
+    out.println("\nconfusion matrix:\n");
+    out.println(confusionmatrix.toString());
+  }
 
+  @Override
+  public String getLongName() {
+    return "confusionmatrixresult";
+  }
 
-    @Override
-    public String getLongName() {
-      return "confusionmatrixresult";
-    }
-
-    @Override
-    public String getShortName() {
-      return "confusionmatrixresult";
-    }
+  @Override
+  public String getShortName() {
+    return "confusionmatrixresult";
+  }
 }
