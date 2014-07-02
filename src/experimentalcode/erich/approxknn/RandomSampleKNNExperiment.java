5f2485fb5ba1b35ec0326391054bf92d6868db90
Erich Schubert <schube@dbs.ifi.lmu.de>
2014-07-02 08:19:00 +0000

    Reorganize outlier detection algorithms into a deeper package hierarchy.

--- src/experimentalcode/erich/approxknn/RandomSampleKNNExperiment.java
+++ src/experimentalcode/erich/approxknn/RandomSampleKNNExperiment.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import de.lmu.ifi.dbs.elki.algorithm.outlier.KNNOutlier;
+import de.lmu.ifi.dbs.elki.algorithm.outlier.distance.KNNOutlier;
 import de.lmu.ifi.dbs.elki.algorithm.outlier.lof.LOF;
 import de.lmu.ifi.dbs.elki.data.NumberVector;
 import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
