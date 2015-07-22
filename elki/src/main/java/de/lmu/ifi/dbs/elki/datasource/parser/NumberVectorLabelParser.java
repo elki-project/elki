1e2a5ee874315aa2c3261f30d2d1f38a0e369594
Erich Schubert <schube@dbs.ifi.lmu.de>
2015-07-22 21:20:32 +0200

    Make CSV parser robust to additional column labels (e.g. for label columns; ELKI currently only supports column labels for numerical columns!)

--- elki/src/main/java/de/lmu/ifi/dbs/elki/datasource/parser/NumberVectorLabelParser.java
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/datasource/parser/NumberVectorLabelParser.java
@@ -1,30 +1,5 @@
 package de.lmu.ifi.dbs.elki.datasource.parser;
 
-/*
- This file is part of ELKI:
- Environment for Developing KDD-Applications Supported by Index-Structures
-
- Copyright (C) 2014
- Ludwig-Maximilians-Universität München
- Lehr- und Forschungseinheit für Datenbanksysteme
- ELKI Development Team
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import gnu.trove.list.array.TDoubleArrayList;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -50,6 +25,31 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.Parameterization;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntListParameter;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.ObjectParameter;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2014
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import gnu.trove.list.array.TDoubleArrayList;
 
 /**
  * Parser for a simple CSV type of format, with columns separated by the given
@@ -327,10 +327,10 @@ public class NumberVectorLabelParser<V extends NumberVector> extends AbstractStr
     }
     if(mindim == maxdim) {
       String[] colnames = null;
-      if(columnnames != null) {
+      if(columnnames != null && mindim <= columnnames.size()) {
         colnames = new String[mindim];
         int j = 0;
-        for(int i = 0; i < columnnames.size(); i++) {
+        for(int i = 0; i < mindim; i++) {
           if(isLabelColumn(i)) {
             continue;
           }
@@ -417,7 +417,7 @@ public class NumberVectorLabelParser<V extends NumberVector> extends AbstractStr
       IntListParameter labelIndicesP = new IntListParameter(LABEL_INDICES_ID, true);
 
       if(config.grab(labelIndicesP)) {
-        labelIndices =  labelIndicesP.getValueAsBitSet();
+        labelIndices = labelIndicesP.getValueAsBitSet();
       }
     }
 
