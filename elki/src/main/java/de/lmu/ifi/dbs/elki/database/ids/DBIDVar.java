c2f77d9b908d4b770a05264bccba5a0fa7213cad
Erich Schubert <schube@dbs.ifi.lmu.de>
2015-08-21 10:06:34 +0200

    API extension for updating DBIDVar from a DBIDDataStore

--- elki/src/main/java/de/lmu/ifi/dbs/elki/database/ids/DBIDVar.java
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/database/ids/DBIDVar.java
@@ -4,7 +4,7 @@ package de.lmu.ifi.dbs.elki.database.ids;
  This file is part of ELKI:
  Environment for Developing KDD-Applications Supported by Index-Structures
 
- Copyright (C) 2014
+ Copyright (C) 2015
  Ludwig-Maximilians-Universität München
  Lehr- und Forschungseinheit für Datenbanksysteme
  ELKI Development Team
@@ -23,22 +23,33 @@ package de.lmu.ifi.dbs.elki.database.ids;
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import de.lmu.ifi.dbs.elki.database.datastore.DBIDDataStore;
+
 /**
  * (Persistent) variable storing a DBID reference.
- * 
+ *
  * In contrast to the {@link DBIDRef} API, which are read-only references, this
  * variable can be updated to point to a different DBID, e.g. the current best
  * candidate.
- * 
+ *
  * @author Erich Schubert
  */
 public interface DBIDVar extends DBIDRef, ArrayDBIDs, SetDBIDs {
   /**
    * Assign a new value for the reference.
-   * 
+   *
    * @param ref Reference
    */
   void set(DBIDRef ref);
+
+  /**
+   * Update variable from a data store.
+   *
+   * @param store Data store
+   * @param ref Reference
+   * @return Self, for inlining.
+   */
+  DBIDVar from(DBIDDataStore store, DBIDRef ref);
 
   /**
    * Clear the contents.
@@ -47,21 +58,21 @@ public interface DBIDVar extends DBIDRef, ArrayDBIDs, SetDBIDs {
 
   /**
    * Test if the variable is well-defined.
-   * 
+   *
    * @return {@code true} when assigned.
    */
   boolean isSet();
 
   /**
    * Assign the first pair member to this variable.
-   * 
+   *
    * @param pair Pair
    */
   void setFirst(DBIDPair pair);
 
   /**
    * Assign the second pair member to this variable.
-   * 
+   *
    * @param pair Pair
    */
   void setSecond(DBIDPair pair);
