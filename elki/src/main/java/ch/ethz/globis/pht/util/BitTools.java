154f1889af2a0a4ef597d677f8cdd4073ddb4cd6
tzaeschke <zoodb@gmx.de>
2015-09-16 14:28:09 +0200

    Fixed two more license references in files

--- elki/src/main/java/ch/ethz/globis/pht/util/BitTools.java
+++ elki/src/main/java/ch/ethz/globis/pht/util/BitTools.java
@@ -1,24 +1,27 @@
-/*
- * Copyright 2009-2014 Tilmann Zaeschke. All rights reserved.
- * 
- * This file is part of ZooDB.
- * 
- * ZooDB is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * ZooDB is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with ZooDB.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * See the README and COPYING files for further information. 
- */
 package ch.ethz.globis.pht.util;
+
+/*
+This file is part of ELKI:
+Environment for Developing KDD-Applications Supported by Index-Structures
+
+Copyright (C) 2009-2015
+Tilmann Zaeschke
+The author can be contacted via email: zoodb@gmx.de
+https://github.com/tzaeschke/critbit
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 public class BitTools {
 
