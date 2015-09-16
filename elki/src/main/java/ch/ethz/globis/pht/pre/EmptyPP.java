4f639edec2a990a361620540c750212ec557fd45
tzaeschke <zoodb@gmx.de>
2015-09-16 12:47:39 +0200

    Removed ColumnType

--- elki/src/main/java/ch/ethz/globis/pht/pre/EmptyPP.java
+++ elki/src/main/java/ch/ethz/globis/pht/pre/EmptyPP.java
@@ -23,58 +23,22 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import java.util.Arrays;
-
-import ch.ethz.globis.pht.pre.ColumnType.DoubleColumn;
-import ch.ethz.globis.pht.pre.ColumnType.FloatColumn;
-import ch.ethz.globis.pht.pre.ColumnType.IntColumn;
-import ch.ethz.globis.pht.pre.ColumnType.LongColumn;
 import ch.ethz.globis.pht.util.BitTools;
 
 public class EmptyPP implements PreProcessorPoint {
 
-	private ColumnType[] types;
-	
-	public EmptyPP() {
-		types = null;
-	}
-	
-	public EmptyPP(ColumnType[] types) {
-		this.types = Arrays.copyOf(types, types.length);
-	}
-	
-	@Override
-	public void pre(double[] raw, long[] pre) {
-		if (types != null) {
-			for (int d=0; d<raw.length; d++)
-				if (types[d] instanceof LongColumn || types[d] instanceof IntColumn)
-					pre[d] = (long)raw[d];
-				else if (types[d] instanceof DoubleColumn)
-					pre[d] = BitTools.toSortableLong(raw[d]);
-				else if (types[d] instanceof FloatColumn)
-					pre[d] = BitTools.toSortableLong((float)raw[d]);
-				else
-					throw new RuntimeException("Unsupported ColumnType");
-		} else {
-			for (int d=0; d<raw.length; d++)
-				pre[d] = BitTools.toSortableLong(raw[d]);
-		}
-	}
+  @Override
+  public void pre(double[] raw, long[] pre) {
+    for (int d=0; d<raw.length; d++) {
+      pre[d] = BitTools.toSortableLong(raw[d]);
+    }
+  }
 
-	@Override
-	public void post(long[] pre, double[] post) {
-		if (types != null) {
-			for (int d=0; d<pre.length; d++)
-				if (types[d] instanceof LongColumn || types[d] instanceof IntColumn)
-					post[d] = (double) pre[d];
-				else if (types[d] instanceof DoubleColumn)
-					post[d] = BitTools.toDouble(pre[d]);
-				else
-					throw new RuntimeException("Unsupported ColumnType");	
-		} else {
-			for (int d=0; d<pre.length; d++)
-				post[d] = BitTools.toDouble(pre[d]);
-		}
-	}
+  @Override
+  public void post(long[] pre, double[] post) {
+    for (int d=0; d<pre.length; d++) {
+      post[d] = BitTools.toDouble(pre[d]);
+    }
+  }
 
 }
--- elki/src/main/java/ch/ethz/globis/pht/pre/EmptyPPR.java
+++ elki/src/main/java/ch/ethz/globis/pht/pre/EmptyPPR.java
@@ -23,60 +23,24 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import java.util.Arrays;
-
-import ch.ethz.globis.pht.pre.ColumnType.IntColumn;
-import ch.ethz.globis.pht.pre.ColumnType.LongColumn;
-
 public class EmptyPPR implements PreProcessorRange {
-
-	private ColumnType[] types;
-	
-	public EmptyPPR() {
-		types = null;
-	}
-	
-	public EmptyPPR(ColumnType[] types) {
-		this.types = Arrays.copyOf(types, types.length);
-	}
 	
 	@Override
 	public void pre(long[] raw1, long[] raw2, long[] pre) {
-		final int pDIM = raw1.length;
-		if (types != null) {
-			for (int d=0; d<pDIM; d++) {
-				if (types[d] instanceof LongColumn || types[d] instanceof IntColumn) {
-					pre[d] = raw1[d];
-					pre[d+pDIM] = raw2[d];
-				} else {
-					throw new RuntimeException("Unsupported ColumnType");
-				}
-			}
-		} else {
-			for (int d=0; d<pDIM; d++) {
-				pre[d] = raw1[d];
-				pre[d+pDIM] = raw2[d];
-			}
-		}
+	  final int pDIM = raw1.length;
+	  for (int d=0; d<pDIM; d++) {
+	    pre[d] = raw1[d];
+	    pre[d+pDIM] = raw2[d];
+	  }
 	}
 
 	@Override
 	public void post(long[] pre, long[] post1, long[] post2) {
-		final int pDIM = post1.length;
-		if (types != null) {
-			for (int d=0; d<pDIM; d++)
-				if (types[d] instanceof LongColumn || types[d] instanceof IntColumn) {
-					post1[d] = pre[d];
-					post2[d] = pre[d+pDIM];
-				} else {
-					throw new RuntimeException("Unsupported ColumnType");
-				}
-		} else {
-			for (int d=0; d<pDIM; d++) {
-				post1[d] = pre[d];
-				post2[d] = pre[d+pDIM];
-			}
-		}
+	  final int pDIM = post1.length;
+	  for (int d=0; d<pDIM; d++) {
+	    post1[d] = pre[d];
+	    post2[d] = pre[d+pDIM];
+	  }
 	}
 
 }
