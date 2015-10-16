fa0141d96637e585bdb96de1bbd84cfb1593c486
Erich Schubert <schube@dbs.ifi.lmu.de>
2015-10-16 22:10:17 +0200

    Meta fix for INFLO: make "Mean" more robust against infinite values.

--- elki/src/main/java/de/lmu/ifi/dbs/elki/math/Mean.java
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/math/Mean.java
@@ -27,27 +27,27 @@ import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
 
 /**
  * Compute the mean using a numerically stable online algorithm.
- * 
+ *
  * This class can repeatedly be fed with data using the add() methods, the
  * resulting values for mean can be queried at any time using getMean().
- * 
+ *
  * Trivial code, but replicated a lot. The class is final so it should come at
  * low cost.
- * 
+ *
  * Related Literature:
- * 
+ *
  * <p>
  * B. P. Welford<br />
  * Note on a method for calculating corrected sums of squares and products<br />
  * in: Technometrics 4(3)
  * </p>
- * 
+ *
  * <p>
  * D.H.D. West<br />
  * Updating Mean and Variance Estimates: An Improved Method<br />
  * In: Communications of the ACM, Volume 22 Issue 9
  * </p>
- * 
+ *
  * @author Erich Schubert
  */
 @Reference(authors = "B. P. Welford", //
@@ -73,7 +73,7 @@ public class Mean {
 
   /**
    * Constructor from other instance
-   * 
+   *
    * @param other other instance to copy data from.
    */
   public Mean(Mean other) {
@@ -83,25 +83,34 @@ public class Mean {
 
   /**
    * Add a single value with weight 1.0
-   * 
+   *
    * @param val Value
    */
   public void put(double val) {
-    n += 1.0;
+    n += 1.;
+    if(m1 == Double.POSITIVE_INFINITY || m1 == Double.NEGATIVE_INFINITY) {
+      m1 += val;
+      return;
+    }
     final double delta = val - m1;
     m1 += delta / n;
   }
 
   /**
    * Add data with a given weight.
-   * 
+   *
    * See also: D.H.D. West<br />
    * Updating Mean and Variance Estimates: An Improved Method
-   * 
+   *
    * @param val data
    * @param weight weight
    */
   public void put(double val, double weight) {
+    if(m1 == Double.POSITIVE_INFINITY || m1 == Double.NEGATIVE_INFINITY) {
+      m1 += val;
+      n += 1.;
+      return;
+    }
     final double nwsum = weight + n;
     final double delta = val - m1;
     final double rval = delta * weight / nwsum;
@@ -111,7 +120,7 @@ public class Mean {
 
   /**
    * Join the data of another MeanVariance instance.
-   * 
+   *
    * @param other Data to join with
    */
   public void put(Mean other) {
@@ -125,7 +134,7 @@ public class Mean {
 
   /**
    * Add values with weight 1.0
-   * 
+   *
    * @param vals Values
    * @return this
    */
@@ -138,7 +147,7 @@ public class Mean {
 
   /**
    * Add values with weight 1.0
-   * 
+   *
    * @param vals Values
    * @return this
    */
@@ -152,7 +161,7 @@ public class Mean {
 
   /**
    * Get the number of points the average is based on.
-   * 
+   *
    * @return number of data points
    */
   public double getCount() {
@@ -161,7 +170,7 @@ public class Mean {
 
   /**
    * Return mean
-   * 
+   *
    * @return mean
    */
   public double getMean() {
@@ -170,7 +179,7 @@ public class Mean {
 
   /**
    * Create and initialize a new array of MeanVariance
-   * 
+   *
    * @param dimensionality Dimensionality
    * @return New and initialized Array
    */
@@ -197,7 +206,7 @@ public class Mean {
 
   /**
    * Static helper function.
-   * 
+   *
    * @param data Data to compute the mean for.
    * @return Mean
    */
