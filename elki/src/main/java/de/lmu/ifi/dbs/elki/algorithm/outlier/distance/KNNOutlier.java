2562989487c40c13ef3fa2ccc5775384bfcc700c
Erich Schubert <schube@dbs.ifi.lmu.de>
2015-10-23 10:20:22 +0200

    Adjust JavaDoc with respect to query point inclusion, simplify code.

--- elki/src/main/java/de/lmu/ifi/dbs/elki/algorithm/outlier/distance/KNNOutlier.java
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/algorithm/outlier/distance/KNNOutlier.java
@@ -4,7 +4,7 @@ package de.lmu.ifi.dbs.elki.algorithm.outlier.distance;
  This file is part of ELKI:
  Environment for Developing KDD-Applications Supported by Index-Structures
 
- Copyright (C) 2014
+ Copyright (C) 2015
  Ludwig-Maximilians-Universität München
  Lehr- und Forschungseinheit für Datenbanksysteme
  ELKI Development Team
@@ -32,7 +32,6 @@ import de.lmu.ifi.dbs.elki.database.datastore.DataStoreFactory;
 import de.lmu.ifi.dbs.elki.database.datastore.DataStoreUtil;
 import de.lmu.ifi.dbs.elki.database.datastore.WritableDoubleDataStore;
 import de.lmu.ifi.dbs.elki.database.ids.DBIDIter;
-import de.lmu.ifi.dbs.elki.database.ids.KNNList;
 import de.lmu.ifi.dbs.elki.database.query.distance.DistanceQuery;
 import de.lmu.ifi.dbs.elki.database.query.knn.KNNQuery;
 import de.lmu.ifi.dbs.elki.database.relation.DoubleRelation;
@@ -57,7 +56,7 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
 /**
  * Outlier Detection based on the distance of an object to its k nearest
  * neighbor.
- * 
+ *
  * This implementation differs from the original pseudocode: the k nearest
  * neighbors do not exclude the point that is currently evaluated. I.e. for k=1
  * the resulting score is the distance to the 1-nearest neighbor that is not the
@@ -65,25 +64,25 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
  * of k=1 in the original code does not make sense, as the 1NN distance will be
  * 0 for every point in the database. If you for any reason want to use the
  * original algorithm, subtract 1 from the k parameter.
- * 
+ *
  * Reference:
  * <p>
- * S. Ramaswamy, R. Rastogi, K. Shim:<br />
+ * S. Ramaswamy, and R. Rastogi, and K. Shim<br />
  * Efficient Algorithms for Mining Outliers from Large Data Sets.<br />
- * In: Proc. of the Int. Conf. on Management of Data, Dallas, Texas, 2000.
+ * In: Proc. Int. Conf. on Management of Data, 2000.
  * </p>
- * 
+ *
  * @author Lisa Reichert
- * 
+ *
  * @apiviz.has KNNQuery
- * 
+ *
  * @param <O> the type of DatabaseObjects handled by this Algorithm
  */
 @Title("KNN outlier: Efficient Algorithms for Mining Outliers from Large Data Sets")
 @Description("Outlier Detection based on the distance of an object to its k nearest neighbor.")
-@Reference(authors = "S. Ramaswamy, R. Rastogi, K. Shim", //
+@Reference(authors = "S. Ramaswamy, and R. Rastogi, and K. Shim", //
 title = "Efficient Algorithms for Mining Outliers from Large Data Sets", //
-booktitle = "Proc. of the Int. Conf. on Management of Data, Dallas, Texas, 2000", //
+booktitle = "Proc. Int. Conf. on Management of Data, 2000", //
 url = "http://dx.doi.org/10.1145/342009.335437")
 @Alias({ "de.lmu.ifi.dbs.elki.algorithm.outlier.KNNOutlier", "knno" })
 public class KNNOutlier<O> extends AbstractDistanceBasedAlgorithm<O, OutlierResult> implements OutlierAlgorithm {
@@ -99,41 +98,52 @@ public class KNNOutlier<O> extends AbstractDistanceBasedAlgorithm<O, OutlierResu
 
   /**
    * Constructor for a single kNN query.
-   * 
+   *
    * @param distanceFunction distance function to use
-   * @param k Value of k (including query point!)
+   * @param k Value of k (excluding query point!)
    */
   public KNNOutlier(DistanceFunction<? super O> distanceFunction, int k) {
     super(distanceFunction);
-    this.k = k;
+    this.k = k + 1; // INCLUDE the query point now
   }
 
   /**
    * Runs the algorithm in the timed evaluation part.
+   *
+   * @param database Database (no longer used)
+   * @param relation Data relation
    */
   public OutlierResult run(Database database, Relation<O> relation) {
-    final DistanceQuery<O> distanceQuery = database.getDistanceQuery(relation, getDistanceFunction());
-    final KNNQuery<O> knnQuery = database.getKNNQuery(distanceQuery, k + 1);
+    return run(relation);
+  }
+
+  /**
+   * Runs the algorithm in the timed evaluation part.
+   *
+   * @param relation Data relation
+   */
+  public OutlierResult run(Relation<O> relation) {
+    final DistanceQuery<O> distanceQuery = relation.getDistanceQuery(getDistanceFunction());
+    final KNNQuery<O> knnQuery = relation.getKNNQuery(distanceQuery, k);
 
     FiniteProgress prog = LOG.isVerbose() ? new FiniteProgress("kNN distance for objects", relation.size(), LOG) : null;
 
     DoubleMinMax minmax = new DoubleMinMax();
     WritableDoubleDataStore knno_score = DataStoreUtil.makeDoubleStorage(relation.getDBIDs(), DataStoreFactory.HINT_STATIC);
     // compute distance to the k nearest neighbor.
-    for(DBIDIter iditer = relation.iterDBIDs(); iditer.valid(); iditer.advance()) {
+    for(DBIDIter it = relation.iterDBIDs(); it.valid(); it.advance()) {
       // distance to the kth nearest neighbor
       // (assuming the query point is always included, with distance 0)
-      final KNNList knns = knnQuery.getKNNForDBID(iditer, k + 1);
-      final double dkn = knns.getKNNDistance();
+      final double dkn = knnQuery.getKNNForDBID(it, k).getKNNDistance();
 
-      knno_score.putDouble(iditer, dkn);
+      knno_score.putDouble(it, dkn);
       minmax.put(dkn);
 
       LOG.incrementProcessed(prog);
     }
     LOG.ensureCompleted(prog);
     DoubleRelation scoreres = new MaterializedDoubleRelation("kNN Outlier Score", "knn-outlier", knno_score, relation.getDBIDs());
-    OutlierScoreMeta meta = new BasicOutlierScoreMeta(minmax.getMin(), minmax.getMax(), 0.0, Double.POSITIVE_INFINITY, 0.0);
+    OutlierScoreMeta meta = new BasicOutlierScoreMeta(minmax.getMin(), minmax.getMax(), 0., Double.POSITIVE_INFINITY, 0.);
     return new OutlierResult(meta, scoreres);
   }
 
@@ -149,9 +159,9 @@ public class KNNOutlier<O> extends AbstractDistanceBasedAlgorithm<O, OutlierResu
 
   /**
    * Parameterization class.
-   * 
+   *
    * @author Erich Schubert
-   * 
+   *
    * @apiviz.exclude
    */
   public static class Parameterizer<O> extends AbstractDistanceBasedAlgorithm.Parameterizer<O> {
@@ -160,7 +170,7 @@ public class KNNOutlier<O> extends AbstractDistanceBasedAlgorithm<O, OutlierResu
      */
     public static final OptionID K_ID = new OptionID("knno.k", //
     "The k nearest neighbor, excluding the query point "//
-        + "(i.e. query point is the 0-nearest-neighbor)");
+    + "(i.e. query point is the 0-nearest-neighbor)");
 
     /**
      * k parameter
