74bb4cdced178133dee4990b6fe19de0a21bb4c1
Erich Schubert <schube@dbs.ifi.lmu.de>
2016-02-08 08:30:01 +0100

    Add basic parallel DBSCAN version.

---
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/algorithm/clustering/gdbscan/parallel/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Parallel versions of Generalized DBSCAN.
+ */
+package de.lmu.ifi.dbs.elki.algorithm.clustering.gdbscan.parallel;
---
+++ elki/src/test/java/de/lmu/ifi/dbs/elki/algorithm/clustering/gdbscan/parallel/ParallelGeneralizedDBSCANTest.java
@@ -0,0 +1,88 @@
+package de.lmu.ifi.dbs.elki.algorithm.clustering.gdbscan.parallel;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2016
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.algorithm.AbstractSimpleAlgorithmTest;
+import de.lmu.ifi.dbs.elki.algorithm.clustering.DBSCAN;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.model.Model;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Performs a full DBSCAN run, and compares the result with a clustering derived
+ * from the data set labels. This test ensures that DBSCAN performance doesn't
+ * unexpectedly drop on this data set (and also ensures that the algorithms
+ * work, as a side effect).
+ * 
+ * @author Erich Schubert
+ * @since 0.7.2
+ */
+public class ParallelGeneralizedDBSCANTest extends AbstractSimpleAlgorithmTest {
+  /**
+   * Run Generalized DBSCAN with fixed parameters and compare the result to a
+   * golden standard.
+   */
+  @Test
+  public void testParallelDBSCANResults() {
+    Database db = makeSimpleDatabase(UNITTEST + "3clusters-and-noise-2d.csv", 330);
+
+    // setup algorithm
+    ListParameterization params = new ListParameterization();
+    params.addParameter(DBSCAN.Parameterizer.EPSILON_ID, 0.04);
+    params.addParameter(DBSCAN.Parameterizer.MINPTS_ID, 20);
+    ParallelGeneralizedDBSCAN dbscan = ClassGenericsUtil.parameterizeOrAbort(ParallelGeneralizedDBSCAN.class, params);
+    testParameterizationOk(params);
+
+    // run DBSCAN on database
+    Clustering<Model> result = dbscan.run(db);
+
+    testFMeasure(db, result, 0.996413);
+    testClusterSizes(result, new int[] { 29, 50, 101, 150 });
+  }
+
+  /**
+   * Run Generalized DBSCAN with fixed parameters and compare the result to a
+   * golden standard.
+   */
+  @Test
+  public void testDBSCANOnSingleLinkDataset() {
+    Database db = makeSimpleDatabase(UNITTEST + "single-link-effect.ascii", 638);
+
+    // Setup algorithm
+    ListParameterization params = new ListParameterization();
+    params.addParameter(DBSCAN.Parameterizer.EPSILON_ID, 11.5);
+    params.addParameter(DBSCAN.Parameterizer.MINPTS_ID, 120);
+    ParallelGeneralizedDBSCAN dbscan = ClassGenericsUtil.parameterizeOrAbort(ParallelGeneralizedDBSCAN.class, params);
+    testParameterizationOk(params);
+
+    // run DBSCAN on database
+    Clustering<Model> result = dbscan.run(db);
+    testFMeasure(db, result, 0.954382);
+    testClusterSizes(result, new int[] { 11, 200, 203, 224 });
+  }
+}
