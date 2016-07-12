bf7ccb0ec4f40cc4c14c2905f511aa05539e8a13
Erich Schubert <schube@dbs.ifi.lmu.de>
2016-07-12 23:36:26 +0200

    Unit tests for ID estimators. Include zero values in the test.

---
+++ elki/src/test/java/de/lmu/ifi/dbs/elki/math/statistics/intrinsicdimensionality/AggregatedHillEstimatorTest.java
@@ -0,0 +1,40 @@
+package de.lmu.ifi.dbs.elki.math.statistics.intrinsicdimensionality;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2016
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.junit.Test;
+
+/**
+ * Unit test for Aggregated Hill estimator.
+ * 
+ * @author Erich Schubert
+ */
+public class AggregatedHillEstimatorTest extends AbstractIntrinsicDimensionalityEstimatorTest {
+  @Test
+  public void testAggregatedHill() {
+    IntrinsicDimensionalityEstimator e = AggregatedHillEstimator.STATIC;
+    regressionTest(e, 5, 1000, 0L, 4.710215390349222);
+    regressionTest(e, 7, 10000, 0L, 6.947193258582592);
+  }
+}
---
+++ elki/src/test/java/de/lmu/ifi/dbs/elki/math/statistics/intrinsicdimensionality/PWMEstimatorTest.java
@@ -0,0 +1,40 @@
+package de.lmu.ifi.dbs.elki.math.statistics.intrinsicdimensionality;
+
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2016
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.junit.Test;
+
+/**
+ * Unit test for PWM estimator.
+ * 
+ * @author Erich Schubert
+ */
+public class PWMEstimatorTest extends AbstractIntrinsicDimensionalityEstimatorTest {
+  @Test
+  public void testPWM() {
+    IntrinsicDimensionalityEstimator est = PWMEstimator.STATIC;
+    regressionTest(est, 5, 1000, 0L, 4.891606982612564);
+    regressionTest(est, 7, 10000, 0L, 6.959066158235904);
+  }
+}
