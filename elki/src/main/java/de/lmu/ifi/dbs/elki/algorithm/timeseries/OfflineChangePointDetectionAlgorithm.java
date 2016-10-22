0517076dad27dd0cdb2dba5cead87899ab2d4ee6
Sebastian Rühl <sebaruehl@gmail.com>
2016-10-23 00:17:25 +0200

    added comments, shorten some method signatures

--- elki/src/main/java/de/lmu/ifi/dbs/elki/algorithm/timeseries/OfflineChangePointDetectionAlgorithm.java
+++ elki/src/main/java/de/lmu/ifi/dbs/elki/algorithm/timeseries/OfflineChangePointDetectionAlgorithm.java
@@ -1,5 +1,28 @@
 package de.lmu.ifi.dbs.elki.algorithm.timeseries;
 
+/*
+ This file is part of ELKI:
+ Environment for Developing KDD-Applications Supported by Index-Structures
+
+ Copyright (C) 2016
+ Ludwig-Maximilians-Universität München
+ Lehr- und Forschungseinheit für Datenbanksysteme
+ ELKI Development Team
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.lmu.ifi.dbs.elki.algorithm.AbstractAlgorithm;
 import de.lmu.ifi.dbs.elki.data.DoubleVector;
 import de.lmu.ifi.dbs.elki.data.LabelList;
@@ -12,6 +35,10 @@ import de.lmu.ifi.dbs.elki.logging.Logging;
 import de.lmu.ifi.dbs.elki.math.Mean;
 import de.lmu.ifi.dbs.elki.math.linearalgebra.VMath;
 import de.lmu.ifi.dbs.elki.result.ChangePointDetectionResult;
+import de.lmu.ifi.dbs.elki.utilities.Alias;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Description;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Reference;
+import de.lmu.ifi.dbs.elki.utilities.documentation.Title;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.AbstractParameterizer;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.OptionID;
 import de.lmu.ifi.dbs.elki.utilities.optionhandling.constraints.CommonConstraints;
@@ -20,28 +47,96 @@ import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameters.IntParameter;
 
 import java.util.*;
 
+/**
+ * Off-line multiple change point detection algorithm
+ *
+ * @author Sebastian Rühl
+ */
+
+@Title("Off-line Change Point Detection: Algorithm for detecting change point in time series")
+@Description("Detects multiple change points in a time series")
+@Alias("de.lmu.ifi.dbs.elki.algorithm.OfflineChangePointDetection")
 public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<ChangePointDetectionResult> {
 
     private int confidence, bootstrapSteps;
 
+    /**
+     * Constructor
+     *
+     * @param confidence Confidence
+     * @param bootstrapSteps Steps for bootstrapping
+     */
     public OfflineChangePointDetectionAlgorithm(int confidence, int bootstrapSteps) {
         this.confidence = confidence;
         this.bootstrapSteps = bootstrapSteps;
     }
 
-    public ChangePointDetectionResult run(Database database, Relation<DoubleVector> relation, Relation<LabelList> labellist) {
+    /**
+     * Executes multiple change point detection for every given time series
+     *
+     * @param relation the relation to process
+     * @param labellist label for the distinct time series
+     * @return list with all the detected change point for every time series
+     */
+    public ChangePointDetectionResult run(Relation<DoubleVector> relation, Relation<LabelList> labellist) {
 
         List<ChangePoints> result = new ArrayList<>();
 
         for(DBIDIter realtion_iter = relation.getDBIDs().iter(); realtion_iter.valid(); realtion_iter.advance()) {
 
             result.add(new ChangePoints(
-                    multipleChangepointsWithConfidence(relation.get(realtion_iter).getValues(), confidence, bootstrapSteps)));
+                    multipleChangepointsWithConfidence(relation.get(realtion_iter).getValues())));
         }
 
         return new ChangePointDetectionResult("Change Point List", "changepoints", result, labellist);
     }
 
+    /**
+     * Performs multiple change point detection for a given time series
+     * Overloaded method, this method initializes the method and then calls other version with set variables
+     *
+     * @param values time series
+     * @return list of change point for given time series
+     */
+    private List<ChangePoint> multipleChangepointsWithConfidence(double[] values){
+        List<ChangePoint> result = new ArrayList<>();
+        result = multipleChangepointsWithConfidence(result, values, 0);
+        return result;
+    }
+
+    /**
+     * Actually performs multiple change point detection for a given time series
+     * This method uses a kind of divide and conquer approach
+     *
+     * @param result list containing detected change point
+     * @param values current time series
+     * @param tmpArraryStartIndex current position in the time series
+     * @return list of change point for given time series
+     */
+
+    private List<ChangePoint> multipleChangepointsWithConfidence(List<ChangePoint> result, double[] values, int tmpArraryStartIndex){
+        double tmpConf = confidenceLevel(values, bootstrapSteps);
+        int tmpMaxPos = tmpArraryStartIndex + getMaximumIndex(likelihoodRatioChangeInMean(values)); // return the detected changepoint
+
+        if(!(tmpConf < confidence || values.length <=3 || (tmpMaxPos - tmpArraryStartIndex + 1 == values.length))){ // cannot split up arrays of size 3, that would make every element a change point
+            multipleChangepointsWithConfidence(result
+                    , Arrays.copyOfRange(values, 0, tmpMaxPos - tmpArraryStartIndex)
+                    , tmpArraryStartIndex);
+            multipleChangepointsWithConfidence(result
+                    , Arrays.copyOfRange(values, tmpMaxPos - tmpArraryStartIndex + 1, values.length)
+                    , tmpMaxPos);
+            result.add(new ChangePoint(tmpMaxPos, tmpConf));
+        }
+
+        return result;
+    }
+
+
+    /**
+     *
+     * @param values time series
+     * @return log likelihood ratio for every observation
+     */
     private double[] likelihoodRatioChangeInMean(double[] values){
         double[] result = new double[values.length];
 
@@ -70,6 +165,8 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return result;
     }
 
+    /*
+    PROBABALY REMOVE
     // DOES NOT WORK - REASON NOT YET INVESTIGATED
     private double[] likelihoodRatioChangeInMeanOptimised(double[] values){
         double[] result = new double[values.length];
@@ -100,32 +197,6 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
 
     }
 
-    private List<ChangePoint> multipleChangepointsWithConfidence(double[] values, int confidence, int bootstrapSteps){
-        List<ChangePoint> result = new ArrayList<>();
-        result = multipleChangepointsWithConfidence(result, values, confidence, bootstrapSteps, 0);
-        return result;
-    }
-
-    private List<ChangePoint> multipleChangepointsWithConfidence(List<ChangePoint> result, double[] values, int confidence, int bootstrapSteps, int tmpArraryStartIndex){
-        double tmpConf = confidenceLevel(values, bootstrapSteps);
-        int tmpMaxPos = tmpArraryStartIndex + getMaximumIndex(likelihoodRatioChangeInMean(values)); // return the detected changepoint
-
-        if(!(tmpConf < confidence || values.length <=3 || (tmpMaxPos - tmpArraryStartIndex + 1 == values.length))){ // cannot split up arrays of size 3, that would make every element a change point
-            multipleChangepointsWithConfidence(result
-                                                    , Arrays.copyOfRange(values, 0, tmpMaxPos - tmpArraryStartIndex)
-                                                    , confidence
-                                                    , bootstrapSteps
-                                                    , tmpArraryStartIndex);
-            multipleChangepointsWithConfidence(result
-                                                    , Arrays.copyOfRange(values, tmpMaxPos - tmpArraryStartIndex + 1, values.length)
-                                                    , confidence
-                                                    , bootstrapSteps
-                                                    , tmpMaxPos);
-            result.add(new ChangePoint(tmpMaxPos, tmpConf));
-        }
-
-        return result;
-    }
 
     //TRY OUT RESULT HANDLING
     private ChangePoint singleChangepointWithConfidence(double[] values, int bootstrapSteps){
@@ -134,7 +205,16 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return new ChangePoint(index, conf);
     }
 
+    */
 
+    /**
+     * Calculates the confidence for the most probable change point of the given timer series.
+     * Confidence is calculated with the help of bootstrapping.
+     *
+     * @param values time series
+     * @param steps steps for bootstrapping
+     * @return confidence for most probable change point
+     */
     private double confidenceLevel(double[] values, int steps){
         double estimator = getBootstrapEstimator(likelihoodRatioChangeInMean(values));
         int x = 0;
@@ -148,11 +228,22 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return 100 * ((double)x/(double)steps);
     }
 
+    /**
+     * Estimator used by bootstrapping
+     *
+     * @param values time series
+     * @return bootstrap estimator
+     */
     private double getBootstrapEstimator(double[] values){
         return getMaximum(values)- getMinimum(values);
     }
 
-    // move to VMath??
+    /**
+     * Shuffles the observations of a time series
+     *
+     * @param values time series
+     * @return shuffled time series
+     */
     private double[] shuffleVector(double[] values)
     {
         double[] result= VMath.copy(values);
@@ -168,7 +259,12 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return result;
     }
 
-    // move to VMath??
+    /**
+     * Returns index of maximum value in double array
+     *
+     * @param values array
+     * @return index of maximum value
+     */
     private int getMaximumIndex(double[] values){
         int result = 0;
         for (int i = 0; i < values.length; i++){
@@ -179,7 +275,12 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return result;
     }
 
-    // move to VMath??
+    /**
+     * Returns maximum of a double array
+     *
+     * @param values array
+     * @return maximum value
+     */
     private double getMaximum(double[] values){
         double result = values[0];
         for(double value : values){
@@ -190,7 +291,12 @@ public class OfflineChangePointDetectionAlgorithm extends AbstractAlgorithm<Chan
         return result;
     }
 
-    // move to VMath??
+    /**
+     * Returns minimum of a double array
+     *
+     * @param values array
+     * @return minimum value
+     */
     private double getMinimum(double[] values){
         double result = values[0];
         for(double value : values){
