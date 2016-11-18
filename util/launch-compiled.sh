8ad219c93b4973a5ff0ec85d8fbe1bc77425cff3
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2016-11-18 22:30:28 +0100

    Some gradle build tweaks.

--- util/launch-compiled.sh
+++ util/launch-compiled.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 bd=$( dirname $( dirname $0 ) )
 java=$( test -z "$JAVA" && echo java || echo $JAVA  )
-core=$( ls -d $bd/elki/target/classes $bd/elki-core*/target/classes )
-mods=$( ls -d $bd/elki*/target/classes | egrep -v "/elki-core|/elki/|elki-docutil" )
-addons=$( ls -d $bd/addons/*/target/classes | egrep -v "addons/bundle" )
+core=$( ls -d $bd/elki/build/*/main $bd/elki-core*/build/*/main )
+mods=$( ls -d $bd/elki*/build/*/main | egrep -v "/elki-core|/elki/|elki-docutil" )
+addons=$( ls -d $bd/addons/*/build/*/main | egrep -v "addons/bundle" )
 # This is really ugly... collect .jars on the class path, but avoid duplicates.
-deps=$( ls -d $bd/*/target/dependency/*.jar $bd/addons/*/target/dependency/*.jar | egrep -v "elki-docutil" \
-| sed -e 's,.*target/dependency/,,' | egrep -v "^elki|^hamcrest|^junit" | sort -u \
-| while read dep; do ls $bd/*/target/dependency/$dep $bd/addons/*/target/dependency/$dep 2>/dev/null | head -1; done )
+deps=$( ls -d $bd/*/build/libs/lib/*.jar $bd/addons/*/build/libs/lib/*.jar | egrep -v "elki-docutil" \
+| sed -e 's,.*build/libs/lib/,,' | egrep -v "^elki|^hamcrest|^junit" | sort -u \
+| while read dep; do ls $bd/*/build/libs/lib/$dep $bd/addons/*/build/libs/lib/$dep 2>/dev/null | head -1; done )
 if [ -z "$core" ]; then
-  echo "ELKI does not appear to be compiled yet. Call 'mvn package' first."  >&2
+  echo "ELKI does not appear to be compiled yet. Call './gradlew compile' first."  >&2
   exit 1
 fi
 cp=$( echo "$core:$mods:$addons:$deps" | paste -s -d: )
--- util/launch-packaged.sh
+++ util/launch-packaged.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 bd=$( dirname $( dirname $0 ) )
 java=$( test -z "$JAVA" && echo java || echo $JAVA  )
-core=$( ls $bd/elki/target/*.jar $bd/elki-core*/target/*.jar | egrep -v "javadoc.jar|sources.jar" )
-mods=$( ls $bd/elki*/target/*.jar | egrep -v "javadoc.jar|sources.jar|/elki-core|/elki/|/elki-docutil" )
-addons=$( ls $bd/addons/*/target/*.jar | egrep -v "javadoc.jar|sources.jar|elki-bundle" )
+core=$( ls $bd/elki/build/libs/*.jar $bd/elki-core*/build/libs/*.jar | egrep -v "javadoc.jar|sources.jar" )
+mods=$( ls $bd/elki*/build/libs/*.jar | egrep -v "javadoc.jar|sources.jar|/elki-core|/elki/|/elki-docutil" )
+addons=$( ls $bd/addons/*/build/libs/*.jar | egrep -v "javadoc.jar|sources.jar|elki-bundle" )
 if [ -z "$core" ]; then
-  echo "ELKI does not appear to be compiled yet. Call 'mvn package' first."  >&2
+  echo "ELKI does not appear to be compiled yet. Call './gradlew jar' first."  >&2
   exit 1
 fi
 cp=$( echo "$core:$mods:$addons" | paste -s -d: )
