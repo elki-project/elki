950c9b8e9f2eb06e7abfc6e4518b65e35583d235
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2017-02-09 17:34:07 +0100

    Implement KNNDD algorithm.

---
+++ elki/src/test/java/de/lmu/ifi/dbs/elki/algorithm/outlier/distance/KNNDDTest.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ *
+ * Copyright (C) 2017
+ * ELKI Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.lmu.ifi.dbs.elki.algorithm.outlier.distance;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.algorithm.AbstractSimpleAlgorithmTest;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.result.outlier.OutlierResult;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Tests the KNNDD algorithm.
+ * 
+ * @author Erich Schubert
+ */
+public class KNNDDTest extends AbstractSimpleAlgorithmTest {
+  @Test
+  public void testKNNOutlier() {
+    Database db = makeSimpleDatabase(UNITTEST + "outlier-3d-3clusters.ascii", 960);
+
+    // Parameterization
+    ListParameterization params = new ListParameterization();
+    params.addParameter(KNNDD.Parameterizer.K_ID, 100);
+
+    // setup Algorithm
+    KNNDD<DoubleVector> knnOutlier = ClassGenericsUtil.parameterizeOrAbort(KNNDD.class, params);
+    testParameterizationOk(params);
+
+    // run KNNDD on database
+    OutlierResult result = knnOutlier.run(db);
+
+    testSingleScore(result, 945, 0.965469269);
+    testAUC(db, "Noise", result, 0.875203703);
+  }
+}
