ace25cb2641810a65687dee55017934ca521fb0e
dd171 <dd171@users.noreply.github.com>
2017-05-24 09:48:15 +0200

    Added a unit test for LengthNormalizationTest.

---
+++ elki-input/src/test/java/de/lmu/ifi/dbs/elki/datasource/filter/normalization/instancewise/LengthNormalizationTest.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ * 
+ * Copyright (C) 2017
+ * ELKI Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.lmu.ifi.dbs.elki.datasource.filter.normalization.instancewise;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.type.FieldTypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.datasource.AbstractDataSourceTest;
+import de.lmu.ifi.dbs.elki.datasource.bundle.MultipleObjectsBundle;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Test the length normalization filter.
+ *
+ * @author Matthew Arcifa
+ */
+public class LengthNormalizationTest extends AbstractDataSourceTest {
+  /**
+   * Test with default parameters.
+   */
+  @Test
+  public void defaultParameters() {
+    String filename = UNITTEST + "normalization-test-1.csv";
+    // Allow loading test data from resources.
+    LengthNormalization<DoubleVector> filter = ClassGenericsUtil.parameterizeOrAbort(LengthNormalization.class, new ListParameterization());
+    MultipleObjectsBundle bundle = readBundle(filename, filter);
+    // Ensure the first column are the vectors.
+    assertTrue("Test file not as expected", TypeUtil.NUMBER_VECTOR_FIELD.isAssignableFromType(bundle.meta(0)));
+    // This cast is now safe (vector field):
+    int dim = ((FieldTypeInformation) bundle.meta(0)).getDimensionality();
+    
+    // Verify that the length of each row vector is 1.
+    for(int row = 0; row < bundle.dataLength(); row++) {
+      Object obj = bundle.data(row, 0);
+      assertEquals("Unexpected data type", DoubleVector.class, obj.getClass());
+      DoubleVector d = (DoubleVector) obj;
+      double len = 0.0;
+      for(int col = 0; col < dim; col++) {
+        final double v = d.doubleValue(col);
+        len += v * v;
+      }
+      assertEquals("Vector length is not 1", 1., len, 1e-15);
+    }
+  }
+}
