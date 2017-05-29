225dd2422bf7a0a5be22e4ef8ef2fb3536ccbe28
dd171 <dd171@users.noreply.github.com>
2017-05-29 13:16:16 +0200

    Added a unit test for the instance-wise log-rank normalization filter.

---
+++ elki-input/src/test/java/de/lmu/ifi/dbs/elki/datasource/filter/normalization/instancewise/InstanceLogRankNormalizationTest.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ * 
+ * Copyright (C) 2017
+ * ELKI Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.lmu.ifi.dbs.elki.datasource.filter.normalization.instancewise;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.type.FieldTypeInformation;
+import de.lmu.ifi.dbs.elki.data.type.TypeUtil;
+import de.lmu.ifi.dbs.elki.datasource.AbstractDataSourceTest;
+import de.lmu.ifi.dbs.elki.datasource.bundle.MultipleObjectsBundle;
+import de.lmu.ifi.dbs.elki.math.DoubleMinMax;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Test the log-rank normalization filter.
+ *
+ * @author Matthew Arcifa
+ */
+public class InstanceLogRankNormalizationTest extends AbstractDataSourceTest {
+  /**
+   * Test with default parameters.
+   */
+  @Test
+  public void defaultParameters() {
+    String filename = UNITTEST + "normalization-test-1.csv";
+    // Allow loading test data from resources.
+    InstanceLogRankNormalization<DoubleVector> filter = ClassGenericsUtil.parameterizeOrAbort(InstanceLogRankNormalization.class, new ListParameterization());
+    MultipleObjectsBundle bundle = readBundle(filename, filter);
+    // Ensure the first column are the vectors.
+    assertTrue("Test file not as expected", TypeUtil.NUMBER_VECTOR_FIELD.isAssignableFromType(bundle.meta(0)));
+    // This cast is now safe (vector field):
+    int dim = ((FieldTypeInformation) bundle.meta(0)).getDimensionality();
+    
+    // Verify that, in each row, the min value is 0 and the max value 1.
+    
+    /* TODO: This filter is essentially the same as the min-max normalization filter,
+     * but this filter scales the values logarithmically rather than linearly.
+     * Is there another expected property of the resulting data which we can
+     * verify? 
+     */
+    DoubleMinMax[] mms = DoubleMinMax.newArray(bundle.dataLength());
+    for(int row = 0; row < bundle.dataLength(); row++) {
+      Object obj = bundle.data(row, 0);
+      assertEquals("Unexpected data type", DoubleVector.class, obj.getClass());
+      DoubleVector d = (DoubleVector) obj;
+      for(int col = 0; col < dim; col++) {
+        final double v = d.doubleValue(col);
+        mms[row].put(v);
+      }
+    }
+    
+    for(int row = 0; row < bundle.dataLength(); row++) {
+      assertEquals("Min value is not 0", 0., mms[row].getMin(), 1e-8);
+      assertEquals("Max value is not 1", 1., mms[row].getMax(), 1e-8);     
+    }
+  }
+}
