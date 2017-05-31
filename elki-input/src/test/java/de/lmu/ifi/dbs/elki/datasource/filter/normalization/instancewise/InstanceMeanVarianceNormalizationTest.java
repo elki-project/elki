57c9e46642dfbf90dab5683c1aa3cfd8384fa2d1
dd171 <dd171@users.noreply.github.com>
2017-05-31 10:32:12 +0200

    Fixed a bug in the instance-wise mean variance normalization filter.
    The bug caused the filter to give results which were incorrect and did
    not have the expected properties.

--- elki-input/src/test/java/de/lmu/ifi/dbs/elki/datasource/filter/normalization/instancewise/InstanceMeanVarianceNormalizationTest.java
+++ elki-input/src/test/java/de/lmu/ifi/dbs/elki/datasource/filter/normalization/instancewise/InstanceMeanVarianceNormalizationTest.java
@@ -55,22 +55,20 @@ public class InstanceMeanVarianceNormalizationTest extends AbstractDataSourceTes
     int dim = ((FieldTypeInformation) bundle.meta(0)).getDimensionality();
     
     // Verify that the resulting data has mean 0 and variance 1 in each row.
-    MeanVariance[] mvs = MeanVariance.newArray(bundle.dataLength());
+    MeanVariance mvs = new MeanVariance();
     for(int row = 0; row < bundle.dataLength(); row++) {
       Object obj = bundle.data(row, 0);
       assertEquals("Unexpected data type", DoubleVector.class, obj.getClass());
       DoubleVector d = (DoubleVector) obj;
+      mvs.reset();
       for(int col = 0; col < dim; col++) {
         final double v = d.doubleValue(col);
         if(v > Double.NEGATIVE_INFINITY && v < Double.POSITIVE_INFINITY) {
-          mvs[row].put(v);
+          mvs.put(v);
         }
       }
-    }
-    
-    for(int row = 0; row < bundle.dataLength(); row++) {
-      assertEquals("Mean is not 0", 0., mvs[row].getMean(), 1);
-      assertEquals("Variance is not 1", 1., mvs[row].getNaiveVariance(), 1);     
+      assertEquals("Mean is not 0", 0., mvs.getMean(), 1e-15);
+      assertEquals("Variance is not 1", 1., mvs.getNaiveVariance(), 1e-15);
     }
   }
 }
