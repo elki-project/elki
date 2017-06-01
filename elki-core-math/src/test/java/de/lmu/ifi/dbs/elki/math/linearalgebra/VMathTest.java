8a029bf319368768e07e8aa908c5fc9bac5d1454
Merlin Dietrich <einoeden@posteo.net>
2017-06-01 03:02:01 +0200

    testing templates an struckter (work in progress)

--- elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathTest.java
+++ elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathTest.java
@@ -26,8 +26,11 @@ import static de.lmu.ifi.dbs.elki.math.linearalgebra.VMath.*;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
 
 import net.jafama.FastMath;
 
@@ -49,7 +52,12 @@ public class VMathTest {
   /**
    * A small number to handle numbers near 0 as 0.
    */
-  private static final double EPSILON = 1E-16;
+  private static final double EPSILON = 1E-15;
+  
+  /**
+   * Error message (in assertions!) when vector dimensionalities do not agree.
+   */
+  private static final String ERR_VEC_DIMENSIONS = "Vector dimensions do not agree.";
   
   /**
    * A collection of v1 testvectors for later use.
@@ -59,34 +67,73 @@ public class VMathTest {
       {1,2,3},
       {4,5,6,0},
   };
- 
   
+  /**
+   * Vector v1 as TestVector
+   */
+  private static double[] v1;
+  
+  /**
+   * Vector v2 as TestVector
+   */
+  private static double[] v2;
+  
+  /**
+   * Skalar s1 as TestSkalar
+   */
+  private static double s1;
+
+  /**
+   * Skalar s2 as TestSkalar
+   */
+  private static double s2;
+  
+  /**
+   * Skalar d as TestSkalar
+   */
+  private static double d;
+  
+  /**
+   * Vector RES_VEC as storage for results
+   */
+  private static double[] RES_VEC;
+
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
   
   @Test //sqrt implemented was missing at first. Case 0 not working jet
   public void testrandomNormalizedVector() {
     
-    for(int i = 1; i < 2; i++){
-      final double[] v = randomNormalizedVector(i);
+    for(int i = 1; i < 10; i++){
+      final double[] v1 = randomNormalizedVector(i);
+      
+      // test if vector is normalized, checks if squared sums equal 1
       double vnorm = 0;
       for(int j=0; j < i;j++) {
-        final double x = v[j];
+        final double x = v1[j];
         vnorm += x*x;
       };
       assertEquals(1,vnorm,EPSILON); 
+      
+      // possible to use VMath euclideanLength()
+      // assertEquals(1,euclideanLength(v1),EPSILON) or
+      // assertEquals(1,squareSum(v1),EPSILON)   
+      
     };
     
   }
   
-  @Rule
-  public final ExpectedException exception = ExpectedException.none();
 
   @Test //index is given Starting 0. 
   public void testunitVector() {
     
+    // test if unitVector is returned
+    // for dimension 3
     assertArrayEquals(new double[] {1,0,0}, unitVector(3,0),0. );
     assertArrayEquals(new double[] {0,1,0}, unitVector(3,1),0. );
     assertArrayEquals(new double[] {0,0,1}, unitVector(3,2),0. );
-
+    
+    // for dimension 5
     assertArrayEquals(new double[] {1,0,0,0,0}, unitVector(5,0),0. );
     assertArrayEquals(new double[] {0,1,0,0,0}, unitVector(5,1),0. );
     assertArrayEquals(new double[] {0,0,1,0,0}, unitVector(5,2),0. );
@@ -123,30 +170,69 @@ public class VMathTest {
   }
   
   
-  
   @Test
   public void testplus() {
-        
-    double[] v = {0,0,0 };
-    double[] v1 ={1,2, 0.123456789123456789123};
-    double[] v2 = {-1,-2,-0.12345678912345678000};
-    assertArrayEquals(v,plus(v1,v2),EPSILON);
-  
+    
+    // test near 0
+    v1      = new double[] { 1, 2, 0.123456789123456789123};
+    v2      = new double[] {-1,-2,-0.123456789123457000000};
+    s1      = 0;
+    s2      = 0;
+    d       = 0;
+    RES_VEC = new double[] {0,0,0 };
+    assertArrayEquals(RES_VEC,plus(v1,v2),EPSILON);
+
+//    // test "normal" vectors
+//    v1      = new double[] {1.6926, 182262, 0.7625, 2 , 10E20, 4};
+//    v2      = new double[] {3, 0.200, 2567E10,-500,3, 2};
+//    RES_VEC = new double[] {};
+//    assertArrayEquals(RES_VEC,plus(v1,v2),EPSILON);
+
+//    // test numeric loss of percision
+//    v1      = new double[] {1,1};
+//    v2      = new double[] {1.12345678912345E17,1};
+//    RES_VEC = new double[] {1.12345678912345001E17,2};
+//    assertArrayEquals(RES_VEC,plus(v1,v2),EPSILON);
+    
+    
+    // test ERR_VEC_DIMENSIONS
+    exception.expect(AssertionError.class);
+    exception.expectMessage(ERR_VEC_DIMENSIONS);
+    
+    v1      = new double[] {1,1};
+    v2      = new double[] {1};
+    RES_VEC = new double[] {};
+    assertArrayEquals(RES_VEC,plus(v1,v2),EPSILON);
+    
   }
   
   /**
-   *
-   *
+   * testing the TimesplusANDplusTimes function of VMath class
    */
-//  @Test
-//  public void EG() {
-//  
-//    assertArrayEquals(v, foo(), EPSILON);
-//
-//    assertEquals(d, foo(), EPSILON);
-//    
-//  }
-//  
-  
+  @Test
+  public void testTimesplusANDplusTimes() {
+    
+    v1      = new double[] { 1,2,3 };
+    s1      = 50;
+    v2      = new double[] { 1,1,1 };
+    RES_VEC = new double[] { 51,101,151 };
+    assertArrayEquals(RES_VEC, timesPlus(v1, s1, v2), EPSILON);
+    
+    s2      = s1;
+    RES_VEC = new double[] { 51,52,53 };
+    assertArrayEquals(RES_VEC, plusTimes(v1, v2, s2), EPSILON);
+    
+    
+    // Test if assertionError ERR_VEC_DIMENSIONS is raised
+    exception.expect(AssertionError.class);
+    exception.expectMessage(ERR_VEC_DIMENSIONS);
+    
+    v1 = new double[] { 1 };
+    v2 = new double[] { 1,1 };
+    assertArrayEquals(RES_VEC, timesPlus(v1, s1, v2), EPSILON);
+    assertArrayEquals(RES_VEC, plusTimes(v1, v2, s2), EPSILON);
+   
+    
+  }
   
 }
