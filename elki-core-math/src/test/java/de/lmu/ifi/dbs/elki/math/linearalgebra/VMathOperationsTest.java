bab7a874ef2a3aabf6d97ce51ed7a61f6ff2df9d
delead <einoeden@posteo.net>
2017-07-04 02:30:03 +0200

    Implemented a assertDimensionMissmatch method, commented it in detail and made some minar changes in structur and implementation.

--- elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathOperationsTest.java
+++ elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathOperationsTest.java
@@ -27,6 +27,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.validator.TestClassValidator;
 
 /**
  * Test the VMath class methods which are non mathimatical operations on Vector or Matrixes.
@@ -42,26 +43,77 @@ public final class VMathOperationsTest {
   protected static final double EPSILON = 1E-15;
   
   /**
+   * Method for testing that exceptions are raised if dimension mismatches in input data occur.
+   *    
+   * This test caches Assertion Errors with a given Message if the -ea option is set or rather if assertions are executed. 
+   * If the -ea option is not set, an ArrayIndexOutOfBoundsException may occur, but out of optimization 
+   * considerations we don't want to guarantee for an exception. So in this case we can't decide 
+   * if a dimension mismatch has occurred. This is why we need the {@link failWrapper} method. <p>
+   * Let's make an example of usage: <br> 
+   * Let's take two Vectors v1, v2 with  different lengths. 
+   * So v1 + v2 should not be possible, so we want v1.length to equal v2.length and assert with the 
+   * {@link VMath#ERR_VEC_DIMENSIONS} error Message. If we think of any implementation of a plus(v1, v2) 
+   * method with vectors as arrays e.g. {@link VMath#plus(double[], double[])}, 
+   * we are going to iterate either over the length of v1 or v2. But with assertions turned of (no -ea set)
+   * either v1+v2 or v2+v1 is  going to raise an ArrayIndexOutOfBoundsException,
+   * while the other is not. 
+   * <pre>
+   * assertDimensionMismatch( {@link VMathVectorTest#ERR_VEC_DIMENSIONS},() -> plus(v1, v2) )
+   * </pre>
+   * 
+   * @param msg Assertion Message to be raised with -ea on
+   * @param r runnable of the method to be tested
+   */
+  protected static void assertDimensionMismatch(String msg, Runnable r) {
+    try {
+      r.run();
+    } catch(AssertionError e) {
+      assertEquals(e.getMessage() , msg);
+      
+      return; // If assertions are enabled.
+    } catch(ArrayIndexOutOfBoundsException e) {
+      return; // Expected failure
+    }
+    // We only guarantee an error if assertions are enabled with -ea. If they are not the next line will do nothing.
+    assert(failWrapper("Failed to raise expected Assertationerrormessage".concat(msg)));
+  }
+
+
+  /**
+   * FailWrapper function, to wrap a fail into an assertion.
+   * 
+   * This is needed to toggle the fail with the -ea option.
+   * 
+   * @param msg the msg of {@link assertDimensionMismatch}
+   * @return ture
+   * 
+   * @see {@link assertDimensionMismatch}
+   */
+  private static boolean failWrapper(String msg) {
+    fail(msg);
+    return true;
+  }
+  
+  /**
    * Test the copy functions of VMath class.
    * 
    * Tested Methods:
-   * copy(vector), TOIMPL: copy(Matrix), columPackedCopy(Matrix), rowPackedcCopy(Matrix)
+   * copy(vector), copy(Matrix), columPackedCopy(Matrix), rowPackedcCopy(Matrix)
    */
   @Test
   public void testCopy() {
     
     // testing copy(vector) method
-    double[] v; double[] v_copy;
     
-    v   = new double[] {1,2,3,4};
-    v_copy = copy(v);
-    assertArrayEquals(v, v_copy,0.);
-    assertNotSame(v, v_copy);
+    final double[] v1 = {1,2,3,4};
+    final double[] v1_res = copy(v1);
+    assertArrayEquals(v1, v1_res,0.);
+    assertNotSame(v1, v1_res);
     
-    v = new double[] {0};
-    v_copy = copy(v);
-    assertArrayEquals(v, v_copy,0.);
-    assertNotSame(v, v_copy);
+    final double[] v2 = VMathVectorTest.TESTVEC;
+    final double[] v2_res = copy(v2);
+    assertArrayEquals(v2, v2_res,0.);
+    assertNotSame(v2, v2_res);
     
     //testing copy(Matrix) method
     final double[][] m1 = VMathMatrixTest.TESTMATRIX;
@@ -71,24 +123,22 @@ public final class VMathOperationsTest {
     
     final double[][] m2 = {{0, 1, 0.123451234512345, 2},
                            {2, 3, 4.123451234512345,-1}};
-    final double[][] m2_copy = copy(m2);
-    assertThat(m2, is(equalTo(m2_copy)));
-    assertNotSame(m2, m2_copy);
+    final double[][] m2_res = copy(m2);
+    assertThat(m2, is(equalTo(m2_res)));
+    assertNotSame(m2, m2_res);
     
     //testing columPackedCopy(Matrix) method
     final double[] m2_colpack = {0,2,  1,3,  0.123451234512345,4.123451234512345,  2,-1};
     
     final double[] m2_colpackres = columnPackedCopy(m2);
     assertArrayEquals(columnPackedCopy(m2), m2_colpack, 0.);
-    assertNotSame(m2_colpack, m2_colpackres);
+    assertNotSame(m2, m2_colpackres);
     
-    //QUEST: row packed = colum packed vis versa?
+    //testing rowPackedCopy(Matrix) method
     final double[] m2_rowpack = {0,1,0.123451234512345,2,  2,3,4.123451234512345,-1};
     final double[] m2_rowpackres = rowPackedCopy(m2);
     assertArrayEquals(rowPackedCopy(m2), m2_rowpack, 0.);
-    assertNotSame(m2_rowpack, m2_rowpackres);
-    
-    
+    assertNotSame(m2, m2_rowpackres);   
   }
   
 
@@ -98,27 +148,29 @@ public final class VMathOperationsTest {
   @Test
   public void testHashcode() {
 
-    // TODO QUEST: hashCode(v1) hashCode(m1) test needed?
+    // TODO: QUEST: hashCode(v1) hashCode(m1) test needed?
   }
   
   /**
-   * Testing the clear(vector), clear(vector) methods of VMath class.
-   * 
-   * QUEST vector and matrix in one mehtod
+   * Testing the clear(vector), clear(matrix) methods of VMath class.
    */
   @Test
   public void testClear() {
     
-    // test clear(vector) with TESTVEC of lenght 5
-    double[] v = VMathVectorTest.TESTVEC; clear(v); 
+    // test clear(vector) 
+    final double[] v = copy(VMathVectorTest.TESTVEC); clear(v); 
     final double[] zeros5 = {0,0,0,0,0};
     assertArrayEquals(zeros5, v, 0.);
     
-    // test clear(matrix) with TESTMATRIX of dimesions 45x5
-    double[][] m = copy(VMathMatrixTest.TESTMATRIX); clear(m);   
-    final double[][] zeros4x5 = {{0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+    // test clear(matrix) 
+    final double[][] m = copy(VMathMatrixTest.TESTMATRIX); clear(m);   
+    final double[][] zeros4x5 = {
+        {0,0,0,0,0}, 
+        {0,0,0,0,0}, 
+        {0,0,0,0,0}, 
+        {0,0,0,0,0}};
     
-    assertTrue(almostEquals(zeros4x5 , m, 0.));
+    assertThat(zeros4x5, is(equalTo(m)));
   }
 
   /**
@@ -126,11 +178,9 @@ public final class VMathOperationsTest {
    */
   @Test
   public void testEquals() {
-    // equals(Matrix)
+    // equals(Vector)
     final double[] v1 = {2,4,3,0,-5,9};
-    
-    // make copy of by hand to be independent of copy 
-    // module to be able to use Equals in testcopy
+    // copy made by hand to be independent of copy module
     final double[] v1_copy = {2,4,3,0,-5,9};
     
     assertThat(v1, is(equalTo(v1)));
@@ -138,22 +188,23 @@ public final class VMathOperationsTest {
     assertThat(unitVector(6, 2), is(not(equalTo(v1))));
     
     
-    // Equals(Matrix)
-    final double[][] m1 = {{ 1, 2, 3},
-                           { 7, 3, 9},
-                           { 0, 2, 1},
-                           {20,-5, 4},
-                           {-3, 0, 3},
-                           { 1, 1, 1}};
+    // equals(Matrix)
+    final double[][] m1 = {
+        { 1, 2, 3},
+        { 7, 3, 9},
+        { 0, 2, 1},
+        {20,-5, 4},
+        {-3, 0, 3},
+        { 1, 1, 1}};
     
-    // make copy of by hand to be independent of copy 
-    // module to be able to use Equals in testcopy
-    final double[][] m1_copy = {{ 1, 2, 3},
-                                { 7, 3, 9},
-                                { 0, 2, 1},
-                                {20,-5, 4},
-                                {-3, 0, 3},
-                                { 1, 1, 1}};
+    // make copy of by hand to be independent of copy module
+    final double[][] m1_copy = {
+        { 1, 2, 3},
+        { 7, 3, 9},
+        { 0, 2, 1},
+        {20,-5, 4},
+        {-3, 0, 3},
+        { 1, 1, 1}};
         
     assertThat(m1, is(equalTo(m1)));
     assertThat(m1, is(equalTo(m1_copy)));
@@ -164,25 +215,22 @@ public final class VMathOperationsTest {
   }
   
   /**
-   * Testing the almostEquals and methods of VMath class.
+   * Testing the almostEquals methods of VMath class.
    * 
    * Note that almostEquals(m1,m2) is equivalent to almostEquals(m1,m2, {@link VMath#DELTA})
-   * @see VMath#almostEquals(double[][], double[][])
    */
   @Test
   public void testMatrixAlmosteq() {
 
     final double[][] m = copy(VMathMatrixTest.TESTMATRIX);
     
-    final double[][] unit4x5 = {
-        {1,0,0,0,0}, {0,1,0,0,0}, {0,0,1,0,0}, {0,0,0,1,0}
-    };
+    // TODO: make copy by hand to be independent of copy. Update structure.
     
     // basic function test
     assertTrue(almostEquals(m , VMathMatrixTest.TESTMATRIX));
     assertTrue(almostEquals(m , VMathMatrixTest.TESTMATRIX, 0.));
-    assertFalse(almostEquals(m, unit4x5));
-    assertFalse(almostEquals(m, unit4x5, EPSILON));
+    assertFalse(almostEquals(m, identity(4, 5)));
+    assertFalse(almostEquals(m, identity(4, 5), EPSILON));
     
     
     // fail if dimensions mismatch
@@ -206,7 +254,7 @@ public final class VMathOperationsTest {
     assertFalse(almostEquals(m, res_diff, 1E10));
     res_diff[2][1] -= 1E10;
     assertTrue(almostEquals(m, res_diff, 1E10));
-    // QUEST: What is this case getClass() for? why is this test failing?
+    // TODO QUEST: What is this case getClass() for? why is this test failing?
     
   }
 
