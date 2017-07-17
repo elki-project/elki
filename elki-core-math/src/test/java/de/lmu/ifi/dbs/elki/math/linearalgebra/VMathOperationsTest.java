8e255ff7e3ebb66caafd66ef9ee34743eb209ef1
delead <einoeden@posteo.net>
2017-07-17 23:00:20 +0200

    Chnaged structure: moved all test for inizialisation, get and set to VMathOperationsTest

--- elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathOperationsTest.java
+++ elki-core-math/src/test/java/de/lmu/ifi/dbs/elki/math/linearalgebra/VMathOperationsTest.java
@@ -21,7 +21,7 @@
 package de.lmu.ifi.dbs.elki.math.linearalgebra;
 
 import static de.lmu.ifi.dbs.elki.math.linearalgebra.VMath.*;
-
+import static de.lmu.ifi.dbs.elki.math.linearalgebra.VMathOperationsTest.assertDimensionMismatch;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -260,7 +260,401 @@ public final class VMathOperationsTest {
     // TODO QUEST: What is this case getClass() for? why is this test failing?
     
   }
+  
+  // ----------- initialization get and set methods ----------------
+  
+  /**
+   * Testing the {@link VMath#unitVector(int, int)} method.
+   */
+  @Test
+  public void testUnitVector() { 
+    // test if unitVector is returned
+    // for dimension 3
+    assertArrayEquals(new double[] {1,0,0}, unitVector(3,0),0. );
+    assertArrayEquals(new double[] {0,1,0}, unitVector(3,1),0. );
+    assertArrayEquals(new double[] {0,0,1}, unitVector(3,2),0. );
+    
+    // for dimension 5
+    assertArrayEquals(new double[] {1,0,0,0,0}, unitVector(5,0),0. );
+    assertArrayEquals(new double[] {0,1,0,0,0}, unitVector(5,1),0. );
+    assertArrayEquals(new double[] {0,0,1,0,0}, unitVector(5,2),0. );
+    assertArrayEquals(new double[] {0,0,0,1,0}, unitVector(5,3),0. );
+    assertArrayEquals(new double[] {0,0,0,0,1}, unitVector(5,4),0. );
+   
+  }
 
-}
-// TODO put testGet testSet here, getDimensionality, testDiagonal
-// TODO put testUniMatrix testUnitvector here
+  /**
+   * Testing the unitMatrix and the identity method of VMath class.
+   */
+  @Test
+  public void testUnitMatrixAndIdentity() {
+    // test unitMatrix(dim) and unitMatrix(dim) equals identity(dim, dim)
+    final double[][] m_unit = {{1,0,0,0,0},
+                               {0,1,0,0,0},
+                               {0,0,1,0,0},
+                               {0,0,0,1,0},
+                               {0,0,0,0,1} };
+
+    assertThat(unitMatrix(5), is(equalTo(m_unit)));
+    assertThat(identity(5, 5), is(equalTo(m_unit)));
+ 
+    // test identity with dimensions 3x5 and  5x3
+    final double[][] m_identity3x5 = {{1,0,0,0,0},
+                                      {0,1,0,0,0},
+                                      {0,0,1,0,0}};
+
+    assertThat(identity(3, 5), is(equalTo(m_identity3x5)));   
+    assertThat(identity(5, 3), is(equalTo(transpose(m_identity3x5))));  
+  }
+
+  /**
+  * Testing the getMatrix, getCol and getRow methods of VMath class.
+  */
+  @Test
+  public void testGet() {
+   
+   // testmatrix with dimensions 7x10  where every entry is unique
+   final double[][] m1 = {
+       {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9},
+       { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+       { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
+       { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39},
+       { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49},
+       { 50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
+       { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69} };
+  
+   // array of row indices
+   final int[] r = {0,3,5};
+   // array of column indices
+   final int[] c = {1,3,3,9};
+   
+   // test getMatrix(Matrix, rows, columns)
+   final double[][] sub1 = {
+       {m1[r[0]][c[0]], m1[r[0]][c[1]], m1[r[0]][c[2]], m1[r[0]][c[3]]},
+       {m1[r[1]][c[0]], m1[r[1]][c[1]], m1[r[1]][c[2]], m1[r[1]][c[3]]},
+       {m1[r[2]][c[0]], m1[r[2]][c[1]], m1[r[2]][c[2]], m1[r[2]][c[3]]} };
+  
+   assertThat(getMatrix(m1, r, c) , is(equalTo(sub1))); 
+   
+   // test getMatrix(Matrix, rowstart, rowend , columns)
+   final double[][] sub2 = {
+       {m1[2][c[0]], m1[2][c[1]], m1[2][c[2]], m1[2][c[3]]},
+       {m1[3][c[0]], m1[3][c[1]], m1[3][c[2]], m1[3][c[3]]},
+       {m1[4][c[0]], m1[4][c[1]], m1[4][c[2]], m1[4][c[3]]},
+       {m1[5][c[0]], m1[5][c[1]], m1[5][c[2]], m1[5][c[3]]} };
+   
+   assertThat(getMatrix(m1, 2, 5, c) , is(equalTo(sub2)));
+   
+   // test getMatrix(Matrix, rows, columnstart, columnend)
+   final double[][] sub3 = {
+       {m1[r[0]][4], m1[r[0]][5], m1[r[0]][6], m1[r[0]][7]},
+       {m1[r[1]][4], m1[r[1]][5], m1[r[1]][6], m1[r[1]][7]},
+       {m1[r[2]][4], m1[r[2]][5], m1[r[2]][6], m1[r[2]][7]} };
+  
+   assertThat(getMatrix(m1, r, 4, 7) , is(equalTo(sub3)));
+   
+   // test getMatrix(Matrix, rowstart, rowend, columnstart, columnend)
+   final double[][] sub4 = {
+   {m1[0][6], m1[0][7], m1[0][8]},
+   {m1[1][6], m1[1][7], m1[1][8]},
+   {m1[2][6], m1[2][7], m1[2][8]} };
+    
+   assertThat(getMatrix(m1, 0, 2, 6, 8) , is(equalTo(sub4)));
+   
+   // cross methods and general testing
+   assertThat(getMatrix(m1, 0, getRowDimensionality(m1)-1, 0, getColumnDimensionality(m1)-1) , is(equalTo(m1)));
+  
+   final int[] riter = {3,4,5};
+   final int[] citer = {0,1,2};
+   assertThat(getMatrix(m1, riter[0], riter[riter.length-1], citer[0], citer[citer.length-1]) , is(equalTo(getMatrix(m1, riter, citer))));
+   assertThat(getMatrix(m1, riter, citer[0], citer[citer.length-1]) , is(equalTo(getMatrix(m1, riter, citer))));
+   assertThat(getMatrix(m1, riter[0], riter[riter.length-1], citer) , is(equalTo(getMatrix(m1, riter, citer))));
+  
+  
+   // test getCol and getRow 
+   // we assume transpose to be correct
+   for(int i = 0; i < VMathMatrixTest.TESTMATRIX.length; i++) {
+     double[] v = VMathMatrixTest.TESTMATRIX[i];
+     assertArrayEquals(v, getRow(VMathMatrixTest.TESTMATRIX, i), 0.);
+     assertArrayEquals(v, getCol(transpose(VMathMatrixTest.TESTMATRIX), i), 0.);
+   }
+  
+  }
+  
+  /**
+  * Testing the getRowDimensionality and getColumnDimensionality methods of VMath class.
+  */
+  @Test
+  public void testGetDimensionality() {
+   final double[][] m3 = {
+       {0,0,0,0,0},
+       {0,0,0,0,0},
+       {0,0,0,0,0} };
+   
+   assertEquals(3, getRowDimensionality(m3));
+   assertEquals(3, getColumnDimensionality(transpose(m3) ));
+  
+   assertEquals(5, getColumnDimensionality(m3));
+   assertEquals(5, getRowDimensionality(transpose(m3) ));
+   
+  }
+  
+  /**
+  * Testing the diagonal and getdiagonal methods of VMath class.
+  */
+  @Test
+  public void testDiagonal() {
+   
+   final double[] m = VMathVectorTest.TESTVEC;
+   
+   final double[][] m_diag = {{m[0],0,0,0,0},
+                              {0,m[1],0,0,0},
+                              {0,0,m[2],0,0},
+                              {0,0,0,m[3],0},
+                              {0,0,0,0,m[4]}};
+   assertThat(diagonal(m), is(equalTo(m_diag)));
+   
+   final double[] dia_TEST = { VMathMatrixTest.TESTMATRIX[0][0],VMathMatrixTest.TESTMATRIX[1][1],VMathMatrixTest.TESTMATRIX[2][2],VMathMatrixTest.TESTMATRIX[3][3] };
+   assertArrayEquals(dia_TEST, getDiagonal(VMathMatrixTest.TESTMATRIX), 0.);
+   
+   // if diagonal    is correct this is a test for getDiagonal
+   // if getDiagonal is correct         a test for diagonal
+   final double[] dia = { -2, 0, 1.21, 4, 7};
+   assertArrayEquals(dia, getDiagonal(diagonal(dia)), 0.);
+  }
+  
+  /**
+  * Testing setMatrix, setCol and setRow methods of VMath class. 
+  * 
+  * Since the get-methods of the VMath class are tested independently of set in {@link #testGet()},
+  * we mainly test here via  those get Methods.
+  */
+  @Test
+  public void testSet() {
+  
+   // Initialize testmatrix
+   final double[][] m1 = new double[20][25];
+   
+   // test setMatrix(Matrix, rows, columns, tosetMatrix)
+   // column an row indexes. Assert that you don't address the same column/row twice  TODO: question assert not same colum row in index see below?
+   final int[] row_index = {3, 5,10, 2,11,19, 8};
+   final int[] col_index = {0,10, 7,19, 3, 6, 23, 5,4};
+   // inputmatix with dimesions  7x9
+   final double[][] sub1 = {
+       {  0,  1,  2,  3,  4,  5,  6,  7,  8},
+       { 10, 11, 12, 13, 14, 15, 16, 17, 18},
+       { 20, 21, 22, 23, 24, 25, 26, 27, 28},
+       { 30, 31, 32, 33, 34, 35, 36, 37, 38},
+       { 40, 41, 42, 43, 44, 45, 46, 47, 48},
+       { 50, 51, 52, 53, 54, 55, 56, 57, 58},
+       { 60, 61, 62, 63, 64, 65, 66, 67, 68} };
+   setMatrix(m1, row_index, col_index, sub1);
+   assertThat(getMatrix(m1, row_index, col_index), is(equalTo(sub1)));
+   
+   // test setMatrix(Matrix, rowstart, rowend, columns, tosetMatrix)
+   // testmatix with dimesions  5x9
+   final double[][] sub2 = {        
+       {  0,  1,  2,  3,  4,  5,  6,  7,  8},
+       { 10, 11, 12, 13, 14, 15, 16, 17, 18},
+       { 20, 21, 22, 23, 24, 25, 26, 27, 28},
+       { 30, 31, 32, 33, 34, 35, 36, 37, 38},
+       { 40, 41, 42, 43, 44, 45, 46, 47, 48}, };
+   setMatrix(m1, 4, 8, col_index, sub2);
+   assertThat(getMatrix(m1, 4, 8, col_index), is(equalTo(sub2)));
+   
+   // clear matrix for next test
+   clear(m1);
+   
+   // test setMatrix(Matrix, rows, colstart, colend, tosetMatrix)
+   // testmatix with dimesions  7x15
+   final double[][] sub3 = {
+       {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,  11,  12,  13,  14},
+       { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 110, 111, 112, 113, 114},
+       { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 212, 213, 214},
+       { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 310, 311, 312, 313, 314},
+       { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 410, 411, 412, 413, 414},
+       { 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 510, 511, 512, 513, 514},
+       { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 610, 611, 612, 613, 614}, };
+   
+   setMatrix(m1, row_index, 2, 16, sub3);
+   assertThat(getMatrix(m1, row_index, 2, 16), is(equalTo(sub3)));
+  
+   // test setMatrix(Matrix, rowstart, rowend, colstart, colend, tosetMatrix)
+   // testmatix with dimesions  7x3
+   final double[][] sub4 = {
+       {  0,  1,  2},
+       { 10, 11, 12},
+       { 20, 21, 22},
+       { 30, 31, 32},
+       { 40, 41, 42},
+       { 50, 51, 52},
+       { 60, 61, 62}, };
+   
+   setMatrix(m1, 0, 6, 16, 18, sub4);
+   assertThat(getMatrix(m1, 0, 6, 16, 18), is(equalTo(sub4)));
+   
+   // check that setting a full matrix
+   final double[][] m2 = VMathMatrixTest.TESTMATRIX;
+   final double[][] res1 = new double[getRowDimensionality(m2)][getColumnDimensionality(m2)]; 
+   setMatrix(res1, 0, getRowDimensionality(m2)-1, 0, getColumnDimensionality(m2)-1, m2);
+   assertThat(res1, is(equalTo(m2)));
+   
+   // check that different the different setMatrix methods to the same if exchangeable
+   final int[] riter = {0,1,2,3}, citer = {0,1,2,3,4};
+   final double[][] res2 = new double[riter.length][citer.length];
+   
+   setMatrix(res2, riter[0], riter[riter.length-1], citer[0], citer[citer.length-1], m2);
+   assertThat(res2, is(equalTo( getMatrix(m2, riter, citer) ))); 
+   clear(res2);
+   
+   setMatrix(res2, riter, citer[0], citer[citer.length-1], m2);
+   assertThat(res2, is(equalTo( getMatrix(m2, riter, citer) ))); 
+   clear(res2);
+   
+   setMatrix(res2, riter[0], riter[riter.length-1], citer, m2);
+   assertThat(res2, is(equalTo( getMatrix(m2, riter, citer) ))); 
+   
+   
+   // testing setCol and setRow
+   final double[] col = VMathVectorTest.TESTVEC;
+   final double[] row = VMathVectorTest.TESTVEC;
+   
+   final double[][] m3 = new double[row.length][col.length];
+   
+   
+   for(int c = 0; c < getRowDimensionality(m3); c++) {
+     // set column c of m to col
+     setCol(m3, c, col);
+     // assert that column c of m is col via getCol
+     assertArrayEquals(col, getCol(m3, c), 0.);
+   }
+   
+   for(int r = 0; r < getColumnDimensionality(m3); r++) {
+     // set column c of m to col
+     setRow(m3, r, row);
+     // assert that row r of m is row via getRow
+     assertArrayEquals(row, getRow(m3, r), 0.);
+   }
+  
+  }
+  
+  /**
+  * Testing the appendColums method of VMath class.
+  * 
+  * FIXME: Question maybe better name appendMatrix? or document to complicated?
+  */
+  @Test
+  public void testAppendColums() {
+   
+   final double[][] m1 = {
+       {1,2,3},
+       {1,2,3},
+       {1,2,3} };
+   
+   final double[][] m2 = {
+       {4,5,6,7},
+       {4,5,6,7},
+       {4,5,6,7} };
+  
+   final double[][] m1_res = {
+       {m1[0][0], m1[0][1], m1[0][2],   m2[0][0], m2[0][1], m2[0][2], m2[0][3]},
+       {m1[1][0], m1[1][1], m1[1][2],   m2[1][0], m2[1][1], m2[1][2], m2[1][3]},
+       {m1[2][0], m1[2][1], m1[2][2],   m2[2][0], m2[2][1], m2[2][2], m2[2][3]},
+   };
+   
+   assertThat(appendColumns(m1, m2), is(equalTo(m1_res)));
+  }
+  
+  /**
+   * Testing that *Equals vector-operations of the {@link VMath} class work in place and testing that other vector-operations create a new instance.
+   * 
+   * Tests of vector-methods where the class of the instance returned differs form class of input method are reasonably omitted,
+   * when testing reference e.g. transposed.
+   * <p>
+   * For a complete list of tested methods: <br>
+   * {@link #testMinus}, {@link #testNormalize}, {@link #testPlus}, {@link #testTimes}, {@link #testRotate90Equals} 
+   */
+  @Test
+  public void testReference() {
+
+    final double[][] m1 = {{1}};
+    
+    // testing the methods as in testUnitMatrix omitted
+    
+    // testing the appendColums method as it is now: not working in place
+    assertNotSame(m1, appendColumns(m1, m1));
+    
+    // testing the methods as in Diagonal omitted
+    
+    // testing that methods as in testGet return new copys of the submatrix to get
+    final double[] inner = {1};
+    final double[][] m2 = {inner};
+    // assert that inner reference is possible
+    assertSame(inner, m2[0]);
+    
+    final int[] rows = {0}, cols = {0};
+    final int c0 = 0, c1 = 0, r0 = 0, r1 = 0;
+
+    assertNotSame(inner, getMatrix(m2, rows, cols)[0]);
+    assertNotSame(inner, getMatrix(m2, r0, r1, cols)[0]);
+    assertNotSame(inner, getMatrix(m2, rows, c0, c1)[0]);
+    assertNotSame(inner, getMatrix(m2, r0, r1, c0, c1)[0]);
+    
+    assertNotSame(inner, getRow(m2, r0));
+    // getCol reasonably omitted
+    
+    // testing that the methods as in testSet, set the rows in the submatrix to refer to a new instance.
+    final double[][] m3 = new double[1][1];
+    
+    setMatrix(m3, rows, cols, m2);
+    assertNotSame(inner, m3[0]);
+    setMatrix(m3, r0, r1, cols, m2);
+    assertNotSame(inner, m3[0]);
+    setMatrix(m3, rows, c0, c1, m2);
+    assertNotSame(inner, m3[0]);
+    setMatrix(m3, r0, r1, c0, c1, m2);
+    assertNotSame(inner, m3[0]);
+    
+    setRow(m3, r0, inner);
+    assertNotSame(inner, m3[0]);
+    // setCol reasonably omitted
+    
+    // testing the methods as in testGetDimensionality omitted
+  }
+  
+  /**
+   * Testing that correct Error is raised when dimension of the input data mismatch the needs of the method.
+   */
+  @Test
+  public void testDimensionMismatch() {
+    
+    // unitVector only use index out of Bound exception in assertDimensionMismatch
+    assertDimensionMismatch("", () -> unitVector(0,0));
+    assertDimensionMismatch("", () -> unitVector(4,4));
+    assertDimensionMismatch("", () -> unitVector(10,15));
+    
+    // testing the methods as in Diagonal omitted
+    
+    // testing the methods as in testGet 
+    final int[] r = {5}, c = {5};
+    final int r1 = 5, c1 = 5;
+    assertDimensionMismatch("", () -> getMatrix(unitMatrix(2), r, c ));
+    assertDimensionMismatch("", () -> getMatrix(unitMatrix(2), 0, r1, c));
+    assertDimensionMismatch("", () -> getMatrix(unitMatrix(2), r, 0, c1));
+    assertDimensionMismatch("", () -> getMatrix(unitMatrix(2), 0, r1, 0, c1));
+    assertDimensionMismatch("", () -> getCol(unitMatrix(2), c1));
+    assertDimensionMismatch("", () -> getRow(unitMatrix(2), r1));
+        
+    // testing the methods as in testSet
+    assertDimensionMismatch("", () -> setMatrix(unitMatrix(2), r, c, unitMatrix(6) ));
+    assertDimensionMismatch("", () -> setMatrix(unitMatrix(2), 0, r1, c, unitMatrix(6)));
+    assertDimensionMismatch("", () -> setMatrix(unitMatrix(2), r, 0, c1, unitMatrix(6)));
+    assertDimensionMismatch("", () -> setMatrix(unitMatrix(2), 0, r1, 0, c1, unitMatrix(6)));
+    assertDimensionMismatch("Dimensionalities do not agree.", () -> setCol(unitMatrix(2), c1, unitVector(6,0)));
+    assertDimensionMismatch("Dimensionalities do not agree.", () -> setRow(unitMatrix(2), r1, unitVector(6,0)));
+    
+    // testing the methods as in testGetDimensionality omitted
+  }
+
+  
+  }
