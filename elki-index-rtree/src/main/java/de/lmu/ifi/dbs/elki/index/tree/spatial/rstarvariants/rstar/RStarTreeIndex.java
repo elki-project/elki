cb4c38c6a66b0f4473aa6c20375cc4377c2a1e3e
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2018-07-25 10:25:36 +0200

    Remove bad special handling of relation.size()==1
    Closes elki-project/elki#47

--- elki-index-rtree/src/main/java/de/lmu/ifi/dbs/elki/index/tree/spatial/rstarvariants/rstar/RStarTreeIndex.java
+++ elki-index-rtree/src/main/java/de/lmu/ifi/dbs/elki/index/tree/spatial/rstarvariants/rstar/RStarTreeIndex.java
@@ -2,7 +2,7 @@
  * This file is part of ELKI:
  * Environment for Developing KDD-Applications Supported by Index-Structures
  *
- * Copyright (C) 2017
+ * Copyright (C) 2018
  * ELKI Development Team
  *
  * This program is free software: you can redistribute it and/or modify
@@ -109,7 +109,7 @@ public class RStarTreeIndex<O extends NumberVector> extends RStarTree implements
    */
   @Override
   public void insertAll(DBIDs ids) {
-    if(ids.isEmpty() || (ids.size() == 1)) {
+    if(ids.isEmpty()) {
       return;
     }
 
