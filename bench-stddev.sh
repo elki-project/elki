d0b8434f579b24d1ea6de9f9b17718f23f0e4a20
Erich Schubert <erich.schubert@tu-dortmund.de>
2019-08-14 10:21:54 +0200

    Leftover file that should not have been committed.

--- bench-stddev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-$ELKI cli -time \
--dbc.in coords-wikidata.csv.gz \
--parser.quote \% \
--dbc.filter z,z,z,z,z,z,z,z,z,z \
--algorithm NullAlgorithm \
--resulthandler DiscardResultHandler
