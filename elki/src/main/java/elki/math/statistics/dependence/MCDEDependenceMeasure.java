b0c6277d2a252dbd5f6182ff76def613c0b8092d
alanmazankiewicz <alan.mazankiewicz@live.de>
2019-09-11 18:08:56 +0200

    added back MCDE related files after merge with upstream, adjusted packages and imports of MCDE files accordingly, adjusted Parametrizer in McdeMwpDependenceMeasure to new Parametrizer class interface after merge, added license to MCDE files, added LESS_THAN_ONE_DOUBLE constrain to McdeMwpDependenceMeasure Parametrizer class

---
+++ elki/src/main/java/elki/math/statistics/dependence/MCDEDependenceMeasure.java
@@ -0,0 +1,192 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ *
+ * Copyright (C) 2019
+ * ELKI Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.math.statistics.dependence;
+
+import elki.utilities.datastructures.arraylike.NumberArrayAdapter;
+import elki.utilities.documentation.Reference;
+import elki.utilities.exceptions.AbortException;
+import elki.utilities.random.RandomFactory;
+import elki.utils.containers.RankStruct;
+import java.util.Arrays;
+import java.util.Random;
+
+
+/**
+ * Implementation of bivariate Monte Carlo Density Estimation as described in
+ * Edouard Fouché & Klemens Böhm<br>
+ * Monte Carlo Density Estimation<br>
+ * Proc. 2019 ACM Int. Conf. on Scientific and Statistical Database Management (SSDBM 2019)
+ *
+ * This is an abstract class. In order to use MCDE extend it and implement an appropriate statistical test that
+ * returns a p-value and index structure for efficient computation of the statistical test.
+ *
+ * The instantiation of MCDE based on the Mann-Whitney U test is called MWP (as described in the paper).
+ * See McdeMwpDependenceMeasure.
+ *
+ * @author Alan Mazankiewicz
+ * @author Edouard Fouché
+ */
+
+@Reference(authors = "Edouard Fouché, Klemens Böhm", //
+        title = "Monte Carlo Density Estimation", //
+        booktitle = "Proc. 2019 ACM Int. Conf. on Scientific and Statistical Database Management (SSDBM 2019)",
+        url = "https://doi.org/10.1145/3335783.3335795", //
+        bibkey = "DBLP:conf/ssdbm/FoucheB19")
+
+
+public abstract class MCDEDependenceMeasure<R extends RankStruct> extends AbstractDependenceMeasure {
+    /**
+     * Monte-Carlo iterations.
+     */
+    protected int m = 50;
+
+    /**
+     * Expected share of instances in slice (independent dimensions).
+     */
+    protected double alpha = 0.5;
+
+    /**
+     * Share of instances in marginal restriction (reference dimension).
+     * Note that in the original paper alpha = beta and as such there is no explicit distinction between the parameters.
+     */
+    protected double beta = 0.5;
+
+    /**
+     * Random generator.
+     */
+    protected RandomFactory rnd;
+
+    /**
+     * Constructor.
+     *
+     * @param m Monte-Carlo iterations
+     * @param alpha Expected share of instances in slice (under independence)
+     * @param beta Share of instances in marginal restriction (reference dimension)
+     * @param rnd Random source
+     */
+    public MCDEDependenceMeasure(int m, double alpha, double beta, RandomFactory rnd){
+        if((beta > 1.0) || beta <= 0.0) throw new AbortException("beta must be in ]0;1]");
+        if((alpha >= 1.0) || alpha <= 0.0) throw new AbortException("alpha must be in ]0;1[ ");
+        if(m < 1) throw new AbortException("m must be > 0");
+
+        this.m = m;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.rnd = rnd;
+    }
+
+    /**
+     * Overloaded wrapper for corrected_ranks()
+     */
+    protected <A> R[] corrected_ranks(final NumberArrayAdapter<?, A> adapter, final A data, int len) {
+        return corrected_ranks(adapter, data, sortedIndex(adapter, data, len));
+    }
+
+    /**
+     * Subclass must implement computation of corrected rank index.
+     *
+     * @param adapter ELKI NumberArrayAdapter Subclass
+     * @param data One dimensional array containing one dimension of the data
+     * @param idx Return value of sortedIndex()
+     * @return Array of RankStruct, acting as rank index
+     */
+    protected abstract <A> R[] corrected_ranks(final NumberArrayAdapter<?, A> adapter, final A data, int[] idx);
+
+    /**
+     * Subclass must implement the computation of the statistical test, based on the slicing scheme.
+     *
+     * @param len No of data instances
+     * @param slice An array of boolean resulting from a random slice
+     * @param corrected_ranks the precomputed index structure for the reference dimension
+     * @return a 1 - p-value
+     */
+    protected abstract double statistical_test(int len, boolean[] slice, R[] corrected_ranks);
+
+    /**
+     * Data Slicing
+     *
+     * @param len No of data instances
+     * @param nonRefIndex Index (see correctedRank()) computed for the dimension that is not the reference dimension
+     * @return Array of booleans that states which instances are part of the slice
+     */
+    protected boolean[] randomSlice(int len, R[] nonRefIndex){
+        final Random random = rnd.getSingleThreadedRandom();
+        boolean slice[] = new boolean[len];
+        Arrays.fill(slice, Boolean.TRUE);
+
+        final int slizeSize = (int) Math.ceil(Math.pow(this.alpha, 1.0) * len);
+        final int start = random.nextInt(len - slizeSize);
+        final int end = start + slizeSize;
+
+        for(int j = 0; j < start; j++){
+            slice[nonRefIndex[j].index] = false;
+        }
+
+        for(int j = end; j < len; j++){
+            slice[(int) nonRefIndex[j].index] = false;
+        }
+
+        return slice;
+    }
+
+    /**
+     * Implements dependence from DependenceMeasure superclass. Corresponds to Algorithm 4 in source paper.
+     * @param adapter1 First data adapter
+     * @param data1 First data set
+     * @param adapter2 Second data adapter
+     * @param data2 Second data set
+     * @param <A> Numeric data type, such as double
+     * @param <B> Numeric data type, such as double
+     * @return MCDE result
+     */
+    @Override
+    public <A, B> double dependence(final NumberArrayAdapter<?, A> adapter1, final A data1, final NumberArrayAdapter<?, B> adapter2, final B data2){
+        final Random random = rnd.getSingleThreadedRandom();
+        final int len = adapter1.size(data1);
+
+        if(len != adapter2.size(data2))
+            throw new AbortException("Size of both arrays must match!");
+
+        final R[] index_0 = corrected_ranks(adapter1, data1, len);
+        final R[] index_1 = corrected_ranks(adapter2, data2, len);
+
+        double mwp = 0;
+      
+        for(int i = 0; i < this.m; i++){
+            int r = random.nextInt(2);
+            R[] ref_index;
+            R[] other_index;
+
+            if(r == 1) {
+                ref_index = index_1;
+                other_index = index_0;
+            }
+            else {
+                ref_index = index_0;
+                other_index = index_1;
+            }
+
+            mwp += statistical_test(len, randomSlice(len, other_index), ref_index);
+        }
+        return mwp / m;
+    }
+
+}
---
+++ elki/src/test/java/elki/math/statistics/dependence/McdeMwpDependenceMeasureTest.java
@@ -0,0 +1,136 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ *
+ * Copyright (C) 2019
+ * ELKI Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.math.statistics.dependence;
+
+import elki.utilities.ELKIBuilder;
+import elki.utilities.datastructures.arraylike.DoubleArrayAdapter;
+import elki.utilities.datastructures.arraylike.NumberArrayAdapter;
+import elki.utilities.random.FastNonThreadsafeRandom;
+import elki.utilities.random.RandomFactory;
+import elki.utils.containers.MwpIndex;
+import org.junit.Test;
+import java.util.Random;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Alan Mazankiewicz
+ * @author Edouard Fouché
+ */
+
+public class McdeMwpDependenceMeasureTest {
+
+    @Test
+    public void runTest() {
+
+        // independent
+        double[] indi1 = new double[1000];
+        double[] indi2 = new double[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            indi1[i] = Math.random();
+            indi2[i] = Math.random();
+        }
+
+        // linear
+        double[] lin1 = new double[1000];
+        double[] lin2 = new double[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            lin1[i] = i;
+            lin2[i] = i * 2;
+        }
+
+        // linear with small noise
+        Random r = new FastNonThreadsafeRandom((long) Math.random());
+        double[] small_noise_lin1 = new double[1000];
+        double[] small_noise_lin2 = new double[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            small_noise_lin1[i] = i;
+            small_noise_lin2[i] = (i * 2) + r.nextGaussian() * 100;
+        }
+
+
+        double[] more_noise_lin1 = new double[1000];
+        double[] more_noise_lin2 = new double[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            more_noise_lin1[i] = i;
+            more_noise_lin2[i] = (i * 2) + r.nextGaussian() * 500;
+        }
+
+        NumberArrayAdapter adapter = DoubleArrayAdapter.STATIC;
+        RandomFactory rnd = new RandomFactory((long) Math.random());
+
+        McdeMwpDependenceMeasure mwp = new ELKIBuilder<>(McdeMwpDependenceMeasure.class) //
+                .with(McdeMwpDependenceMeasure.Par.M_ID, 1000) //
+                .build();
+
+        test_result(indi1, indi2, 0.65, 0.35, 0.67, 0.33, adapter, mwp);
+        test_result(lin1, lin2, 1.0, 0.99, 1.0, 0.97, adapter, mwp);
+        test_result(small_noise_lin1, small_noise_lin2, 1.0, 0.96, 1.0, 0.85, adapter, mwp);
+        test_result(more_noise_lin1, more_noise_lin2, 1.0, 0.85, 1.0, 0.83, adapter, mwp);
+
+        test_rankIndex(adapter, mwp);
+    }
+
+    public static void test_result(double[] data1, double[] data2, double strict_upper, double strict_lower,
+                                   double lax_upper, double lax_lower, NumberArrayAdapter adapter, McdeMwpDependenceMeasure mwp) {
+        double total_res = 0;
+        for (int i = 0; i < 100; i++) {
+            double res = mwp.dependence(adapter, data1, adapter, data2);
+            total_res += res;
+            assertTrue("MWP result out of acceptable range. Result: " + res + ". Lax upper bound: " + lax_upper, (res <= lax_upper));
+            assertTrue("MWP result out of acceptable range. Result: " + res + ". Lax lower bound: " + lax_lower, (res >= lax_lower));
+        }
+        total_res /= 100;
+        assertTrue("MWP result out of acceptable range. Result: " + total_res + ". Strict upper bound: " + strict_upper, (total_res <= strict_upper));
+        assertTrue("MWP result out of acceptable range. Result: " + total_res + ". Strict lower bound: " + strict_lower, (total_res >= strict_lower));
+    }
+
+    public static void test_rankIndex(NumberArrayAdapter adapter, McdeMwpDependenceMeasure mwp)
+    {
+        double[] input_no_duplicates = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        MwpIndex[] result_no_duplicates = mwp.corrected_ranks(adapter, input_no_duplicates, 10);
+
+        for(int i = 0; i < 10; i++){
+            assertTrue("Error in corrected_rank index construction for MCDE MWP", input_no_duplicates[i] == result_no_duplicates[i].adjusted);
+        }
+
+        double[] input_duplicates_middle = {0, 1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9};
+        MwpIndex[] output_duplicates_middle = mwp.corrected_ranks(adapter, input_duplicates_middle, 13);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_middle[1].adjusted, 1.5, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_middle[2].adjusted, 1.5, 0);
+        assertEquals( "Error in corrected_rank index construction for MCDE MWP", output_duplicates_middle[6].adjusted, 7.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_middle[7].adjusted, 7.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_middle[8].adjusted, 7.0, 0);
+
+        double[] input_duplicates_start_end = {0, 0, 0, 2, 3, 4, 5, 5, 5, 6, 9, 9, 9};
+        MwpIndex[] output_duplicates_start_end = mwp.corrected_ranks(adapter, input_duplicates_start_end, 13);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[0].adjusted, 1.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[1].adjusted, 1.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[2].adjusted, 1.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[10].adjusted, 11.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[11].adjusted, 11.0, 0);
+        assertEquals("Error in corrected_rank index construction for MCDE MWP", output_duplicates_start_end[12].adjusted, 11.0, 0);
+    }
+}
