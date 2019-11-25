3a12ae91937e8ef294bcf9d15b26992e46271afd
ARandomTree <robert.gehde@udo.edu>
2019-11-25 14:56:35 +0100

    Added SetMatchingPurity test

---
+++ elki-clustering/src/test/java/elki/evaluation/clustering/SetMatchingPurityTest.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ * 
+ * Copyright (C) 2019
+ * ELKI Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.evaluation.clustering;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import elki.database.ids.DBIDRange;
+import elki.database.ids.DBIDUtil;
+
+/**
+ * Validate {@link SetMatchingPurity} with the SKlearn example
+ * 
+ * @author Robert Gehde
+ *
+ */
+public class SetMatchingPurityTest {
+
+  /**
+   * Validate {@link SetMatchingPurity} with the SKlearn example
+   */
+  @Test
+  public void test() {
+    int[] a = { 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
+    int[] b = { 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 3, 1, 3, 3, 3, 2, 2 };
+    DBIDRange ids = DBIDUtil.generateStaticDBIDRange(a.length);
+    SetMatchingPurity smp = new SetMatchingPurity(new ClusterContingencyTable(false, false, EntropyTest.makeClustering(ids.iter(), a), EntropyTest.makeClustering(ids.iter(), b)));
+
+    assertEquals("Similarity not as expected", 0.705882352941176, smp.purity(), 1e-15);
+    assertEquals("Inv. Similarity not as expected", .705882352941176, smp.inversePurity(), 1e-15);
+    assertEquals("F1-Measure not as expected", 0.705882352941176, smp.f1Measure(), 1e-15);
+    assertEquals("F1-Measure (first) not as expected", 0.707390648567119, smp.fMeasureFirst(), 1e-15);
+    assertEquals("F1-Measure (second) not as expected", 0.704374057315234, smp.fMeasureSecond(), 1e-15);
+  }
+}
