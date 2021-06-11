2280cc8b678513572aa9bcb1c9f0b3d93957116d
Andreas Lang <andreas.lang@tu-dortmund.de>
2021-06-11 12:15:33 +0200

    Single Assingment Sperical k-Means as baseline

---
+++ elki-clustering/src/main/java/elki/clustering/kmeans/spherical/SphericalSingleAssignmentKMeans.java
@@ -0,0 +1,118 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ * 
+ * Copyright (C) 2020
+ * ELKI Development Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.clustering.kmeans.spherical;
+
+import elki.clustering.kmeans.initialization.KMeansInitialization;
+import elki.data.Clustering;
+import elki.data.NumberVector;
+import elki.data.model.KMeansModel;
+import elki.database.relation.Relation;
+import elki.logging.Logging;
+import elki.utilities.optionhandling.parameterization.Parameterization;
+
+/**
+ * Pseudo-k-Means variations, that assigns each object to the nearest center.
+ *
+ * @author Alexander Voß
+ * @author Erich Schubert
+ * @author Andreas Lang
+ *
+ * @navassoc - - - KMeansModel
+ *
+ * @param <V> vector datatype
+ */
+public class SphericalSingleAssignmentKMeans<V extends NumberVector> extends SphericalKMeans<V> {
+  /**
+   * Class logger
+   */
+  private static final Logging LOG = Logging.getLogger(SphericalSingleAssignmentKMeans.class);
+
+  /**
+   * Constructor.
+   *
+   * @param k Number of clusters
+   * @param maxiter Maximum number of iterations
+   * @param initializer Initialization class
+   */
+  public SphericalSingleAssignmentKMeans(int k, KMeansInitialization initializer) {
+    super(k, 1, initializer);
+  }
+
+  @Override
+  public Clustering<KMeansModel> run(Relation<V> relation) {
+    Instance instance = new Instance(relation, initialMeans(relation));
+    instance.run(1);
+    return instance.buildResult(false, relation);
+  }
+
+  @Override
+  protected Logging getLogger() {
+    return LOG;
+  }
+
+  /**
+   * Instance for a particular data set.
+   * 
+   * @author Alexander Voß
+   * @author Andreas Lang
+   */
+  public static class Instance extends SphericalKMeans.Instance {
+    /**
+     * Constructor.
+     *
+     * @param relation Data relation
+     * @param means Initial cluster means
+     */
+    public Instance(Relation<? extends NumberVector> relation, double[][] means) {
+      super(relation, means);
+    }
+
+    @Override
+    public int iterate(int iteration) {
+      assert (iteration == 1);
+      return -assignToNearestCluster();
+    }
+
+    @Override
+    protected Logging getLogger() {
+      return LOG;
+    }
+  }
+
+  /**
+   * Parameterization class.
+   *
+   * @author Alexander Voß
+   * @author Andreas Lang
+   */
+  public static class Par<V extends NumberVector> extends SphericalKMeans.Par<V> {
+    @Override
+    public void configure(Parameterization config) {
+      getParameterK(config);
+      getParameterInitialization(config);
+    }
+
+    @Override
+    public SphericalSingleAssignmentKMeans<V> make() {
+      return new SphericalSingleAssignmentKMeans<V>(k, initializer);
+    }
+  }
+}
---
+++ elki-clustering/src/test/java/elki/clustering/kmeans/spherical/SphericalSingleAssignmentKMeansTest.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ *
+ * Copyright (C) 2019
+ * ELKI Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.clustering.kmeans.spherical;
+
+import org.junit.Test;
+
+import elki.clustering.AbstractClusterAlgorithmTest;
+import elki.clustering.kmeans.KMeans;
+import elki.clustering.kmeans.SingleAssignmentKMeans;
+import elki.data.Clustering;
+import elki.data.DoubleVector;
+import elki.database.Database;
+import elki.datasource.AbstractDatabaseConnection;
+import elki.datasource.filter.normalization.instancewise.LengthNormalization;
+import elki.distance.CosineDistance;
+import elki.distance.SqrtCosineDistance;
+import elki.utilities.ELKIBuilder;
+import elki.utilities.optionhandling.parameterization.ListParameterization;
+
+/**
+ * Regression test spherical k-means (although on not very well suited data).
+ *
+ * @author Erich Schubert
+ */
+public class SphericalSingleAssignmentKMeansTest extends AbstractClusterAlgorithmTest {
+  @Test
+  public void testSphericalKMeans() {
+    Database db = makeSimpleDatabase(UNITTEST + "different-densities-2d-no-noise.ascii", 1000, new ListParameterization() //
+        .addParameter(AbstractDatabaseConnection.Par.FILTERS_ID, LengthNormalization.class));
+    Clustering<?> result = new ELKIBuilder<SphericalSingleAssignmentKMeans<DoubleVector>>(SphericalSingleAssignmentKMeans.class) //
+        .with(KMeans.K_ID, 5) //
+        .with(KMeans.SEED_ID, 1) //
+        .build().autorun(db);
+    assertFMeasure(db, result, 0.862175);
+    assertClusterSizes(result, new int[] { 128, 198, 200, 202, 272 });
+    // Results should be similar to:
+    result = new ELKIBuilder<SingleAssignmentKMeans<DoubleVector>>(SingleAssignmentKMeans.class) //
+        .with(KMeans.DISTANCE_FUNCTION_ID, SqrtCosineDistance.class) //
+        .with(KMeans.K_ID, 5) //
+        .with(KMeans.SEED_ID, 1) //
+        .build().autorun(db);
+    assertFMeasure(db, result, 0.862175);
+    assertClusterSizes(result, new int[] { 128, 198, 200, 202, 272 });
+    // Results should be similar to:
+    result = new ELKIBuilder<SingleAssignmentKMeans<DoubleVector>>(SingleAssignmentKMeans.class) //
+        .with(KMeans.DISTANCE_FUNCTION_ID, CosineDistance.class) //
+        .with(KMeans.K_ID, 5) //
+        .with(KMeans.SEED_ID, 1) //
+        .build().autorun(db);
+    assertFMeasure(db, result, 0.862175);
+    assertClusterSizes(result, new int[] { 128, 198, 200, 202, 272 });
+  }
+}
