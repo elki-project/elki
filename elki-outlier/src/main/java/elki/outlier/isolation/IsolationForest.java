27efefe206089413fa5244e9113fbe07c14005b7
BraulioSanchez <braulio.sanchez@usp.br>
2021-07-12 08:17:49 -0300

    Some fixes in IsolationForest source code

--- elki-outlier/src/main/java/elki/outlier/isolation/IsolationForest.java
+++ elki-outlier/src/main/java/elki/outlier/isolation/IsolationForest.java
@@ -21,6 +21,7 @@
 package elki.outlier.isolation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -41,12 +42,15 @@ import elki.database.relation.Relation;
 import elki.database.relation.RelationUtil;
 import elki.logging.Logging;
 import elki.logging.progress.FiniteProgress;
+import elki.logging.progress.StepProgress;
 import elki.math.DoubleMinMax;
+import elki.math.MathUtil;
 import elki.outlier.OutlierAlgorithm;
 import elki.result.outlier.BasicOutlierScoreMeta;
 import elki.result.outlier.OutlierResult;
 import elki.result.outlier.OutlierScoreMeta;
 import elki.utilities.Alias;
+import elki.utilities.datastructures.arraylike.IntegerArray;
 import elki.utilities.documentation.Description;
 import elki.utilities.documentation.Reference;
 import elki.utilities.documentation.Title;
@@ -116,19 +120,18 @@ public class IsolationForest implements OutlierAlgorithm {
    * Constructor.
    *
    * @param numTrees
-   * @param subSamplingSize
+   * @param subsampleSize
    * @param rnd
    */
-  public IsolationForest(int numTrees, int subSamplingSize, RandomFactory rnd) {
+  public IsolationForest(int numTrees, int subsampleSize, RandomFactory rnd) {
     this.numTrees = numTrees;
-    this.subsampleSize = subSamplingSize;
+    this.subsampleSize = subsampleSize;
     this.rnd = rnd;
   }
 
   public OutlierResult run(Database db, Relation<? extends NumberVector> relation) {
     // Output data storage
     WritableDoubleDataStore scores = DataStoreUtil.makeDoubleStorage(relation.getDBIDs(), DataStoreFactory.HINT_DB);
-    FiniteProgress prog = LOG.isVerbose() ? new FiniteProgress("IsolationForest progress", relation.size(), LOG) : null;
     // Track minimum and maximum scores
     DoubleMinMax minmax = new DoubleMinMax();
 
@@ -138,6 +141,10 @@ public class IsolationForest implements OutlierAlgorithm {
     }
 
     {
+      StepProgress stepprog = LOG.isVerbose() ? new StepProgress(2) : null;
+
+      LOG.beginStep(stepprog, 1, "Generating trees.");
+      FiniteProgress prog1 = LOG.isVerbose() ? new FiniteProgress("IsolationForest progress", numTrees, LOG) : null;
       final Random random = rnd.getSingleThreadedRandom();
       // Generate trees
       trees = new ArrayList<>(numTrees);
@@ -148,19 +155,22 @@ public class IsolationForest implements OutlierAlgorithm {
             random, //
             0, //
             (int) FastMath.ceil(FastMath.log(relation.size()) / FastMath.log(2)), //
+            RelationUtil.dimensionality(relation), //
             relation));
-        LOG.incrementProcessed(prog);
+        LOG.incrementProcessed(prog1);
       }
-      LOG.ensureCompleted(prog);
+      LOG.ensureCompleted(prog1);
 
+      LOG.beginStep(stepprog, 2, "Computing distribution of scores.");
+      FiniteProgress prog2 = LOG.isVerbose() ? new FiniteProgress("IsolationForest scores", relation.size(), LOG) : null;
       // Iterate over all objects
       for(DBIDIter iter = relation.iterDBIDs(); iter.valid(); iter.advance()) {
         final double score = distributionForInstance(relation.get(iter));
         scores.putDouble(iter, score);
         minmax.put(score);
-        LOG.incrementProcessed(prog);
+        LOG.incrementProcessed(prog2);
       }
-      LOG.ensureCompleted(prog);
+      LOG.ensureCompleted(prog2);
     }
 
     // Wrap the result in the standard containers
@@ -169,7 +179,7 @@ public class IsolationForest implements OutlierAlgorithm {
         minmax.getMin(), minmax.getMax(),
         // Theoretical minimum and maximum: no variance to infinite variance
         0, Double.POSITIVE_INFINITY);
-    DoubleRelation rel = new MaterializedDoubleRelation("iforest anomaly score", relation.getDBIDs(), scores);
+    DoubleRelation rel = new MaterializedDoubleRelation("IsolationForest", relation.getDBIDs(), scores);
     return new OutlierResult(meta, rel);
   }
 
@@ -181,13 +191,12 @@ public class IsolationForest implements OutlierAlgorithm {
     if(n <= 1.0) {
       return 0;
     }
-    return 2 * (FastMath.log(n - 1) + 0.5772156649) - (2 * (n - 1) / n);
+    return 2 * (FastMath.log(n - 1) + MathUtil.EULERMASCHERONI) - (2 * (n - 1) / n);
   }
 
   /**
    * Returns distribution of scores.
    */
-  // protected double[] distributionForInstance(V vector) {
   protected double distributionForInstance(NumberVector vector) {
     double avgPathLength = 0;
     for(Tree m_tree : trees) {
@@ -196,8 +205,6 @@ public class IsolationForest implements OutlierAlgorithm {
     avgPathLength /= trees.size();
 
     return FastMath.pow(2, -avgPathLength / c(subsampleSize));
-
-    // return scores;
   }
 
   /**
@@ -232,8 +239,10 @@ public class IsolationForest implements OutlierAlgorithm {
      * @param r Random generator
      * @param height
      * @param maxHeight
+     * @param dim Dimensionality of the relation.
+     * @param relation Relation to index
      */
-    protected Tree(ModifiableDBIDs data, Random r, int height, int maxHeight, Relation<? extends NumberVector> relation) {
+    protected Tree(ModifiableDBIDs data, Random r, int height, int maxHeight, int dim, Relation<? extends NumberVector> relation) {
       // Set size of node
       size = data.size();
 
@@ -243,72 +252,71 @@ public class IsolationForest implements OutlierAlgorithm {
       }
 
       // Compute mins and maxs and eligible attributes
-      final int dim = RelationUtil.dimensionality(relation);
-      double[][] minmax;
-      {
-        double[] mins = new double[dim], maxs = new double[dim];
-        for(int i = 0; i < dim; i++) {
-          mins[i] = Double.MAX_VALUE;
-          maxs[i] = -Double.MAX_VALUE;
+      double[] mins = new double[dim], maxs = new double[dim];
+      Arrays.fill(mins, Double.MAX_VALUE);
+      Arrays.fill(maxs, -Double.MAX_VALUE);
+      for(DBIDIter iditer = data.iter(); iditer.valid(); iditer.advance()) {
+        final NumberVector o = relation.get(iditer);
+        for(int d = 0; d < dim; d++) {
+          final double v = o.doubleValue(d);
+          mins[d] = (v < mins[d]) ? v : mins[d];
+          maxs[d] = (v > maxs[d]) ? v : maxs[d];
         }
-        for(DBIDIter iditer = data.iter(); iditer.valid(); iditer.advance()) {
-          final NumberVector o = relation.get(iditer);
-          for(int d = 0; d < dim; d++) {
-            final double v = o.doubleValue(d);
-            mins[d] = Math.min(v, mins[d]);
-            maxs[d] = Math.max(v, maxs[d]);
-          }
-        }
-        minmax = new double[][] { mins, maxs };
       }
 
-      ArrayList<Integer> al = new ArrayList<Integer>();
+      IntegerArray al = new IntegerArray();
       for(int j = 0; j < dim; j++) {
-        if(minmax[0][j] < minmax[1][j]) {
+        if(mins[j] < maxs[j]) {
           al.add(j);
         }
       }
 
       // Check whether any eligible attributes have been found
-      if(al.size() == 0) {
+      if(al.isEmpty()) {
         return;
       }
-      else {
-        // Randomly pick an attribute and split point
-        a = al.get(r.nextInt(al.size()));
-        q = (r.nextDouble() * (minmax[1][a] - minmax[0][a])) + minmax[0][a];
+      // Randomly pick an attribute and split point
+      a = al.get(r.nextInt(al.size()));
+      q = (r.nextDouble() * (maxs[a] - mins[a])) + mins[a];
 
-        // Create sub trees
-        successors = new ArrayList<>(2);
-        for(int i = 0; i < 2; i++) {
-          ArrayModifiableDBIDs tmpData = DBIDUtil.newArray();
-          for(DBIDIter iditer = data.iter(); iditer.valid(); iditer.advance()) {
-            NumberVector vector = relation.get(iditer);
-            if((i == 0) && (vector.doubleValue(a) < q)) {
-              tmpData.add(iditer);
-            }
-            if((i == 1) && (vector.doubleValue(a) >= q)) {
-              tmpData.add(iditer);
-            }
-          }
-          successors.add(new Tree(tmpData, r, height + 1, maxHeight, relation));
+      // Create sub trees
+      successors = new ArrayList<>(2);
+      ArrayModifiableDBIDs left = DBIDUtil.newArray();
+      ArrayModifiableDBIDs right = DBIDUtil.newArray();
+      for(DBIDIter iditer = data.iter(); iditer.valid(); iditer.advance()) {
+        NumberVector vector = relation.get(iditer);
+        if(vector.doubleValue(a) < q) {
+          left.add(iditer);
+        }
+        if(vector.doubleValue(a) >= q) {
+          right.add(iditer);
         }
       }
+      successors.add(new Tree(left, r, height + 1, maxHeight, dim, relation));
+      successors.add(new Tree(right, r, height + 1, maxHeight, dim, relation));
     }
 
     /**
      * Returns path length according to algorithm.
      */
     protected double pathLength(NumberVector vector) {
-      if(successors == null) {
-        return c(size);
+      List<Tree> _successors = successors;
+      double depth = 0.;
+      int _size = size;
+      int _a = a;
+      double _q = q;
+      while(_successors != null) {
+        int child = 1;
+        if(vector.doubleValue(_a) < _q) {
+          child = 0;
+        }
+        depth++;
+        _size = _successors.get(child).size;
+        _a = _successors.get(child).a;
+        _q = _successors.get(child).q;
+        _successors = _successors.get(child).successors;
       }
-      if(vector.doubleValue(a) < q) {
-        return successors.get(0).pathLength(vector) + 1.0;
-      }
-      else {
-        return successors.get(1).pathLength(vector) + 1.0;
-      }
+      return c(_size) + depth;
     }
   }
 
@@ -320,9 +328,6 @@ public class IsolationForest implements OutlierAlgorithm {
   /**
    * Parameterization class
    * 
-   * @hidden
-   * 
-   * @param <V> Vector type
    */
   public static class Par implements Parameterizer {
     /**
@@ -340,16 +345,25 @@ public class IsolationForest implements OutlierAlgorithm {
      */
     public static final OptionID SEED_ID = new OptionID("iforest.seed", "The seed to use for initializing Random.");
 
+    /**
+     * Number of trees
+     */
     protected int numTrees = 100;
 
+    /**
+     * Size of the sample set
+     */
     protected int subsampleSize = 256;
 
+    /**
+     * Random generator
+     */
     protected RandomFactory rnd;
 
     @Override
     public void configure(Parameterization config) {
       new IntParameter(NUM_TREES_ID, 100) //
-          .addConstraint(CommonConstraints.GREATER_THAN_ONE_INT) //
+          .addConstraint(CommonConstraints.GREATER_EQUAL_ONE_INT) //
           .grab(config, x -> numTrees = x);
       new IntParameter(SUBSAMPLE_SIZE_ID, 256) //
           .addConstraint(CommonConstraints.GREATER_THAN_ONE_INT) //
