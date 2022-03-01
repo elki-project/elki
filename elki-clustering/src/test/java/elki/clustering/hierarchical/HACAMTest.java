f77cd1243fff7b9d2c967e573dd687fde6bbb32c
Erich Schubert <erich.schubert@tu-dortmund.de>
2022-03-01 17:28:58 +0100

    Add unit tests for HACAM.

---
+++ elki-clustering/src/test/java/elki/clustering/hierarchical/HACAMTest.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of ELKI:
+ * Environment for Developing KDD-Applications Supported by Index-Structures
+ *
+ * Copyright (C) 2019
+ * ELKI Development Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package elki.clustering.hierarchical;
+
+import org.junit.Test;
+
+import elki.Algorithm;
+import elki.clustering.AbstractClusterAlgorithmTest;
+import elki.clustering.hierarchical.extraction.CutDendrogramByNumberOfClusters;
+import elki.data.Clustering;
+import elki.database.Database;
+import elki.utilities.ELKIBuilder;
+
+/**
+ * Perform hierarchical clustering around medoids.
+ *
+ * @author Erich Schubert
+ */
+public class HACAMTest extends AbstractClusterAlgorithmTest {
+  @Test
+  public void testHACAM() {
+    Database db = makeSimpleDatabase(UNITTEST + "single-link-effect.ascii", 638);
+    Clustering<?> clustering = new ELKIBuilder<>(CutDendrogramByNumberOfClusters.class) //
+        .with(CutDendrogramByNumberOfClusters.Par.MINCLUSTERS_ID, 3) //
+        .with(Algorithm.Utils.ALGORITHM_ID, HACAM.class) //
+        .build().autorun(db);
+    assertFMeasure(db, clustering, 0.93998);
+    assertClusterSizes(clustering, new int[] { 200, 204, 234 });
+  }
+
+  @Test
+  public void testHACAM2() {
+    Database db = makeSimpleDatabase(UNITTEST + "3clusters-and-noise-2d.csv", 330);
+    Clustering<?> clustering = new ELKIBuilder<>(CutDendrogramByNumberOfClusters.class) //
+        .with(CutDendrogramByNumberOfClusters.Par.MINCLUSTERS_ID, 3) //
+        .with(Algorithm.Utils.ALGORITHM_ID, HACAM.class) //
+        .build().autorun(db);
+    assertFMeasure(db, clustering, 0.915037);
+    assertClusterSizes(clustering, new int[] { 55, 119, 156 });
+  }
+}
