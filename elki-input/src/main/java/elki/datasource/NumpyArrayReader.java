6462355ff1fc0441ab129cd64a783d11a8d5d722
Andreas Lang <andreas.lang@tu-dortmund.de>
2024-03-26 09:44:16 +0100

    labels and string handling for numpy array reader

--- elki-input/src/main/java/elki/datasource/NumpyArrayReader.java
+++ elki-input/src/main/java/elki/datasource/NumpyArrayReader.java
@@ -27,6 +27,7 @@ import java.nio.CharBuffer;
 import java.nio.DoubleBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.nio.LongBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
@@ -39,6 +40,8 @@ import java.util.List;
 import elki.data.DoubleVector;
 import elki.data.FloatVector;
 import elki.data.IntegerVector;
+import elki.data.LabelList;
+import elki.data.type.TypeUtil;
 import elki.data.type.VectorFieldTypeInformation;
 import elki.datasource.bundle.MultipleObjectsBundle;
 import elki.datasource.filter.ObjectFilter;
@@ -50,10 +53,8 @@ import elki.utilities.optionhandling.parameterization.Parameterization;
 import elki.utilities.optionhandling.parameters.FileParameter;
 
 /**
- * Produce a database of random double vectors with each dimension in [0:1].
  * 
- * @author Erich Schubert
- * @since 0.4.0
+ * @author Andreas Lang
  */
 public class NumpyArrayReader extends AbstractDatabaseConnection {
   /**
@@ -63,20 +64,20 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
 
   URI infile;
 
+  URI labelfile;
+
   /**
    * Constructor.
    * 
-   * @param dim Dimensionality
-   * @param size Database size
-   * @param rnd Random generator
    * @param filters Filters to use
    */
-  public NumpyArrayReader(URI infile, List<? extends ObjectFilter> filters) {
+  public NumpyArrayReader(URI infile, URI labelfile, List<? extends ObjectFilter> filters) {
     super(filters);
     this.infile = infile;
+    this.labelfile = labelfile;
   }
 
-  private MultipleObjectsBundle readNumpy(FileChannel file) throws IOException {
+  private void readNumpy(FileChannel file, MultipleObjectsBundle bundle, boolean label) throws IOException {
     Charset cs = Charset.forName("UTF-8");
       MappedByteBuffer buffer = file.map(MapMode.READ_ONLY,0,10);
       if(buffer.get() != -109) {
@@ -101,7 +102,7 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
       buffer.get(header);
       String headerString = new String(header, cs);
       headerString = headerString.replaceAll(" ", "");
-      headerString = headerString.substring(1).split("}")[0];
+      headerString = headerString.substring(1).split("\\}")[0];
       System.out.println("header: " + headerString);
       String dtype = headerString.split("'descr':")[1].split("'")[1];
       boolean fOrder = Boolean.parseBoolean(headerString.split("'fortran_order':")[1].split(",")[0]);
@@ -110,8 +111,12 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
       }
       String shape = headerString.split("'shape':\\(")[1];
       shape = shape.split("\\)")[0];
-      int rows = Integer.parseInt(shape.split(",")[0]);
-      int cols = Integer.parseInt(shape.split(",")[1]);
+      String[] row_col = shape.split(",");
+      int rows = Integer.parseInt(row_col[0]);
+      int cols = 1;
+      if (row_col.length == 2) {
+        cols = Integer.parseInt(row_col[1]);
+      }
       boolean littleEndian = dtype.startsWith("<");
       if(dtype.endsWith("f4")) {
         List<FloatVector> vectors = new ArrayList<>(rows);
@@ -132,9 +137,21 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
           }
           System.gc();
         }
+        if(label) {
+          ArrayList<LabelList> labellist = new ArrayList<>();
+          for(i = 0; i < data.length; i++) {
+            List<String> row = new ArrayList<>();
+            for(int j = 0; j < data[i].length; j++) {
+              row.add(Float.toString(data[i][j]));
+            }
+            labellist.add(LabelList.make(row));
+          }
+          bundle.appendColumn(TypeUtil.LABELLIST, labellist);
+          return;
+        }
         VectorFieldTypeInformation<FloatVector> type = new VectorFieldTypeInformation<>(FloatVector.FACTORY, cols);
         System.out.println("reading Numpy Array Successfull");
-        return MultipleObjectsBundle.makeSimple(type, vectors);
+        bundle.appendColumn(type, vectors);
       }
       else if(dtype.endsWith("f8")) {
         List<DoubleVector> vectors = new ArrayList<>(rows);
@@ -155,14 +172,26 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
           }
           System.gc();
         }
+        if(label) {
+          ArrayList<LabelList> labellist = new ArrayList<>();
+          for(i = 0; i < data.length; i++) {
+            List<String> row = new ArrayList<>();
+            for(int j = 0; j < data[i].length; j++) {
+              row.add(Double.toString(data[i][j]));
+            }
+            labellist.add(LabelList.make(row));
+          }
+          bundle.appendColumn(TypeUtil.LABELLIST, labellist);
+          return;
+        }
         VectorFieldTypeInformation<DoubleVector> type = new VectorFieldTypeInformation<>(DoubleVector.FACTORY, cols);
         System.out.println("reading Numpy Array Succressfull");
-        return MultipleObjectsBundle.makeSimple(type, vectors);
+        bundle.appendColumn(type, vectors);
       }
       else if(dtype.endsWith("i4")){
         List<IntegerVector> vectors = new ArrayList<>(rows);
         System.out.println("int32");
-        long columnSize = 8 * cols;
+        long columnSize = 4 * cols;
         int[][] data = new int[rows][cols];
         int remaining = rows;
         int i = 0;
@@ -178,20 +207,32 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
           }
           System.gc();
         }
+        if(label) {
+          ArrayList<LabelList> labellist = new ArrayList<>();
+          for(i = 0; i < data.length; i++) {
+            List<String> row = new ArrayList<>();
+            for(int j = 0; j < data[i].length; j++) {
+              row.add(Integer.toString(data[i][j]));
+            }
+            labellist.add(LabelList.make(row));
+          }
+          bundle.appendColumn(TypeUtil.LABELLIST, labellist);
+          return;
+        }
         VectorFieldTypeInformation<IntegerVector> type = new VectorFieldTypeInformation<>(IntegerVector.STATIC, cols);
         System.out.println("reading Numpy Array Succressfull");
-        return MultipleObjectsBundle.makeSimple(type, vectors);
+        bundle.appendColumn(type, vectors);
       }
       else if (dtype.contains("U")){
-        Charset scs = null;
+        Charset scs;
         if(littleEndian){
           scs = Charset.forName("UTF-32LE");
         } else {
           scs = Charset.forName("UTF-32BE");
         }
         int size = Integer.parseInt(dtype.split("U")[1]);
-        System.out.println("Char"+size);
-        long columnSize = 2 * size * cols;
+        System.out.println("Char "+size);
+        long columnSize = 4 * size * cols;
         ArrayList<ArrayList<String>> data = new ArrayList<>();
         char[] char_data = new char[size];
         int remaining = rows;
@@ -212,14 +253,69 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
           }
           System.gc();
         }
-        throw new IOException("String format not yet supported" + dtype);
-        // VectorFieldTypeInformation<IntegerVector> type = new VectorFieldTypeInformation<>(IntegerVector.STATIC, cols);
-        // System.out.println("reading Numpy Array Succressfull");
-        // return MultipleObjectsBundle.makeSimple(type, vectors);
+        if(label){
+          ArrayList<LabelList> labellist = new ArrayList<>();
+          for(i = 0; i < data.size(); i++) {
+            labellist.add(LabelList.make(data.get(i)));
+          }
+          bundle.appendColumn(TypeUtil.LABELLIST, labellist);
+          return;
+        }
+        ArrayList<ArrayList<String>> col_list = new ArrayList<>();
+        for (i = 0; i < cols; i++) {
+          col_list.add(new ArrayList<>());
+        }
+        for(i = 0; i < rows; i++) {
+          for(int j = 0; j < cols; j++) {
+            col_list.get(j).add(data.get(i).get(j));
+          }
+        }
+        for (i = 0; i < cols; i++) {
+          bundle.appendColumn(TypeUtil.STRING, col_list.get(i));
+        }
       }
-      // else if(dtype.endsWith("i8") {
-      //   System.out.println("int64");
-      // }
+      else if(dtype.endsWith("i8")) {
+                List<IntegerVector> vectors = new ArrayList<>(rows);
+        System.out.println("int64 - long will be converted to int");
+        long columnSize = 8 * cols;
+        int[][] data = new int[rows][cols];
+        int remaining = rows;
+        int i = 0;
+        long[] row = new long[cols];
+        while(remaining >0){
+          long read = Math.min(Integer.MAX_VALUE / columnSize, remaining);
+          buffer = file.map(MapMode.READ_ONLY, 10L + len + i * columnSize, read * columnSize);
+          buffer.order(littleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+          LongBuffer longBuffer = buffer.asLongBuffer();
+          remaining -= read;
+          for(int j = 0; j < read; j++) {
+            longBuffer.get(row, 0, cols);
+            for (int k = 0; k < cols; k++) {
+              if (Integer.MAX_VALUE < row[k]) {
+                throw new IOException("Value too large for int aborting");
+              }
+              data[i][k] =(int) row[k];
+            }
+            vectors.add(IntegerVector.wrap(data[i++]));
+          }
+          System.gc();
+        }
+        if(label) {
+          ArrayList<LabelList> labellist = new ArrayList<>();
+          for(i = 0; i < data.length; i++) {
+            List<String> label_row = new ArrayList<>();
+            for(int j = 0; j < data[i].length; j++) {
+              label_row.add(Integer.toString(data[i][j]));
+            }
+            labellist.add(LabelList.make(label_row));
+          }
+          bundle.appendColumn(TypeUtil.LABELLIST, labellist);
+          return;
+        }
+        VectorFieldTypeInformation<IntegerVector> type = new VectorFieldTypeInformation<>(IntegerVector.STATIC, cols);
+        System.out.println("reading Numpy Array Succressfull");
+        bundle.appendColumn(type, vectors);
+      }
       else {
         throw new IOException("Invalid dtype" + dtype);
       }
@@ -229,18 +325,32 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
   public MultipleObjectsBundle loadData() {
     // test of numpy reading
     System.out.println("Reading numpy file");
+    MultipleObjectsBundle result = new MultipleObjectsBundle();
     Path path = Paths.get(infile);
     try (FileChannel channel = FileChannel.open(path)) {
       Duration loadingTime = LOG.newDuration(getClass().getName() + ".loadtime").begin();
-      MultipleObjectsBundle result = readNumpy(channel);
+      readNumpy(channel, result, false);
       LOG.statistics(loadingTime.end());
       channel.close();
-      System.gc();
-      return result;
     }
     catch(IOException e) {
       throw new AbortException("IO error loading numpy file", e);
     }
+    System.gc();
+    if(labelfile == null) {
+      return result;
+    }
+    path = Paths.get(labelfile);
+    try(FileChannel channel = FileChannel.open(path)) {
+      Duration loadingTime = LOG.newDuration(getClass().getName() + ".loadtime").begin();
+      readNumpy(channel, result, true);
+      LOG.statistics(loadingTime.end());
+      channel.close();
+    }
+    catch(IOException e) {
+      throw new AbortException("IO error loading numpy file", e);
+    }
+    return result;
   }
 
   @Override
@@ -251,7 +361,7 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
   /**
    * Parameterization class.
    * 
-   * @author Erich Schubert
+   * @author Andreas Lang
    */
   public static class Par extends AbstractDatabaseConnection.Par {
 
@@ -261,20 +371,30 @@ public class NumpyArrayReader extends AbstractDatabaseConnection {
     public static final OptionID INPUT_ID = new OptionID("dbc.in", "The name of the input file to be parsed.");
 
     /**
+     * Parameter that specifies the name of label file to be parsed.
+     */
+    public static final OptionID LABEL_ID = new OptionID("dbc.lbl", "The name of the label file to be parsed.");
+
+    /**
      * Random generator.
      */
     protected URI infile;
+
+    protected URI labelfile;
 
     @Override
     public void configure(Parameterization config) {
       new FileParameter(INPUT_ID, FileParameter.FileType.INPUT_FILE) //
           .grab(config, x -> infile = x);
+      new FileParameter(LABEL_ID, FileParameter.FileType.INPUT_FILE) //
+          .setOptional(true)//
+          .grab(config, x -> labelfile = x);
       configFilters(config);
     }
 
     @Override
     public NumpyArrayReader make() {
-      return new NumpyArrayReader(infile, filters);
+      return new NumpyArrayReader(infile, labelfile, filters);
     }
   }
 }
